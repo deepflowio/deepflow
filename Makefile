8b80744bb617d58f6417873a137e1a837e4b5f80
邱布衣 <buyi@yunshan.net.cn>
2018-07-18 14:33:53 +0800

    初始化droplet-libs repo
    **复现步骤**
    
    非问题不涉及
    
    **原因和解决方案**
    
    初始化droplet-libs repo
    
    从droplet已验证通过的代码迁移
    
    **影响范围**
    
    暂无
    
    **验证方案**
    
    已通过droplet验证
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [x] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @all

---
+++ Makefile
@@ -0,0 +1,19 @@
+GOPATH = $(shell go env GOPATH)
+DROPLET_LIBS_ROOT = ${GOPATH}/src/gitlab.x.lan/yunshan/droplet-libs
+
+deps:
+	[ -f ${GOPATH}/bin/dep ] || curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+	mkdir -p ${GOPATH}/src/gitlab.x.lan/yunshan/
+	[ -d ${DROPLET_LIBS_ROOT} ] || ln -snf ${CURDIR} ${DROPLET_LIBS_ROOT}
+	(cd ${DROPLET_LIBS_ROOT}; dep ensure)
+
+format:
+	(cd ${DROPLET_LIBS_ROOT}; go fmt ./...)
+
+verify:
+	(cd ${DROPLET_LIBS_ROOT}; go vet ./...)
+	(cd ${DROPLET_LIBS_ROOT}; go test -short ./...)
+
+.DEFAULT_GOAL := verify
+
+.PHONY: deps format verify
---
+++ logger/logger.go
@@ -0,0 +1,72 @@
+package logger
+
+import (
+	"os"
+	"path"
+	"time"
+
+	"github.com/lestrrat/go-file-rotatelogs"
+	"github.com/op/go-logging"
+)
+
+const (
+	LOG_ROTATION_INTERVAL = 24 * time.Hour      // every day
+	LOG_MAX_AGE           = 30 * 24 * time.Hour // every month
+	LOG_FORMAT            = "%{time:2006-01-02 15:04:05.000} [%{level:.4s}] %{shortfile} %{message}"
+	LOG_COLOR_FORMAT      = "%{color}%{time:2006-01-02 15:04:05.000} [%{level:.4s}]%{color:reset} %{shortfile} %{message}"
+)
+
+var log = logging.MustGetLogger(os.Args[0])
+
+func InitConsoleLog() {
+	stdout := logging.NewBackendFormatter(
+		logging.NewLogBackend(os.Stdout, "", 0),
+		logging.MustStringFormatter(LOG_COLOR_FORMAT),
+	)
+	logging.SetBackend(stdout)
+}
+
+func InitLog(filePath string, levelString string) {
+	level, err := logging.LogLevel(levelString)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(-1)
+	}
+
+	stdout := logging.AddModuleLevel(
+		logging.NewBackendFormatter(
+			logging.NewLogBackend(os.Stdout, "", 0),
+			logging.MustStringFormatter(LOG_COLOR_FORMAT),
+		),
+	)
+	stdout.SetLevel(level, "")
+
+	dir := path.Dir(filePath)
+	if _, err := os.Stat(dir); err != nil {
+		if os.IsNotExist(err) {
+			os.MkdirAll(dir, 0777)
+		} else {
+			log.Error(err.Error())
+		}
+	}
+
+	ioWriter, err := rotatelogs.New(
+		filePath+".%Y-%m-%d",
+		rotatelogs.WithLinkName(filePath),
+		rotatelogs.WithMaxAge(LOG_MAX_AGE),
+		rotatelogs.WithRotationTime(LOG_ROTATION_INTERVAL),
+	)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(-1)
+	}
+
+	file := logging.AddModuleLevel(
+		logging.NewBackendFormatter(
+			logging.NewLogBackend(ioWriter, "", 0),
+			logging.MustStringFormatter(LOG_FORMAT),
+		),
+	)
+	file.SetLevel(level, "")
+	logging.SetBackend(stdout, file)
+}
