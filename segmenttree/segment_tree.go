8e0fedacdf3bf2065ae89d6b7ef31f72775f76ad
邱布衣 <buyi@yunshan.net.cn>
2018-07-26 11:49:27 +0800

    实现segment tree
    **复现步骤**
    
    新开发不涉及
    
    **原因和解决方案**
    
    需要支持线段树，参照Poseidon现有的线段树实现，因为使用了bitset来合并结果，
    因此比起Java使用Set的版本来说，应该理论上的性能会更强
    
    **影响范围**
    
    无，仅仅是增加API
    
    **验证方案**
    
    单元测试通过
    
    性能测试包含构造深度大约为4层的线段树和查询深度大约为4层的线段树，
    其性能数据分别如下：
    
    ```
    BenchmarkBuildSegmentTree-20  30000 40917 ns/op
    BenchmarkQuerySegmentTree-20 300000  4855 ns/op
    ```
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [x] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @buyi
    
    /cc @all

---
+++ segmenttree/segment_tree.go
@@ -0,0 +1,87 @@
+package segmenttree
+
+import (
+	"sync"
+
+	"github.com/golang-collections/go-datastructures/bitarray"
+)
+
+func intervalToIntegerRange(interval Interval) IntegerRange {
+	lower, lowerClosed := interval.Lower()
+	upper, upperClosed := interval.Upper()
+	return ranged(lower, lowerClosed, upper, upperClosed)
+}
+
+type SegmentTree struct {
+	trees  []ImmutableSegmentTree
+	values []Value
+}
+
+func (t *SegmentTree) Query(intervals ...Interval) []Value {
+	dimension := len(t.trees)
+	if len(intervals) != dimension {
+		return nil
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(dimension)
+	results := make([]bitarray.BitArray, dimension)
+	for d := 0; d < dimension; d++ {
+		go func(d int) {
+			results[d] = t.trees[d].query(intervalToIntegerRange(intervals[d]))
+			wg.Done()
+		}(d)
+	}
+	wg.Wait()
+	indexBitSet := results[0]
+	for d := 1; d < len(t.trees); d++ {
+		indexBitSet = indexBitSet.And(results[d])
+	}
+
+	values := make([]Value, 0, len(t.values))
+	valueSet := bitarray.NewSparseBitArray()
+	for iterator := indexBitSet.Blocks(); iterator.Next(); {
+		blockIndex, block := iterator.Value()
+		for i := uint64(0); i < 64; i++ {
+			if 1<<i&block == 0 {
+				continue
+			}
+			index := blockIndex*64 + i
+			value := t.values[index]
+			if found, _ := valueSet.GetBit(value.Id()); found {
+				continue
+			}
+			values = append(values, value)
+			valueSet.SetBit(value.Id())
+		}
+	}
+	return values
+}
+
+type Entry struct {
+	intervals []Interval
+	value     Value
+}
+
+func New(dimension int, entries ...Entry) (Tree, error) {
+	if dimension == 0 {
+		return nil, InvalidDimension
+	}
+
+	values := make([]Value, len(entries))
+	for i, entry := range entries {
+		values[i] = entry.value
+		if len(entry.intervals) < dimension {
+			return nil, InsufficientIntervalLength
+		}
+	}
+	trees := make([]ImmutableSegmentTree, dimension)
+	for d := 0; d < dimension; d++ {
+		intervals := make([]IntegerRange, len(entries))
+		for i, entry := range entries {
+			intervals[i] = intervalToIntegerRange(entry.intervals[d])
+		}
+		trees[d].init(intervals)
+	}
+	return &SegmentTree{trees, values}, nil
+}
