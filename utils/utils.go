1ee52d3500abfc7be36a19169c6bbcd66d753070
邱布衣 <buyi@yunshan.net.cn>
2018-08-03 11:48:31 +0800

    稍微修改一下base_type.go
    **复现步骤**
    
    非问题不涉及
    
    **原因和解决方案**
    
    base_type一下代码需要修改
    
    **影响范围**
    
    新增代码，没有影响
    
    **验证方案**
    
    单元测试成功
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @all

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,23 @@
 package utils
 
+import (
+	"encoding/binary"
+	"net"
+)
+
 func UintMin(x, y uint) uint {
 	if x < y {
 		return x
 	}
 	return y
+}
+
+func Mac2Uint64(mac net.HardwareAddr) uint64 {
+	return uint64(binary.BigEndian.Uint16(mac[:]))<<32 | uint64(binary.BigEndian.Uint32(mac[2:]))
+}
+
+func Uint64ToMac(v uint64) net.HardwareAddr {
+	bytes := [8]byte{}
+	binary.BigEndian.PutUint64(bytes[:], v)
+	return net.HardwareAddr(bytes[2:])
 }
