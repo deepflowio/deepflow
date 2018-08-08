5d789c9f7a3eff415163afe30e87430c13d5beec
yuanchao <yuanchao@yunshan.net.cn>
2018-08-08 11:28:07 +0800

    trident adapt修复tx_pkt未统计的问题
    **复现步骤**
    
    * 无
    
    **原因和解决方案**
    
    * 修复tx_pkt未统计的问题
    * 模块交互使用droplet-lib/queue
    * 在adapt模块就做流量分发
    * start的时候不需要wait
    * 修复cache溢出问题
    * 修复rx_drop统计不准确的问题
    * sequential_decoder.go去掉无用的字符串拼装
    * sequential_decoder.go添加性能测试例
    
    **影响范围**
    
    * 对接模块需要使用droplet-lib/queue
    
    **验证方案**
    
    * 运行成功
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @all

--- cmd/droplet/main.go
+++ cmd/droplet/main.go
@@ -7,21 +7,21 @@ import (
 	"github.com/op/go-logging"
 	. "gitlab.x.lan/yunshan/droplet-libs/logger"
 	_ "gitlab.x.lan/yunshan/droplet-libs/monitor"
+	. "gitlab.x.lan/yunshan/droplet-libs/queue"
 
 	"gitlab.x.lan/yunshan/droplet/adapt"
-	"gitlab.x.lan/yunshan/droplet/handler"
 )
 
 var log = logging.MustGetLogger(os.Args[0])
 
 func main() {
 	InitConsoleLog()
-	ch := make(chan handler.MetaPktHdr, 100)
-	trident_adapt := (&adapt.TridentAdapt{}).Init(nil, ch)
+	queue := NewOverwriteQueue("AdaptToFilter", 1000)
+	trident_adapt := (&adapt.TridentAdapt{}).Init(queue)
 	trident_adapt.Start(true)
 	log.Info("It worked!")
 	for {
-		pkt := <-ch
-		fmt.Println(pkt.String())
+		pkt := queue.Get()
+		fmt.Println(pkt)
 	}
 }
