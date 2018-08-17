513dca94874dea816610e76111347606a859a8e8
suying <suying@yunshan.net.cn>
2018-08-17 17:49:50 +0800

    mapreduce的修改
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    一个mapreduce来接收不同的流， 从而实现窗口滑动
    
    **影响范围**
    
    影响聚合发送
    
    **验证方案**
    
    能正常发送zmq数据
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @all

--- mapreduce/metering_map_reduce.go
+++ mapreduce/metering_map_reduce.go
@@ -2,31 +2,10 @@ package mapreduce
 
 import (
 	isp "gitlab.x.lan/application/droplet-app/pkg/metering/bw_usage_isp_usage"
-	"gitlab.x.lan/platform/droplet-mapreduce"
-	"gitlab.x.lan/yunshan/droplet-libs/datatype"
-	"gitlab.x.lan/yunshan/droplet-libs/queue"
-
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/api"
+	"gitlab.x.lan/platform/droplet-mapreduce/pkg/handler"
 )
 
-type MeteringMapProcessor struct {
-	queue.OverwriteQueue
-}
-
-func (m *MeteringMapProcessor) FlowProcessor() {
-	taggedMetering := m.Get().(*datatype.TaggedMetering)
-	m.MeteringHandler(taggedMetering)
-}
-
-func (m *MeteringMapProcessor) MeteringHandler(metering *datatype.TaggedMetering) []string {
-	var processors []api.MeteringProcessor
-	processor := isp.NewProcessor()
-	processors = append(processors, processor)
-	meteringHandler := droplet_mapreduce.NewMeteringHandler(processors)
-	meteringHandler.Process(*metering)
-	var a []string
-	for i := 0; i < meteringHandler.NumberOfApps; i++ {
-		a = meteringHandler.Stats[0][i].KeySet()
-	}
-	return a
+func NewMeteringMapProcess() *handler.MeteringHandler {
+	return handler.NewMeteringHandler([]api.MeteringProcessor{isp.NewProcessor()})
 }
