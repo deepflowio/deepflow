61d5fa4327802eef1b940ce11402b094434c5177
yuanchao <yuanchao@yunshan.net.cn>
2018-08-20 15:42:48 +0800

    Metering添加String方法
    **复现步骤**
    
    * 无
    
    **原因和解决方案**
    
    * droplet-ctl queue monitor打印需要
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 编译通过
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @buyi
    
    /cc @all

--- datatype/metering.go
+++ datatype/metering.go
@@ -1,6 +1,7 @@
 package datatype
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/gopacket/layers"
@@ -22,4 +23,13 @@ type Metering struct {
 	PacketCount1 uint64
 	L3EpcID0     uint32
 	L3EpcID1     uint32
+}
+
+func (m *Metering) String() string {
+	return fmt.Sprintf("TIMESTAMP: %d EXPORTER: %v INPORT: 0x%X VLAN: %d\n"+
+		"    IP: %v -> %v PROTO: %d L3EpcID: %d -> %d PORT: %d -> %d\n"+
+		"    ByteCount: %d -> %d PacketCount: %d -> %d",
+		m.Timestamp, m.Exporter, m.InPort0, m.VLAN,
+		m.IPSrc, m.IPDst, m.Proto, m.L3EpcID0, m.L3EpcID1, m.PortSrc, m.PortDst,
+		m.ByteCount0, m.ByteCount1, m.PacketCount0, m.PacketCount1)
 }
