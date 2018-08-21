64a82ca20d5268dcf71edf466c83f477d9d3d84a
yuanchao <yuanchao@yunshan.net.cn>
2018-08-21 17:19:23 +0800

    labeler添加dump-platform命令
    **复现步骤**
    
    * 无
    
    **原因和解决方案**
    
    * droplet-ctl labeler dump-platform inport=100,mac=11:22:33:44:55:66,ip=12.1.1.1
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @all

--- labeler/labeler_manager.go
+++ labeler/labeler_manager.go
@@ -1,10 +1,21 @@
 package labeler
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+
 	"github.com/op/go-logging"
+	"github.com/spf13/cobra"
 	"gitlab.x.lan/yunshan/droplet-libs/datatype"
 	"gitlab.x.lan/yunshan/droplet-libs/policy"
 	"gitlab.x.lan/yunshan/droplet-libs/queue"
+	. "gitlab.x.lan/yunshan/droplet-libs/utils"
+
+	"gitlab.x.lan/yunshan/droplet/dropletctl"
 )
 
 var log = logging.MustGetLogger("labeler")
@@ -17,13 +28,25 @@ type LabelerManager struct {
 	running       bool
 }
 
+const (
+	LABELER_CMD_DUMP_PLATFORM = iota
+)
+
+type DumpKey struct {
+	Mac    uint64
+	Ip     uint32
+	InPort uint32
+}
+
 func NewLabelerManager(readQueue queue.QueueReader, meteringQueue queue.QueueWriter, appQueue ...queue.QueueWriter) *LabelerManager {
-	return &LabelerManager{
+	labeler := &LabelerManager{
 		policyTable:   policy.NewPolicyTable(policy.ACTION_FLOW_STAT),
 		readQueue:     readQueue,
 		meteringQueue: meteringQueue,
 		appQueue:      appQueue,
 	}
+	dropletctl.Register(dropletctl.DROPLETCTL_LABELER, labeler)
+	return labeler
 }
 
 func (l *LabelerManager) OnPlatformDataChange(data []*datatype.PlatformData) {
@@ -157,4 +180,126 @@ func (l *LabelerManager) Stop(wait bool) {
 		log.Info("Stop labeler manager")
 		l.running = false
 	}
+}
+
+func (l *LabelerManager) recvDumpPlatform(conn *net.UDPConn, port int, arg *bytes.Buffer) {
+	key := DumpKey{}
+	buffer := bytes.Buffer{}
+
+	decoder := gob.NewDecoder(arg)
+	if err := decoder.Decode(&key); err != nil {
+		log.Error(err)
+		dropletctl.SendToDropletCtl(conn, port, 1, nil)
+		return
+	}
+
+	info := l.policyTable.GetEndpointInfo(key.Mac, key.Ip, key.InPort)
+	if info == nil {
+		log.Warningf("GetEndpointInfo(%+v) return nil", key)
+		dropletctl.SendToDropletCtl(conn, port, 1, nil)
+		return
+	}
+	encoder := gob.NewEncoder(&buffer)
+	if err := encoder.Encode(info); err != nil {
+		log.Errorf("encoder.Encode: %s", err)
+		dropletctl.SendToDropletCtl(conn, port, 1, nil)
+		return
+	}
+	dropletctl.SendToDropletCtl(conn, port, 0, &buffer)
+}
+
+func (l *LabelerManager) RecvCommand(conn *net.UDPConn, port int, operate uint16, arg *bytes.Buffer) {
+	switch operate {
+	case LABELER_CMD_DUMP_PLATFORM:
+		l.recvDumpPlatform(conn, port, arg)
+	}
+}
+
+func parseUint(s string) (uint32, error) {
+	if s[0:2] == "0x" {
+		x, err := strconv.ParseUint(s[2:], 16, 64)
+		return uint32(x), err
+	} else {
+		x, err := strconv.ParseUint(s, 10, 64)
+		return uint32(x), err
+	}
+}
+
+func newDumpKey(cmdLine string) *DumpKey {
+	key := &DumpKey{}
+	keyValues := strings.Split(cmdLine, ",")
+	for _, keyValue := range keyValues {
+		parts := strings.Split(keyValue, "=")
+		switch parts[0] {
+		case "mac":
+			mac, err := net.ParseMAC(parts[1])
+			if err != nil {
+				fmt.Printf("unknown mac address from: %s[%v]\n", cmdLine, err)
+				return nil
+			}
+			key.Mac = Mac2Uint64(mac)
+		case "ip":
+			key.Ip = IpToUint32(net.ParseIP(parts[1]))
+		case "inport":
+			inport, err := parseUint(parts[1])
+			if err != nil {
+				fmt.Printf("%s: %v\n", cmdLine, err)
+				return nil
+			}
+			key.InPort = inport
+		default:
+			fmt.Printf("unknown key %s from %s\n", parts[0], cmdLine)
+			return nil
+		}
+	}
+	return key
+}
+
+func dumpPlatform(cmdLine string) {
+	key := newDumpKey(cmdLine)
+	if key == nil {
+		return
+	}
+	buffer := bytes.Buffer{}
+	info := datatype.EndpointInfo{}
+	encoder := gob.NewEncoder(&buffer)
+	if err := encoder.Encode(key); err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, result, err := dropletctl.SendToDroplet(dropletctl.DROPLETCTL_LABELER, LABELER_CMD_DUMP_PLATFORM, &buffer)
+	if err != nil {
+		log.Warning(err)
+		return
+	}
+	decoder := gob.NewDecoder(result)
+	if err := decoder.Decode(&info); err != nil {
+		log.Error(err)
+		return
+	}
+	fmt.Printf("%s:\n\t%+v\n", cmdLine, info)
+}
+
+func RegisterCommand() *cobra.Command {
+	labeler := &cobra.Command{
+		Use:   "labeler",
+		Short: "config droplet labeler module",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("please run with arguments 'dump-platform'.\n")
+		},
+	}
+	dump := &cobra.Command{
+		Use:     "dump-platform [filter]",
+		Short:   "dump platform data infomation",
+		Example: "droplet-ctl labeler dump-platform inport=1000,mac=12:34:56:78:9a:bc,ip=127.0.0.1",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Printf("filter is nil, Example: %s\n", cmd.Example)
+				return
+			}
+			dumpPlatform(args[0])
+		},
+	}
+	labeler.AddCommand(dump)
+	return labeler
 }
