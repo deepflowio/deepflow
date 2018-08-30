9721738060e6a17f4e90a60f06d17aeda85121e4
suying <suying@yunshan.net.cn>
2018-08-30 17:12:50 +0800

    droplet-ctl里需要支持mapreduce的queue dump
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    droplet-ctl里需要支持mapreduce的queue
    dump，在droplet里构建handler的时候传入需要监控的queue
    
    **影响范围**
    
    dump调试
    
    **验证方案**
    
    droplet-ctl 命令可以监控queue里的数据
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @all

--- mapreduce/flow_map_reduce.go
+++ mapreduce/flow_map_reduce.go
@@ -4,8 +4,9 @@ import (
 	epc "gitlab.x.lan/application/droplet-app/pkg/flow/epc_flow_topo"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/api"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/handler"
+	"gitlab.x.lan/yunshan/droplet-libs/queue"
 )
 
-func NewFlowMapProcess() *handler.FlowHandler {
-	return handler.NewFlowHandler([]api.FlowProcessor{epc.NewProcessor()})
+func NewFlowMapProcess(zmqFlowQueue queue.QueueWriter, zmqAppQueue queue.QueueWriter) *handler.FlowHandler {
+	return handler.NewFlowHandler([]api.FlowProcessor{epc.NewProcessor()}, zmqFlowQueue, zmqAppQueue)
 }
--- mapreduce/metering_map_reduce.go
+++ mapreduce/metering_map_reduce.go
@@ -4,8 +4,9 @@ import (
 	isp "gitlab.x.lan/application/droplet-app/pkg/metering/bw_usage_isp_usage"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/api"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/handler"
+	"gitlab.x.lan/yunshan/droplet-libs/queue"
 )
 
-func NewMeteringMapProcess() *handler.MeteringHandler {
-	return handler.NewMeteringHandler([]api.MeteringProcessor{isp.NewProcessor()})
+func NewMeteringMapProcess(zmqMeteringQueue queue.QueueWriter) *handler.MeteringHandler {
+	return handler.NewMeteringHandler([]api.MeteringProcessor{isp.NewProcessor()}, zmqMeteringQueue)
 }
