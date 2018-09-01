27496b4873816784723bdb667ec1ecad1d9a27ab
Jin Jie <jinjie@yunshan.net.cn>
2018-09-01 16:05:26 +0800

    修改usage，增加flow的处理
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    将原来的usage和flow app取消，替换为正确的
    
    **影响范围**
    
    无
    
    **验证方案**
    
    无
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [x] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @all

--- mapreduce/flow_map_reduce.go
+++ mapreduce/flow_map_reduce.go
@@ -1,12 +1,12 @@
 package mapreduce
 
 import (
-	epc "gitlab.x.lan/application/droplet-app/pkg/flow/epc_flow_topo"
+	"gitlab.x.lan/application/droplet-app/pkg/mapper/flow"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/api"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/handler"
 	"gitlab.x.lan/yunshan/droplet-libs/queue"
 )
 
 func NewFlowMapProcess(zmqFlowQueue queue.QueueWriter, zmqAppQueue queue.QueueWriter) *handler.FlowHandler {
-	return handler.NewFlowHandler([]api.FlowProcessor{epc.NewProcessor()}, zmqFlowQueue, zmqAppQueue)
+	return handler.NewFlowHandler([]api.FlowProcessor{flow.NewProcessor()}, zmqFlowQueue, zmqAppQueue)
 }
