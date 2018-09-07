cb9fc4cd36b29ad7a10d51dc76167474a600ae2a
yuanchao <yuanchao@yunshan.net.cn>
2018-09-07 16:59:51 +0800

    添加taggedFlow的string方法
    **复现步骤**
    
    * ./droplet-ctl queue monitor ** 看不到flow打的tag
    
    **原因和解决方案**
    
    * 添加string方法
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @buyi
    
    /cc @all

--- datatype/tagged_flow.go
+++ datatype/tagged_flow.go
@@ -1,6 +1,14 @@
 package datatype
 
+import (
+	"fmt"
+)
+
 type TaggedFlow struct {
 	Flow
 	Tag
+}
+
+func (f *TaggedFlow) String() string {
+	return fmt.Sprintf("Flow: %s, Tag: %+v", &f.Flow, f.Tag)
 }
