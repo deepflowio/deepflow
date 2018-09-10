ebd43b34626498af0b3df2b5eba616d29c9bfda0
Jin Jie <jinjie@yunshan.net.cn>
2018-09-10 19:16:04 +0800

    添加perf app
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    无
    
    **影响范围**
    
    无
    
    **验证方案**
    
    无
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [x] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @all

--- mapreduce/flow_map_reduce.go
+++ mapreduce/flow_map_reduce.go
@@ -2,11 +2,15 @@ package mapreduce
 
 import (
 	"gitlab.x.lan/application/droplet-app/pkg/mapper/flow"
+	"gitlab.x.lan/application/droplet-app/pkg/mapper/perf"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/api"
 	"gitlab.x.lan/platform/droplet-mapreduce/pkg/handler"
 	"gitlab.x.lan/yunshan/droplet-libs/queue"
 )
 
 func NewFlowMapProcess(zmqAppQueue queue.QueueWriter) *handler.FlowHandler {
-	return handler.NewFlowHandler([]api.FlowProcessor{flow.NewProcessor()}, zmqAppQueue)
+	return handler.NewFlowHandler([]api.FlowProcessor{
+		flow.NewProcessor(),
+		perf.NewProcessor(),
+	}, zmqAppQueue)
 }
