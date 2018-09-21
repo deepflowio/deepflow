9fc69da39ee99a3b12504f3332d919adb23e9544
Jin Jie <jinjie@yunshan.net.cn>
2018-09-21 20:41:23 +0800

    app添加prepare方法
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    多线程处理时，每个app processor需要各自进行初始化
    
    **影响范围**
    
    无
    
    **验证方案**
    
    无
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [x] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @buyi

--- app/processor.go
+++ app/processor.go
@@ -4,10 +4,12 @@ import "gitlab.x.lan/yunshan/droplet-libs/datatype"
 
 type MeteringProcessor interface {
 	GetName() string
+	Prepare()
 	Process(*datatype.MetaPacket, bool) []*Document
 }
 
 type FlowProcessor interface {
 	GetName() string
+	Prepare()
 	Process(*datatype.TaggedFlow, bool) []*Document
 }
