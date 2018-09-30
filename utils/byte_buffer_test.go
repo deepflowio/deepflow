a5159cbacefecacd14d54b704f2abe44d16e0e12
向阳 <xiangyang@yunshan.net.cn>
2018-09-30 13:07:49 +0800

    byte buffer增加保护，避免使用者忘记调用Reset
    **复现步骤**
    
    **原因和解决方案**
    
    - 增加保护，避免OOM
    - 增加Finalizer，目前queue中的对象在被覆盖时不会被Put
    
    **影响范围**
    
    **验证方案**
    
    已在daily验证
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @buyi
    
    /cc @buyi @jinjie @zhuofeng

--- utils/byte_buffer_test.go
+++ utils/byte_buffer_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestByteBufferUse(t *testing.T) {
-	bytes := &ByteBuffer{}
+	bytes := &ByteBuffer{quota: 10}
 	buf := bytes.Use(10)
 	if len(buf) != 10 || len(bytes.Bytes()) != 10 {
 		t.Error("Use函数处理不正确")
@@ -13,11 +13,12 @@ func TestByteBufferUse(t *testing.T) {
 }
 
 func TestByteBufferUseTwice(t *testing.T) {
-	bytes := &ByteBuffer{}
+	bytes := &ByteBuffer{quota: 10}
 	buf := bytes.Use(10)
 	if len(buf) != 10 || len(bytes.Bytes()) != 10 {
 		t.Error("第一次调用Use函数处理不正确")
 	}
+	bytes.SetQuota(30)
 	buf = bytes.Use(20)
 	if len(buf) != 20 || len(bytes.Bytes()) != 30 {
 		t.Error("第二次调用Use函数处理不正确")
@@ -25,7 +26,7 @@ func TestByteBufferUseTwice(t *testing.T) {
 }
 
 func TestByteBufferReset(t *testing.T) {
-	bytes := &ByteBuffer{}
+	bytes := &ByteBuffer{quota: 10}
 	buf := bytes.Use(10)
 	if len(buf) != 10 || len(bytes.Bytes()) != 10 {
 		t.Error("Use函数处理不正确")
