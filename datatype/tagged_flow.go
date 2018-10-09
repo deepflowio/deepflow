e0f453076def1eb8263fc16c31c34b14b9f1f50f
chenzhe <chenzhe@yunshan.net.cn>
2018-10-09 16:02:40 +0800

    TaggedFlow增加Pool相关操作
    **复现步骤**
    
    现有的方式可能导致内存上涨的问题
    
    **原因和解决方案**
    
    修改为显式申请及释放，同时依然提供隐式释放接口
    
    **影响范围**
    
    TaggedFlow拷贝处需要使用Clone函数
    
    **验证方案**
    
    针对隐式释放的方式，于153测试正常
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    
    /assign @buyi
    
    /cc @wangkai

--- datatype/tagged_flow.go
+++ datatype/tagged_flow.go
@@ -2,6 +2,7 @@ package datatype
 
 import (
 	"fmt"
+	"sync"
 )
 
 type TaggedFlow struct {
@@ -9,6 +10,39 @@ type TaggedFlow struct {
 	Tag
 }
 
+var taggedFlowPool sync.Pool
+
+func AcquireTaggedFlow() *TaggedFlow {
+	return taggedFlowPool.Get().(*TaggedFlow)
+}
+
+func ReleaseTaggedFlow(x interface{}) {
+	taggedFlowPool.Put(x)
+}
+
+func CloneTaggedFlow(taggedFlow *TaggedFlow) *TaggedFlow {
+	newTaggedFlow := taggedFlowPool.Get().(*TaggedFlow)
+	*newTaggedFlow = *taggedFlow
+	return newTaggedFlow
+}
+
+func CloneTaggedFlowHelper(items []interface{}) []interface{} {
+	newItems := make([]interface{}, len(items))
+	for i, e := range items {
+		taggedFlow := e.(*TaggedFlow)
+		item := taggedFlowPool.Get().(*TaggedFlow)
+		*item = *taggedFlow
+		newItems[i] = item
+	}
+	return newItems
+}
+
 func (f *TaggedFlow) String() string {
 	return fmt.Sprintf("Flow: %s, Tag: %+v", f.Flow.String(), f.Tag)
+}
+
+func init() {
+	taggedFlowPool.New = func() interface{} {
+		return new(TaggedFlow)
+	}
 }
