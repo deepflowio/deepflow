186270533da453a36aa04c947450f8b9177f6da0
邱布衣 <buyi@yunshan.net.cn>
2018-10-12 11:30:24 +0800

    增加MetaPacket的Acquire/Release/Clone
    **复现步骤**
    
    非问题不涉及
    
    **原因和解决方案**
    
    需要增加MetaPacket的ARC
    
    **影响范围**
    
    影响编译
    
    **验证方案**
    
    单元测试成功
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang

--- datatype/meta_packet_test.go
+++ datatype/meta_packet_test.go
@@ -119,6 +119,19 @@ func TestParseIspPackets(t *testing.T) {
 	}
 }
 
+func TestAcquireReleaseClone(t *testing.T) {
+	p := AcquireMetaPacket()
+	ReleaseMetaPacket(p)
+	expected := &MetaPacket{PacketLen: 10086}
+	dup := CloneMetaPacket(expected)
+	if p != dup { // pointer compare
+		t.Error("Expected same pointer but actually not")
+	}
+	if dup.PacketLen != expected.PacketLen {
+		t.Error("Not duplicated")
+	}
+}
+
 func BenchmarkParsePacket(b *testing.B) {
 	packets := loadPcap("meta_packet_test.pcap")
 	b.ResetTimer()
