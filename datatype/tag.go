2d0cc24247bd72a8915dae229c2f292b70c46f53
向阳 <xiangyang@yunshan.net.cn>
2018-10-12 11:37:58 +0800

    删除不再使用的whitelist和customer_tag字段
    **复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @jinjie
    
    /cc @buyi

--- datatype/tag.go
+++ datatype/tag.go
@@ -1,11 +1,7 @@
 package datatype
 
 type Tag struct {
-	GroupIDs0         []uint32
-	GroupIDs1         []uint32
-	WhitelistRuleIDs0 []uint32
-	WhitelistRuleIDs1 []uint32
-	CustomTagIDs0     []uint32
-	CustomTagIDs1     []uint32
-	PolicyData        *PolicyData
+	GroupIDs0  []uint32
+	GroupIDs1  []uint32
+	PolicyData *PolicyData
 }
