d8cd798cbc2685e20386c13037024ee13ca8d317
zhuofeng <zhuofeng@yunshan.net.cn>
2018-10-29 15:44:07 +0800

    支持动态调整日志级别接口
    **复现步骤**
    
    不涉及
    
    **原因和解决方案**
    
    **影响范围**
    
    readme
    
    **验证方案**
    
    不涉及
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @xiangyang @yuanchao

---
+++ dropletctl/loglevel_control.go
@@ -0,0 +1,157 @@
+package dropletctl
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"net"
+
+	"github.com/op/go-logging"
+	"github.com/spf13/cobra"
+)
+
+const (
+	LOGLEVEL_CMD_SHOW = iota
+	LOGLEVEL_CMD_SET
+)
+
+type LoglevelControl struct {
+}
+
+func NewLoglevelControl() *LoglevelControl {
+	loglevelProcess := &LoglevelControl{}
+	// 服务端注册处理函数
+	Register(DROPLETCTL_LOGLEVEL, loglevelProcess)
+	return loglevelProcess
+}
+
+func decodeLoglevel(arg *bytes.Buffer) (string, error) {
+	var level string
+	decoder := gob.NewDecoder(arg)
+	if err := decoder.Decode(&level); err != nil {
+		log.Error(err)
+		return "", err
+	}
+	return level, nil
+}
+
+func encodeLoglevel(level string) (*bytes.Buffer, error) {
+	buffer := bytes.Buffer{}
+	encoder := gob.NewEncoder(&buffer)
+	if err := encoder.Encode(level); err != nil {
+		log.Errorf("encoder.Encode: %s", err)
+		return nil, err
+	}
+	return &buffer, nil
+}
+
+func (l *LoglevelControl) RecvCommand(conn *net.UDPConn, port int, operate uint16, arg *bytes.Buffer) {
+	switch operate {
+	case LOGLEVEL_CMD_SHOW:
+		loglevel := getLogLevel()
+		enc, err := encodeLoglevel(loglevel)
+		if err != nil {
+			SendToDropletCtl(conn, port, 1, nil)
+		} else {
+			SendToDropletCtl(conn, port, 0, enc)
+		}
+	case LOGLEVEL_CMD_SET:
+		loglevel, err := decodeLoglevel(arg)
+		if err != nil {
+			SendToDropletCtl(conn, port, 1, nil)
+		} else {
+			log.Infof("set loglevel to (%s)", loglevel)
+			if err := setLogLevel(loglevel); err != nil {
+				log.Warningf("set loglevel(%s) failed: %s", loglevel, err)
+				SendToDropletCtl(conn, port, 1, nil)
+			} else {
+				if enc, err := encodeLoglevel(loglevel); err != nil {
+					SendToDropletCtl(conn, port, 0, nil)
+				} else {
+					SendToDropletCtl(conn, port, 0, enc)
+				}
+			}
+		}
+	}
+}
+
+func getLogLevel() string {
+	levelId := logging.GetLevel("")
+	return levelId.String()
+}
+
+func setLogLevel(level string) error {
+	levelId, err := logging.LogLevel(level)
+	if err != nil {
+		return err
+	}
+	logging.SetLevel(levelId, "")
+	return nil
+}
+
+func sendCmd(operate int, loglevel string, out interface{}) bool {
+	buffer := bytes.Buffer{}
+	encoder := gob.NewEncoder(&buffer)
+	if err := encoder.Encode(loglevel); err != nil {
+		fmt.Printf("%v: %s\n", err, loglevel)
+		return false
+	}
+
+	_, result, err := SendToDroplet(DROPLETCTL_LOGLEVEL, DropletCtlModuleOperate(operate), &buffer)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+
+	decoder := gob.NewDecoder(result)
+	if err = decoder.Decode(out); err != nil {
+		fmt.Printf("%v: %v\n", err, out)
+		return false
+	}
+	return true
+}
+
+// 客户端注册命令
+func RegisterLoglevelCommand() *cobra.Command {
+	loglevel := &cobra.Command{
+		Use:   "loglevel",
+		Short: "control loglevel",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("please run with arguments 'show | set'.\n")
+		},
+	}
+
+	show := &cobra.Command{
+		Use:   "show",
+		Short: "show current loglevel",
+		Run: func(cmd *cobra.Command, args []string) {
+			var level string
+			if sendCmd(LOGLEVEL_CMD_SHOW, "", &level) {
+				fmt.Printf("Current loglevel: %s\n", level)
+			}
+		},
+	}
+
+	set := &cobra.Command{
+		Use:   "set {loglevel}",
+		Short: "set loglevel",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) != 1 {
+				fmt.Println("please run with 'debug|info|warning|error'.")
+				return
+			}
+			if args[0] != "debug" && args[0] != "info" && args[0] != "warning" && args[0] != "error" {
+				fmt.Println("please run with 'debug|info|warning|error'.")
+				return
+			}
+			var level string
+			if sendCmd(LOGLEVEL_CMD_SET, args[0], &level) {
+				fmt.Printf("Set loglevel: %s\n", level)
+			}
+		},
+	}
+
+	loglevel.AddCommand(show)
+	loglevel.AddCommand(set)
+	return loglevel
+}
