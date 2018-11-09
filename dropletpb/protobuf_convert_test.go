007140278ef4bcd4f1151ad9c0ba9c3779f9e699
Huiqing Xiang <huiqing@yunshan.net.cn>
2018-11-09 15:29:55 +0800

    支持ip_ranges转化为CIDR格式
    **复现步骤**
    
    新增ip_ranges格式
    
    **原因和解决方案**
    
    支持ip_ranges转化为CIDR格式
    
    **影响范围**
    
    无
    
    **验证方案**
    
    待验证
    
    **涉及分支**
    
    * master
    * 5.4.1
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @buyi
    
    /cc @buyi @wangkai @xiangyang @xiaofeng01 @yuanchao

---
+++ dropletpb/protobuf_convert_test.go
@@ -0,0 +1,80 @@
+package dropletpb
+
+import (
+	"net"
+	"reflect"
+	"testing"
+)
+
+var ipRanges = []struct{ start, end string }{
+	{"0.0.0.0", "0.0.0.0"},
+	{"0.0.0.0", "0.0.0.1"},
+	{"255.255.255.251", "255.255.255.255"},
+	{"255.255.255.255", "255.255.255.255"},
+}
+
+var basicResultGroup = [][]string{
+	{"0.0.0.0/32"},
+	{"0.0.0.0/31"},
+	{"255.255.255.251/32", "255.255.255.252/30"},
+	{"255.255.255.255/32"},
+}
+
+var (
+	start1       = net.ParseIP("10.30.100.0").To4()
+	end1         = net.ParseIP("10.30.100.15").To4()
+	basicResult1 = []string{"10.30.100.0/28"}
+
+	start2       = net.ParseIP("10.25.0.1").To4()
+	end2         = net.ParseIP("10.25.0.10").To4()
+	basicResult2 = []string{"10.25.0.1/32", "10.25.0.2/31", "10.25.0.4/30", "10.25.0.8/31", "10.25.0.10/32"}
+)
+
+func generateConvertResult(ips []*net.IPNet) []string {
+	result := make([]string, 0)
+	for _, ip := range ips {
+		result = append(result, ip.String())
+	}
+	return result
+}
+
+func checkParsedIPRangesResult(t *testing.T, basicResult, targetResult []string) bool {
+	if !reflect.DeepEqual(basicResult, targetResult) {
+		t.Log("Result:", targetResult, "\n")
+		t.Log("Expect:", basicResult, "\n")
+		return false
+	} else {
+		return true
+	}
+}
+
+// 特殊IP
+func TestSpecialIPsConvert(t *testing.T) {
+	for index, ipRange := range ipRanges {
+		start := net.ParseIP(ipRange.start).To4()
+		end := net.ParseIP(ipRange.end).To4()
+		ips := ipRangeConvert2CIDR(start, end)
+		result := generateConvertResult(ips)
+		if !checkParsedIPRangesResult(t, basicResultGroup[index], result) {
+			t.Error("TestSpecialIPsConvert Check Failed!")
+		}
+	}
+}
+
+// 可转换成一个网段的ip
+func TestOneNetIPsConvert(t *testing.T) {
+	ips := ipRangeConvert2CIDR(start1, end1)
+	result := generateConvertResult(ips)
+	if !checkParsedIPRangesResult(t, basicResult1, result) {
+		t.Error("TestOneNetIPsConvert Check Failed!")
+	}
+}
+
+// 可转换成多个网段的ip
+func TestMoreNetIPsConvert(t *testing.T) {
+	ips := ipRangeConvert2CIDR(start2, end2)
+	result := generateConvertResult(ips)
+	if !checkParsedIPRangesResult(t, basicResult2, result) {
+		t.Error("TestMoreNetIPsConvert Check Failed!")
+	}
+}
