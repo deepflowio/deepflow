a7550f1afeaf65f643dcc6d66e5bf30d9481053b
邱布衣 <buyi@yunshan.net.cn>
2018-11-12 18:41:28 +0800

    增加trident NPB的调用接口
    **复现步骤**
    
    非问题不涉及
    
    **原因和解决方案**
    
    需要支持trident查询NPB策略
    
    **影响范围**
    
    影响编译
    
    **验证方案**
    
    编译成功
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang

--- policy/policy.go
+++ policy/policy.go
@@ -87,7 +87,7 @@ func getAvailableMapSize(queueCount int, mapSize uint32) uint32 {
 
 func NewPolicyTable(actionFlags ActionFlag, queueCount int, mapSize uint32, fastPathDisable bool) *PolicyTable { // 传入Protobuf结构体指针
 	// 使用actionFlags过滤，例如
-	// Trident仅关心PACKET_RAW_BROKERING和PACKET_CAPTURING，
+	// Trident仅关心PACKET_BROKERING和PACKET_CAPTURING，
 	// 那么就不要将EPC等云平台信息进行计算。
 	// droplet关心**几乎**所有，对关心的信息进行计算
 	availableMapSize := getAvailableMapSize(queueCount, mapSize)
@@ -180,20 +180,26 @@ func (t *PolicyTable) GetCounter() interface{} {
 	return counter
 }
 
-// Trident用于PACKET_RAW_BROKERING和PACKET_CAPTURING
-func (t *PolicyTable) LookupActionByKey(key *LookupKey) *PolicyData {
-	// 将匹配策略的所有Action merge以后返回
-	// FIXME: 注意将查找过程中的性能监控数据发送到statsd
-	return nil
+// Trident用于PACKET_BROKERING和PACKET_CAPTURING
+func (t *PolicyTable) LookupPolicyByKey(key *LookupKey) *PolicyData {
+	// TODO: 将查找过程中的性能监控数据发送到statsd
+	if !key.Tap.CheckTapType(key.Tap) {
+		return nil
+	}
+	endpoint, policy := t.policyLabeler.GetPolicyByFastPath(key)
+	if policy == nil {
+		policy = t.policyLabeler.GetPolicyByFirstPath(endpoint, key)
+	}
+	return policy
 }
 
-// River用于PACKET_RAW_BROKERING，Stream用于PACKET_CAPTURING
+// River用于PACKET_BROKERING，Stream用于PACKET_CAPTURING
 func (t *PolicyTable) LookupActionByPolicyId(policyId PolicyId) *PolicyData {
 	// FIXME
 	return nil
 }
 
-// Droplet用于*_COUNTING、PACKET_RAW_BROKERING、PACKET_CAPTURING
+// Droplet用于*_COUNTING、PACKET_BROKERING、PACKET_CAPTURING
 func (t *PolicyTable) LookupAllByKey(key *LookupKey) (*EndpointData, *PolicyData) {
 	if !key.Tap.CheckTapType(key.Tap) {
 		return INVALID_ENDPOINT_DATA, INVALID_POLICY_DATA
