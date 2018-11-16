768f0885a266beb106747638c343272b33c1a81f
邱布衣 <buyi@yunshan.net.cn>
2018-11-16 17:53:39 +0800

    LeakyBucket在rate小于1ns时会产生除0错误
    **复现步骤**
    
    make test
    
    **原因和解决方案**
    
    rate小于1ns时，得出的interval为0，将其作为除数使用时会产生除0错误
    
    增加unit，当rate小于1ns时，计算unit，否则unit默认为0，也就是rate大于
    10^9时，rate应当为10^9的倍数，否则会被向下取整
    
    **影响范围**
    
    LeakyBucket在作为bps使用时的可用性
    
    **验证方案**
    
    单元测试成功
    
    **涉及分支**
    
    * feature-npb
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang

--- utils/leaky_bucket_test.go
+++ utils/leaky_bucket_test.go
@@ -3,11 +3,14 @@ package utils
 import (
 	"testing"
 	"time"
+
+	"github.com/docker/go-units"
 )
 
 func TestLeakyBucket(t *testing.T) {
 	timestamp := time.Second
-	b := NewLeakyBucket(1000) // 1000 pps, one per 1ms
+	b := LeakyBucket{}
+	b.SetRate(1000) // 1000 pps, one per 1ms
 	b.last = timestamp
 	if b.Acquire(timestamp, 1) {
 		t.Error("Should not acquired")
@@ -44,5 +47,16 @@ func TestLeakyBucket(t *testing.T) {
 	}
 	if !b.Acquire(timestamp, 1000) {
 		t.Error("Should acquired")
+	}
+}
+
+func TestLeakyBucketBps(t *testing.T) {
+	b := LeakyBucket{}
+	b.SetRate(units.TB * 8) // 100Tbps
+	b.last = time.Second
+	for i := 1; i <= 1000; i++ {
+		if !b.Acquire(time.Second+time.Millisecond*time.Duration(i), units.GB*8) {
+			t.Error("Should acquired")
+		}
 	}
 }
