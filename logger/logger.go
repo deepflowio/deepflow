f378f173dc3608ac2fb4c5627f8c553ad8e64b6b
邱布衣 <buyi@yunshan.net.cn>
2018-11-22 14:15:11 +0800

    修改logger的实现
    **复现步骤**
    
    非问题不涉及
    
    **原因和解决方案**
    
    logger的实现和go-logging的意图不太符合
    
    所有的backends都不需要AddModuleLevel，
    因为SetBackend时go-logging都会添加，
    syslog/rsyslog也不需要转换level，
    go-logging已经做了level转换的事情
    
    logger也不需要提供SetLevel，go-logging自身就提供了SetLevel
    
    **影响范围**
    
    影响logger日志
    
    **验证方案**
    
    通过trident验证成功
    
    **涉及分支**
    
    * feature-npb
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang

--- logger/logger.go
+++ logger/logger.go
@@ -1,11 +1,11 @@
 package logger
 
 import (
+	"log/syslog"
 	"os"
 	"path"
+	"strings"
 	"time"
-
-	"log/syslog"
 
 	"github.com/lestrrat/go-file-rotatelogs"
 	"github.com/op/go-logging"
@@ -18,39 +18,31 @@ const (
 	LOG_COLOR_FORMAT      = "%{color}%{time:2006-01-02 15:04:05.000} [%{level:.4s}]%{color:reset} %{shortfile} %{message}"
 )
 
-var log = logging.MustGetLogger(path.Base(os.Args[0]))
+var (
+	stdoutBackend   logging.Backend
+	fileBackend     logging.Backend
+	syslogBackend   logging.Backend
+	rsyslogBackends []logging.Backend
+)
 
-type StdoutLog string
-type FileLog string
-type Rsyslog string
-type SyslogLog string
-
-func Stdout() StdoutLog {
-	return ""
-}
-
-func Syslog() SyslogLog {
-	return ""
-}
-
-func stdoutBackend(level logging.Level) logging.Backend {
-	stdout := logging.AddModuleLevel(
-		logging.NewBackendFormatter(
-			logging.NewLogBackend(os.Stdout, "", 0),
-			logging.MustStringFormatter(LOG_COLOR_FORMAT),
-		),
+func EnableStdoutLog() {
+	if stdoutBackend != nil {
+		return
+	}
+	stdoutBackend = logging.NewBackendFormatter(
+		logging.NewLogBackend(os.Stdout, "", 0),
+		logging.MustStringFormatter(LOG_COLOR_FORMAT),
 	)
-	stdout.SetLevel(level, "")
-	return stdout
+	applyBackendChange()
 }
 
-func fileBackend(level logging.Level, logPath string) logging.Backend {
+func EnableFileLog(logPath string) error {
 	dir := path.Dir(logPath)
 	if _, err := os.Stat(dir); err != nil {
 		if os.IsNotExist(err) {
 			os.MkdirAll(dir, 0755)
 		} else {
-			log.Error(err.Error())
+			return err
 		}
 	}
 
@@ -61,92 +53,65 @@ func fileBackend(level logging.Level, logPath string) logging.Backend {
 		rotatelogs.WithRotationTime(LOG_ROTATION_INTERVAL),
 	)
 	if err != nil {
-		log.Error(err.Error())
-		os.Exit(-1)
-	}
-
-	file := logging.AddModuleLevel(
-		logging.NewBackendFormatter(
-			logging.NewLogBackend(ioWriter, "", 0),
-			logging.MustStringFormatter(LOG_FORMAT),
-		),
-	)
-	file.SetLevel(level, "")
-	return file
-}
-
-func logLevelToPriority(level logging.Level) syslog.Priority {
-	switch level {
-	case logging.CRITICAL:
-		return syslog.LOG_CRIT
-	case logging.ERROR:
-		return syslog.LOG_ERR
-	case logging.WARNING:
-		return syslog.LOG_WARNING
-	case logging.NOTICE:
-		return syslog.LOG_NOTICE
-	case logging.INFO:
-		return syslog.LOG_INFO
-	case logging.DEBUG:
-		return syslog.LOG_DEBUG
-	default:
-		panic("invalid type")
-	}
-}
-
-func syslogBackend(level logging.Level) logging.Backend {
-	processName := path.Base(os.Args[0])
-	processName = processName + "/" + processName
-	syslogBackend, err := logging.NewSyslogBackendPriority(processName, logLevelToPriority(level)|syslog.LOG_LOCAL2)
-	if err != nil {
-		log.Error(err.Error())
-		os.Exit(-1)
-	}
-	syslog := logging.AddModuleLevel(syslogBackend)
-	syslog.SetLevel(level, "")
-	return syslog
-}
-
-func rsyslogWriter(level logging.Level, remote string) (logging.Backend, error) {
-	rsyslogWriter, err := syslog.Dial("udp", remote, logLevelToPriority(level), path.Base(os.Args[0]))
-	if err != nil {
-		return nil, err
-	}
-	rsyslog := logging.AddModuleLevel(
-		logging.NewBackendFormatter(
-			logging.NewLogBackend(rsyslogWriter, "", 0),
-			logging.MustStringFormatter(LOG_FORMAT),
-		),
-	)
-	rsyslog.SetLevel(level, "")
-	return rsyslog, nil
-}
-
-func InitLog(levelString string, loggers ...interface{}) error {
-	level, err := logging.LogLevel(levelString)
-	if err != nil {
 		return err
 	}
 
-	backends := make([]logging.Backend, 0, len(loggers))
-	for _, logger := range loggers {
-		switch logger.(type) {
-		case StdoutLog:
-			backends = append(backends, stdoutBackend(level))
-		case FileLog:
-			backends = append(backends, fileBackend(level, (string)(logger.(FileLog))))
-		case SyslogLog:
-			backends = append(backends, syslogBackend(level))
-		case Rsyslog:
-			rsyslog, err := rsyslogWriter(level, (string)(logger.(Rsyslog)))
-			if err != nil {
-				return err
-			}
-			backends = append(backends, rsyslog)
-		default:
-			continue
-		}
-	}
-	logging.SetBackend(backends...)
+	fileBackend = logging.NewBackendFormatter(
+		logging.NewLogBackend(ioWriter, "", 0),
+		logging.MustStringFormatter(LOG_FORMAT),
+	)
+	applyBackendChange()
 	return nil
+}
+
+func EnableSyslog() error {
+	if syslogBackend != nil {
+		return nil
+	}
+
+	processName := path.Base(os.Args[0])
+	processName = processName + "/" + processName
+	syslog, err := logging.NewSyslogBackend(processName)
+	if err != nil {
+		return err
+	}
+	syslogBackend = syslog
+	applyBackendChange()
+	return nil
+}
+
+func EnableRsyslog(remotes ...string) error {
+	rsyslogBackends = rsyslogBackends[:0]
+	for _, remote := range remotes {
+		if !strings.Contains(remote, ":") {
+			remote += ":514"
+		}
+		rsyslogWriter, err := syslog.Dial("udp", remote, syslog.LOG_CRIT, path.Base(os.Args[0]))
+		if err != nil {
+			return err
+		}
+		rsyslogBackends = append(rsyslogBackends, logging.NewBackendFormatter(
+			logging.NewLogBackend(rsyslogWriter, "", 0),
+			logging.MustStringFormatter(LOG_FORMAT),
+		))
+	}
+	applyBackendChange()
+	return nil
+}
+
+func applyBackendChange() {
+	var backends []logging.Backend
+	if stdoutBackend != nil {
+		backends = append(backends, stdoutBackend)
+	}
+	if fileBackend != nil {
+		backends = append(backends, fileBackend)
+	}
+	if syslogBackend != nil {
+		backends = append(backends, syslogBackend)
+	}
+	backends = append(backends, rsyslogBackends...)
+	level := logging.GetLevel("")
+	logging.SetBackend(backends...)
+	logging.SetLevel(level, "")
 }
