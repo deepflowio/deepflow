ee406497fc188d8f89c2a3bae432e623552d9269
Jin Jie <jinjie@yunshan.net.cn>
2018-12-01 11:40:31 +0800

    将droplet和zero的通信改为Push server
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    为了提高发送性能
    
    **影响范围**
    
    无
    
    **验证方案**
    
    验证通信正常
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @xiangyang
    
    /cc @buyi

--- sender/zero_document_sender.go
+++ sender/zero_document_sender.go
@@ -11,23 +11,20 @@ import (
 type ZeroDocumentSender struct {
 	inputQueues []queue.MultiQueueReader
 	queueCounts []int
-	ips         []string
-	ports       []uint16
+	listenPorts []uint16
 }
 
 type zeroDocumentSenderBuilder struct {
 	inputQueues []queue.MultiQueueReader
 	queueCounts []int
-	ips         []string
-	ports       []uint16
+	listenPorts []uint16
 }
 
 func NewZeroDocumentSenderBuilder() *zeroDocumentSenderBuilder {
 	return &zeroDocumentSenderBuilder{
 		inputQueues: make([]queue.MultiQueueReader, 0, 2),
 		queueCounts: make([]int, 0, 2),
-		ips:         make([]string, 0, 2),
-		ports:       make([]uint16, 0, 2),
+		listenPorts: make([]uint16, 0, 2),
 	}
 }
 
@@ -42,14 +39,16 @@ func (b *zeroDocumentSenderBuilder) AddQueue(q queue.MultiQueueReader, count int
 	return b
 }
 
-func (b *zeroDocumentSenderBuilder) AddZero(ip string, port uint16) *zeroDocumentSenderBuilder {
-	for i := range b.ips {
-		if ip == b.ips[i] && b.ports[i] == port {
-			return b
+func (b *zeroDocumentSenderBuilder) AddListenPorts(ports ...uint16) *zeroDocumentSenderBuilder {
+OUTER:
+	for _, newPort := range ports {
+		for _, oldPort := range b.listenPorts {
+			if newPort == oldPort {
+				continue OUTER
+			}
 		}
+		b.listenPorts = append(b.listenPorts, newPort)
 	}
-	b.ips = append(b.ips, ip)
-	b.ports = append(b.ports, port)
 	return b
 }
 
@@ -57,29 +56,25 @@ func (b *zeroDocumentSenderBuilder) Build() *ZeroDocumentSender {
 	return &ZeroDocumentSender{
 		inputQueues: b.inputQueues,
 		queueCounts: b.queueCounts,
-		ips:         b.ips,
-		ports:       b.ports,
+		listenPorts: b.listenPorts,
 	}
 }
 
 func (s *ZeroDocumentSender) Start(queueSize int) {
-	queueReaders := make([]queue.QueueReader, len(s.ips))
-	queueWriters := make([]queue.QueueWriter, len(s.ips))
-	for i := 0; i < len(s.ips); i++ {
+	lenOfPorts := len(s.listenPorts)
+	queueWriters := make([]queue.QueueWriter, lenOfPorts)
+	for i := 0; i < lenOfPorts; i++ {
 		q := queue.NewOverwriteQueue(
 			"6-all-doc-to-zero", queueSize,
 			queue.OptionRelease(func(p interface{}) { utils.ReleaseByteBuffer(p.(*utils.ByteBuffer)) }),
 			stats.OptionStatTags{"index": strconv.Itoa(i)},
 		)
-		queueReaders[i] = q
 		queueWriters[i] = q
+		go NewZMQBytePusher("*", s.listenPorts[i], queueSize).QueueForward(q)
 	}
 	for i, q := range s.inputQueues {
 		for key := 0; key < s.queueCounts[i]; key++ {
 			go NewZeroDocumentMarshaller(q, queue.HashKey(key), queueWriters...).Start()
 		}
-	}
-	for i := range s.ips {
-		go NewZMQBytePusher(s.ips[i], s.ports[i], queueSize).QueueForward(queueReaders[i])
 	}
 }
