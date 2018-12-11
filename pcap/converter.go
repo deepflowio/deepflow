0b3d2c2f8477716eb70f70aa0e30bd3c97d60b4b
Jin Jie <jinjie@yunshan.net.cn>
2018-12-11 15:05:58 +0800

    pcap写入性能优化
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    去掉填充0的操作
    
    **影响范围**
    
    无
    
    **验证方案**
    
    验证pcap正常
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @xiangyang
    
    /cc @buyi

--- pcap/converter.go
+++ pcap/converter.go
@@ -54,10 +54,6 @@ const (
 
 func (p RawPacket) fillEthernet(packet *datatype.MetaPacket, start int) int {
 	base := p[start:]
-	for i := 0; i < ETHERNET_LEN+VLAN_LEN; i++ {
-		base[i] = 0
-	}
-
 	offset := 0
 
 	macIntToBytes(packet.MacDst, base[offset:])
@@ -78,6 +74,7 @@ func (p RawPacket) fillEthernet(packet *datatype.MetaPacket, start int) int {
 
 const (
 	IPV4_VERSION_IHL_OFFSET     = 0
+	IPV4_DSCP_ECN_OFFSET        = 1
 	IPV4_TOTAL_LENGTH_OFFSET    = 2
 	IPV4_ID_OFFSET              = 4
 	IPV4_FLAGS_FRAGMENT_OFFSET  = 6
@@ -95,11 +92,9 @@ const (
 
 func (p RawPacket) fillIPv4(packet *datatype.MetaPacket, start int) int {
 	base := p[start:]
-	for i := 0; i < IPV4_LEN; i++ {
-		base[i] = 0
-	}
 
 	base[IPV4_VERSION_IHL_OFFSET] = (IPV4_VERSION << 4) | packet.IHL
+	base[IPV4_DSCP_ECN_OFFSET] = 0
 	binary.BigEndian.PutUint16(base[IPV4_TOTAL_LENGTH_OFFSET:], packet.PacketLen-uint16(start))
 	binary.BigEndian.PutUint16(base[IPV4_ID_OFFSET:], packet.IpID)
 	binary.BigEndian.PutUint16(base[IPV4_FLAGS_FRAGMENT_OFFSET:], packet.IpFlags)
@@ -140,6 +135,7 @@ const (
 	TCP_FLAGS_OFFSET       = 13 // NS not included
 	TCP_WINDOW_SIZE_OFFSET = 14
 	TCP_CHECKSUM_OFFSET    = 16
+	TCP_URG_PTR_OFFSET     = 18
 	TCP_OPTIONS_OFFSET     = 20
 	TCP_MAX_OPTIONS_LEN    = 40
 	TCP_MIN_LEN            = 20
@@ -152,62 +148,51 @@ const (
 )
 
 func (p RawPacket) fillTCP(packet *datatype.MetaPacket, start, ipv4Offset int) int {
-	base := p[start:]
-	for i := 0; i < TCP_OPTIONS_OFFSET+TCP_MAX_OPTIONS_LEN; i++ {
-		base[i] = 0
+	if packet.Invalid || packet.TcpData == nil {
+		return 0
 	}
 
-	length := TCP_MIN_LEN
+	base := p[start:]
 
 	binary.BigEndian.PutUint16(base[TCP_SPORT_OFFSET:], packet.PortSrc)
 	binary.BigEndian.PutUint16(base[TCP_DPORT_OFFSET:], packet.PortDst)
 
-	if packet.TcpData != nil {
-		binary.BigEndian.PutUint32(base[TCP_SEQ_NUMBER_OFFSET:], packet.TcpData.Seq)
-		binary.BigEndian.PutUint32(base[TCP_ACK_NUMBER_OFFSET:], packet.TcpData.Ack)
-		base[TCP_DATA_OFFSET_OFFSET] = packet.TcpData.DataOffset << 4
-		base[TCP_FLAGS_OFFSET] = packet.TcpData.Flags
-		binary.BigEndian.PutUint16(base[TCP_WINDOW_SIZE_OFFSET:], packet.TcpData.WinSize)
+	binary.BigEndian.PutUint32(base[TCP_SEQ_NUMBER_OFFSET:], packet.TcpData.Seq)
+	binary.BigEndian.PutUint32(base[TCP_ACK_NUMBER_OFFSET:], packet.TcpData.Ack)
+	base[TCP_DATA_OFFSET_OFFSET] = packet.TcpData.DataOffset << 4
+	base[TCP_FLAGS_OFFSET] = packet.TcpData.Flags
+	binary.BigEndian.PutUint16(base[TCP_WINDOW_SIZE_OFFSET:], packet.TcpData.WinSize)
+	binary.BigEndian.PutUint16(base[TCP_URG_PTR_OFFSET:], 0)
 
-		optOffset := 0
-		if packet.TcpData.MSS > 0 {
-			base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindMSS)
-			base[TCP_OPTIONS_OFFSET+optOffset+1] = TCP_OPTION_KIND_MSS_LEN
-			binary.BigEndian.PutUint16(base[TCP_OPTIONS_OFFSET+optOffset+2:], packet.TcpData.MSS)
-			optOffset += TCP_OPTION_KIND_MSS_LEN
-		}
-		if packet.TcpData.WinScale > 0 {
-			base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindWindowScale)
-			base[TCP_OPTIONS_OFFSET+optOffset+1] = TCP_OPTION_KIND_WINDOW_SCALE_LEN
-			base[TCP_OPTIONS_OFFSET+optOffset+2] = packet.TcpData.WinScale
-			optOffset += TCP_OPTION_KIND_WINDOW_SCALE_LEN
-		}
-		if packet.TcpData.SACKPermitted {
-			base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindSACKPermitted)
-			base[TCP_OPTIONS_OFFSET+optOffset+1] = TCP_OPTION_KIND_SACK_PERMITTED_LEN
-			optOffset += TCP_OPTION_KIND_SACK_PERMITTED_LEN
-		}
-		if sackLen := len(packet.TcpData.Sack); sackLen > 0 {
-			if sackLen&0x7 != 0 || sackLen > 32 { // not multiple of 8
-				log.Debugf("SAck length %d incorrect", sackLen)
-			} else {
-				base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindSACK)
-				base[TCP_OPTIONS_OFFSET+optOffset+1] = byte(sackLen + 2)
-				copy(base[TCP_OPTIONS_OFFSET+optOffset+2:], packet.TcpData.Sack)
-				optOffset += sackLen + 2
-			}
-		}
-		if optOffset > 0 {
-			optOffset++ // end of options
-			if optOffset&0x3 != 0 {
-				optOffset = (optOffset | 0x3) + 1
-			}
-			length += optOffset
-		}
-		if uint8(length>>2) != packet.TcpData.DataOffset {
-			log.Debugf("DataOffset %d mismatch with real length %d", packet.TcpData.DataOffset, length)
+	optOffset := 0
+	if packet.TcpData.MSS > 0 {
+		base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindMSS)
+		base[TCP_OPTIONS_OFFSET+optOffset+1] = TCP_OPTION_KIND_MSS_LEN
+		binary.BigEndian.PutUint16(base[TCP_OPTIONS_OFFSET+optOffset+2:], packet.TcpData.MSS)
+		optOffset += TCP_OPTION_KIND_MSS_LEN
+	}
+	if packet.TcpData.WinScale > 0 {
+		base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindWindowScale)
+		base[TCP_OPTIONS_OFFSET+optOffset+1] = TCP_OPTION_KIND_WINDOW_SCALE_LEN
+		base[TCP_OPTIONS_OFFSET+optOffset+2] = packet.TcpData.WinScale
+		optOffset += TCP_OPTION_KIND_WINDOW_SCALE_LEN
+	}
+	if packet.TcpData.SACKPermitted {
+		base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindSACKPermitted)
+		base[TCP_OPTIONS_OFFSET+optOffset+1] = TCP_OPTION_KIND_SACK_PERMITTED_LEN
+		optOffset += TCP_OPTION_KIND_SACK_PERMITTED_LEN
+	}
+	if sackLen := len(packet.TcpData.Sack); sackLen > 0 {
+		if sackLen&0x7 != 0 || sackLen > 32 { // not multiple of 8
+			log.Debugf("SAck length %d incorrect", sackLen)
+		} else {
+			base[TCP_OPTIONS_OFFSET+optOffset] = byte(layers.TCPOptionKindSACK)
+			base[TCP_OPTIONS_OFFSET+optOffset+1] = byte(sackLen + 2)
+			copy(base[TCP_OPTIONS_OFFSET+optOffset+2:], packet.TcpData.Sack)
+			optOffset += sackLen + 2
 		}
 	}
+	length := int(packet.TcpData.DataOffset) << 2
 
 	binary.BigEndian.PutUint16(base[TCP_CHECKSUM_OFFSET:], p.tcpIPChecksum(layers.IPProtocolTCP, ipv4Offset, start, length))
 
