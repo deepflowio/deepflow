82f55f3edcf156d886b773e09c9c6366e4f7ef1a
zhuofeng <zhuofeng@yunshan.net.cn>
2018-12-17 18:42:01 +0800

    序列化支持对整形u32和u64压缩
    **复现步骤**
    
    * 无
    
    **原因和解决方案**
    
    * 参照github.com/golang/protobuf/proto/encode.go和decode.go
    * 使用varint和zigzag压缩, 正数建议用varint(最高位为0), 无论正数负数zigzag都支持，但性能略差些
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @buyi @jinjie

---
+++ codec/varint_codec.go
@@ -0,0 +1,176 @@
+// reference github.com/golang/protobuf/proto encode.go decode.go
+package codec
+
+import (
+	"errors"
+	"io"
+)
+
+// encode
+func (e *SimpleEncoder) encodeVarint(x uint64) {
+	for x >= 1<<7 {
+		e.buf = append(e.buf, uint8(x&0x7f|0x80))
+		x >>= 7
+	}
+	e.buf = append(e.buf, uint8(x))
+}
+
+func (e *SimpleEncoder) WriteZigzagU32(v uint32) {
+	e.encodeVarint(uint64((v << 1) ^ uint32((int32(v) >> 31))))
+}
+
+func (e *SimpleEncoder) WriteZigzagU64(v uint64) {
+	e.encodeVarint(uint64((v << 1) ^ uint64((int64(v) >> 63))))
+}
+
+func (e *SimpleEncoder) WriteVarintU32(v uint32) {
+	e.encodeVarint(uint64(v))
+}
+
+func (e *SimpleEncoder) WriteVarintU64(v uint64) {
+	e.encodeVarint(v)
+}
+
+// decode
+func (d *SimpleDecoder) ReadZigzagU32() uint32 {
+	x := d.DecodeVarint()
+	return (uint32(x) >> 1) ^ uint32((int32(x&1)<<31)>>31)
+}
+
+func (d *SimpleDecoder) ReadZigzagU64() uint64 {
+	x := d.DecodeVarint()
+	return (x >> 1) ^ uint64((int64(x&1)<<63)>>63)
+}
+
+func (d *SimpleDecoder) ReadVarintU32() uint32 {
+	return (uint32)(d.DecodeVarint())
+}
+
+func (d *SimpleDecoder) ReadVarintU64() uint64 {
+	return d.DecodeVarint()
+}
+
+func (d *SimpleDecoder) DecodeVarint() uint64 {
+	i := d.offset
+	buf := d.buf
+
+	if i >= len(buf) {
+		d.err++
+		return 0
+	} else if buf[i] < 0x80 {
+		d.offset++
+		return uint64(buf[i])
+	} else if len(buf)-i < 10 {
+		v, _ := d.decodeVarintSlow()
+		return v
+	}
+
+	var b uint64
+	// we already checked the first byte
+	x := uint64(buf[i]) - 0x80
+	i++
+
+	b = uint64(buf[i])
+	i++
+	x += b << 7
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 7
+
+	b = uint64(buf[i])
+	i++
+	x += b << 14
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 14
+
+	b = uint64(buf[i])
+	i++
+	x += b << 21
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 21
+
+	b = uint64(buf[i])
+	i++
+	x += b << 28
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 28
+
+	b = uint64(buf[i])
+	i++
+	x += b << 35
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 35
+
+	b = uint64(buf[i])
+	i++
+	x += b << 42
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 42
+
+	b = uint64(buf[i])
+	i++
+	x += b << 49
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 49
+
+	b = uint64(buf[i])
+	i++
+	x += b << 56
+	if b&0x80 == 0 {
+		goto done
+	}
+	x -= 0x80 << 56
+
+	b = uint64(buf[i])
+	i++
+	x += b << 63
+	if b&0x80 == 0 {
+		goto done
+	}
+	// x -= 0x80 << 63 // Always zero.
+
+	d.err++
+	return 0
+
+done:
+	d.offset = i
+	return x
+}
+
+func (d *SimpleDecoder) decodeVarintSlow() (x uint64, err error) {
+	i := d.offset
+	l := len(d.buf)
+
+	for shift := uint(0); shift < 64; shift += 7 {
+		if i >= l {
+			err = io.ErrUnexpectedEOF
+			d.err++
+			return
+		}
+		b := d.buf[i]
+		i++
+		x |= (uint64(b) & 0x7F) << shift
+		if b < 0x80 {
+			d.offset = i
+			return
+		}
+	}
+
+	// The number is too large to represent in a 64-bit value.
+	err = errors.New("decode: integer overflow")
+	d.err++
+	return
+}
