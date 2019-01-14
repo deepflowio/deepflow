0ae0b6bbfa7dbc0e3a7d62a2b74e470d808ad078
jxf <xiaofeng01@yunshan.net.cn>
2019-01-14 11:22:03 +0800

    aclgid将源和目的合并到一个bitmap
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    优化内存
    
    **影响范围**
    
    无
    
    **验证方案**
    
    待验证
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @buyi
    
    /cc @buyi

--- datatype/policy_test.go
+++ datatype/policy_test.go
@@ -22,19 +22,13 @@ func compareGroupSlice(v, exp [][]int32) bool {
 }
 
 func TestFillGroupID_1(t *testing.T) {
-	aclGidbitmap0 := AclGidBitmap(0)
-	aclGidbitmap0.SetSrcFlag()
-	aclGidbitmap0.SetMapOffset(0)
+	aclGidbitmap := AclGidBitmap(0).SetSrcAndDstFlag().SetSrcMapOffset(0).SetDstMapOffset(0)
 
-	aclGidbitmap1 := AclGidBitmap(0)
-	aclGidbitmap1.SetDstFlag()
-	aclGidbitmap1.SetMapOffset(0)
-
-	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(2)
+	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(1)
 	allGroupIDs := [][]uint32{[]uint32{1, 2}, []uint32{3, 4}}
 	aclGroupIDs := [][]int32{[]int32{5, 6}, []int32{7, 8}}
 
-	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap0, aclGidbitmap1}, allGroupIDs, aclGroupIDs)
+	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap}, allGroupIDs, aclGroupIDs)
 	exp := [][]int32{[]int32{}, []int32{}}
 	if !compareGroupSlice(aclGroupIDs, exp) {
 		t.Errorf("TestFillGroupID Check Failed: Mapbits=0, expect %v, found %v", exp, aclGroupIDs)
@@ -42,21 +36,14 @@ func TestFillGroupID_1(t *testing.T) {
 }
 
 func TestFillGroupID_2(t *testing.T) {
-	aclGidbitmap0 := AclGidBitmap(0)
-	aclGidbitmap0.SetSrcFlag()
-	aclGidbitmap0.SetMapOffset(0)
-	aclGidbitmap0.SetMapBits(0)
+	aclGidbitmap := AclGidBitmap(0).SetSrcAndDstFlag().SetSrcMapOffset(0).SetSrcMapBits(0)
+	aclGidbitmap = aclGidbitmap.SetDstMapOffset(0).SetDstMapBits(0)
 
-	aclGidbitmap1 := AclGidBitmap(0)
-	aclGidbitmap1.SetDstFlag()
-	aclGidbitmap1.SetMapOffset(0)
-	aclGidbitmap1.SetMapBits(0)
-
-	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(2)
+	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(1)
 	allGroupIDs := [][]uint32{[]uint32{1, 2}, []uint32{3, 4}}
 	aclGroupIDs := [][]int32{[]int32{}, []int32{}}
 
-	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap0, aclGidbitmap1}, allGroupIDs, aclGroupIDs)
+	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap}, allGroupIDs, aclGroupIDs)
 	exp := [][]int32{[]int32{1}, []int32{3}}
 	if !compareGroupSlice(aclGroupIDs, exp) {
 		t.Errorf("TestFillGroupID Check Failed: Mapbits=0x1, expect %v, found %v", exp, aclGroupIDs)
@@ -64,22 +51,14 @@ func TestFillGroupID_2(t *testing.T) {
 }
 
 func TestFillGroupID_3(t *testing.T) {
-	aclGidbitmap0 := AclGidBitmap(0)
-	aclGidbitmap0.SetSrcFlag()
-	aclGidbitmap0.SetMapOffset(0)
-	aclGidbitmap0.SetMapBits(0)
-	aclGidbitmap0.SetMapBits(1)
-	aclGidbitmap1 := AclGidBitmap(0)
-	aclGidbitmap1.SetDstFlag()
-	aclGidbitmap1.SetMapOffset(0)
-	aclGidbitmap1.SetMapBits(0)
-	aclGidbitmap1.SetMapBits(1)
+	aclGidbitmap := AclGidBitmap(0).SetSrcAndDstFlag().SetSrcMapOffset(0).SetSrcMapBits(0).SetSrcMapBits(1)
+	aclGidbitmap = aclGidbitmap.SetDstMapOffset(0).SetDstMapBits(0).SetDstMapBits(1)
 
-	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(2)
+	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(1)
 	allGroupIDs := [][]uint32{[]uint32{1, 2}, []uint32{3, 4}}
 	aclGroupIDs := [][]int32{[]int32{}, []int32{}}
 
-	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap0, aclGidbitmap1}, allGroupIDs, aclGroupIDs)
+	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap}, allGroupIDs, aclGroupIDs)
 	exp := [][]int32{[]int32{1, 2}, []int32{3, 4}}
 	if !compareGroupSlice(aclGroupIDs, exp) {
 		t.Errorf("TestFillGroupID Check Failed: Mapbits=0x3, expect %v, found %v", exp, aclGroupIDs)
@@ -87,21 +66,14 @@ func TestFillGroupID_3(t *testing.T) {
 }
 
 func TestFillGroupID_4(t *testing.T) {
-	aclGidbitmap0 := AclGidBitmap(0)
-	aclGidbitmap0.SetSrcFlag()
-	aclGidbitmap0.SetMapOffset(0)
-	aclGidbitmap0.SetMapBits(1)
+	aclGidbitmap := AclGidBitmap(0).SetSrcAndDstFlag().SetSrcMapOffset(0).SetSrcMapBits(1)
+	aclGidbitmap = aclGidbitmap.SetDstMapOffset(0).SetDstMapBits(1)
 
-	aclGidbitmap1 := AclGidBitmap(0)
-	aclGidbitmap1.SetDstFlag()
-	aclGidbitmap1.SetMapOffset(0)
-	aclGidbitmap1.SetMapBits(1)
-
-	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(2)
+	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(1)
 	allGroupIDs := [][]uint32{[]uint32{1, 2}, []uint32{3, 4}}
 	aclGroupIDs := [][]int32{[]int32{}, []int32{}}
 
-	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap0, aclGidbitmap1}, allGroupIDs, aclGroupIDs)
+	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap}, allGroupIDs, aclGroupIDs)
 	exp := [][]int32{[]int32{2}, []int32{4}}
 	if !compareGroupSlice(aclGroupIDs, exp) {
 		t.Errorf("TestFillGroupID Check Failed: Mapbits=0x2, expect %v, found %v", exp, aclGroupIDs)
@@ -110,29 +82,33 @@ func TestFillGroupID_4(t *testing.T) {
 
 func TestFillGroupID_5(t *testing.T) {
 
-	aclGidbitmap0 := AclGidBitmap(0)
-	aclGidbitmap0.SetSrcFlag()
-	aclGidbitmap0.SetMapOffset(56) // 必须为56的倍数
-	aclGidbitmap0.SetMapBits(0)
+	aclGidbitmap := AclGidBitmap(0).SetSrcAndDstFlag().SetSrcMapOffset(24).SetSrcMapBits(0)
+	aclGidbitmap = aclGidbitmap.SetDstMapOffset(24).SetDstMapBits(0)
 
-	aclGidbitmap1 := AclGidBitmap(0)
-	aclGidbitmap1.SetDstFlag()
-	aclGidbitmap1.SetMapOffset(56) // 必须为56的倍数
-	aclGidbitmap1.SetMapBits(0)
-
-	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(2)
+	aclAction := AclAction(0).SetAclGidBitmapOffset(0).SetAclGidBitmapCount(1)
 	allGroupIDs := [][]uint32{[]uint32{}, []uint32{}}
 	aclGroupIDs := [][]int32{[]int32{}, []int32{}}
 
-	for i := uint32(0); i < 56+2; i++ {
+	for i := uint32(0); i < 24+2; i++ {
 		allGroupIDs[0] = append(allGroupIDs[0], i)
 		allGroupIDs[1] = append(allGroupIDs[1], 100+i)
 	}
 
-	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap0, aclGidbitmap1}, allGroupIDs, aclGroupIDs)
-	exp := [][]int32{[]int32{56}, []int32{156}}
+	FillGroupID(aclAction, []AclGidBitmap{aclGidbitmap}, allGroupIDs, aclGroupIDs)
+	exp := [][]int32{[]int32{24}, []int32{124}}
 	if !compareGroupSlice(aclGroupIDs, exp) {
-		t.Errorf("%v %v %v %v", aclGidbitmap0, aclGidbitmap1, aclGroupIDs, exp)
+		t.Errorf("%v %v %v", aclGidbitmap, aclGroupIDs, exp)
 		t.Errorf("TestFillGroupID Check Failed: Mapbits=0x1, offset=1, expect %v, found %v", exp, aclGroupIDs)
+	}
+}
+
+func TestReverseGroupType(t *testing.T) {
+	aclGidBitmap := AclGidBitmap(0).SetSrcAndDstFlag().SetSrcMapOffset(0).SetSrcMapBits(2)
+	aclGidBitmap = aclGidBitmap.SetDstMapOffset(24).SetDstMapBits(4)
+	basicAclGidBitmap := aclGidBitmap
+	aclGidBitmap.ReverseGroupType()
+	if aclGidBitmap.GetSrcMapOffset() != basicAclGidBitmap.GetDstMapOffset() ||
+		aclGidBitmap.GetSrcMapBits() != basicAclGidBitmap.GetDstMapBits() {
+		t.Errorf("ReverseGroupType Failed: reverseAclGidBitmap(%v) basicAclGidBitmap(%v)", aclGidBitmap, basicAclGidBitmap)
 	}
 }
