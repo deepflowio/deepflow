96e83a40e064613957649757d81d8e5c6c63d82a
zhuofeng <zhuofeng@yunshan.net.cn>
2019-01-22 18:48:51 +0800

    增加统一的写influxdb接口
    **复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    本地验证基本功能
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @jinjie

---
+++ store/influxdb_writer_test.go
@@ -0,0 +1,168 @@
+package store
+
+import (
+	"encoding/binary"
+	"fmt"
+	"testing"
+	"time"
+
+	"bou.ke/monkey"
+	"github.com/influxdata/influxdb/models"
+
+	. "github.com/golang/mock/gomock"
+	"github.com/influxdata/influxdb/client/v2"
+	"gitlab.x.lan/yunshan/droplet-libs/store/mock_client"
+)
+
+const (
+	INFLUXDB_HTTP_ADDR = "http://127.0.0.1:9086"
+)
+
+type testInfluxdbWrite struct {
+	db          string
+	measurement string
+	tag         string
+	field       string
+	timestamp   uint32
+}
+
+func (w *testInfluxdbWrite) MarshalToBytes(buffer []byte) int {
+	offset := 0
+	size := copy(buffer[offset+4:], w.measurement)
+
+	size += copy(buffer[offset+4+size:], w.tag)
+
+	binary.BigEndian.PutUint32(buffer[offset:], uint32(size))
+	offset += (4 + size)
+
+	size = copy(buffer[offset+4:], w.field)
+	binary.BigEndian.PutUint32(buffer[offset:], uint32(size))
+	offset += (4 + size)
+
+	offset += MarshalTimestampTo(w.timestamp, buffer[offset:])
+
+	return offset
+}
+
+func (w *testInfluxdbWrite) GetDBName() string {
+	return w.db
+}
+
+func (w *testInfluxdbWrite) Release() {
+}
+
+func newWriteItem(db, measurement, tag, field string, timestamp uint32) InfluxdbItem {
+	return &testInfluxdbWrite{
+		db:          db,
+		measurement: measurement,
+		tag:         tag,
+		field:       field,
+		timestamp:   timestamp,
+	}
+}
+
+func newResp(tags map[string]string, fieldNames []string) *client.Response {
+	resp := &client.Response{}
+	resp.Err = ""
+
+	resp.Results = make([]client.Result, 0)
+
+	result := client.Result{}
+	result.Series = make([]models.Row, 0)
+
+	row := models.Row{}
+	row.Name = "rowName"
+	row.Tags = tags
+	row.Columns = fieldNames
+	row.Values = make([][]interface{}, 0)
+
+	result.Series = append(result.Series, row)
+	resp.Results = append(resp.Results, result)
+
+	return resp
+}
+
+func TestInfluxdbItem(t *testing.T) {
+	ctrl := NewController(t)
+	defer ctrl.Finish()
+	c := mock_client.NewMockClient(ctrl)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+
+	c.EXPECT().Write(Any()).Return(nil)
+	c.EXPECT().Write(Any()).Return(nil)
+	c.EXPECT().Write(Any()).Return(nil)
+
+	defer monkey.UnpatchAll()
+	monkey.Patch(client.NewHTTPClient, func(_ client.HTTPConfig) (client.Client, error) {
+		return c, nil
+	})
+
+	queueCount := 3
+
+	iw, _ := NewInfluxdbWriter(INFLUXDB_HTTP_ADDR, queueCount)
+	iw.SetBatchTimeout(0)
+	iw.SetQueueSize(100)
+	for i := 0; i < 100; i++ {
+		item := newWriteItem(fmt.Sprintf("db%d", i%queueCount),
+			fmt.Sprintf("m%d", i%2),
+			fmt.Sprintf(",t1=%d,t2=%d", i, i+1),
+			fmt.Sprintf("f1=%di,f2=%di", i, i+1),
+			uint32(time.Now().Unix())+uint32(i))
+		iw.Put(i%queueCount, item)
+	}
+	iw.Run()
+	time.Sleep(4 * time.Second / 3)
+}
+
+func TestInfluxdbPoint(t *testing.T) {
+	ctrl := NewController(t)
+	defer ctrl.Finish()
+	c := mock_client.NewMockClient(ctrl)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+	c.EXPECT().Query(Any()).Return(newResp(nil, nil), nil)
+
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+	c.EXPECT().Ping(Any()).Return(time.Duration(0), "", nil)
+
+	c.EXPECT().Write(Any()).Return(nil)
+	c.EXPECT().Write(Any()).Return(nil)
+	c.EXPECT().Write(Any()).Return(nil)
+
+	defer monkey.UnpatchAll()
+	monkey.Patch(client.NewHTTPClient, func(_ client.HTTPConfig) (client.Client, error) {
+		return c, nil
+	})
+
+	queueCount := 3
+	iw, _ := NewInfluxdbWriter(INFLUXDB_HTTP_ADDR, queueCount)
+	iw.SetBatchTimeout(0)
+	iw.SetQueueSize(100)
+	for i := 0; i < 100; i++ {
+		tag := make(map[string]string)
+		tag["t1"] = "tag1"
+		tag["t2"] = "tag2"
+		field := make(map[string]int64)
+		field["f1"] = int64(i)
+		field["f2"] = int64(i * 10)
+		iw.PutPoint(i%queueCount,
+			fmt.Sprintf("dbp%d", i%queueCount),
+			fmt.Sprintf("m%d", i%2),
+			tag,
+			field,
+			uint32(time.Now().Unix())+uint32(i))
+	}
+	iw.Run()
+	time.Sleep(4 * time.Second / 3)
+}
