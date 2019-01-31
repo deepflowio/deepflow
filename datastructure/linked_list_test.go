194d8fc547521f3624714a2dba64ed9882e009d0
邱布衣 <buyi@yunshan.net.cn>
2019-01-31 13:37:17 +0800

    stats代码有可能导致崩溃的bug
    **复现步骤**
    
    remove一个statsSource
    
    **原因和解决方案**
    
    remove之后没有重新从iterator获取下一个statSource，
    而linked_list也没有做太多防护
    
    **影响范围**
    
    程序稳定性
    
    **验证方案**
    
    通过trident验证成功
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang

--- datastructure/linked_list_test.go
+++ datastructure/linked_list_test.go
@@ -4,6 +4,17 @@ import (
 	"testing"
 )
 
+func TestRemoveFirst(t *testing.T) {
+	list := LinkedList{}
+	list.PushBack(1)
+	list.PushBack(2)
+	it := list.Iterator()
+	t.Log(list.Remove(&it))
+	if v := list.Remove(&it); v != 2 {
+		t.Error("Should be 2, actually", v)
+	}
+}
+
 func TestRemoveLast(t *testing.T) {
 	list := LinkedList{}
 	list.PushBack(1)
