58acaf888e76b1ea0265b469afe0fc90a9ae540a
Wang Tuo <wangtuo@yunshan.net.cn>
2019-02-03 14:11:22 +0800

    解决adapter对trident解压icmp异常问题
    **复现步骤**
    
    发送东西向icmp报文到分析器
    
    **原因和解决方案**
    
    东西向icmp报文经trident压缩后，将checksum的2个字节压缩掉了；导致同镜像流量一起解析时越界
    提供新的东西向icmp报文解析函数
    
    **影响范围**
    
    adapter解析trident发送的icmp包
    
    **验证方案**
    
    19环境验证
    
    **涉及分支**
    
    * master
    * 5.4.2
    * 5.4.3
    * 5.5.0
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 日志打印不会造成磁盘占用空间过多
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @buyi
    
    /cc @buyi @wangkai

--- adapter/sequential_decoder_test.go
+++ adapter/sequential_decoder_test.go
@@ -1,6 +1,9 @@
 package adapter
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -11,6 +14,45 @@ import (
 const (
 	MIN_PPS = 5000000 // 5Mpps
 )
+
+func TestDecoder(t *testing.T) {
+	var buffer bytes.Buffer
+	padding := [UDP_BUFFER_SIZE - PAYLOAD_MAX]byte{0xff, 0xff}
+	f, _ := os.Open("icmp_decode_test.pcap")
+	r, _ := pcapgo.NewReader(f)
+	for {
+		packet, info, err := r.ReadPacketData()
+		if err != nil || packet == nil {
+			break
+		}
+		packet = packet[42:]
+		if info.CaptureLength-42 < UDP_BUFFER_SIZE-PAYLOAD_MAX {
+			packet = append(packet, padding[:]...)
+		}
+
+		decoder := NewSequentialDecoder(packet, 0)
+		decoder.DecodeHeader()
+		for {
+			meta := &MetaPacket{}
+			if decoder.NextPacket(meta) {
+				break
+			}
+			if len(meta.RawHeader) > 0 {
+				buffer.Write(meta.RawHeader)
+			}
+		}
+	}
+	f.Close()
+
+	expectFile := "icmp_decode_test.result"
+	content, _ := ioutil.ReadFile(expectFile)
+	expected := string(content)
+	actual := buffer.String()
+	if expected != actual {
+		ioutil.WriteFile("actual_icmp.txt", []byte(actual), 0644)
+		t.Error(fmt.Sprintf("Inconsistent with %s, written to actual_icmp.txt", expectFile))
+	}
+}
 
 func BenchmarkDecoder(b *testing.B) {
 	b.StopTimer()
