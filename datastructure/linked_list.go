8bba6fa9ba1303185cdbb3b0ed45321660d84176
邱布衣 <buyi@yunshan.net.cn>
2019-02-20 10:34:10 +0000

    Revert "使用treiber stack实现lock free pool"
    This reverts commit 2e9998da79d41a6c44fef2688f70af06eaa2a293

--- datastructure/linked_list.go
+++ datastructure/linked_list.go
@@ -1,42 +1,68 @@
 // Golang的list简直就是辣鸡
 package datastructure
 
+import (
+	"gitlab.x.lan/yunshan/droplet-libs/pool"
+)
+
+var elementPool = pool.NewLockFreePool(func() interface{} {
+	return new(Element)
+})
+
+type Element struct {
+	value interface{}
+	next  *Element
+}
+
 type LinkedList struct {
 	head *Element
 	tail *Element
 	size int
 }
 
+func element(v interface{}) *Element {
+	e := elementPool.Get().(*Element)
+	e.value = v
+	return e
+}
+
+func releaseElement(e *Element) {
+	*e = Element{}
+	elementPool.Put(e)
+}
+
 func (q *LinkedList) PushFront(v interface{}) {
-	e := WrapElement(v)
+	e := element(v)
 	if q.size == 0 {
 		q.tail = e
 	} else {
-		e.Next = q.head
+		e.next = q.head
 	}
 	q.head = e
 	q.size++
 }
 
 func (q *LinkedList) PushBack(v interface{}) {
-	e := WrapElement(v)
+	e := element(v)
 	if q.size == 0 {
 		q.head = e
 	} else {
-		q.tail.Next = e
+		q.tail.next = e
 	}
 	q.tail = e
 	q.size++
 }
 
 func (q *LinkedList) PopFront() interface{} {
+	var toRelease *Element
 	if q.size == 0 {
 		return nil
 	}
-	element := q.head
-	q.head = q.head.Next
+	v := q.head.value
+	toRelease, q.head = q.head, q.head.next
+	releaseElement(toRelease)
 	q.size--
-	return UnwrapElement(element)
+	return v
 }
 
 func (q *LinkedList) Remove(it *Iterator) interface{} {
@@ -49,13 +75,15 @@ func (q *LinkedList) Remove(it *Iterator) interface{} {
 		return q.PopFront()
 	}
 	current := it.current
-	it.prev.Next = current.Next
+	it.prev.next = current.next
+	v := current.value
 	if q.tail == current {
 		q.tail = it.prev
 	}
 	q.size--
 	it.Next()
-	return UnwrapElement(current)
+	releaseElement(current)
+	return v
 }
 
 func (q *LinkedList) Len() int {
@@ -73,7 +101,7 @@ type Iterator struct {
 }
 
 func (it *Iterator) Next() {
-	it.prev, it.current = it.current, it.current.Next
+	it.prev, it.current = it.current, it.current.next
 }
 
 func (it *Iterator) Empty() bool {
@@ -81,5 +109,5 @@ func (it *Iterator) Empty() bool {
 }
 
 func (it *Iterator) Value() interface{} {
-	return it.current.Value
+	return it.current.value
 }
--- pool/pool.go
+++ pool/pool.go
@@ -1,45 +1,62 @@
 package pool
 
 import (
-	"unsafe"
-
-	. "gitlab.x.lan/yunshan/droplet-libs/datastructure"
+	"sync"
 )
 
-type pointer = unsafe.Pointer
 type Option = interface{}
-type OptionPoolSize int
+type OptionPoolSizePerCPU int
 
-const DEFAULT_POOL_SIZE = 65536
+const POOL_SIZE_PER_CPU = OptionPoolSizePerCPU(256)
 
+// sync.Pool对于每一个系统线程，能够无锁提取存放一个元素，
+// 其余元素会通过锁追加到数组中。为了能够尽可能避免锁的使用，
+// 我们需要利用好这一个元素的位置，所以在这个元素上放置slice指针
+// 作为实际的pool使用，每次Get/Put时，先拿到slice，弹出/推入元素后再
+// 将slice放回，以尽可能无锁分配释放资源
 type LockFreePool struct {
-	TreiberStack
+	sync.Pool
 
 	alloc func() interface{}
 }
 
 func (p *LockFreePool) Get() interface{} {
-	if element := p.Pop(); element != nil {
-		return element
+	elemPool := p.Pool.Get().(*[]interface{}) // avoid convT2Eslice
+	pool := *elemPool
+	var e interface{}
+	if len(pool) > 0 {
+		e = pool[len(pool)-1]
+		*elemPool = pool[:len(pool)-1]
 	} else {
-		return p.alloc()
+		e = p.alloc()
 	}
+	p.Pool.Put(elemPool)
+	return e
 }
 
 func (p *LockFreePool) Put(x interface{}) {
-	p.Push(x)
+	pool := p.Pool.Get().(*[]interface{}) // avoid convT2Eslice
+	if len(*pool) < cap(*pool) {
+		*pool = append(*pool, x)
+	}
+	p.Pool.Put(pool)
 }
 
 func NewLockFreePool(alloc func() interface{}, options ...Option) LockFreePool {
-	poolSize := DEFAULT_POOL_SIZE
+	poolSizePerCPU := POOL_SIZE_PER_CPU
 	for _, opt := range options {
-		if size, ok := opt.(OptionPoolSize); ok {
-			poolSize = int(size)
+		if size, ok := opt.(OptionPoolSizePerCPU); ok {
+			poolSizePerCPU = size
 		}
 	}
-
+	newSlice := func() interface{} {
+		p := make([]interface{}, 0, poolSizePerCPU)
+		return &p
+	}
 	return LockFreePool{
-		TreiberStack: NewTreiberStack(poolSize),
-		alloc:        alloc,
+		Pool: sync.Pool{
+			New: newSlice,
+		},
+		alloc: alloc,
 	}
 }
