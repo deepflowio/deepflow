709517b4a2d0655c0b2c02734ca4a5b15865b23f
邱布衣 <buyi@yunshan.net.cn>
2019-02-28 06:27:29 +0000

    实现无锁的leaky_bucket
    **复现步骤**
    
    非问题不涉及
    
    **原因和解决方案**
    
    trident多协程性能优化需要实现无锁的leaky-bucket
    
    **影响范围**
    
    leaky-bucket
    
    **验证方案**
    
    需要通过trident验证
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yahui @wangkai
    
    
    (cherry picked from commit 88283fe74f25ded00de22db359537e517f9e6010)

--- datastructure/leaky_bucket.go
+++ datastructure/leaky_bucket.go
@@ -2,18 +2,19 @@ package datastructure
 
 import (
 	"math"
-	"sync"
+	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
+// 这里的last并非最后一次Acquire的时间，而是最后一次Acquire后，
+// 按剩余bucket数量倒推出来的等效时间戳，也就是它不仅包含了时间戳的记录，
+// 还包含了剩余bucket数量
 type LeakyBucket struct {
-	sync.Mutex
-
-	last      time.Duration
-	rate      uint64
-	interval  time.Duration
-	unit      uint64
-	available uint64
+	rate     uint64
+	interval time.Duration // 生成一次token的间隔
+	unit     uint64        // 生成一次token的数量，当interval少于1ns时，unit > 1，否则unit = 1
+	last     time.Duration
 }
 
 func (b *LeakyBucket) SetRate(rate uint64) {
@@ -36,21 +37,44 @@ func (b *LeakyBucket) Acquire(timestamp time.Duration, size uint64) bool {
 	if timestamp < b.last {
 		return false
 	}
-	b.available += uint64((timestamp-b.last)/b.interval) * b.unit
-	if b.available > b.rate {
-		b.available = b.rate
+	full := false
+	available := uint64((timestamp-b.last)/b.interval) * b.unit
+	if available > b.rate {
+		available = b.rate
+		full = true
 	}
-	b.last += (timestamp - b.last) / b.interval * b.interval
-	acquirable := b.available >= size
-	if acquirable {
-		b.available -= size
+	if available >= size {
+		available -= size
+		if full {
+			b.last = timestamp - time.Duration(available/b.unit)*b.interval
+		} else {
+			b.last += time.Duration(size/b.unit) * b.interval
+		}
+		return true
 	}
-	return acquirable
+	return false
 }
 
 func (b *LeakyBucket) SafeAcquire(timestamp time.Duration, size uint64) bool {
-	b.Lock()
-	acquirable := b.Acquire(timestamp, size)
-	b.Unlock()
-	return acquirable
+	if timestamp < b.last {
+		return false
+	}
+	full := false
+	available := uint64((timestamp-b.last)/b.interval) * b.unit
+	if available > b.rate {
+		available = b.rate
+		full = true
+	}
+	if available >= size {
+		available -= size
+		if full {
+			last := timestamp - time.Duration(available/b.unit)*b.interval
+			if atomic.CompareAndSwapUint64((*uint64)(unsafe.Pointer(&b.last)), uint64(b.last), uint64(last)) {
+				return true
+			}
+		}
+		atomic.AddUint64((*uint64)(unsafe.Pointer(&b.last)), size/b.unit*uint64(b.interval))
+		return true
+	}
+	return false
 }
