e6144075dc849969e65c02a7a51d4f171efe0931
邱布衣 <buyi@yunshan.net.cn>
2019-03-07 19:52:43 +0800

    需要将gogo加入到依赖中
    **简述**
    
    需要将gogo加入到依赖中
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 预留字段id值以便后续添加
    - [ ] 修改内容向后兼容(如果修改了字段id值)
    - [ ] 不要使用required描述符(一方面导致调试困难，另一方面并不能阻止指针的生成)
    
    /assign @xiangyang

--- dfi/stub.go
+++ dfi/stub.go
@@ -1,3 +1,8 @@
 package dfi
 
+import (
+	_ "github.com/gogo/protobuf/gogoproto"
+	_ "github.com/gogo/protobuf/proto"
+)
+
 //go:generate protoc -I=.. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gogo_out=plugins=grpc:. ../dfi.proto
