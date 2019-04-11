24ccd819c0e3315d64194c956cb7f7726776bc1c
chenyahui <yahui@yunshan.net.cn>
2019-04-11 14:13:57 +0800

    一个时间戳无关的leakybucket实现
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    优化leakybucket
    
    **影响范围**
    
    无
    
    **验证方案**
    
    performance
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新dep依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    @wangkai @buyi @xiangyang

--- datastructure/leaky_bucket_test.go
+++ datastructure/leaky_bucket_test.go
@@ -7,56 +7,70 @@ import (
 	"github.com/docker/go-units"
 )
 
-func TestLeakyBucket(t *testing.T) {
-	timestamp := time.Second
+func rateTest(rate uint64, t *testing.T) {
 	b := LeakyBucket{}
-	b.SetRate(1000) // 1000 pps, one per 1ms
-	b.last = timestamp
-	if b.Acquire(timestamp, 1) {
+	b.Init(rate)
+	interval := 100 * time.Millisecond                        // bucket interval
+	if !b.Acquire(10 * rate / uint64(time.Second/interval)) { //burst
+		t.Error("Should acquired")
+	}
+	if b.Acquire(1) {
 		t.Error("Should not acquired")
 	}
-	timestamp += time.Millisecond
-	if !b.Acquire(timestamp, 1) {
-		t.Error("Should acquired")
-	}
-	timestamp += time.Millisecond - time.Microsecond
-	if b.Acquire(timestamp, 1) {
-		t.Error("Should not acquired")
-	}
-	timestamp += time.Microsecond * 2
-	if !b.Acquire(timestamp, 1) {
-		t.Error("Should acquired")
-	}
-	timestamp += time.Millisecond * 2
-	if !b.Acquire(timestamp, 1) {
-		t.Error("Should acquired")
-	}
-	if !b.Acquire(timestamp, 1) {
-		t.Error("Should acquired")
-	}
-	timestamp = 2 * time.Second
-	if b.Acquire(timestamp, 1000) {
-		t.Error("Should not acquired")
-	}
-	if !b.Acquire(timestamp, 1000-6) {
-		t.Error("Should acquired")
-	}
-	timestamp = 4 * time.Second
-	if b.Acquire(timestamp, 2000) {
-		t.Error("Should not acquired")
-	}
-	if !b.Acquire(timestamp, 1000) {
-		t.Error("Should acquired")
-	}
-}
-
-func TestLeakyBucketBps(t *testing.T) {
-	b := LeakyBucket{}
-	b.SetRate(0) // almost infinity
-	b.last = time.Second
-	for i := 1; i <= 1000; i++ {
-		if !b.Acquire(time.Second+time.Millisecond*time.Duration(i), units.GB*8) {
+	time.Sleep(interval + interval/2)
+	for i := 0; i < 10; i++ {
+		if !b.Acquire(rate / 10 / uint64(time.Second/interval)) {
 			t.Error("Should acquired")
 		}
+	}
+	if b.Acquire(rate / 10 / uint64(time.Second/interval)) {
+		t.Error("Should not acquired")
+	}
+	b.Close()
+}
+
+func TestLeakyBucket(t *testing.T) {
+	var rate uint64 = 1000
+	rateTest(rate, t)
+	rate = 10000000 //10mbps
+	rateTest(rate, t)
+
+	rate = 0
+	b := LeakyBucket{}
+	b.Init(rate)
+	for i := 1; i <= 1000; i++ {
+		if !b.Acquire(units.GB * 8) {
+			t.Error("Should acquired")
+		}
+	}
+
+}
+func BenchmarkLeakyBucket(b *testing.B) {
+	b.StopTimer()
+
+	rate := 0
+	lb := LeakyBucket{}
+	lb.Init(uint64(rate))
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		lb.Acquire(1000)
+	}
+}
+func Benchmark4Routines(b *testing.B) {
+	b.StopTimer()
+
+	rate := 0
+	lb := LeakyBucket{}
+	lb.Init(uint64(rate))
+	for i := 0; i < 3; i++ {
+		go func() {
+			for {
+				lb.Acquire(1)
+			}
+		}()
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		lb.Acquire(1)
 	}
 }
