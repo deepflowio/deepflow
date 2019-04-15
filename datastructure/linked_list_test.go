f7fcf5e118d7884638d70766965eca0541385b40
向阳 <xiangyang@yunshan.net.cn>
2019-04-15 16:00:53 +0000

    Fix linked list remove
    **复现步骤**
    
    **原因和解决方案**
    
    remove时未正确的更新tail
    
    **影响范围**
    
    statsd
    
    **验证方案**
    
    增加UT
    
    **涉及分支**
    
    * master
    * 5.5.1
    * 5.5.2
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @buyi
    
    /cc @buyi
    
    
    (cherry picked from commit b30ccf5b8e1fc7f804cdeaa6b5e780b63ebbaddf)

--- datastructure/linked_list_test.go
+++ datastructure/linked_list_test.go
@@ -16,6 +16,19 @@ func TestRemoveFirst(t *testing.T) {
 	if v := it.Value(); v != 2 {
 		t.Error("Should be 2, actually", v)
 	}
+
+	list.PushFront(3)
+	if list.Len() != 2 {
+		t.Error("Should be 2, actually", list.Len())
+	}
+	it = list.Iterator()
+	if v := it.Value(); v != 3 {
+		t.Error("Should be 3, actually", v)
+	}
+	it.Next()
+	if v := it.Value(); v != 2 {
+		t.Error("Should be 2, actually", v)
+	}
 }
 
 func TestRemoveLast(t *testing.T) {
@@ -31,7 +44,37 @@ func TestRemoveLast(t *testing.T) {
 	if !it.Empty() {
 		t.Error("Iterator should be empty")
 	}
-	if list.PopFront() != 1 {
-		t.Error("Should be 1")
+
+	list.PushBack(3)
+	if list.Len() != 2 {
+		t.Error("Should be 2, actually", list.Len())
 	}
+	it = list.Iterator()
+	if v := it.Value(); v != 1 {
+		t.Error("Should be 1, actually", v)
+	}
+	it.Next()
+	if v := it.Value(); v != 3 {
+		t.Error("Should be 3, actually", v)
+	}
+}
+
+func TestRemoveToEmpty(t *testing.T) {
+	list := LinkedList{}
+	list.PushBack(1)
+	list.PushBack(2)
+	list.PopFront()
+	list.PopFront()
+
+	list.PushFront(1)
+	list.PopFront()
+	list.PushFront(2)
+	list.PopFront()
+
+	list.PushFront(1)
+	list.PushFront(2)
+	list.PopFront()
+	list.PushFront(3)
+	list.PopFront()
+	list.PopFront()
 }
