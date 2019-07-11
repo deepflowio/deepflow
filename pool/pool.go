aa22b23ef65db001f3aed88a3e631f7e252f8da4
向阳 <xiangyang@yunshan.net.cn>
2019-07-11 23:57:22 +0800

    WIP: 优化内存池
    **复现步骤**
    
    **原因和解决方案**
    
    当前的内存池，当reader或writer是单线程时表现比较糟糕。
    使用两个Pool对其进行优化，避免CPU自身永远拿到不适合自身的Slice。
    
    **影响范围**
    
    UT可保证正确性
    
    **验证方案**
    
    已在10.50.1.19测试性能，有明显提升
    
    **涉及分支**
    
    * master
    * 5.5.4
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @buyi
    
    /cc @buyi @yuanchao @jinjie @zhuofeng @wangtuo

--- pool/pool.go
+++ pool/pool.go
@@ -6,8 +6,10 @@ import (
 
 type Option = interface{}
 type OptionPoolSizePerCPU int
+type OptionInitFullPoolSize int // 太大会导致Get操作卡顿，太小会导致创建过多的slice
 
 const POOL_SIZE_PER_CPU = OptionPoolSizePerCPU(256)
+const INIT_FULL_POOL_SIZE = OptionInitFullPoolSize(256)
 
 // sync.Pool对于每一个系统线程，能够无锁提取存放一个元素，
 // 其余元素会通过锁追加到数组中。为了能够尽可能避免锁的使用，
@@ -15,47 +17,67 @@ const POOL_SIZE_PER_CPU = OptionPoolSizePerCPU(256)
 // 作为实际的pool使用，每次Get/Put时，先拿到slice，弹出/推入元素后再
 // 将slice放回，以尽可能无锁分配释放资源
 type LockFreePool struct {
-	sync.Pool
+	emptyPool *sync.Pool
+	fullPool  *sync.Pool
 
 	alloc func() interface{}
 }
 
 func (p *LockFreePool) Get() interface{} {
-	elemPool := p.Pool.Get().(*[]interface{}) // avoid convT2Eslice
+	elemPool := p.fullPool.Get().(*[]interface{}) // avoid convT2Eslice
 	pool := *elemPool
-	var e interface{}
-	if len(pool) > 0 {
-		e = pool[len(pool)-1]
-		*elemPool = pool[:len(pool)-1]
+	e := pool[len(pool)-1]
+	*elemPool = pool[:len(pool)-1]
+	if len(pool) > 1 {
+		p.fullPool.Put(elemPool)
 	} else {
-		e = p.alloc()
+		p.emptyPool.Put(elemPool) // Empty, 还给别的CPU
 	}
-	p.Pool.Put(elemPool)
 	return e
 }
 
 func (p *LockFreePool) Put(x interface{}) {
-	pool := p.Pool.Get().(*[]interface{}) // avoid convT2Eslice
+	pool := p.emptyPool.Get().(*[]interface{}) // avoid convT2Eslice
+	*pool = append(*pool, x)
 	if len(*pool) < cap(*pool) {
-		*pool = append(*pool, x)
+		p.emptyPool.Put(pool)
+	} else {
+		p.fullPool.Put(pool) // Full, 还给别的CPU
 	}
-	p.Pool.Put(pool)
 }
 
+// 注意OptionInitFullPoolSize不能大于OptionPoolSizePerCPU，且不能小于等于0
 func NewLockFreePool(alloc func() interface{}, options ...Option) LockFreePool {
 	poolSizePerCPU := POOL_SIZE_PER_CPU
+	initFullPoolSize := INIT_FULL_POOL_SIZE
 	for _, opt := range options {
 		if size, ok := opt.(OptionPoolSizePerCPU); ok {
 			poolSizePerCPU = size
+		} else if size, ok := opt.(OptionInitFullPoolSize); ok {
+			initFullPoolSize = size
 		}
 	}
-	newSlice := func() interface{} {
+	if poolSizePerCPU < OptionPoolSizePerCPU(initFullPoolSize) || initFullPoolSize <= 0 {
+		poolSizePerCPU = POOL_SIZE_PER_CPU
+		initFullPoolSize = INIT_FULL_POOL_SIZE
+	}
+	newEmptySlice := func() interface{} {
 		p := make([]interface{}, 0, poolSizePerCPU)
 		return &p
 	}
+	newFullSlice := func() interface{} {
+		p := make([]interface{}, initFullPoolSize, poolSizePerCPU)
+		for i := OptionInitFullPoolSize(0); i < initFullPoolSize; i++ {
+			p[i] = alloc()
+		}
+		return &p
+	}
 	return LockFreePool{
-		Pool: sync.Pool{
-			New: newSlice,
+		emptyPool: &sync.Pool{
+			New: newEmptySlice,
+		},
+		fullPool: &sync.Pool{
+			New: newFullSlice,
 		},
 		alloc: alloc,
 	}
