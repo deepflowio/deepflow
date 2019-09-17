c40c880b7a94122772311e09fac1b822af1ec7e3
向阳 <xiangyang@yunshan.net.cn>
2019-09-17 09:43:05 +0800

    新增高速U64 key LRU
    **复现步骤**
    
    **原因和解决方案**
    
    性能优化
    
    ```
    BenchmarkU64LRUAdd-20           21892117                52.8 ns/op
    BenchmarkU64LRURemove-20        50342298                24.3 ns/op
    BenchmarkU64LRUGet-20           61410669                16.7 ns/op
    BenchmarkU64LRUPeek-20          100000000               16.2 ns/op
    
    BenchmarkOldLRUAdd-20            3324456               393 ns/op
    BenchmarkOldLRURemove-20         8725460               155 ns/op
    BenchmarkOldLRUGet-20           15685113               102 ns/op
    BenchmarkOldLRUPeek-20          19253054                97.4 ns/op
    ```
    
    6x ~ 8x
    
    **影响范围**
    
    **验证方案**
    
    UT & Bench
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @yuanchao
    
    /cc @buyi @yuanchao @jinjie @zhuofeng @wangtuo

---
+++ hmap/lru/u64_lru.go
@@ -0,0 +1,256 @@
+package lru
+
+import (
+	"sync"
+
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("lru")
+
+const (
+	_BLOCK_SIZE_BITS = 8
+	_BLOCK_SIZE      = 1 << _BLOCK_SIZE_BITS
+	_BLOCK_SIZE_MASK = _BLOCK_SIZE - 1
+)
+
+type u64LRUNode struct {
+	key   uint64
+	value interface{}
+
+	hashListNext int32 // 表示节点所在冲突链的下一个节点的 buffer 数组下标，-1 表示不存在
+	hashListPrev int32 // 表示节点所在冲突链的上一个节点的 buffer 数组下标，-1 表示不存在
+	timeListNext int32 // 时间链表，含义与冲突链类似
+	timeListPrev int32 // 时间链表，含义与冲突链类似
+}
+
+var blankU64LRUNodeForInit u64LRUNode
+
+type u64LRUNodeBlock []u64LRUNode
+
+var u64LRUNodeBlockPool = sync.Pool{New: func() interface{} {
+	return u64LRUNodeBlock(make([]u64LRUNode, _BLOCK_SIZE))
+}}
+
+// 注意：不是线程安全的
+type U64LRU struct {
+	ringBuffer       []u64LRUNodeBlock // 存储Map节点，以矩阵环的方式组织，提升内存申请释放效率
+	bufferStartIndex int32             // ringBuffer中的开始下标（二维矩阵下标），闭区间
+	bufferEndIndex   int32             // ringBuffer中的结束下标（二维矩阵下标），开区间
+
+	hashSlots    int32  // 上取整至2^N，哈希桶个数
+	hashSlotBits uint32 // hashSlots中低位连续0比特个数
+
+	hashSlotHead []int32 // 哈希桶，hashSlotHead[i] 表示哈希值为 i 的冲突链的第一个节点为 buffer[[ hashSlotHead[i] ]]
+	timeListHead int32
+	timeListTail int32
+
+	capacity int // 最大容纳的Flow个数
+	size     int // 当前容纳的Flow个数
+}
+
+func (m *U64LRU) incIndex(index int32) int32 {
+	index++
+	if index>>_BLOCK_SIZE_BITS >= int32(len(m.ringBuffer)) {
+		return 0
+	}
+	return index
+}
+
+func (m *U64LRU) decIndex(index int32) int32 {
+	if index <= 0 {
+		return int32(len(m.ringBuffer)<<_BLOCK_SIZE_BITS) - 1
+	}
+	return index - 1
+}
+
+func (m *U64LRU) getNode(index int32) *u64LRUNode {
+	return &m.ringBuffer[index>>_BLOCK_SIZE_BITS][index&_BLOCK_SIZE_MASK]
+}
+
+func (m *U64LRU) pushNodeToHashList(node *u64LRUNode, nodeIndex int32, hash int32) {
+	node.hashListNext = m.hashSlotHead[hash]
+	node.hashListPrev = -1
+	if node.hashListNext != -1 {
+		m.getNode(node.hashListNext).hashListPrev = nodeIndex
+	}
+	m.hashSlotHead[hash] = nodeIndex
+}
+
+func (m *U64LRU) pushNodeToTimeList(node *u64LRUNode, nodeIndex int32) {
+	node.timeListNext = m.timeListHead
+	node.timeListPrev = -1
+	if node.timeListNext != -1 {
+		m.getNode(node.timeListNext).timeListPrev = nodeIndex
+	}
+	m.timeListHead = nodeIndex
+	if m.timeListTail == -1 {
+		m.timeListTail = nodeIndex
+	}
+}
+
+func (m *U64LRU) removeNodeFromHashList(node *u64LRUNode, newNext, newPrev int32) {
+	if node.hashListPrev != -1 {
+		prevNode := m.getNode(node.hashListPrev)
+		prevNode.hashListNext = newNext
+	} else {
+		m.hashSlotHead[m.compressHash(node.key)] = newNext
+	}
+
+	if node.hashListNext != -1 {
+		nextNode := m.getNode(node.hashListNext)
+		nextNode.hashListPrev = newPrev
+	}
+}
+
+func (m *U64LRU) removeNodeFromTimeList(node *u64LRUNode, newNext, newPrev int32) {
+	if node.timeListPrev != -1 {
+		prevNode := m.getNode(node.timeListPrev)
+		prevNode.timeListNext = newNext
+	} else {
+		m.timeListHead = newNext
+	}
+
+	if node.timeListNext != -1 {
+		nextNode := m.getNode(node.timeListNext)
+		nextNode.timeListPrev = newPrev
+	} else {
+		m.timeListTail = newPrev
+	}
+}
+
+func (m *U64LRU) removeNode(node *u64LRUNode, nodeIndex int32) {
+	// 从哈希链表、时间链表中删除
+	m.removeNodeFromHashList(node, node.hashListNext, node.hashListPrev)
+	m.removeNodeFromTimeList(node, node.timeListNext, node.timeListPrev)
+
+	// 将节点交换至buffer头部
+	if nodeIndex != m.bufferStartIndex {
+		firstNode := m.getNode(m.bufferStartIndex)
+		// 将firstNode内容拷贝至node
+		*node = *firstNode
+		// 修改firstNode在哈希链、时间链的上下游指向node
+		m.removeNodeFromHashList(firstNode, nodeIndex, nodeIndex)
+		m.removeNodeFromTimeList(firstNode, nodeIndex, nodeIndex)
+		// 将firstNode初始化
+		*firstNode = blankU64LRUNodeForInit
+	} else {
+		*node = blankU64LRUNodeForInit
+	}
+
+	// 释放头部节点
+	if m.bufferStartIndex&_BLOCK_SIZE_MASK == _BLOCK_SIZE_MASK {
+		u64LRUNodeBlockPool.Put(m.ringBuffer[m.bufferStartIndex>>_BLOCK_SIZE_BITS])
+		m.ringBuffer[m.bufferStartIndex>>_BLOCK_SIZE_BITS] = nil
+	}
+	m.bufferStartIndex = m.incIndex(m.bufferStartIndex)
+
+	m.size--
+}
+
+func (m *U64LRU) updateNode(node *u64LRUNode, nodeIndex int32, value interface{}) {
+	if nodeIndex != m.timeListHead {
+		// 从时间链表中删除
+		m.removeNodeFromTimeList(node, node.timeListNext, node.timeListPrev)
+		// 插入时间链表头部
+		m.pushNodeToTimeList(node, nodeIndex)
+	}
+
+	node.value = value
+}
+
+func (m *U64LRU) newNode(key uint64, value interface{}) {
+	// buffer空间检查
+	if m.size >= m.capacity {
+		node := m.getNode(m.timeListTail)
+		m.removeNode(node, m.timeListTail)
+	}
+	row := m.bufferEndIndex >> _BLOCK_SIZE_BITS
+	col := m.bufferEndIndex & _BLOCK_SIZE_MASK
+	if m.ringBuffer[row] == nil {
+		m.ringBuffer[row] = u64LRUNodeBlockPool.Get().(u64LRUNodeBlock)
+	}
+	node := &m.ringBuffer[row][col]
+	m.size++
+
+	// 新节点加入哈希链
+	m.pushNodeToHashList(node, m.bufferEndIndex, m.compressHash(key))
+	// 新节点加入时间链
+	m.pushNodeToTimeList(node, m.bufferEndIndex)
+	// 更新key、value
+	node.key = key
+	node.value = value
+
+	// 更新buffer信息
+	m.bufferEndIndex = m.incIndex(m.bufferEndIndex)
+}
+
+func (m *U64LRU) Add(key uint64, value interface{}) {
+	for hashListNext := m.hashSlotHead[m.compressHash(key)]; hashListNext != -1; {
+		node := m.getNode(hashListNext)
+		if node.key == key {
+			m.updateNode(node, hashListNext, value)
+			return
+		}
+		hashListNext = node.hashListNext
+	}
+	m.newNode(key, value)
+}
+
+func (m *U64LRU) Remove(key uint64) {
+	for hashListNext := m.hashSlotHead[m.compressHash(key)]; hashListNext != -1; {
+		node := m.getNode(hashListNext)
+		if node.key == key {
+			m.removeNode(node, hashListNext)
+			return
+		}
+		hashListNext = node.hashListNext
+	}
+}
+
+func (m *U64LRU) Get(key uint64, peek bool) (interface{}, bool) {
+	for hashListNext := m.hashSlotHead[m.compressHash(key)]; hashListNext != -1; {
+		node := m.getNode(hashListNext)
+		if node.key == key {
+			if !peek {
+				m.updateNode(node, hashListNext, node.value)
+			}
+			return node.value, true
+		}
+		hashListNext = node.hashListNext
+	}
+	return nil, false
+}
+
+func minPowerOfTwo(v int) (int, int) {
+	for i := 0; i < 30; i++ {
+		if v <= 1<<uint64(i) {
+			return 1 << uint64(i), i
+		}
+	}
+	return 1, 0
+}
+
+func (m *U64LRU) compressHash(hash uint64) int32 {
+	return int32((((((hash>>m.hashSlotBits)^hash)>>m.hashSlotBits)^hash)>>m.hashSlotBits)^hash) & (m.hashSlots - 1)
+}
+
+func NewU64LRU(hashSlots, capacity int) *U64LRU {
+	hashSlots, hashSlotBits := minPowerOfTwo(hashSlots)
+
+	m := &U64LRU{
+		ringBuffer:   make([]u64LRUNodeBlock, (capacity+_BLOCK_SIZE)/_BLOCK_SIZE*_BLOCK_SIZE+1),
+		hashSlots:    int32(hashSlots),
+		hashSlotBits: uint32(hashSlotBits),
+		hashSlotHead: make([]int32, hashSlots),
+		timeListHead: -1,
+		timeListTail: -1,
+		capacity:     capacity,
+	}
+
+	for i := 0; i < len(m.hashSlotHead); i++ {
+		m.hashSlotHead[i] = -1
+	}
+
+	return m
+}
