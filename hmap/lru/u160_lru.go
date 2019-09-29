a6724493d356a56031616bbe0bfef1ec03348698
向阳 <xiangyang@yunshan.net.cn>
2019-09-29 16:16:52 +0800

    增加U160 LRU
    **复现步骤**
    
    **原因和解决方案**
    
    IPv6 service table需要
    
    **影响范围**
    
    **验证方案**
    
    UT & Bench
    
    **涉及分支**
    
    * master
    * 556
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @yuanchao
    
    /cc @buyi @yuanchao @jinjie @zhuofeng @wangtuo

---
+++ hmap/lru/u160_lru.go
@@ -0,0 +1,296 @@
+package lru
+
+import (
+	"bytes"
+	. "encoding/binary"
+	"fmt"
+	"sync"
+)
+
+const (
+	_U160_KEY_SIZE = 160 / 8 // 必须为4字节的倍数
+)
+
+type u160LRUNode struct {
+	key   [_U160_KEY_SIZE]byte
+	hash  uint32 // key的hash值
+	value interface{}
+
+	hashListNext int32 // 表示节点所在冲突链的下一个节点的 buffer 数组下标，-1 表示不存在
+	hashListPrev int32 // 表示节点所在冲突链的上一个节点的 buffer 数组下标，-1 表示不存在
+	timeListNext int32 // 时间链表，含义与冲突链类似
+	timeListPrev int32 // 时间链表，含义与冲突链类似
+}
+
+func (n *u160LRUNode) equal(hash uint32, key []byte) bool {
+	return n.hash == hash && bytes.Compare(n.key[:], key) == 0
+}
+
+var blankU160LRUNodeForInit u160LRUNode
+
+type u160LRUNodeBlock []u160LRUNode
+
+var u160LRUNodeBlockPool = sync.Pool{New: func() interface{} {
+	return u160LRUNodeBlock(make([]u160LRUNode, _BLOCK_SIZE))
+}}
+
+// 注意：不是线程安全的
+type U160LRU struct {
+	ringBuffer       []u160LRUNodeBlock // 存储Map节点，以矩阵环的方式组织，提升内存申请释放效率
+	bufferStartIndex int32              // ringBuffer中的开始下标（二维矩阵下标），闭区间
+	bufferEndIndex   int32              // ringBuffer中的结束下标（二维矩阵下标），开区间
+
+	hashSlots    int32  // 上取整至2^N，哈希桶个数
+	hashSlotBits uint32 // hashSlots中低位连续0比特个数
+
+	hashSlotHead []int32 // 哈希桶，hashSlotHead[i] 表示哈希值为 i 的冲突链的第一个节点为 buffer[[ hashSlotHead[i] ]]
+	timeListHead int32
+	timeListTail int32
+
+	capacity int // 最大容纳的Flow个数
+	size     int // 当前容纳的Flow个数
+}
+
+func (m *U160LRU) Size() int {
+	return m.size
+}
+
+func (m *U160LRU) incIndex(index int32) int32 {
+	index++
+	if index>>_BLOCK_SIZE_BITS >= int32(len(m.ringBuffer)) {
+		return 0
+	}
+	return index
+}
+
+func (m *U160LRU) decIndex(index int32) int32 {
+	if index <= 0 {
+		return int32(len(m.ringBuffer)<<_BLOCK_SIZE_BITS) - 1
+	}
+	return index - 1
+}
+
+func (m *U160LRU) getNode(index int32) *u160LRUNode {
+	return &m.ringBuffer[index>>_BLOCK_SIZE_BITS][index&_BLOCK_SIZE_MASK]
+}
+
+func (m *U160LRU) pushNodeToHashList(node *u160LRUNode, nodeIndex int32, hash int32) {
+	node.hashListNext = m.hashSlotHead[hash]
+	node.hashListPrev = -1
+	if node.hashListNext != -1 {
+		m.getNode(node.hashListNext).hashListPrev = nodeIndex
+	}
+	m.hashSlotHead[hash] = nodeIndex
+}
+
+func (m *U160LRU) pushNodeToTimeList(node *u160LRUNode, nodeIndex int32) {
+	node.timeListNext = m.timeListHead
+	node.timeListPrev = -1
+	if node.timeListNext != -1 {
+		m.getNode(node.timeListNext).timeListPrev = nodeIndex
+	}
+	m.timeListHead = nodeIndex
+	if m.timeListTail == -1 {
+		m.timeListTail = nodeIndex
+	}
+}
+
+func (m *U160LRU) removeNodeFromHashList(node *u160LRUNode, newNext, newPrev int32) {
+	if node.hashListPrev != -1 {
+		prevNode := m.getNode(node.hashListPrev)
+		prevNode.hashListNext = newNext
+	} else {
+		m.hashSlotHead[m.compressHash(node.hash)] = newNext
+	}
+
+	if node.hashListNext != -1 {
+		nextNode := m.getNode(node.hashListNext)
+		nextNode.hashListPrev = newPrev
+	}
+}
+
+func (m *U160LRU) removeNodeFromTimeList(node *u160LRUNode, newNext, newPrev int32) {
+	if node.timeListPrev != -1 {
+		prevNode := m.getNode(node.timeListPrev)
+		prevNode.timeListNext = newNext
+	} else {
+		m.timeListHead = newNext
+	}
+
+	if node.timeListNext != -1 {
+		nextNode := m.getNode(node.timeListNext)
+		nextNode.timeListPrev = newPrev
+	} else {
+		m.timeListTail = newPrev
+	}
+}
+
+func (m *U160LRU) removeNode(node *u160LRUNode, nodeIndex int32) {
+	// 从哈希链表、时间链表中删除
+	m.removeNodeFromHashList(node, node.hashListNext, node.hashListPrev)
+	m.removeNodeFromTimeList(node, node.timeListNext, node.timeListPrev)
+
+	// 将节点交换至buffer头部
+	if nodeIndex != m.bufferStartIndex {
+		firstNode := m.getNode(m.bufferStartIndex)
+		// 将firstNode内容拷贝至node
+		*node = *firstNode
+		// 修改firstNode在哈希链、时间链的上下游指向node
+		m.removeNodeFromHashList(firstNode, nodeIndex, nodeIndex)
+		m.removeNodeFromTimeList(firstNode, nodeIndex, nodeIndex)
+		// 将firstNode初始化
+		*firstNode = blankU160LRUNodeForInit
+	} else {
+		*node = blankU160LRUNodeForInit
+	}
+
+	// 释放头部节点
+	if m.bufferStartIndex&_BLOCK_SIZE_MASK == _BLOCK_SIZE_MASK {
+		u160LRUNodeBlockPool.Put(m.ringBuffer[m.bufferStartIndex>>_BLOCK_SIZE_BITS])
+		m.ringBuffer[m.bufferStartIndex>>_BLOCK_SIZE_BITS] = nil
+	}
+	m.bufferStartIndex = m.incIndex(m.bufferStartIndex)
+
+	m.size--
+}
+
+func (m *U160LRU) updateNode(node *u160LRUNode, nodeIndex int32, value interface{}) {
+	if nodeIndex != m.timeListHead {
+		// 从时间链表中删除
+		m.removeNodeFromTimeList(node, node.timeListNext, node.timeListPrev)
+		// 插入时间链表头部
+		m.pushNodeToTimeList(node, nodeIndex)
+	}
+
+	node.value = value
+}
+
+func (m *U160LRU) newNode(hash uint32, key []byte, value interface{}) {
+	// buffer空间检查
+	if m.size >= m.capacity {
+		node := m.getNode(m.timeListTail)
+		m.removeNode(node, m.timeListTail)
+	}
+	row := m.bufferEndIndex >> _BLOCK_SIZE_BITS
+	col := m.bufferEndIndex & _BLOCK_SIZE_MASK
+	if m.ringBuffer[row] == nil {
+		m.ringBuffer[row] = u160LRUNodeBlockPool.Get().(u160LRUNodeBlock)
+	}
+	node := &m.ringBuffer[row][col]
+	m.size++
+
+	// 新节点加入哈希链
+	m.pushNodeToHashList(node, m.bufferEndIndex, m.compressHash(hash))
+	// 新节点加入时间链
+	m.pushNodeToTimeList(node, m.bufferEndIndex)
+	// 更新key、value
+	copy(node.key[:], key)
+	node.hash = hash
+	node.value = value
+
+	// 更新buffer信息
+	m.bufferEndIndex = m.incIndex(m.bufferEndIndex)
+}
+
+func (m *U160LRU) Add(key []byte, value interface{}) {
+	if len(key) != _U160_KEY_SIZE {
+		panic(fmt.Sprintf("传入key的长度不等于 %d 字节", _U160_KEY_SIZE))
+	}
+
+	hash := m.genHash(key)
+	for hashListNext := m.hashSlotHead[m.compressHash(hash)]; hashListNext != -1; {
+		node := m.getNode(hashListNext)
+		if node.equal(hash, key) {
+			m.updateNode(node, hashListNext, value)
+			return
+		}
+		hashListNext = node.hashListNext
+	}
+	m.newNode(hash, key, value)
+}
+
+func (m *U160LRU) Remove(key []byte) {
+	if len(key) != _U160_KEY_SIZE {
+		panic(fmt.Sprintf("传入key的长度不等于 %d 字节", _U160_KEY_SIZE))
+	}
+
+	hash := m.genHash(key)
+	for hashListNext := m.hashSlotHead[m.compressHash(hash)]; hashListNext != -1; {
+		node := m.getNode(hashListNext)
+		if node.equal(hash, key) {
+			m.removeNode(node, hashListNext)
+			return
+		}
+		hashListNext = node.hashListNext
+	}
+}
+
+func (m *U160LRU) Get(key []byte, peek bool) (interface{}, bool) {
+	if len(key) != _U160_KEY_SIZE {
+		panic(fmt.Sprintf("传入key的长度不等于 %d 字节", _U160_KEY_SIZE))
+	}
+
+	hash := m.genHash(key)
+	for hashListNext := m.hashSlotHead[m.compressHash(hash)]; hashListNext != -1; {
+		node := m.getNode(hashListNext)
+		if node.equal(hash, key) {
+			if !peek {
+				m.updateNode(node, hashListNext, node.value)
+			}
+			return node.value, true
+		}
+		hashListNext = node.hashListNext
+	}
+	return nil, false
+}
+
+func (m *U160LRU) Clear() {
+	for i := range m.ringBuffer {
+		if m.ringBuffer[i] != nil {
+			u160LRUNodeBlockPool.Put(m.ringBuffer[i])
+			m.ringBuffer[i] = nil
+		}
+	}
+	m.bufferStartIndex = 0
+	m.bufferEndIndex = 0
+
+	for i := range m.hashSlotHead {
+		m.hashSlotHead[i] = -1
+	}
+	m.timeListHead = -1
+	m.timeListTail = -1
+
+	m.size = 0
+}
+
+func (m *U160LRU) genHash(key []byte) uint32 {
+	hash := uint32(0)
+	for i := 0; i < _U160_KEY_SIZE; i += 4 {
+		hash ^= BigEndian.Uint32(key[i:])
+	}
+	return hash
+}
+
+func (m *U160LRU) compressHash(hash uint32) int32 {
+	return int32((((((hash>>m.hashSlotBits)^hash)>>m.hashSlotBits)^hash)>>m.hashSlotBits)^hash) & (m.hashSlots - 1)
+}
+
+func NewU160LRU(hashSlots, capacity int) *U160LRU {
+	hashSlots, hashSlotBits := minPowerOfTwo(hashSlots)
+
+	m := &U160LRU{
+		ringBuffer:   make([]u160LRUNodeBlock, (capacity+_BLOCK_SIZE)/_BLOCK_SIZE*_BLOCK_SIZE+1),
+		hashSlots:    int32(hashSlots),
+		hashSlotBits: uint32(hashSlotBits),
+		hashSlotHead: make([]int32, hashSlots),
+		timeListHead: -1,
+		timeListTail: -1,
+		capacity:     capacity,
+	}
+
+	for i := 0; i < len(m.hashSlotHead); i++ {
+		m.hashSlotHead[i] = -1
+	}
+
+	return m
+}
