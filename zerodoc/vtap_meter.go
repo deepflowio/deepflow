47db7a50017546c19cf43ba4576f0fc96ae0b0b0
zhuofeng <zhuofeng@yunshan.net.cn>
2019-10-24 15:51:43 +0800

    VTAP使用统一的VTAPUsageMeter
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    - 删除vtap_edge_meter
    
    **影响范围**
    
    无
    
    **验证方案**
    
    单元测试通过
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @jinjie

--- zerodoc/vtap_meter.go
+++ zerodoc/vtap_meter.go
@@ -1,93 +1,73 @@
 package zerodoc
 
 import (
+	"strconv"
+
 	"gitlab.x.lan/yunshan/droplet-libs/app"
 	"gitlab.x.lan/yunshan/droplet-libs/codec"
 )
 
-type Metrics struct {
-	TxBytes   uint64
-	RxBytes   uint64
-	TxPackets uint64
-	RxPackets uint64
+type VTAPUsageMeter struct {
+	TxBytes   uint64 `db:"tx_bytes"`
+	RxBytes   uint64 `db:"rx_bytes"`
+	TxPackets uint64 `db:"tx_packets"`
+	RxPackets uint64 `db:"rx_packets"`
 }
 
-type VTAPUsageMeter Metrics
-
-func (m *Metrics) Encode(encoder *codec.SimpleEncoder) {
+func (m *VTAPUsageMeter) Encode(encoder *codec.SimpleEncoder) {
 	encoder.WriteVarintU64(m.TxBytes)
 	encoder.WriteVarintU64(m.RxBytes)
 	encoder.WriteVarintU64(m.TxPackets)
 	encoder.WriteVarintU64(m.RxPackets)
 }
 
-func (m *Metrics) Decode(decoder *codec.SimpleDecoder) {
+func (m *VTAPUsageMeter) Decode(decoder *codec.SimpleDecoder) {
 	m.TxBytes = decoder.ReadVarintU64()
 	m.RxBytes = decoder.ReadVarintU64()
 	m.TxPackets = decoder.ReadVarintU64()
 	m.RxPackets = decoder.ReadVarintU64()
 }
 
-func (m *Metrics) SortKey() uint64 {
+func (m *VTAPUsageMeter) SortKey() uint64 {
 	return uint64(m.TxBytes) + uint64(m.RxBytes)
 }
 
-func (m *Metrics) ToKVString() string {
-	panic("not supported!")
+func (m *VTAPUsageMeter) ToKVString() string {
+	buffer := make([]byte, app.MAX_DOC_STRING_LENGTH)
+	size := m.MarshalTo(buffer)
+	return string(buffer[:size])
 }
 
-func (m *Metrics) MarshalTo(b []byte) int {
-	panic("not supported!")
+func (m *VTAPUsageMeter) MarshalTo(b []byte) int {
+	offset := 0
+	offset += copy(b[offset:], "tx_bytes=")
+	offset += copy(b[offset:], strconv.FormatUint(m.TxBytes, 10))
+	offset += copy(b[offset:], "i,rx_bytes=")
+	offset += copy(b[offset:], strconv.FormatUint(m.RxBytes, 10))
+	offset += copy(b[offset:], "i,bytes=")
+	offset += copy(b[offset:], strconv.FormatUint(m.TxBytes+m.RxBytes, 10))
+	offset += copy(b[offset:], "i,tx_packets=")
+	offset += copy(b[offset:], strconv.FormatUint(m.TxPackets, 10))
+	offset += copy(b[offset:], "i,rx_packets=")
+	offset += copy(b[offset:], strconv.FormatUint(m.RxPackets, 10))
+	offset += copy(b[offset:], "i,packets=")
+	offset += copy(b[offset:], strconv.FormatUint(m.TxPackets+m.RxPackets, 10))
+	b[offset] = 'i'
+	offset++
+
+	return offset
 }
 
-func (m *Metrics) Clone() app.Meter {
-	panic("not supported!")
-}
-
-func (m *Metrics) Release() {
-	panic("not supported!")
-}
-
-func (m *Metrics) Merge(other *Metrics) {
+func (m *VTAPUsageMeter) Merge(other *VTAPUsageMeter) {
 	m.TxBytes += other.TxBytes
 	m.RxBytes += other.RxBytes
 	m.TxPackets += other.TxPackets
 	m.RxPackets += other.RxPackets
 }
 
-func (m *Metrics) ConcurrentMerge(other app.Meter) {
-	if other, ok := other.(*Metrics); ok {
-		m.Merge(other)
-	}
-}
-
-func (m *Metrics) SequentialMerge(other app.Meter) {
-	m.ConcurrentMerge(other)
-}
-
-func (m *VTAPUsageMeter) Encode(encoder *codec.SimpleEncoder) {
-	(*Metrics)(m).Encode(encoder)
-}
-
-func (m *VTAPUsageMeter) Decode(decoder *codec.SimpleDecoder) {
-	(*Metrics)(m).Decode(decoder)
-}
-
-func (m *VTAPUsageMeter) SortKey() uint64 {
-	panic("not supported!")
-}
-
-func (m *VTAPUsageMeter) ToKVString() string {
-	panic("not supported!")
-}
-
-func (m *VTAPUsageMeter) MarshalTo(b []byte) int {
-	panic("not supported!")
-}
-
 func (m *VTAPUsageMeter) ConcurrentMerge(other app.Meter) {
 	if other, ok := other.(*VTAPUsageMeter); ok {
-		(*Metrics)(m).Merge((*Metrics)(other))
+		m.Merge(other)
 	}
 }
 
