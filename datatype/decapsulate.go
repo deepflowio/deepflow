fc297e2dc7c602b6b83120be3584e9a769dbb1e4
haojian <haojian@yunshan.net.cn>
2019-11-19 10:34:18 +0800

    分析器接收ERSPAN镜像流量，隧道解析支持
    **复现步骤**
    
    支持ERSPAN Type II、ERSPAN Type III 类型解析
    
    **原因和解决方案**
    
    目前当trident运行于分析器上时(tap_mode=analyzer)，支持从分析器的物理网卡接收镜像流量。
    当镜像流量有外层VXLAN封装时，trident会自动解封装并将隧道信息放到压缩包头中发给droplet。
    
    目前仅支持对VXLAN镜像流量的解封装，客户需求需要支持ERSPAN的解封装。
    
    **影响范围**
    
    支持ERSPAN的解封装
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie

--- datatype/decapsulate.go
+++ datatype/decapsulate.go
@@ -14,12 +14,16 @@ type TunnelType uint8
 const (
 	TUNNEL_TYPE_NONE TunnelType = iota
 	TUNNEL_TYPE_VXLAN
+	TUNNEL_TYPE_ERSPAN
 )
 
 func (t TunnelType) String() string {
 	if t == TUNNEL_TYPE_VXLAN {
 		return "vxlan"
+	} else if t == TUNNEL_TYPE_ERSPAN {
+		return "erspan"
 	}
+
 	return "none"
 }
 
@@ -36,8 +40,8 @@ func (t *TunnelInfo) String() string {
 
 func (t *TunnelInfo) Decapsulate(l3Packet []byte) int {
 	t.Type = TUNNEL_TYPE_NONE
-	// 通过长度判断是否是VxLAN的同时保证不会数组越界
-	if len(l3Packet) < OFFSET_VXLAN_FLAGS+VXLAN_HEADER_SIZE {
+	// 通过ERSPANIII_HEADER_SIZE(12 bytes)+ERSPANIII_SUBHEADER_SIZE(8 bytes)判断，保证不会数组越界
+	if len(l3Packet) < OFFSET_ERSPAN_VER+ERSPANIII_HEADER_SIZE+ERSPANIII_SUBHEADER_SIZE {
 		return 0
 	}
 	_ = l3Packet[OFFSET_VXLAN_VNI-ETH_HEADER_SIZE] // early bound check hint
@@ -49,15 +53,39 @@ func (t *TunnelInfo) Decapsulate(l3Packet []byte) int {
 	isUDP := assumeIpProtocol == IPProtocolUDP
 	isVxlanUDP := isUDP && assumeDPort == 4789
 	isVxlan := isVxlanUDP && assumeVxlanFlags == 0x8
-	if !isVxlan {
+
+	isGRE := assumeIpProtocol == IPProtocolGRE
+
+	if !isVxlan && !isGRE {
 		return 0
 	}
-	t.Type = TUNNEL_TYPE_VXLAN
+
 	t.Src = IPv4Int(BigEndian.Uint32(l3Packet[OFFSET_SIP-ETH_HEADER_SIZE:]))
 	t.Dst = IPv4Int(BigEndian.Uint32(l3Packet[OFFSET_DIP-ETH_HEADER_SIZE:]))
-	t.Id = BigEndian.Uint32(l3Packet[OFFSET_VXLAN_VNI-ETH_HEADER_SIZE:]) >> 8
-	// return offset start from L3
-	return OFFSET_VXLAN_FLAGS - ETH_HEADER_SIZE + VXLAN_HEADER_SIZE
+	if isVxlan {
+		t.Type = TUNNEL_TYPE_VXLAN
+		t.Id = BigEndian.Uint32(l3Packet[OFFSET_VXLAN_VNI-ETH_HEADER_SIZE:]) >> 8
+		// return offset start from L3
+		return OFFSET_VXLAN_FLAGS - ETH_HEADER_SIZE + VXLAN_HEADER_SIZE
+	} else {
+		greProtocolType := BigEndian.Uint16(l3Packet[OFFSET_GRE_PROTOCOL_TYPE-ETH_HEADER_SIZE:])
+		if greProtocolType == 0x88BE { // ERSPAN II
+			t.Type = TUNNEL_TYPE_ERSPAN
+			t.Id = BigEndian.Uint32(l3Packet[OFFSET_ERSPAN_VER-ETH_HEADER_SIZE:]) & 0x3ff
+			return OFFSET_ERSPAN_VER - ETH_HEADER_SIZE + ERSPANII_HEADER_SIZE
+		} else if greProtocolType == 0x22EB { // ERSPAN III
+			t.Type = TUNNEL_TYPE_ERSPAN
+			t.Id = BigEndian.Uint32(l3Packet[OFFSET_ERSPAN_VER-ETH_HEADER_SIZE:]) & 0x3ff
+			oFlag := l3Packet[OFFSET_ERSPAN_VER-ETH_HEADER_SIZE+ERSPANIII_HEADER_SIZE-1] & 0x1
+			if oFlag == 0 {
+				return OFFSET_ERSPAN_VER - ETH_HEADER_SIZE + ERSPANIII_HEADER_SIZE
+			} else {
+				return OFFSET_ERSPAN_VER - ETH_HEADER_SIZE + ERSPANIII_HEADER_SIZE + ERSPANIII_SUBHEADER_SIZE
+			}
+		}
+	}
+
+	return 0
 }
 
 func (t *TunnelInfo) Valid() bool {
