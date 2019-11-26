3f5c608fc1c38159f1d435adf8aa26db845a3238
haojian <haojian@yunshan.net.cn>
2019-11-26 20:58:45 +0800

    分析器接收ERSPAN镜像流量，隧道解析支持，添加UT
    **复现步骤**
    
    **原因和解决方案**
    
    添加单元测试用例
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie

--- datatype/decapsulate_test.go
+++ datatype/decapsulate_test.go
@@ -7,14 +7,52 @@ import (
 	"testing"
 )
 
-func TestDecapsulate(t *testing.T) {
-	expectedErspan := &TunnelInfo{
-		Src:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.16.1.103").To4())),
-		Dst:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.20.1.171").To4())),
-		Id:   123,
+func TestDecapsulateErspanII(t *testing.T) {
+	expected := &TunnelInfo{
+		Src:  IPv4Int(BigEndian.Uint32(net.ParseIP("2.2.2.2").To4())),
+		Dst:  IPv4Int(BigEndian.Uint32(net.ParseIP("1.1.1.1").To4())),
+		Id:   100,
 		Type: TUNNEL_TYPE_ERSPAN,
 	}
-	expectedVxlan := &TunnelInfo{
+
+	packets, _ := loadPcap("decapsulate_test.pcap")
+	packet1 := packets[0]
+	packet2 := packets[1]
+
+	l2Len := 14
+	actual := &TunnelInfo{}
+	actual.Decapsulate(packet1[l2Len:])
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expectedErspanII: %+v\n actual: %+v", expected, actual)
+	}
+	actual = &TunnelInfo{}
+	actual.Decapsulate(packet2[l2Len:])
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expectedErspanII: %+v\n actual: %+v", expected, actual)
+	}
+}
+
+func TestDecapsulateIII(t *testing.T) {
+	expected := &TunnelInfo{
+		Src:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.16.1.103").To4())),
+		Dst:  IPv4Int(BigEndian.Uint32(net.ParseIP("10.30.101.132").To4())),
+		Id:   0,
+		Type: TUNNEL_TYPE_ERSPAN,
+	}
+
+	packets, _ := loadPcap("decapsulate_test.pcap")
+	packet := packets[3]
+
+	l2Len := 14
+	actual := &TunnelInfo{}
+	actual.Decapsulate(packet[l2Len:])
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expectedErspanIII: %+v\n actual: %+v", expected, actual)
+	}
+}
+
+func TestDecapsulateVxlan(t *testing.T) {
+	expected := &TunnelInfo{
 		Src:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.16.1.103").To4())),
 		Dst:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.20.1.171").To4())),
 		Id:   123,
@@ -22,18 +60,12 @@ func TestDecapsulate(t *testing.T) {
 	}
 
 	packets, _ := loadPcap("decapsulate_test.pcap")
-	packetErspan := packets[0]
-	packetVxlan := packets[1]
+	packet := packets[2]
 
 	l2Len := 14
 	actual := &TunnelInfo{}
-	actual.Decapsulate(packetErspan[l2Len:])
-	if !reflect.DeepEqual(expectedErspan, actual) {
-		t.Errorf("expectedErspan: %+v\n actual: %+v", expectedErspan, actual)
-	}
-	actual = &TunnelInfo{}
-	actual.Decapsulate(packetVxlan[l2Len:])
-	if !reflect.DeepEqual(expectedVxlan, actual) {
-		t.Errorf("expectedVxlan: %+v\n actual: %+v", expectedVxlan, actual)
+	actual.Decapsulate(packet[l2Len:])
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expectedVxlan: %+v\n actual: %+v", expected, actual)
 	}
 }
