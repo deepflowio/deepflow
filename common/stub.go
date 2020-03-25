821275de3988a97a9cafb45f32e0d3f5c43083a3
Jin Jie <jinjie@yunshan.net.cn>
2020-03-25 15:51:32 +0800

    trident信息同步返回类型
    **简述**
    
    genesis需要用类型区分感兴趣的信息
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [x] 预留字段id值以便后续添加
    - [x] 修改内容向后兼容(如果修改了字段id值)
    - [x] 不要使用required描述符(一方面导致调试困难，另一方面并不能阻止指针的生成)
    
    /assign @xiangyang
    
    /cc @xiangyang
    
    Change-Id: I1c7fbedbd1552f9d7c1f25d38fa256c3559936fa

--- common/stub.go
+++ common/stub.go
@@ -1,3 +1,3 @@
 package common
 
-//go:generate protoc --go_opt=paths=source_relative --gofast_out=plugins=grpc:. -I.. ../common.proto
+//go:generate protoc --go_opt=paths=source_relative --go_out=plugins=grpc:. -I.. ../common.proto
