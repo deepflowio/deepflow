c9e8343ad7a1830c320e73c88b8a22a46066d759
zhuofeng <zhuofeng@yunshan.net.cn>
2020-04-22 16:21:27 +0800

    zmq支持ipv6
    **复现步骤**
    
    * 无
    
    **原因和解决方案**
    
    * ip地址加[]
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    * 5.6.0
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- zmq/puller.go
+++ zmq/puller.go
@@ -34,10 +34,14 @@ func NewPuller(ip string, port int, hwm int, recvBlockTimeout time.Duration, mod
 		return nil, err
 	}
 
+	if err := s.SetIpv6(true); err != nil {
+		return nil, err
+	}
+
 	if mode == CLIENT {
-		err = s.Connect(fmt.Sprintf("tcp://%s:%d", ip, port))
+		err = s.Connect(fmt.Sprintf("tcp://%s:%d", ipFormat(ip), port))
 	} else {
-		err = s.Bind(fmt.Sprintf("tcp://%s:%d", ip, port))
+		err = s.Bind(fmt.Sprintf("tcp://%s:%d", ipFormat(ip), port))
 	}
 	if err != nil {
 		return nil, err
