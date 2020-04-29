5ebb271b80d425e03d4e07211cb802089480023f
zhuofeng <zhuofeng@yunshan.net.cn>
2020-04-29 15:03:32 +0800

    不用FlowSecondMeter
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    - 只用FlowMeter
    - 修改doc版本
    
    **影响范围**
    
    无
    
    **验证方案**
    
    无
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @jinjie

--- app/document.go
+++ app/document.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	VERSION               = 20200330 // 修改Document的序列化结构时需同步修改此常量
+	VERSION               = 20200429 // 修改Document的序列化结构时需同步修改此常量
 	MAX_DOC_STRING_LENGTH = 1024
 )
 
