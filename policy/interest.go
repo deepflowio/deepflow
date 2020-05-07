68a37484d124ae78cf19eaa395c2672e18088f3a
yuanchao <yuanchao@yunshan.net.cn>
2020-05-07 14:29:28 +0800

    内存优化
    **复现步骤**
    
    * 无
    
    **原因和解决方案**
    
    * trident内存超过1G，目前看实际使用设置端口的策略比较少，端口这里不需要细分
    * trident pcap写入通过flags判断
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    * 5.6.0
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- policy/interest.go
+++ policy/interest.go
@@ -7,56 +7,49 @@ import (
 )
 
 type InterestTable struct {
-	InterestProtoMaps *[TAP_MAX][math.MaxUint8 + 1]bool
-	InterestPortMaps  *[TAP_MAX][math.MaxUint16 + 1]PortRange
+	InterestPortMaps *[math.MaxUint16 + 1]PortRange
 }
 
 func (t *InterestTable) Init() {
-	t.InterestProtoMaps = &[TAP_MAX][math.MaxUint8 + 1]bool{}
-	t.InterestPortMaps = &[TAP_MAX][math.MaxUint16 + 1]PortRange{}
+	t.InterestPortMaps = &[math.MaxUint16 + 1]PortRange{}
 }
 
 func (t *InterestTable) generateInterestPortMap(acls []*Acl) {
-	interestPortMaps := &[TAP_MAX][math.MaxUint16 + 1]PortRange{}
+	interestPortMaps := &[math.MaxUint16 + 1]PortRange{}
 	ports := make([]PortRange, 0, 1000)
 
-	for tapType := TAP_MIN - 1; tapType < TAP_MAX; tapType++ {
-		ports = ports[:0]
-		for _, acl := range acls {
-			if acl.TapType == tapType || acl.TapType == TAP_ANY || tapType == TAP_ANY {
-				ports = append(ports, acl.SrcPortRange...)
-				ports = append(ports, acl.DstPortRange...)
+	for _, acl := range acls {
+		ports = append(ports, acl.SrcPortRange...)
+		ports = append(ports, acl.DstPortRange...)
+	}
+
+	ports = GetPortRanges(ports)
+
+	lastMax, portOther := uint16(0), uint16(0)
+	for index, port := range ports {
+		if index == 0 {
+			lastMax = port.Max()
+		} else if portOther != 0 {
+			if port.Min()-lastMax > 1 {
+				portOther = lastMax + 1
 			}
+			lastMax = port.Max()
 		}
 
-		ports = GetPortRanges(ports)
-
-		lastMax, portOther := uint16(0), uint16(0)
-		for index, port := range ports {
-			if index == 0 {
-				lastMax = port.Max()
-			} else if portOther != 0 {
-				if port.Min()-lastMax > 1 {
-					portOther = lastMax + 1
-				}
-				lastMax = port.Max()
-			}
-
-			for i := int(port.Min()); i <= int(port.Max()); i++ {
-				interestPortMaps[tapType][i] = port
-			}
+		for i := int(port.Min()); i <= int(port.Max()); i++ {
+			interestPortMaps[i] = port
 		}
-		if portOther == 0 && math.MaxUint16-lastMax > 1 {
-			portOther = lastMax + 1
-		}
+	}
+	if portOther == 0 && math.MaxUint16-lastMax > 1 {
+		portOther = lastMax + 1
+	}
 
-		if portOther != 0 {
-			// portOther为非策略端口中的的第一个端口
-			// 为了减少内存，减少fastPath项， 所有不在策略中的端口使用portOther来建立查询fastPath
-			for i := 1; i <= math.MaxUint16; i++ {
-				if 0 == interestPortMaps[tapType][i] {
-					interestPortMaps[tapType][i] = NewPortRange(portOther, portOther)
-				}
+	if portOther != 0 {
+		// portOther为非策略端口中的的第一个端口
+		// 为了减少内存，减少fastPath项， 所有不在策略中的端口使用portOther来建立查询fastPath
+		for i := 1; i <= math.MaxUint16; i++ {
+			if 0 == interestPortMaps[i] {
+				interestPortMaps[i] = NewPortRange(portOther, portOther)
 			}
 		}
 	}
@@ -64,7 +57,7 @@ func (t *InterestTable) generateInterestPortMap(acls []*Acl) {
 	for _, acl := range acls {
 		for _, port := range acl.SrcPortRange {
 			for i := int(port.Min()); i <= int(port.Max()); {
-				portRangs := interestPortMaps[acl.TapType][i]
+				portRangs := interestPortMaps[i]
 				acl.SrcPorts = append(acl.SrcPorts, portRangs.Min())
 				i = int(portRangs.Max()) + 1
 			}
@@ -72,7 +65,7 @@ func (t *InterestTable) generateInterestPortMap(acls []*Acl) {
 
 		for _, port := range acl.DstPortRange {
 			for i := int(port.Min()); i <= int(port.Max()); {
-				portRangs := interestPortMaps[acl.TapType][i]
+				portRangs := interestPortMaps[i]
 				acl.DstPorts = append(acl.DstPorts, portRangs.Min())
 				i = int(portRangs.Max()) + 1
 			}
@@ -81,32 +74,13 @@ func (t *InterestTable) generateInterestPortMap(acls []*Acl) {
 	t.InterestPortMaps = interestPortMaps
 }
 
-func (t *InterestTable) generateInterestProtoMaps(acls []*Acl) {
-	interestProtoMaps := &[TAP_MAX][math.MaxUint8 + 1]bool{}
-
-	for _, acl := range acls {
-		if !acl.TapType.CheckTapType(acl.TapType) || acl.Proto == PROTO_ALL {
-			continue
-		}
-		if acl.TapType != TAP_ANY {
-			interestProtoMaps[acl.TapType][acl.Proto] = true
-		} else {
-			for tapType := TAP_MIN; tapType < TAP_MAX; tapType++ {
-				interestProtoMaps[tapType][acl.Proto] = true
-			}
-		}
-	}
-	t.InterestProtoMaps = interestProtoMaps
-}
-
 func (t *InterestTable) GenerateInterestMaps(acls []*Acl) {
 	t.generateInterestPortMap(acls)
-	t.generateInterestProtoMaps(acls)
 }
 
 func (t *InterestTable) getFastInterestKeys(packet *LookupKey) {
-	ports := t.InterestPortMaps[packet.TapType][packet.SrcPort]
+	ports := t.InterestPortMaps[packet.SrcPort]
 	packet.SrcPort = ports.Min()
-	ports = t.InterestPortMaps[packet.TapType][packet.DstPort]
+	ports = t.InterestPortMaps[packet.DstPort]
 	packet.DstPort = ports.Min()
 }
