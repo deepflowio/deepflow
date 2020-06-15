a873228472dcff9a48f185ea9585717c2946c1a3
Jin Jie <jinjie@yunshan.net.cn>
2020-06-15 11:34:23 +0000

    增加mini tag
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    trident中不需要这么多字段，可以减少内存占用
    
    **影响范围**
    
    无
    
    **验证方案**
    
    验证数据正常
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo
    
    (cherry picked from commit 9b7aff6900f886ae17f4f0c5c82e6f37b5d7164f)

--- zerodoc/tag_test.go
+++ zerodoc/tag_test.go
@@ -1,6 +1,7 @@
 package zerodoc
 
 import (
+	"encoding/binary"
 	"math"
 	"net"
 	"sort"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/google/gopacket/layers"
 
+	"gitlab.x.lan/yunshan/droplet-libs/codec"
 	"gitlab.x.lan/yunshan/droplet-libs/datatype"
 )
 
@@ -263,5 +265,207 @@ func TestMarshallToInfluxdb(t *testing.T) {
 		if s != strs[i] {
 			t.Error("tag的keys在打包时, 没有按字典顺序排序在:", s)
 		}
+	}
+}
+
+func checkTagAndMiniTagEqual(tag *Tag, miniTag *MiniTag) bool {
+	reversed := false
+	if miniTag.Code&Direction != 0 && miniTag.HasEdgeTagField() || tag.Code&TAPSide != 0 {
+		if miniTag.Code&Direction == 0 || !miniTag.HasEdgeTagField() {
+			return false
+		}
+		if tag.Code&TAPSide == 0 {
+			return false
+		}
+		if tag.Code & ^TAPSide != miniTag.Code & ^Direction {
+			return false
+		}
+		reversed = tag.TAPSide == Server
+	} else if tag.Code != miniTag.Code {
+		return false
+	}
+	if tag.GlobalThreadID != miniTag.GlobalThreadID {
+		return false
+	}
+
+	srcIP, dstIP := miniTag.IP(), miniTag.IP1()
+	srcEpc, dstEpc := miniTag.L3EpcID, miniTag.L3EpcID1
+	if reversed {
+		srcIP, dstIP = dstIP, srcIP
+		srcEpc, dstEpc = dstEpc, srcEpc
+	}
+
+	code := tag.Code
+
+	if code&IP != 0 {
+		if tag.IsIPv6 != 0 && miniTag.IsIPv6 != 0 {
+			if !tag.IP6.Equal(srcIP) {
+				return false
+			}
+		} else if tag.IsIPv6 == 0 && miniTag.IsIPv6 == 0 {
+			if tag.IP != binary.BigEndian.Uint32(srcIP) {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	if code&L3EpcID != 0 {
+		if tag.L3EpcID != srcEpc {
+			return false
+		}
+	}
+
+	if code&IPPath != 0 {
+		if tag.IsIPv6 != 0 && miniTag.IsIPv6 != 0 {
+			if !tag.IP6.Equal(srcIP) {
+				return false
+			}
+			if !tag.IP61.Equal(dstIP) {
+				return false
+			}
+		} else if tag.IsIPv6 == 0 && miniTag.IsIPv6 == 0 {
+			if tag.IP != binary.BigEndian.Uint32(srcIP) {
+				return false
+			}
+			if tag.IP1 != binary.BigEndian.Uint32(dstIP) {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	if code&L3EpcIDPath != 0 {
+		if tag.L3EpcID != srcEpc {
+			return false
+		}
+		if tag.L3EpcID1 != dstEpc {
+			return false
+		}
+	}
+
+	if code&ACLGID != 0 {
+		if tag.ACLGID != miniTag.ACLGID {
+			return false
+		}
+	}
+	if code&Protocol != 0 {
+		if tag.Protocol != miniTag.Protocol {
+			return false
+		}
+	}
+	if code&ServerPort != 0 {
+		if tag.ServerPort != miniTag.ServerPort {
+			return false
+		}
+	}
+	if code&VTAPID != 0 {
+		if tag.VTAPID != miniTag.VTAPID {
+			return false
+		}
+	}
+	if code&TAPType != 0 {
+		if tag.TAPType != miniTag.TAPType {
+			return false
+		}
+	}
+	if code&TAPSide != 0 {
+		if !(tag.TAPSide == Client && miniTag.Direction == ClientToServer || tag.TAPSide == Server && miniTag.Direction == ServerToClient) {
+			return false
+		}
+	}
+
+	if code&TagType != 0 {
+		if tag.TagType != miniTag.TagType {
+			return false
+		}
+	}
+	if code&TagValue != 0 {
+		if tag.TagValue != miniTag.TagValue {
+			return false
+		}
+	}
+	return true
+}
+
+func TestEncodeMiniTag(t *testing.T) {
+	encoder := &codec.SimpleEncoder{}
+	decoder := &codec.SimpleDecoder{}
+
+	sideMiniTag := &MiniTag{
+		MiniField: &MiniField{
+			IsIPv6:     0,
+			rawIP:      [net.IPv6len]byte{10, 11, 1, 123},
+			L3EpcID:    15,
+			VTAPID:     24,
+			Protocol:   layers.IPProtocolTCP,
+			ServerPort: 5324,
+			Direction:  ClientToServer,
+			TAPType:    ToR,
+			ACLGID:     16,
+			TagType:    TAG_TYPE_TCP_FLAG,
+			TagValue:   18,
+		},
+		Code: IP | L3EpcID | VTAPID | Protocol | ServerPort | Direction | TAPType | ACLGID | TagType | TagValue,
+	}
+
+	sideMiniTag.Encode(encoder)
+	decoder.Init(encoder.Bytes())
+
+	sideTag := &Tag{Field: &Field{}}
+	sideTag.Decode(decoder)
+
+	if !checkTagAndMiniTagEqual(sideTag, sideMiniTag) {
+		t.Error("mini tag and tag mismatch")
+		t.Error("tag:     ", sideTag)
+		t.Error("mini tag:", sideMiniTag)
+	}
+
+	encoder.Reset()
+
+	edgeMiniTag := &MiniTag{
+		MiniField: &MiniField{
+			IsIPv6:     1,
+			rawIP:      [net.IPv6len]byte{0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+			rawIP1:     [net.IPv6len]byte{0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+			L3EpcID:    15,
+			L3EpcID1:   16,
+			VTAPID:     24,
+			Protocol:   layers.IPProtocolTCP,
+			ServerPort: 5324,
+			Direction:  ClientToServer,
+			TAPType:    ToR,
+			ACLGID:     16,
+			TagType:    TAG_TYPE_TCP_FLAG,
+			TagValue:   18,
+		},
+		Code: IPPath | L3EpcIDPath | VTAPID | Protocol | ServerPort | Direction | TAPType | ACLGID | TagType | TagValue,
+	}
+
+	edgeMiniTag.Encode(encoder)
+	decoder.Init(encoder.Bytes())
+
+	edgeTag := &Tag{Field: &Field{}}
+	edgeTag.Decode(decoder)
+
+	if !checkTagAndMiniTagEqual(edgeTag, edgeMiniTag) {
+		t.Error("mini tag and tag mismatch")
+		t.Error("tag:     ", edgeTag)
+		t.Error("mini tag:", edgeMiniTag)
+	}
+
+	encoder.Reset()
+	edgeMiniTag.Direction = ServerToClient
+
+	edgeMiniTag.Encode(encoder)
+	decoder.Init(encoder.Bytes())
+
+	edgeTag = &Tag{Field: &Field{}}
+	edgeTag.Decode(decoder)
+
+	if !checkTagAndMiniTagEqual(edgeTag, edgeMiniTag) {
+		t.Error("mini tag and tag mismatch")
+		t.Error("tag:     ", edgeTag)
+		t.Error("mini tag:", edgeMiniTag)
 	}
 }
