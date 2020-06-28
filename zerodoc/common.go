fafc067b24cfa256f5db5c62163944c67be0e2eb
zhuofeng <zhuofeng@yunshan.net.cn>
2020-06-28 15:46:32 +0800

    数据库支持读取http和dns rrt等
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    无
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [x] 编译通过
    - [x] 单元测试通过
    
    /assign @xiangyang
    
    /cc @jinjie wangtuo

--- zerodoc/common.go
+++ zerodoc/common.go
@@ -67,18 +67,35 @@ const (
 	_METER_FLOW
 	_METER_NEW_FLOW
 	_METER_CLOSED_FLOW
+	_METER_HTTP_REQUEST
+	_METER_HTTP_RESPONSE
+	_METER_DNS_REQUEST
+	_METER_DNS_RESPONSE
+
 	_METER_CLIENT_RST_FLOW
 	_METER_SERVER_RST_FLOW
-	_METER_CLIENT_HALF_OPEN_FLOW
-	_METER_SERVER_HALF_OPEN_FLOW
+	_METER_CLIENT_SYN_REPEAT
+	_METER_SERVER_SYN_ACK_REPEAT
 	_METER_CLIENT_HALF_CLOSE_FLOW
 	_METER_SERVER_HALF_CLOSE_FLOW
-	_METER_TIMEOUT_TCP_FLOW
+	_METER_CLIENT_NO_RESPONSE
+	_METER_CLIENT_SOURCE_PORT_REUSE
+	_METER_CLIENT_SYN_RETRY_LACK
+	_METER_SERVER_RESET
+	_METER_SERVER_NO_RESPONSE
+	_METER_SERVER_QUEUE_LACK
+	_METER_HTTP_CLIENT_ERROR
+	_METER_HTTP_SERVER_ERROR
+	_METER_DNS_CLIENT_ERROR
+	_METER_DNS_SERVER_ERROR
+
 	_METER_RTT
 	_METER_RTT_CLIENT
 	_METER_RTT_SERVER
 	_METER_SRT
 	_METER_ART
+	_METER_HTTP_RRT
+	_METER_DNS_RRT
 	_METER_RETRANS_TX
 	_METER_RETRANS_RX
 	_METER_ZERO_WIN_TX
@@ -144,29 +161,49 @@ var COLUMN_IDS map[string]uint8 = map[string]uint8{
 	"pod_ns_id_0":      _TAG_POD_NS_ID_0,
 	"pod_ns_id_1":      _TAG_POD_NS_ID_1,
 
-	"packet_tx":              _METER_PACKET_TX,
-	"packet_rx":              _METER_PACKET_RX,
-	"byte_tx":                _METER_BYTE_TX,
-	"byte_rx":                _METER_BYTE_RX,
-	"flow":                   _METER_FLOW,
-	"new_flow":               _METER_NEW_FLOW,
-	"closed_flow":            _METER_CLOSED_FLOW,
-	"client_rst_flow":        _METER_CLIENT_RST_FLOW,
-	"server_rst_flow":        _METER_SERVER_RST_FLOW,
-	"client_half_open_flow":  _METER_CLIENT_HALF_OPEN_FLOW,
-	"server_half_open_flow":  _METER_SERVER_HALF_OPEN_FLOW,
-	"client_half_close_flow": _METER_CLIENT_HALF_CLOSE_FLOW,
-	"server_half_close_flow": _METER_SERVER_HALF_CLOSE_FLOW,
-	"timeout_tcp_flow":       _METER_TIMEOUT_TCP_FLOW,
-	"rtt":                    _METER_RTT,
-	"rtt_client":             _METER_RTT_CLIENT,
-	"rtt_server":             _METER_RTT_SERVER,
-	"srt":                    _METER_SRT,
-	"art":                    _METER_ART,
-	"retrans_tx":             _METER_RETRANS_TX,
-	"retrans_rx":             _METER_RETRANS_RX,
-	"zero_win_tx":            _METER_ZERO_WIN_TX,
-	"zero_win_rx":            _METER_ZERO_WIN_RX,
+	"packet_tx":   _METER_PACKET_TX,
+	"packet_rx":   _METER_PACKET_RX,
+	"byte_tx":     _METER_BYTE_TX,
+	"byte_rx":     _METER_BYTE_RX,
+	"flow":        _METER_FLOW,
+	"new_flow":    _METER_NEW_FLOW,
+	"closed_flow": _METER_CLOSED_FLOW,
+
+	"http_request":  _METER_HTTP_REQUEST,
+	"http_response": _METER_HTTP_RESPONSE,
+	"dns_request":   _METER_DNS_REQUEST,
+	"dns_response":  _METER_DNS_RESPONSE,
+
+	"client_rst_flow":       _METER_CLIENT_RST_FLOW,
+	"server_rst_flow":       _METER_SERVER_RST_FLOW,
+	"server_syn_ack_repeat": _METER_SERVER_SYN_ACK_REPEAT,
+	"client_syn_repeat":     _METER_CLIENT_SYN_REPEAT,
+
+	"client_half_close_flow":   _METER_CLIENT_HALF_CLOSE_FLOW,
+	"server_half_close_flow":   _METER_SERVER_HALF_CLOSE_FLOW,
+	"client_no_response":       _METER_CLIENT_NO_RESPONSE,
+	"client_source_port_reuse": _METER_CLIENT_SOURCE_PORT_REUSE,
+	"client_syn_retry_lack":    _METER_CLIENT_SYN_RETRY_LACK,
+	"server_reset":             _METER_SERVER_RESET,
+	"server_no_response":       _METER_SERVER_NO_RESPONSE,
+	"server_queue_lack":        _METER_SERVER_QUEUE_LACK,
+	"http_client_error":        _METER_HTTP_CLIENT_ERROR,
+	"http_server_error":        _METER_HTTP_SERVER_ERROR,
+	"dns_client_error":         _METER_DNS_CLIENT_ERROR,
+	"dns_server_error":         _METER_DNS_SERVER_ERROR,
+
+	"rtt":        _METER_RTT,
+	"rtt_client": _METER_RTT_CLIENT,
+	"rtt_server": _METER_RTT_SERVER,
+	"srt":        _METER_SRT,
+	"art":        _METER_ART,
+	"http_rrt":   _METER_HTTP_RRT,
+	"dns_rrt":    _METER_DNS_RRT,
+
+	"retrans_tx":  _METER_RETRANS_TX,
+	"retrans_rx":  _METER_RETRANS_RX,
+	"zero_win_tx": _METER_ZERO_WIN_TX,
+	"zero_win_rx": _METER_ZERO_WIN_RX,
 }
 
 func GetColumnIDs(columnNames []string) []uint8 {
