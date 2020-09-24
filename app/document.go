17345ca9d2e6a87eb39940bce4a78c4e369a5e10
zhuofeng <zhuofeng@yunshan.net.cn>
2020-09-24 12:54:44 +0800

    遥测数据包头增加发送vtapID
    **复现步骤**
    
    无
    
    **原因和解决方案**
    
    无
    
    **影响范围**
    
    无
    
    **验证方案**
    
    验证正常
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang

--- app/document.go
+++ app/document.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	VERSION               = 20200915 // 修改Document的序列化结构时需同步修改此常量
+	VERSION               = 20200924 // 修改Document的序列化结构时需同步修改此常量
 	MAX_DOC_STRING_LENGTH = 1024
 )
 
