01c422a084a857af644b4a5130864a5ed9aa917e
yuanchao <yuanchao@yunshan.net.cn>
2020-12-21 11:20:34 +0800

    增加deviceType字段，decap修改
    **复现步骤**
    
    (描述问题出现的条件)
    
    **原因和解决方案**
    
    * 增加deviceType字段
    * decap修改保存最外层的隧道信息
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- datatype/platform.go
+++ datatype/platform.go
@@ -3,6 +3,8 @@ package datatype
 import (
 	"net"
 	"time"
+
+	pb "gitlab.x.lan/yunshan/message/trident"
 )
 
 const (
@@ -12,7 +14,8 @@ const (
 	MAX_MASK6_LEN     = 128
 	MASK_LEN_NUM      = MAX_MASK_LEN + 1
 
-	IF_TYPE_WAN = 3
+	IF_TYPE_WAN          = 3
+	DEVICE_TYPE_POD_NODE = pb.DeviceType_DEVICE_TYPE_POD_NODE
 
 	DATA_VALID_TIME = 1 * time.Minute
 	ARP_VALID_TIME  = 1 * time.Minute
@@ -31,6 +34,7 @@ type PlatformData struct {
 	Id             uint32
 	RegionId       uint32
 	IfType         uint8
+	DeviceType     uint8
 	IsVIPInterface bool
 	// 适配windows hyper-v场景出现的在不同Region存在相同MAC，PlatformData查询GRPC下发的Region id,
 	// PlatformData不在同一Region中，该字段为True, 若为true不会创建mac表
