7da2670760cda50cd5095734ca7d750afa09aba3
yuanchao <yuanchao@yunshan.net.cn>
2020-12-30 10:38:34 +0800

    支持解析erspan1
    **复现步骤**
    
    **原因和解决方案**
    
    * erspan1类型与erspan2一样，区别在gre.version和gre.flags为0的是erspan1
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    * 5.6.1
    * 5.6.2
    * 5.6.3
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- datatype/decapsulate_test.go
+++ datatype/decapsulate_test.go
@@ -42,6 +42,35 @@ func loadPcap(file string) ([]RawPacket, []PacketLen) {
 	return packets, packetLens
 }
 
+func TestDecapsulateErspanI(t *testing.T) {
+	expected := &TunnelInfo{
+		Src:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.28.25.108").To4())),
+		Dst:  IPv4Int(BigEndian.Uint32(net.ParseIP("172.28.28.70").To4())),
+		Id:   0,
+		Type: TUNNEL_TYPE_ERSPAN,
+		Tier: 1,
+	}
+
+	packets, _ := loadPcap("decapsulate_erspan1.pcap")
+	packet1 := packets[0]
+	packet2 := packets[1]
+
+	l2Len := 18
+	actual := &TunnelInfo{}
+	offset := actual.Decapsulate(packet1[l2Len:], TUNNEL_TYPE_ERSPAN)
+	expectedOffset := 20 + 8 // IP_HEADER_SIZE + GRE_HEADER_SIZE
+	if !reflect.DeepEqual(expected, actual) || offset != expectedOffset {
+		t.Errorf("expectedErspanI: %+v\n actual: %+v, expectedOffset:%v, offset:%v",
+			expected, actual, expectedOffset, offset)
+	}
+	actual = &TunnelInfo{}
+	actual.Decapsulate(packet2[l2Len:], TUNNEL_TYPE_ERSPAN)
+	if !reflect.DeepEqual(expected, actual) || offset != expectedOffset {
+		t.Errorf("expectedErspanI: %+v\n actual: %+v, expectedOffset:%v, offset:%v",
+			expected, actual, expectedOffset, offset)
+	}
+}
+
 func TestDecapsulateErspanII(t *testing.T) {
 	expected := &TunnelInfo{
 		Src:  IPv4Int(BigEndian.Uint32(net.ParseIP("2.2.2.2").To4())),
