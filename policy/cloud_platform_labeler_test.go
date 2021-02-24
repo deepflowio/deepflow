86e1564f50c58ae15d646e76274fb8e7c8b24917
yuanchao <yuanchao@yunshan.net.cn>
2021-02-24 11:06:52 +0800

    修改epc查询逻辑
    **复现步骤**
    
    **原因和解决方案**
    
    * 当前，采集器策略模块的路由匹配策略为外网IP优先原则，即匹配优先级为：
      * A. 匹配外网IP地址
      * B. 匹配外网子网
      * C. 根据对端MAC或IP已经获取到的VPC，在该VPC的对等连接（及云企业网）中匹配IP和子网
      * D. 根据对端MAC或IP已经获取到的VPC，在该VPC内部匹配IP和子网
    * 为了解决万科的需求，修改为本地路由优先原则，即匹配优先级为（范围有小到大，由Local到Global）：
      * D
      * C
      * A
      * B
    
    **影响范围**
    
    * 无
    
    **验证方案**
    
    * 验证通过
    
    **涉及分支**
    
    * master
    * 5.6.3-r3
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- policy/cloud_platform_labeler_test.go
+++ policy/cloud_platform_labeler_test.go
@@ -77,3 +77,42 @@ func TestCidr(t *testing.T) {
 		t.Errorf("Dst L3EpcId error: %v", endpointData)
 	}
 }
+
+func TestEpcOrder(t *testing.T) {
+	labeler := NewCloudPlatformLabeler(1, 1024)
+	// platform
+	platforms := make([]*PlatformData, 0, 2)
+	platform := generatePlatformDataByParam(cidrSrcIp, cidrSrcMac, cidrSrcEpc, 4) // EPC: 10
+	platforms = append(platforms, platform)
+	platform = generatePlatformDataByParam(cidrPeerDstIp, 0, cidrPeerDstEpc, 4) // EPC: 20
+	platforms = append(platforms, platform)
+	labeler.UpdateInterfaceTable(platforms)
+
+	// cidr
+	cidrs := make([]*Cidr, 0, 2)
+	cidr := generateCidr(cidrSrcEpc, CIDR_TYPE_WAN, cidrPeerDst) // EPC: 10
+	cidrs = append(cidrs, cidr)
+	labeler.UpdateCidr(cidrs)
+
+	key := generateLookupKey(cidrSrcMac, 0, cidrSrcIp, cidrPeerDstIp, 0, 0, 0)
+	endpointData := labeler.GetEndpointData(key)
+	if endpointData.DstInfo.L3EpcId != cidrSrcEpc {
+		t.Errorf("Dst L3EpcId error: %v", endpointData)
+	}
+
+	// Peer Connection
+	connections := make([]*PeerConnection, 0, 2)
+	connection := generatePeerConnection(1, cidrSrcEpc, cidrPeerDstEpc) // Peer Epc: 10 <-> 20
+	connections = append(connections, connection)
+	labeler.UpdatePeerConnectionTable(connections)
+
+	labeler.UpdateInterfaceTable(platforms)
+	labeler.UpdateCidr(cidrs)
+	key = generateLookupKey(cidrSrcMac, 0, cidrSrcIp, cidrPeerDstIp, 0, 0, 0)
+	endpointData = labeler.GetEndpointData(key)
+	// 加入对等连接后，对等连接查询优先级高于WAN
+	if endpointData.DstInfo.L3EpcId != cidrPeerDstEpc {
+		t.Errorf("Dst L3EpcId error: %v", endpointData)
+	}
+
+}
