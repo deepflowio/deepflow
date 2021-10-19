bc9b4764ce22e3f4ce7db680decc0e695509fbeb
yuanchao <yuanchao@yunshan.net.cn>
2021-10-19 11:08:46 +0800

    添加mysql
    **复现步骤**
    
    **原因和解决方案**
    
    * 如题
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

---
+++ datatype/sql.go
@@ -0,0 +1,107 @@
+package datatype
+
+import (
+	"fmt"
+
+	"gitlab.yunshan.net/yunshan/droplet-libs/codec"
+	"gitlab.yunshan.net/yunshan/droplet-libs/pool"
+)
+
+var mysqlInfoPool = pool.NewLockFreePool(func() interface{} {
+	return new(MysqlInfo)
+})
+
+func AcquireMYSQLInfo() *MysqlInfo {
+	return mysqlInfoPool.Get().(*MysqlInfo)
+}
+
+func ReleaseMYSQLInfo(d *MysqlInfo) {
+	*d = MysqlInfo{}
+	mysqlInfoPool.Put(d)
+}
+
+const (
+	MYSQL_COMMAND_QUIT = iota + 1
+	MYSQL_COMMAND_USE_DATABASE
+	MYSQL_COMMAND_QUERY
+	MYSQL_COMMAND_SHOW_FIELD
+	MYSQL_COMMAND_MAX
+)
+
+const (
+	MYSQL_RESPONSE_CODE_OK  = 0
+	MYSQL_RESPONSE_CODE_ERR = 0xff
+	MYSQL_RESPONSE_CODE_EOF = 0xfe
+)
+
+type MysqlInfo struct {
+	// Server Greeting
+	ProtocolVersion uint8
+	ServerVersion   string
+	ServerThreadID  uint32
+	// Request
+	Command uint8
+	Context string
+	// Response
+	ResponseCode uint8
+	AffectedRows uint8
+	ErrorCode    uint16
+}
+
+func (i *MysqlInfo) Encode(encoder *codec.SimpleEncoder, msgType LogMessageType, code uint16) {
+	switch msgType {
+	case MSG_T_OTHER:
+		encoder.WriteU8(i.ProtocolVersion)
+		encoder.WriteString255(i.ServerVersion)
+		encoder.WriteU32(i.ServerThreadID)
+	case MSG_T_REQUEST:
+		encoder.WriteU8(i.Command)
+		encoder.WriteString255(i.Context)
+	case MSG_T_RESPONSE:
+		encoder.WriteU8(i.ResponseCode)
+		encoder.WriteU8(i.AffectedRows)
+		encoder.WriteU16(i.ErrorCode)
+	case MSG_T_SESSION:
+		encoder.WriteU8(i.Command)
+		encoder.WriteString255(i.Context)
+
+		encoder.WriteU8(i.ResponseCode)
+		encoder.WriteU8(i.AffectedRows)
+		encoder.WriteU16(i.ErrorCode)
+	}
+}
+
+func (i *MysqlInfo) Decode(decoder *codec.SimpleDecoder, msgType LogMessageType, code uint16) {
+	switch msgType {
+	case MSG_T_OTHER:
+		i.ProtocolVersion = decoder.ReadU8()
+		i.ServerVersion = decoder.ReadString255()
+		i.ServerThreadID = decoder.ReadU32()
+	case MSG_T_REQUEST:
+		i.Command = decoder.ReadU8()
+		i.Context = decoder.ReadString255()
+	case MSG_T_RESPONSE:
+		i.ResponseCode = decoder.ReadU8()
+		i.AffectedRows = decoder.ReadU8()
+		i.ErrorCode = decoder.ReadU16()
+	case MSG_T_SESSION:
+		i.Command = decoder.ReadU8()
+		i.Context = decoder.ReadString255()
+
+		i.ResponseCode = decoder.ReadU8()
+		i.AffectedRows = decoder.ReadU8()
+		i.ErrorCode = decoder.ReadU16()
+	}
+}
+
+func (i *MysqlInfo) String() string {
+	return fmt.Sprintf("%#v", i)
+}
+
+func (i *MysqlInfo) Merge(r interface{}) {
+	if response, ok := r.(*MysqlInfo); ok {
+		i.ResponseCode = response.ResponseCode
+		i.AffectedRows = response.AffectedRows
+		i.ErrorCode = response.ErrorCode
+	}
+}
