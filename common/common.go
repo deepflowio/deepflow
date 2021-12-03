729b1c713c038a7e2f50f77448510c6301db9c0a
zhuofeng <zhuofeng@yunshan.net.cn>
2021-12-03 14:26:42 +0800

    若使用Mac查询平台数据失败，则使用IP查
    **复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    * 5.7.3
    * 6.0.0
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 日志打印不会造成磁盘占用空间过多
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @xiangyang
    /cc @yuanchao

--- common/common.go
+++ common/common.go
@@ -3,13 +3,11 @@ package common
 import (
 	"database/sql"
 	"fmt"
-	"math"
 	"net"
 
 	clickhouse "github.com/ClickHouse/clickhouse-go"
 	logging "github.com/op/go-logging"
 	"gitlab.yunshan.net/yunshan/droplet-libs/grpc"
-	"gitlab.yunshan.net/yunshan/message/trident"
 )
 
 var log = logging.MustGetLogger("common")
@@ -29,22 +27,10 @@ func NewCKConnection(addr, username, password string) (*sql.DB, error) {
 }
 
 // 如果通过MAC匹配平台信息失败，则需要通过IP再获取, 解决工单122/126问题
-// 此时通过IP获取的平台信息中，仅返回subnet_id,az_id,region_id,device_type，且device_type设置为路由器类型
 func RegetInfoFromIP(isIPv6 bool, ip6 net.IP, ip4 uint32, epcID int16, platformData *grpc.PlatformInfoTable) *grpc.Info {
-	var info *grpc.Info
 	if isIPv6 {
-		info = platformData.QueryIPV6Infos(epcID, ip6)
+		return platformData.QueryIPV6Infos(epcID, ip6)
 	} else {
-		info = platformData.QueryIPV4Infos(epcID, ip4)
+		return platformData.QueryIPV4Infos(epcID, ip4)
 	}
-	if info != nil {
-		newInfo := &grpc.Info{}
-		newInfo.SubnetID = info.SubnetID
-		newInfo.AZID = info.AZID
-		newInfo.RegionID = info.RegionID
-		newInfo.DeviceType = uint32(trident.DeviceType_DEVICE_TYPE_NSP_VGATEWAY)
-		newInfo.DeviceID = math.MaxInt32 // 若为0，WEB会认为无效值，故取Int32最大值，api层会特殊处理
-		info = newInfo
-	}
-	return info
 }
