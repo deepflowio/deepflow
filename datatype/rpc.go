c7e8c3f0a3e8d9c72727b8759f6f5a247aaff337
xiangwang <xiangwang@yunshan.net>
2022-01-12 19:44:00 +0800

    libs针对"601PS：流量日志字段修改"适配
    **复现步骤**
    - 需求
    
    **原因和解决方案**
    - HTTP traceid 和 spanid 的拆分
    - L7 日志中状态字段拆分为客户端异常、服务端异常
    - HTTP、Dubbo拆分请求字节数及响应字节数
    
    **影响范围**
    - L7:metric、log
    
    **验证方案**
    - 待联调
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- datatype/rpc.go
+++ datatype/rpc.go
@@ -25,60 +25,96 @@ type DubboInfo struct {
 	// header
 	SerialID uint8
 	Type     uint8
-	BodyLen  uint32
 	ID       int64
 
-	// body
+	// req
+	ReqBodyLen     int32
 	DubboVersion   string
 	ServiceName    string
 	ServiceVersion string
 	MethodName     string
 
-	// Attachments
+	// resp
+	RespBodyLen int32
 }
 
 func (i *DubboInfo) Encode(encoder *codec.SimpleEncoder, msgType LogMessageType, code uint16) {
 	encoder.WriteU8(i.SerialID)
 	encoder.WriteU8(i.Type)
-	encoder.WriteU32(i.BodyLen)
 	encoder.WriteU64(uint64(i.ID))
-	if msgType == MSG_T_SESSION || msgType == MSG_T_REQUEST {
+
+	switch msgType {
+	case MSG_T_REQUEST:
+		encoder.WriteU32(uint32(i.ReqBodyLen))
 		encoder.WriteString255(i.DubboVersion)
 		encoder.WriteString255(i.ServiceName)
 		encoder.WriteString255(i.ServiceVersion)
 		encoder.WriteString255(i.MethodName)
-	}
+	case MSG_T_RESPONSE:
+		encoder.WriteU32(uint32(i.RespBodyLen))
+	case MSG_T_SESSION:
+		encoder.WriteU32(uint32(i.ReqBodyLen))
+		encoder.WriteString255(i.DubboVersion)
+		encoder.WriteString255(i.ServiceName)
+		encoder.WriteString255(i.ServiceVersion)
+		encoder.WriteString255(i.MethodName)
 
+		encoder.WriteU32(uint32(i.RespBodyLen))
+	}
 }
 
 func (i *DubboInfo) WriteToPB(p *pb.DubboInfo, msgType LogMessageType) {
 	p.SerialID = uint32(i.SerialID)
 	p.Type = uint32(i.Type)
-	p.BodyLen = i.BodyLen
 	p.ID = uint32(i.ID)
-	if msgType == MSG_T_SESSION || msgType == MSG_T_REQUEST {
+	switch msgType {
+	case MSG_T_REQUEST:
+		p.ReqBodyLen = i.ReqBodyLen
 		p.DubboVersion = i.DubboVersion
 		p.ServiceName = i.ServiceName
 		p.ServiceVersion = i.ServiceVersion
 		p.MethodName = i.MethodName
-	} else {
+		p.RespBodyLen = 0
+	case MSG_T_RESPONSE:
+		p.RespBodyLen = i.RespBodyLen
+		p.ReqBodyLen = 0
 		p.DubboVersion = ""
 		p.ServiceName = ""
 		p.ServiceVersion = ""
 		p.MethodName = ""
+	case MSG_T_SESSION:
+		p.ReqBodyLen = i.ReqBodyLen
+		p.DubboVersion = i.DubboVersion
+		p.ServiceName = i.ServiceName
+		p.ServiceVersion = i.ServiceVersion
+		p.MethodName = i.MethodName
+
+		p.RespBodyLen = i.RespBodyLen
 	}
 }
 
 func (i *DubboInfo) Decode(decoder *codec.SimpleDecoder, msgType LogMessageType, code uint16) {
 	i.SerialID = decoder.ReadU8()
 	i.Type = decoder.ReadU8()
-	i.BodyLen = decoder.ReadU32()
 	i.ID = int64(decoder.ReadU64())
-	if msgType == MSG_T_SESSION || msgType == MSG_T_REQUEST {
+
+	switch msgType {
+	case MSG_T_REQUEST:
+		i.ReqBodyLen = int32(decoder.ReadU32())
 		i.DubboVersion = decoder.ReadString255()
 		i.ServiceName = decoder.ReadString255()
 		i.ServiceVersion = decoder.ReadString255()
 		i.MethodName = decoder.ReadString255()
+	case MSG_T_RESPONSE:
+		i.RespBodyLen = int32(decoder.ReadU32())
+	case MSG_T_SESSION:
+		i.ReqBodyLen = int32(decoder.ReadU32())
+		i.DubboVersion = decoder.ReadString255()
+		i.ServiceName = decoder.ReadString255()
+		i.ServiceVersion = decoder.ReadString255()
+		i.MethodName = decoder.ReadString255()
+
+		i.RespBodyLen = int32(decoder.ReadU32())
 	}
 }
 
@@ -86,4 +122,8 @@ func (i *DubboInfo) String() string {
 	return fmt.Sprintf("%#v", i)
 }
 
-func (h *DubboInfo) Merge(_ interface{}) {}
+func (i *DubboInfo) Merge(r interface{}) {
+	if dubbo, ok := r.(*DubboInfo); ok {
+		i.RespBodyLen = dubbo.RespBodyLen
+	}
+}
