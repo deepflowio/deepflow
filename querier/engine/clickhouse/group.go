f9b124372986fa7cf89a86397317861d13ae51e5
Liu Chao <liuchao@yunshan.net>
2022-04-14 11:35:43 +0800

    querier-自动分组支持subnet
    **复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * main
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @songzhen
    
    /cc @yuanchao @jinjie @zhuofeng

--- querier/engine/clickhouse/group.go
+++ querier/engine/clickhouse/group.go
@@ -22,11 +22,12 @@ func GetGroup(name string, asTagMap map[string]string, db, table string) ([]Stat
 				resourceIDSuffix := resourceName + "_id" + suffix
 				if strings.Trim(name, "`") == resourceName+suffix {
 					ipTag := fmt.Sprintf("multiIf(%s=0 and is_ipv4=1,IPv4NumToString(%s), %s=0 and is_ipv4=0,IPv6NumToString(%s),%s!=0 and is_ipv4=1,'0.0.0.0','::')", resourceIDSuffix, ip4Suffix, resourceIDSuffix, ip6Suffix, resourceIDSuffix)
-					subnetTag := "subnet_id" + suffix
+					subnetIDSuffix := "subnet_id" + suffix
+					subnetTag := fmt.Sprintf("if(%s=0,%s,0)", resourceIDSuffix, subnetIDSuffix)
 					ipSelectStmt := &SelectTag{Value: ipTag, Alias: "ip" + suffix}
-					subnetSelectStmt := &SelectTag{Value: subnetTag}
+					subnetSelectStmt := &SelectTag{Value: subnetTag, Alias: subnetIDSuffix}
 					ipGroupStmt := &GroupTag{Value: "ip" + suffix}
-					subnetGroupStmt := &GroupTag{Value: subnetTag}
+					subnetGroupStmt := &GroupTag{Value: subnetIDSuffix}
 					stmts = append(stmts, ipSelectStmt)
 					stmts = append(stmts, subnetSelectStmt)
 					stmts = append(stmts, ipGroupStmt)
