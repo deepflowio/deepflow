b05fd71484d645bdd3d1a6b1981d34d1b9ef2bc3
zhicong <zhicong@yunshan.net>
2022-04-18 09:31:25 +0800

    flow-map 修复生成流的closeType=ForcedReport问题
    **现象及复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要同步修改(trident-dump、trident-copy或trident-testsrv)
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @lizhuofeng
    
    /cc @xiangyang @yuanchao @jinjie

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -4,3 +4,11 @@ mod synchronizer;
 
 pub(crate) use session::{Session, DEFAULT_TIMEOUT};
 pub(crate) use synchronizer::{HttpConfig, StaticConfig, Status, Synchronizer};
+
+// todo 用synchronizer ntp 获取timestamp
+use std::time::{Duration, SystemTime};
+pub fn get_timestamp() -> Duration {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+}
