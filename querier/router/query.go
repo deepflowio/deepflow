e4c35d6a3d55eabc21dd8d6135366742986a372c
luyao <luyao@yunshan.net.cn>
2022-04-22 19:12:00 +0800

    querier-panic记录log
    **复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * main
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @songzhen

--- querier/router/query.go
+++ querier/router/query.go
@@ -4,8 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	// "github.com/k0kubun/pp"
+	logging "github.com/op/go-logging"
 	"metaflow/querier/service"
 )
+
+var log = logging.MustGetLogger("router")
 
 func QueryRouter(e *gin.Engine) {
 	e.POST("/v1/query/", executeQuery())
@@ -13,6 +16,12 @@ func QueryRouter(e *gin.Engine) {
 
 func executeQuery() gin.HandlerFunc {
 	return gin.HandlerFunc(func(c *gin.Context) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				log.Error(err)
+			}
+		}()
 		args := make(map[string]string)
 		args["debug"] = c.Query("debug")
 		args["query_uuid"] = c.Query("query_uuid")
