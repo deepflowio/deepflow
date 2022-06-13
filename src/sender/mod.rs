8bec607b1b15ed7cae569995caa923ec31b936f0
zhicong <zhicong@yunshan.net>
2022-06-13 11:23:16 +0800

    增加Prometheus/telegraf指标数据代理
    **现象及复现步骤**
    
    [需求](http://apidocs.yunshan.net/deepflow-ps/PS/6.1.1/08-%E7%B3%BB%E7%BB%9F/07-%E6%94%AF%E6%8C%81%E6%8E%A5%E6%94%B6%E5%AD%98%E5%82%A8%E6%9F%A5%E8%AF%A2%E5%A4%96%E9%83%A8Metrics%E6%95%B0%E6%8D%AE.html)
    
    **原因和解决方案**
    
    接收到prometheus metric按照snappy压缩的pb数据格式发送sender
    
    接收到telegraf metric数据按照influx行协议文本格式发送到sender
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要同步修改(trident-dump、trident-copy或trident-testsrv)
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 打包成功(如果有修改Makefile)
    
    /assign @jinjie
    
    /cc @xiangyang @yuanchao @jinjie

--- src/sender/mod.rs
+++ src/sender/mod.rs
@@ -8,7 +8,7 @@ use std::fmt;
 use std::time::Duration;
 
 use crate::common::tagged_flow::TaggedFlow;
-use crate::external_metrics::OpenTelemetry;
+use crate::external_metrics::{OpenTelemetry, PrometheusMetric, TelegrafMetric};
 use crate::flow_generator::AppProtoLogsData;
 use crate::metric::document::Document;
 
@@ -19,12 +19,16 @@ const ERR_INTERVAL: Duration = Duration::from_secs(30);
 const FLOW_LOG_VERSION: u32 = 20220128;
 const METRICS_VERSION: u32 = 20220117;
 const OPEN_TELEMETRY: u32 = 20220607;
+const PROMETHEUS: u32 = 20220613;
+const TELEGRAF: u32 = 20220613;
 
 pub enum SendItem {
     L4FlowLog(TaggedFlow),
     L7FlowLog(AppProtoLogsData),
     Metrics(Document),
     ExternalOtel(OpenTelemetry),
+    ExternalProm(PrometheusMetric),
+    ExternalTelegraf(TelegrafMetric),
 }
 
 impl SendItem {
@@ -34,6 +38,8 @@ impl SendItem {
             Self::L7FlowLog(l7) => l7.encode(buf),
             Self::Metrics(m) => m.encode(buf),
             Self::ExternalOtel(o) => o.encode(buf),
+            Self::ExternalProm(p) => p.encode(buf),
+            Self::ExternalTelegraf(p) => p.encode(buf),
         }
     }
 
@@ -43,6 +49,8 @@ impl SendItem {
             Self::L7FlowLog(_) => SendMessageType::ProtocolLog,
             Self::Metrics(_) => SendMessageType::Metrics,
             Self::ExternalOtel(_) => SendMessageType::OpenTelemetry,
+            Self::ExternalProm(_) => SendMessageType::Prometheus,
+            Self::ExternalTelegraf(_) => SendMessageType::Telegraf,
         }
     }
 
@@ -52,6 +60,8 @@ impl SendItem {
             Self::L7FlowLog(_) => FLOW_LOG_VERSION,
             Self::Metrics(_) => METRICS_VERSION,
             Self::ExternalOtel(_) => OPEN_TELEMETRY,
+            Self::ExternalProm(_) => PROMETHEUS,
+            Self::ExternalTelegraf(_) => TELEGRAF,
         }
     }
 }
@@ -63,6 +73,8 @@ impl fmt::Display for SendItem {
             Self::L7FlowLog(l) => write!(f, "l7: {}", l),
             Self::Metrics(l) => write!(f, "metric: {:?}", l),
             Self::ExternalOtel(o) => write!(f, "open_telemetry: {:?}", o),
+            Self::ExternalProm(p) => write!(f, "prometheus: {:?}", p),
+            Self::ExternalTelegraf(p) => write!(f, "telegraf: {:?}", p),
         }
     }
 }
@@ -74,6 +86,8 @@ impl fmt::Debug for SendItem {
             Self::L7FlowLog(l) => write!(f, "l7: {}", l),
             Self::Metrics(l) => write!(f, "metric: {:?}", l),
             Self::ExternalOtel(o) => write!(f, "open_telemetry: {:?}", o),
+            Self::ExternalProm(p) => write!(f, "prometheus: {:?}", p),
+            Self::ExternalTelegraf(p) => write!(f, "telegraf: {:?}", p),
         }
     }
 }
@@ -88,6 +102,8 @@ pub enum SendMessageType {
     TaggedFlow = 4,
     ProtocolLog = 5,
     OpenTelemetry = 6,
+    Prometheus = 7,
+    Telegraf = 8,
 }
 
 impl fmt::Display for SendMessageType {
@@ -100,6 +116,8 @@ impl fmt::Display for SendMessageType {
             Self::TaggedFlow => write!(f, "l4_log"),
             Self::ProtocolLog => write!(f, "l7_log"),
             Self::OpenTelemetry => write!(f, "open_telemetry"),
+            Self::Prometheus => write!(f, "prometheus"),
+            Self::Telegraf => write!(f, "telegraf"),
         }
     }
 }
