eae1987f90caf6d41a27e442d65fe45cc7dd6ff9
shuxin <shuxin@yunshan.net>
2022-06-17 15:07:42 +0800

    添加MQTT支持
    **复现步骤**
    
    **原因和解决方案**
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * master
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @xiangyang
    
    /cc @yuanchao @jinjie @zhuofeng @wangtuo

--- datatype/mq.go
+++ datatype/mq.go
@@ -110,3 +110,59 @@ func (i *KafkaInfo) Merge(r interface{}) {
 		i.RespMsgSize = kafka.RespMsgSize
 	}
 }
+
+var mqttInfoPool = pool.NewLockFreePool(func() interface{} {
+	return new(MqttInfo)
+})
+
+func AcquireMqttInfo() *MqttInfo {
+	return mqttInfoPool.Get().(*MqttInfo)
+}
+
+func ReleaseMqttInfo(d *MqttInfo) {
+	*d = MqttInfo{}
+	mqttInfoPool.Put(d)
+}
+
+type MqttInfo struct {
+	MqttType string
+
+	// request
+	ReqMsgSize   int32
+	ProtoVersion uint16
+	ClientID     string
+
+	// reponse
+	RespMsgSize int32
+}
+
+func (i *MqttInfo) WriteToPB(p *pb.MqttInfo, msgType LogMessageType) {
+	*p = pb.MqttInfo{}
+	p.MqttType = i.MqttType
+	switch msgType {
+	case MSG_T_REQUEST:
+		p.ReqMsgSize = i.ReqMsgSize
+		p.ProtoVersion = uint32(i.ProtoVersion)
+		p.ClientID = i.ClientID
+
+		p.RespMsgSize = 0
+	case MSG_T_RESPONSE:
+		p.RespMsgSize = i.RespMsgSize
+	case MSG_T_SESSION:
+		p.ReqMsgSize = i.ReqMsgSize
+		p.ProtoVersion = uint32(i.ProtoVersion)
+		p.ClientID = i.ClientID
+
+		p.RespMsgSize = i.RespMsgSize
+	}
+}
+
+func (i *MqttInfo) String() string {
+	return fmt.Sprintf("%#v", i)
+}
+
+func (i *MqttInfo) Merge(r interface{}) {
+	if mqtt, ok := r.(*MqttInfo); ok {
+		i.RespMsgSize = mqtt.RespMsgSize
+	}
+}
