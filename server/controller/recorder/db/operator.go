d7f2beed83f053b6f3c98381ac1147b7d71c7b7e
zhengya <zhengya@yunshan.net.cn>
2022-06-22 15:33:54 +0800

    recorder-优化检查dup机制
    **复现步骤**
    
    **原因和解决方案**
    
      - 批量入库检查出有lcuuid重复直接异常中断，导致正确数据也无法入库：排除异常数据，正确数据入库
    
    **影响范围**
    
    **验证方案**
    
    **涉及分支**
    
    * main
    
    **检查项**
    
    - [ ] 需要更新依赖
    - [ ] 是共性问题(代码中存在类似问题)
    - [ ] 编译通过
    - [ ] 单元测试通过
    
    /assign @songzhen

--- server/controller/recorder/db/operator.go
+++ server/controller/recorder/db/operator.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/op/go-logging"
 
+	"server/controller/common"
 	"server/controller/db/mysql"
 	"server/controller/recorder/constraint"
 )
@@ -31,20 +32,20 @@ type OperatorBase[MT constraint.MySQLModel] struct {
 }
 
 func (o *OperatorBase[MT]) AddBatch(dbItems []*MT) ([]*MT, bool) {
-	dbItems, lcuuids, ok := o.checkIfAddLcuuidsDup(dbItems)
+	dbItemsToAdd, lcuuids, ok := o.formatDBItemsToAdd(dbItems)
 	if !ok {
 		return nil, false
 	}
-	err := mysql.Db.Create(&dbItems).Error
+	err := mysql.Db.Create(&dbItemsToAdd).Error
 	if err != nil {
 		log.Errorf("add %s batch failed: %v", o.resourceTypeName, err)
 		log.Errorf("add %s (lcuuids: %v) failed", o.resourceTypeName, lcuuids)
 		return nil, false
 	}
-	for _, dbItem := range dbItems {
+	for _, dbItem := range dbItemsToAdd {
 		log.Infof("add %s (detail: %+v) success", o.resourceTypeName, dbItem)
 	}
-	return dbItems, true
+	return dbItemsToAdd, true
 }
 
 func (o *OperatorBase[MT]) Update(lcuuid string, updateInfo map[string]interface{}) (*MT, bool) {
@@ -74,9 +75,9 @@ func (o *OperatorBase[MT]) DeleteBatch(lcuuids []string) bool {
 
 // 在插入DB前检查是否有lcuuid重复的数据，
 // 若资源有软删除需求，将lcuuid存在的数据ID赋值给新数据，删除旧数据，新数据入库；
-// 若资源无软删除需求，抛出lcuuid重复异常。
-func (o *OperatorBase[MT]) checkIfAddLcuuidsDup(dbItems []*MT) (reorganizedDBItems []*MT, lcuuids []string, ok bool) {
-	lcuuids = make([]string, 0, len(dbItems))
+// 若资源无软删除需求，记录lcuuid重复异常，筛掉异常数据，剩余数据入库。
+func (o *OperatorBase[MT]) formatDBItemsToAdd(dbItems []*MT) (dbItemsToAdd []*MT, lcuuidsToAdd []string, ok bool) {
+	lcuuids := make([]string, 0, len(dbItems))
 	lcuuidToDBItem := make(map[string]*MT)
 	for _, dbItem := range dbItems {
 		lcuuid := (*dbItem).GetLcuuid()
@@ -98,19 +99,30 @@ func (o *OperatorBase[MT]) checkIfAddLcuuidsDup(dbItems []*MT) (reorganizedDBIte
 				dupLcuuids = append(dupLcuuids, (*dupLcuuidDBItem).GetLcuuid())
 			}
 			log.Errorf("%s data is duplicated (lcuuids: %v)", o.resourceTypeName, dupLcuuids)
-			return
-		}
-		for _, dupLcuuidDBItem := range dupLcuuidDBItems {
-			dbItem, exists := lcuuidToDBItem[(*dupLcuuidDBItem).GetLcuuid()]
-			if !exists {
-				continue
+
+			num := len(dbItems) - len(dupLcuuidDBItems)
+			dbItemsToAdd := make([]*MT, 0, num)
+			lcuuidsToAdd := make([]string, 0, num)
+			for lcuuid, dbItem := range lcuuidToDBItem {
+				if !common.Contains(dupLcuuids, lcuuid) {
+					dbItemsToAdd = append(dbItemsToAdd, dbItem)
+					lcuuidsToAdd = append(lcuuidsToAdd, lcuuid)
+				}
 			}
-			o.setter.setDBItemID(dbItem, (*dupLcuuidDBItem).GetID())
-		}
-		err = mysql.Db.Unscoped().Delete(&dupLcuuidDBItems).Error
-		if err != nil {
-			log.Errorf("%s lcuuid duplicated (lcuuids:)", o.resourceTypeName)
-			return
+			return dbItemsToAdd, lcuuidsToAdd, true
+		} else {
+			for _, dupLcuuidDBItem := range dupLcuuidDBItems {
+				dbItem, exists := lcuuidToDBItem[(*dupLcuuidDBItem).GetLcuuid()]
+				if !exists {
+					continue
+				}
+				o.setter.setDBItemID(dbItem, (*dupLcuuidDBItem).GetID())
+			}
+			err = mysql.Db.Unscoped().Delete(&dupLcuuidDBItems).Error
+			if err != nil {
+				log.Errorf("%s lcuuid duplicated (lcuuids:)", o.resourceTypeName)
+				return dbItems, lcuuids, true
+			}
 		}
 	}
 	return dbItems, lcuuids, true
---
+++ server/controller/recorder/db/operator_test.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+
+	"server/controller/db/mysql"
+)
+
+func (t *SuiteTest) TestformatDBItemsToAdd() {
+	operator := NewVInterface()
+	vifs := []*mysql.VInterface{newDBVInterface(), newDBVInterface()}
+	vif1 := vifs[0]
+	vif2 := vifs[1]
+	mysql.Db.Create(&vif1)
+
+	vifsToAdd, lcuuidsToAdd, ok := operator.formatDBItemsToAdd(vifs)
+	assert.True(t.T(), ok)
+	assert.Equal(t.T(), 1, len(vifsToAdd))
+	assert.Equal(t.T(), vif2.Lcuuid, lcuuidsToAdd[0])
+	t.db.Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&mysql.VInterface{})
+}
