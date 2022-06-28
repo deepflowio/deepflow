ee83c880c0b9d16bb553ee9977c949a664f9c962
Song Zhen <songzhen@yunshan.net>
2022-06-28 13:26:40 +0800

    Support agent-group config cli
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    - metaflow-ctl agent-group create -f xxx.yaml
    - metaflow-ctl agent-group update -f xxx.yaml
    - metaflow-ctl agent-group delete g-xxxx
    
    **Impactions**
    
    **Test method**
    
    ./metaflow-ctl agent-group-config list
    ./metaflow-ctl agent-group-config list woFYi5035l
    ./metaflow-ctl agent-group-config list -o yaml
    ./metaflow-ctl agent-group-config list woFYi5035l -o yaml
    ./metaflow-ctl agent-group-config create -f vtap_group_create.yaml
    ./metaflow-ctl agent-group-config update woFYi5035l -f vtap_group_create.yaml
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- cli/ctl/agent_group_config.go
+++ cli/ctl/agent_group_config.go
@@ -2,6 +2,7 @@ package ctl
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -14,7 +15,51 @@ func RegisterAgentGroupConfigCommand() *cobra.Command {
 		Use:   "agent-group-config",
 		Short: "agent-group config operation commands",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("please run with 'example | list | create | delete'.\n")
+			fmt.Printf("please run with 'example | list | create | update | delete'.\n")
+		},
+	}
+
+	var listOutput string
+	list := &cobra.Command{
+		Use:     "list [agent-group ID]",
+		Short:   "list config",
+		Example: "metaflow-ctl agent-group-config list g-xxxxxx",
+		Run: func(cmd *cobra.Command, args []string) {
+			listAgentGroupConfig(cmd, args, listOutput)
+		},
+	}
+	list.Flags().StringVarP(&listOutput, "output", "o", "", "output format")
+
+	var createFilename string
+	create := &cobra.Command{
+		Use:     "create -f <filename>",
+		Short:   "create config",
+		Example: "metaflow-ctl agent-group-config create -f metaflow-config.yaml",
+		Run: func(cmd *cobra.Command, args []string) {
+			createAgentGroupConfig(cmd, args, createFilename)
+		},
+	}
+	create.Flags().StringVarP(&createFilename, "filename", "f", "", "file to use create agent-group config")
+	create.MarkFlagRequired("filename")
+
+	var updateFilename string
+	update := &cobra.Command{
+		Use:     "update <agent-group ID> -f <filename>",
+		Short:   "update agent-group config",
+		Example: "metaflow-ctl agent-group-config update g-xxxxxx -f metaflow-config,yaml",
+		Run: func(cmd *cobra.Command, args []string) {
+			updateAgentGroupConfig(cmd, args, updateFilename)
+		},
+	}
+	update.Flags().StringVarP(&updateFilename, "filename", "f", "", "file to use update agent-group config")
+	update.MarkFlagRequired("filename")
+
+	delete := &cobra.Command{
+		Use:     "delete [agent-group ID]",
+		Short:   "delete agent-group config",
+		Example: "metaflow-ctl agent-group-config delete g-xxxxxx",
+		Run: func(cmd *cobra.Command, args []string) {
+			deleteAgentGroupConfig(cmd, args)
 		},
 	}
 
@@ -22,20 +67,135 @@ func RegisterAgentGroupConfigCommand() *cobra.Command {
 		Use:   "example",
 		Short: "example agent-group config",
 		Run: func(cmd *cobra.Command, args []string) {
-			exampleAgentConfig(cmd, args)
+			exampleAgentGroupConfig(cmd, args)
 		},
 	}
 	agentGroupConfig.AddCommand(example)
+	agentGroupConfig.AddCommand(list)
+	agentGroupConfig.AddCommand(create)
+	agentGroupConfig.AddCommand(update)
+	agentGroupConfig.AddCommand(delete)
 	return agentGroupConfig
 }
 
-func exampleAgentConfig(cmd *cobra.Command, args []string) {
+func exampleAgentGroupConfig(cmd *cobra.Command, args []string) {
 	// TODO read metaflow-server host from file
 	url := "http://metaflow-server:20417/v1/vtap-group-configuration/example/"
-	response, err := common.CURLPerform("GET", url, nil)
+	response, err := common.CURLPerform("GET", url, nil, "")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 	fmt.Printf(response.Get("DATA").MustString())
+}
+
+func listAgentGroupConfig(cmd *cobra.Command, args []string, output string) {
+	agentGroupShortUUID := ""
+	if len(args) > 0 {
+		agentGroupShortUUID = args[0]
+	}
+
+	// TODO: read metaflow-server host from file
+	url := "http://metaflow-server:20417/v1/vtap-group-configuration/"
+	if output == "yaml" {
+		if agentGroupShortUUID != "" {
+			url += fmt.Sprintf("filter/?vtap_group_id=%s", agentGroupShortUUID)
+		} else {
+			url += "advanced/"
+		}
+
+		response, err := common.CURLPerform("GET", url, nil, "")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+
+		if agentGroupShortUUID != "" {
+			fmt.Println(response.Get("DATA").MustString())
+		} else {
+			for i := range response.Get("DATA").MustArray() {
+				fmt.Println(response.Get("DATA").GetIndex(i).MustString())
+			}
+		}
+	} else {
+		response, err := common.CURLPerform("GET", url, nil, "")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return
+		}
+
+		cmdFormat := "%-46s %s\n"
+		fmt.Printf(cmdFormat, "NAME", "AGENT_GROUP_ID")
+		for i := range response.Get("DATA").MustArray() {
+			config := response.Get("DATA").GetIndex(i)
+			if agentGroupShortUUID != "" && config.Get("VTAP_GROUP_ID").MustString() != agentGroupShortUUID {
+				continue
+			}
+			fmt.Printf(
+				cmdFormat, config.Get("VTAP_GROUP_NAME").MustString(),
+				config.Get("VTAP_GROUP_ID").MustString(),
+			)
+		}
+	}
+}
+
+func createAgentGroupConfig(cmd *cobra.Command, args []string, createFilename string) {
+	url := "http://metaflow-server:20417/v1/vtap-group-configuration/advanced/"
+	yamlFile, err := ioutil.ReadFile(createFilename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	_, err = common.CURLPerform("POST", url, nil, string(yamlFile))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+}
+
+func updateAgentGroupConfig(cmd *cobra.Command, args []string, updateFilename string) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "must specify name. Use: %s", cmd.Use)
+		return
+	}
+
+	url := fmt.Sprintf("http://metaflow-server:20417/v1/vtap-group-configuration/?vtap_group_id=%s", args[0])
+	// call vtap-group api, get lcuuid
+	response, err := common.CURLPerform("GET", url, nil, "")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	if len(response.Get("DATA").MustArray()) == 0 {
+		fmt.Fprintln(os.Stderr, "agent-group (%s) not exist")
+	}
+	group := response.Get("DATA").GetIndex(0)
+	lcuuid := group.Get("LCUUID").MustString()
+
+	// call vtap-group config update api
+	url = fmt.Sprintf("http://metaflow-server:20417/v1/vtap-group-configuration/advanced/%s/", lcuuid)
+	yamlFile, err := ioutil.ReadFile(updateFilename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	_, err = common.CURLPerform("PATCH", url, nil, string(yamlFile))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+}
+
+func deleteAgentGroupConfig(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "must specify agent-group ID. Use: %s", cmd.Use)
+		return
+	}
+
+	// TODO: read metaflow-server host from file
+	url := fmt.Sprintf("http://metaflow-server:20417/v1/vtap-group-configuration/filter/?vtap_group_id=%s", args[0])
+	_, err := common.CURLPerform("DELETE", url, nil, "")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
 }
--- server/controller/service/vtap_group.go
+++ server/controller/service/vtap_group.go
@@ -28,6 +28,9 @@ func GetVtapGroups(filter map[string]interface{}) (resp []model.VtapGroup, err e
 	if _, ok := filter["name"]; ok {
 		Db = Db.Where("name = ?", filter["name"])
 	}
+	if _, ok := filter["short_uuid"]; ok {
+		Db = Db.Where("short_uuid = ?", filter["short_uuid"])
+	}
 	Db.Order("created_at DESC").Find(&vtapGroups)
 
 	for _, vtapGroup := range vtapGroups {
