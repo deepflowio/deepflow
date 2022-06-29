bfbe9ad86dfb76d25471895a36efe82a39f42115
kangxiang <kangxiang@yunshan.net>
2022-06-29 11:09:32 +0800

    metaflow-server cancel forward to py genesis
    **Phenomenon and reproduction steps**
    Temporarily cancel the forward and may be deleted later
    
    **Root cause and solution**
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/genesis/config/config.go
+++ server/controller/genesis/config/config.go
@@ -5,8 +5,6 @@ type GenesisConfig struct {
 	VinterfaceAgingTime     float64  `default:"300" yaml:"vinterface_aging_time"`
 	LocalIPRanges           []string `yaml:"local_ip_ranges"`
 	ExcludeIPRanges         []string `yaml:"exclude_ip_ranges"`
-	GRPCServerHost          string   `default:"genesis" yaml:"grpc_server_host"`
-	GRPCServerPort          int      `default:"20036" yaml:"grpc_server_port"`
 	QueueLengths            int      `default:"60" yaml:"queue_length"`
 	DataPersistenceInterval int      `default:"60" yaml:"data_persistence_interval"`
 	IPv4CIDRMaxMask         int      `default:"16" yaml:"ipv4_cidr_max_mask"`
