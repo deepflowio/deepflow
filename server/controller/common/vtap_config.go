6675784cbfa1b0c942102700843508fb7cd1befe
JIN XiaoFeng <xiaofeng01@yunshan.net.cn>
2022-06-29 17:55:12 +0800

    fix panic bug
    **Phenomenon and reproduction steps**
    
    panic
    
    **Root cause and solution**
    
    panic for nil point
    
    **Impactions**
    
    none
    
    **Test method**
    
    run normal
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/common/vtap_config.go
+++ server/controller/common/vtap_config.go
@@ -69,7 +69,7 @@ var (
 	RsyslogEnabled                = 1
 	MaxTxBandwidth                = int64(0)
 	BandwidthProbeInterval        = 10
-	TapInterfaceRegex             = "^tap.*"
+	TapInterfaceRegex             = "^(tap|cali|veth|eth).*"
 	MaxEscapeSeconds              = 3600
 	Mtu                           = 1500
 	OutputVlan                    = 0
