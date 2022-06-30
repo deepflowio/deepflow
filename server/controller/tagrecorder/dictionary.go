91061c471f2905a68d687f82b722082c9c22a49b
Liu Chao <liuchao@yunshan.net>
2022-06-30 14:56:43 +0800

    Clickhouse port use yaml
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/tagrecorder/dictionary.go
+++ server/controller/tagrecorder/dictionary.go
@@ -55,160 +55,136 @@ func (c *TagRecorder) UpdateChDictionary() {
 		} else {
 			replicaSQL = fmt.Sprintf("REPLICA (HOST '%s%s' PRIORITY %s)", masterRegionPrefix, "mysql", "1")
 		}
-
-		log.Infof("get clickhouse port in (%s)", analyzer.IP)
 		c.cfg.ClickHouseCfg.Host = analyzer.IP
 		connect, err := clickhouse.Connect(c.cfg.ClickHouseCfg)
 		if err != nil {
 			continue
 		}
-		var ports []struct {
-			Port string `db:"port"`
-		}
-		if err := connect.Select(&ports, fmt.Sprintf("SELECT port FROM system.clusters WHERE host_address ='%s'", analyzer.IP)); err != nil {
+		log.Infof("refresh clickhouse dictionary in (%s: %d)", analyzer.IP, c.cfg.ClickHouseCfg.Port)
+		var databases []string
+
+		// 检查并创建数据库
+		if err := connect.Select(&databases, "SHOW DATABASES"); err != nil {
 			log.Error(err)
 			connect.Close()
 			continue
 		}
-		connect.Close()
-
-		for _, port := range ports {
-			log.Infof("refresh clickhouse dictionary in (%s: %s)", analyzer.IP, port.Port)
-			var databases []string
-			portInt, err := strconv.Atoi(port.Port)
+		sort.Strings(databases)
+		databaseIndex := sort.SearchStrings(databases, c.cfg.ClickHouseCfg.Database)
+		if len(databases) == 0 || databaseIndex == len(databases) || databases[databaseIndex] != c.cfg.ClickHouseCfg.Database {
+			log.Infof("create database %s", c.cfg.ClickHouseCfg.Database)
+			sql := fmt.Sprintf("CREATE DATABASE %s", c.cfg.ClickHouseCfg.Database)
+			_, err = connect.Exec(sql)
 			if err != nil {
-				log.Error(err)
-				continue
-			}
-			c.cfg.ClickHouseCfg.Port = uint32(portInt)
-			connect, err := clickhouse.Connect(c.cfg.ClickHouseCfg)
-			if err != nil {
-				continue
-			}
-
-			// 检查并创建数据库
-			if err := connect.Select(&databases, "SHOW DATABASES"); err != nil {
 				log.Error(err)
 				connect.Close()
 				continue
 			}
-			sort.Strings(databases)
-			databaseIndex := sort.SearchStrings(databases, c.cfg.ClickHouseCfg.Database)
-			if len(databases) == 0 || databaseIndex == len(databases) || databases[databaseIndex] != c.cfg.ClickHouseCfg.Database {
-				log.Infof("create database %s", c.cfg.ClickHouseCfg.Database)
-				sql := fmt.Sprintf("CREATE DATABASE %s", c.cfg.ClickHouseCfg.Database)
-				_, err = connect.Exec(sql)
-				if err != nil {
-					log.Error(err)
-					connect.Close()
-					continue
-				}
-			}
-
-			// 获取数据库中当前的字典
-			dictionaries := []string{}
-			if err := connect.Select(&dictionaries, fmt.Sprintf("SHOW DICTIONARIES IN %s", c.cfg.ClickHouseCfg.Database)); err != nil {
-				log.Error(err)
-				connect.Close()
-				continue
-			}
-			wantedDicts := mapset.NewSet(
-				CH_DICTIONARY_IP_RESOURCE,
-				CH_DICTIONARY_IP_RELATION,
-				CH_DICTIONARY_K8S_LABEL,
-				CH_DICTIONARY_REGION,
-				CH_DICTIONARY_AZ,
-				CH_DICTIONARY_VPC,
-				CH_DICTIONARY_VL2,
-				CH_DICTIONARY_POD_CLUSTER,
-				CH_DICTIONARY_POD_NAMESPACE,
-				CH_DICTIONARY_POD_NODE,
-				CH_DICTIONARY_POD_GROUP,
-				CH_DICTIONARY_POD,
-				CH_DICTIONARY_DEVICE,
-				CH_DICTIONARY_VTAP_PORT,
-				CH_DICTIONARY_TAP_TYPE,
-				CH_DICTIONARY_VTAP,
-				CH_DICTIONARY_VTAP_PORT,
-				CH_DICTIONARY_POD_NODE_PORT,
-				CH_DICTIONARY_POD_GROUP_PORT,
-				CH_DICTIONARY_POD_PORT,
-				CH_DICTIONARY_DEVICE_PORT,
-				CH_DICTIONARY_IP_PORT,
-				CH_DICTIONARY_SERVER_PORT,
-				CH_DICTIONARY_LB_LISTENER)
-			chDicts := mapset.NewSet()
-			for _, dictionary := range dictionaries {
-				chDicts.Add(dictionary)
-			}
-
-			// 删除不存在的字典
-			delDicts := chDicts.Difference(wantedDicts)
-			for _, dict := range delDicts.ToSlice() {
-				dropSQL := fmt.Sprintf("DROP DICTIONARY %s.%s", c.cfg.ClickHouseCfg.Database, dict)
-				_, err = connect.Exec(dropSQL)
-				if err != nil {
-					log.Error(err)
-					connect.Close()
-					continue
-				}
-			}
-
-			// 创建期望的字典
-			addDicts := wantedDicts.Difference(chDicts)
-			for _, dict := range addDicts.ToSlice() {
-				dictName := dict.(string)
-				chTable := "ch_" + strings.TrimSuffix(dictName, "_map")
-				createSQL := CREATE_SQL_MAP[dictName]
-				mysqlPortStr := strconv.Itoa(int(c.cfg.MySqlCfg.Port))
-				createSQL = fmt.Sprintf(createSQL, c.cfg.ClickHouseCfg.Database, dictName, mysqlPortStr, c.cfg.MySqlCfg.UserName, c.cfg.MySqlCfg.UserPassword, replicaSQL, c.cfg.MySqlCfg.Database, chTable, chTable)
-				log.Infof("create dictionary %s", dictName)
-				log.Info(createSQL)
-				_, err = connect.Exec(createSQL)
-				if err != nil {
-					log.Error(err)
-					connect.Close()
-					continue
-				}
-			}
-
-			// 检查并更新已存在字典
-			checkDicts := chDicts.Intersect(wantedDicts)
-			for _, dict := range checkDicts.ToSlice() {
-				dictName := dict.(string)
-				chTable := "ch_" + strings.TrimSuffix(dictName, "_map")
-				showSQL := fmt.Sprintf("SHOW CREATE DICTIONARY %s.%s", c.cfg.ClickHouseCfg.Database, dictName)
-				dictSQL := make([]string, 0)
-				if err := connect.Select(&dictSQL, showSQL); err != nil {
-					log.Error(err)
-					connect.Close()
-					continue
-				}
-				createSQL := CREATE_SQL_MAP[dictName]
-				mysqlPortStr := strconv.Itoa(int(c.cfg.MySqlCfg.Port))
-				createSQL = fmt.Sprintf(createSQL, c.cfg.ClickHouseCfg.Database, dictName, mysqlPortStr, c.cfg.MySqlCfg.UserName, c.cfg.MySqlCfg.UserPassword, replicaSQL, c.cfg.MySqlCfg.Database, chTable, chTable)
-				if createSQL == dictSQL[0] {
-					continue
-				}
-				log.Infof("update dictionary %s", dictName)
-				log.Infof("exist dictionary %s", dictSQL[0])
-				log.Infof("wanted dictionary %s", createSQL)
-				dropSQL := fmt.Sprintf("DROP DICTIONARY %s.%s", c.cfg.ClickHouseCfg.Database, dictName)
-				_, err = connect.Exec(dropSQL)
-				if err != nil {
-					log.Error(err)
-					connect.Close()
-					continue
-				}
-				_, err = connect.Exec(createSQL)
-				if err != nil {
-					log.Error(err)
-					connect.Close()
-					continue
-				}
-			}
-			connect.Close()
 		}
+
+		// 获取数据库中当前的字典
+		dictionaries := []string{}
+		if err := connect.Select(&dictionaries, fmt.Sprintf("SHOW DICTIONARIES IN %s", c.cfg.ClickHouseCfg.Database)); err != nil {
+			log.Error(err)
+			connect.Close()
+			continue
+		}
+		wantedDicts := mapset.NewSet(
+			CH_DICTIONARY_IP_RESOURCE,
+			CH_DICTIONARY_IP_RELATION,
+			CH_DICTIONARY_K8S_LABEL,
+			CH_DICTIONARY_REGION,
+			CH_DICTIONARY_AZ,
+			CH_DICTIONARY_VPC,
+			CH_DICTIONARY_VL2,
+			CH_DICTIONARY_POD_CLUSTER,
+			CH_DICTIONARY_POD_NAMESPACE,
+			CH_DICTIONARY_POD_NODE,
+			CH_DICTIONARY_POD_GROUP,
+			CH_DICTIONARY_POD,
+			CH_DICTIONARY_DEVICE,
+			CH_DICTIONARY_VTAP_PORT,
+			CH_DICTIONARY_TAP_TYPE,
+			CH_DICTIONARY_VTAP,
+			CH_DICTIONARY_VTAP_PORT,
+			CH_DICTIONARY_POD_NODE_PORT,
+			CH_DICTIONARY_POD_GROUP_PORT,
+			CH_DICTIONARY_POD_PORT,
+			CH_DICTIONARY_DEVICE_PORT,
+			CH_DICTIONARY_IP_PORT,
+			CH_DICTIONARY_SERVER_PORT,
+			CH_DICTIONARY_LB_LISTENER)
+		chDicts := mapset.NewSet()
+		for _, dictionary := range dictionaries {
+			chDicts.Add(dictionary)
+		}
+
+		// 删除不存在的字典
+		delDicts := chDicts.Difference(wantedDicts)
+		for _, dict := range delDicts.ToSlice() {
+			dropSQL := fmt.Sprintf("DROP DICTIONARY %s.%s", c.cfg.ClickHouseCfg.Database, dict)
+			_, err = connect.Exec(dropSQL)
+			if err != nil {
+				log.Error(err)
+				connect.Close()
+				continue
+			}
+		}
+
+		// 创建期望的字典
+		addDicts := wantedDicts.Difference(chDicts)
+		for _, dict := range addDicts.ToSlice() {
+			dictName := dict.(string)
+			chTable := "ch_" + strings.TrimSuffix(dictName, "_map")
+			createSQL := CREATE_SQL_MAP[dictName]
+			mysqlPortStr := strconv.Itoa(int(c.cfg.MySqlCfg.Port))
+			createSQL = fmt.Sprintf(createSQL, c.cfg.ClickHouseCfg.Database, dictName, mysqlPortStr, c.cfg.MySqlCfg.UserName, c.cfg.MySqlCfg.UserPassword, replicaSQL, c.cfg.MySqlCfg.Database, chTable, chTable)
+			log.Infof("create dictionary %s", dictName)
+			log.Info(createSQL)
+			_, err = connect.Exec(createSQL)
+			if err != nil {
+				log.Error(err)
+				connect.Close()
+				continue
+			}
+		}
+
+		// 检查并更新已存在字典
+		checkDicts := chDicts.Intersect(wantedDicts)
+		for _, dict := range checkDicts.ToSlice() {
+			dictName := dict.(string)
+			chTable := "ch_" + strings.TrimSuffix(dictName, "_map")
+			showSQL := fmt.Sprintf("SHOW CREATE DICTIONARY %s.%s", c.cfg.ClickHouseCfg.Database, dictName)
+			dictSQL := make([]string, 0)
+			if err := connect.Select(&dictSQL, showSQL); err != nil {
+				log.Error(err)
+				connect.Close()
+				continue
+			}
+			createSQL := CREATE_SQL_MAP[dictName]
+			mysqlPortStr := strconv.Itoa(int(c.cfg.MySqlCfg.Port))
+			createSQL = fmt.Sprintf(createSQL, c.cfg.ClickHouseCfg.Database, dictName, mysqlPortStr, c.cfg.MySqlCfg.UserName, c.cfg.MySqlCfg.UserPassword, replicaSQL, c.cfg.MySqlCfg.Database, chTable, chTable)
+			if createSQL == dictSQL[0] {
+				continue
+			}
+			log.Infof("update dictionary %s", dictName)
+			log.Infof("exist dictionary %s", dictSQL[0])
+			log.Infof("wanted dictionary %s", createSQL)
+			dropSQL := fmt.Sprintf("DROP DICTIONARY %s.%s", c.cfg.ClickHouseCfg.Database, dictName)
+			_, err = connect.Exec(dropSQL)
+			if err != nil {
+				log.Error(err)
+				connect.Close()
+				continue
+			}
+			_, err = connect.Exec(createSQL)
+			if err != nil {
+				log.Error(err)
+				connect.Close()
+				continue
+			}
+		}
+		connect.Close()
 	}
 	return
 }
