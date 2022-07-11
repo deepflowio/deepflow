584a0fd5aa0e96cf71aec42e41ba5537bf6b3213
Zheng Ya <zhengya@yunshan.net>
2022-07-11 15:21:37 +0800

    [CONTROLLER-SERVICE] auto create az and vpc for k8s domain
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    **Impactions**
    
    **Test method**
    
    - test create k8s domain
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/cloud/kubernetes_gather/vpc.go
+++ server/controller/cloud/kubernetes_gather/vpc.go
@@ -7,10 +7,14 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+func GetVPCLcuuidFromUUIDGenerate(uuidGenerate string) string {
+	return common.GetUUID(uuidGenerate+K8S_VPC_NAME, uuid.Nil)
+}
+
 func (k *KubernetesGather) getVPC() (model.VPC, error) {
 	log.Debug("get vpc starting")
 	if k.VPCUuid == "" {
-		k.VPCUuid = common.GetUUID(k.UuidGenerate+K8S_VPC_NAME, uuid.Nil)
+		k.VPCUuid = GetVPCLcuuidFromUUIDGenerate(k.UuidGenerate)
 	}
 	vpc := model.VPC{
 		Lcuuid:       k.VPCUuid,
