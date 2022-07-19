89101656b28c0c2f57212a3c63c54793f8bfc581
LU yao <luyao@yunshan.net.cn>
2022-07-19 17:41:04 +0800

    [controller]add alarm_policy sql
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    add alarm_policy sql
    1. vtap warning log
    2. vtap error log
    3. sync k8sinfo delay
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/db/mysql/migrate/rawsql/init.sql
+++ server/controller/db/mysql/migrate/rawsql/init.sql
@@ -1277,6 +1277,24 @@ INSERT INTO alarm_policy(sub_view_type, sub_view_url, sub_view_params, sub_view_
     upper_threshold, lower_threshold, lcuuid)
     values(1, "/v1/alarm/vtap-sys-free-memory/", "{}", "[{\"OPERATOR\": {\"return_field\": \"sysalarm_value\", \"return_field_description\": \"系统空闲内存比例与空闲内存限制的比例5分钟内最大值\", \"return_field_unit\": \"%\"}}]", "采集器系统空闲内存比例超限",  0, 1, 1, 21, 1, "", "", "sysalarm_value", NULL, 150, @lcuuid);
 
+set @lcuuid = (select uuid());
+INSERT INTO alarm_policy(sub_view_type, sub_view_url, sub_view_params, sub_view_metrics, name, level, state,
+    app_type, sub_type, contrast_type, target_line_uid, target_line_name, target_field,
+    upper_threshold, lower_threshold, lcuuid)
+    values(1, "/v1/alarm/vtap-logcount-warning/", "{}", "[{\"OPERATOR\": {\"return_field\": \"sysalarm_value\", \"return_field_description\": \"WARN日志条数1分钟总量\", \"return_field_unit\": \"条\"}}]", "采集器的WARN日志条数超限",  0, 1, 1, 20, 1, "", "", "sysalarm_value", 1, NULL, @lcuuid);
+
+set @lcuuid = (select uuid());
+INSERT INTO alarm_policy(sub_view_type, sub_view_url, sub_view_params, sub_view_metrics, name, level, state,
+    app_type, sub_type, contrast_type, target_line_uid, target_line_name, target_field,
+    upper_threshold, lower_threshold, lcuuid)
+    values(1, "/v1/alarm/vtap-logcount-error/", "{}", "[{\"OPERATOR\": {\"return_field\": \"sysalarm_value\", \"return_field_description\": \"ERR日志条数1分钟总量\", \"return_field_unit\": \"条\"}}]", "采集器的ERR日志条数超限",  0, 1, 1, 20, 1, "", "", "sysalarm_value", 1, NULL, @lcuuid);
+
+set @lcuuid = (select uuid());
+INSERT INTO alarm_policy(sub_view_type, sub_view_url, sub_view_params, sub_view_metrics, name, level, state,
+    app_type, sub_type, contrast_type, target_line_uid, target_line_name, target_field,
+    upper_threshold, lower_threshold, lcuuid)
+    values(1, "/v1/alarm/sync-k8sinfo-delay/", "{}", "[{\"OPERATOR\": {\"return_field\": \"sysalarm_value\", \"return_field_description\": \"同步滞后时间\", \"return_field_unit\": \"秒\"}}]", "K8s容器信息同步滞后",  0, 1, 1, 23, 1, "", "", "sysalarm_value", 600, NULL, @lcuuid);
+
 CREATE TABLE IF NOT EXISTS report_policy (
     id                      INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name                    CHAR(64) NOT NULL,
