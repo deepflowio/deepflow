5acdcc121a4b16356510bd93e4cc6651ce407fbc
Song Zhen <songzhen@yunshan.net>
2022-07-25 20:36:58 +0800

    modify vtap config tap_interface_regex default value
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    - add lxc & lo
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/common/vtap_config.go
+++ server/controller/common/vtap_config.go
@@ -88,7 +88,7 @@ var (
 	RsyslogEnabled                = 1
 	MaxTxBandwidth                = int64(0)
 	BandwidthProbeInterval        = 10
-	TapInterfaceRegex             = "^(tap|cali|veth|eth|en[ospx]).*"
+	TapInterfaceRegex             = "^(tap.*|cali.*|veth.*|eth.*|en[ospx].*|lxc.*|lo)$"
 	MaxEscapeSeconds              = 3600
 	Mtu                           = 1500
 	OutputVlan                    = 0
