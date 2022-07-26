120c30e1fc191159c68a231ffe606b6b1b2372f2
LU yao <luyao@yunshan.net.cn>
2022-07-26 10:02:43 +0800

    [querier]ext_metrics remove int values
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    ext_metrics remove int values
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/querier/engine/clickhouse/metrics/ext_common.go
+++ server/querier/engine/clickhouse/metrics/ext_common.go
@@ -36,40 +36,21 @@ func GetExtMetrics(db, table, where string) (map[string]*Metrics, error) {
 			Password: config.Cfg.Clickhouse.Password,
 			DB:       db,
 		}
-		externalMetricIntSql := fmt.Sprintf("SELECT arrayJoin(metrics_int_names) AS metrics_int_name FROM (SELECT metrics_int_names FROM %s) GROUP BY metrics_int_name", table)
 		externalMetricFloatSql := fmt.Sprintf("SELECT arrayJoin(metrics_float_names) AS metrics_float_name FROM (SELECT metrics_float_names FROM %s) GROUP BY metrics_float_name", table)
-		externalMetricIntRst, err := externalChClient.DoQuery(externalMetricIntSql, nil, "")
-		if err != nil {
-			log.Error(err)
-			return nil, err
-		}
 		externalMetricFloatRst, err := externalChClient.DoQuery(externalMetricFloatSql, nil, "")
 		if err != nil {
 			log.Error(err)
 			return nil, err
-		}
-		for i, _tagName := range externalMetricIntRst["values"] {
-			tagName := _tagName.([]interface{})[0]
-			externalTag := tagName.(string)
-			dbField := fmt.Sprintf("metrics_int_values[indexOf(metrics_int_names, '%s')]", externalTag)
-			lm := NewMetrics(
-				i, dbField, externalTag, "", METRICS_TYPE_COUNTER,
-				"原始Tag", []bool{true, true, true}, "",
-				table,
-			)
-			metricName := fmt.Sprintf("%s.%s", "int", externalTag)
-			loadMetrics[metricName] = lm
 		}
 		for i, _tagName := range externalMetricFloatRst["values"] {
 			tagName := _tagName.([]interface{})[0]
 			externalTag := tagName.(string)
 			dbField := fmt.Sprintf("metrics_float_values[indexOf(metrics_float_names, '%s')]", externalTag)
 			lm := NewMetrics(
-				i+len(externalMetricIntRst["values"]), dbField, externalTag, "", METRICS_TYPE_COUNTER,
+				i, dbField, externalTag, "", METRICS_TYPE_COUNTER,
 				"原始Tag", []bool{true, true, true}, "", table,
 			)
-			metricName := fmt.Sprintf("%s.%s", "float", externalTag)
-			loadMetrics[metricName] = lm
+			loadMetrics[externalTag] = lm
 		}
 	}
 	return loadMetrics, err
