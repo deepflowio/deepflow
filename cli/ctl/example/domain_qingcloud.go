ef15e422d026cb53e5e206b17b221a57e4cc8f4b
Song Zhen <songzhen@yunshan.net>
2022-08-03 17:58:19 +0800

    [cli] modify domain example output
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    - controller ip is not must specified
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- cli/ctl/example/domain_qingcloud.go
+++ cli/ctl/example/domain_qingcloud.go
@@ -25,7 +25,7 @@ config:
   # 所属区域标识
   region_uuid: ffffffff-ffff-ffff-ffff-ffffffffffff
   # 资源同步控制器
-  controller_ip: 127.0.0.1
+  #controller_ip: 127.0.0.1
   # API 密钥 ID
   # 在青云主页面右上角-API密钥-API密钥管理-API密钥ID
   secret_id: xxxxxxxx
