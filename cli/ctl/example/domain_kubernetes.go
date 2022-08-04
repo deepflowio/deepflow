deaf3db00430577894c13276c24e88989ffe0b39
Song Zhen <107971920+SongZhen0704@users.noreply.github.com>
2022-08-04 09:56:24 +0800

    [cli] modify domain example output (#512)
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    - controller ip is not must specified
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)
    
    Co-authored-by: jianchang <jianchang@yunshan.net>

--- cli/ctl/example/domain_kubernetes.go
+++ cli/ctl/example/domain_kubernetes.go
@@ -25,7 +25,7 @@ config:
   # 所属区域标识
   region_uuid: ffffffff-ffff-ffff-ffff-ffffffffffff
   # 资源同步控制器
-  controller_ip: 127.0.0.1
+  #controller_ip: 127.0.0.1
   # POD子网IPv4地址最大掩码
   pod_net_ipv4_cidr_max_mask: 16
   # POD子网IPv6地址最大掩码
