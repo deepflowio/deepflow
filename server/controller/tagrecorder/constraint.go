a0d40d1450b9d583609867749135dcafe964fd02
Ericsssss <lvqiao@yunshan.net>
2022-08-04 20:23:17 +0800

    [Tagrecorder] Add dictionary pod_ingress_map
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
      - add dictionary pod_ingress_map
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/tagrecorder/constraint.go
+++ server/controller/tagrecorder/constraint.go
@@ -23,7 +23,7 @@ import (
 
 // 资源的MySQL orm对象
 type MySQLChModel interface {
-	mysql.ChPodGroupPort | mysql.ChPodPort | mysql.ChVTapPort | mysql.ChAZ | mysql.ChIPResource | mysql.ChK8sLabel |
+	mysql.ChPodIngress | mysql.ChPodGroupPort | mysql.ChPodPort | mysql.ChVTapPort | mysql.ChAZ | mysql.ChIPResource | mysql.ChK8sLabel |
 		mysql.ChLBListener | mysql.ChPodNodePort | mysql.ChIPPort | mysql.ChDevicePort | mysql.ChRegion | mysql.ChVPC |
 		mysql.ChDevice | mysql.ChIPRelation | mysql.ChPodGroup | mysql.ChNetwork | mysql.ChPod | mysql.ChPodCluster |
 		mysql.ChPodNode | mysql.ChPodNamespace | mysql.ChTapType | mysql.ChVTap
