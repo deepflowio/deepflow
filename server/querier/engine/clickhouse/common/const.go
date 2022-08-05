4d4f3eec31bfe955f15bfb672d23b8e7635920b8
Song Zhen <songzhen@yunshan.net>
2022-08-05 10:48:05 +0800

    [querier] add time field in show tags in deepflow_system database
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    - time field not in _custom_field
    
    **Impactions**
    
    **Test method**
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/querier/engine/clickhouse/common/const.go
+++ server/querier/engine/clickhouse/common/const.go
@@ -22,5 +22,5 @@ var DB_TABLE_MAP = map[string][]string{
 	"flow_log":        []string{"l4_flow_log", "l7_flow_log"},
 	"flow_metrics":    []string{"vtap_flow_port", "vtap_flow_edge_port", "vtap_app_port", "vtap_app_edge_port"},
 	"ext_metrics":     []string{"ext_common"},
-	"deepflow_system": []string{},
+	"deepflow_system": []string{"deepflow_system_common"},
 }
