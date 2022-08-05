6811eb675e54476129760378c3f0287222cc493f
zhengya <zhengya@yunshan.net.cn>
2022-08-05 12:36:27 +0800

    [CONTROLLER-RECORDER]fix dedup error
    **Phenomenon and reproduction steps**
    
    **Root cause and solution**
    
    - set slice cap to negative num
    
    **Impactions**
    
    **Test method**
    
    - UT
    
    **Affected branch(es)**
    
    * main
    
    **Checklist**
    
    - [ ] Dependencies update required
    - [ ] Common bug (similar problem in other repo)

--- server/controller/recorder/db/operator_test.go
+++ server/controller/recorder/db/operator_test.go
@@ -30,6 +30,10 @@ func (t *SuiteTest) TestformatDBItemsToAdd() {
 	vif2 := vifs[1]
 	vifs = append(vifs, vifs[1])
 	mysql.Db.Create(&vif1)
+	vif1.ID += 1
+	mysql.Db.Create(&vif1)
+	vif1.ID += 1
+	mysql.Db.Create(&vif1)
 
 	vifsToAdd, lcuuidsToAdd, ok := operator.formatDBItemsToAdd(vifs)
 	assert.True(t.T(), ok)
