a8b71536e90b39fe688bdc6c88ca672f7e02a483
Liu Chao <liuchao@yunshan.net>
2022-08-29 17:41:15 +0800

    [tagrecorder] add ch_k8s_labels

--- server/controller/tagrecorder/constraint.go
+++ server/controller/tagrecorder/constraint.go
@@ -26,11 +26,11 @@ type MySQLChModel interface {
 	mysql.ChPodIngress | mysql.ChPodGroupPort | mysql.ChPodPort | mysql.ChVTapPort | mysql.ChAZ | mysql.ChIPResource | mysql.ChK8sLabel |
 		mysql.ChLBListener | mysql.ChPodNodePort | mysql.ChIPPort | mysql.ChDevicePort | mysql.ChRegion | mysql.ChVPC |
 		mysql.ChDevice | mysql.ChIPRelation | mysql.ChPodGroup | mysql.ChNetwork | mysql.ChPod | mysql.ChPodCluster |
-		mysql.ChPodNode | mysql.ChPodNamespace | mysql.ChTapType | mysql.ChVTap
+		mysql.ChPodNode | mysql.ChPodNamespace | mysql.ChTapType | mysql.ChVTap | mysql.ChK8sLabels
 }
 
 // ch资源的组合key
 type ChModelKey interface {
 	VtapPortKey | IPResourceKey | K8sLabelKey | PortIDKey | PortIPKey | PortDeviceKey | IDKey | DeviceKey |
-		IPRelationKey | TapTypeKey
+		IPRelationKey | TapTypeKey | K8sLabelsKey
 }
