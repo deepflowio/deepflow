4c2409819f3bd74f42a74a5d5f832f0bec4b0ca2
zhuofeng <zhuofeng@yunshan.net.cn>
2022-10-31 17:11:29 +0800

    [server/ingester] support ingester write the clickhouse of this Node first

--- server/ingester/config/watcher.go
+++ server/ingester/config/watcher.go
@@ -40,15 +40,19 @@ type Endpoint struct {
 }
 
 type Watcher struct {
-	ServerPodNamesWatch   *ServerInstanceInfo
-	EndpointWatch         libs.Watcher
-	clickhouseEndpointKey string
-	myPodName             string
-	myClickhouseEndpoint  Endpoint
-	lastServerPodNames    []string
+	NodePodNamesWatch             *ServerInstanceInfo
+	EndpointWatch                 libs.Watcher
+	clickhouseEndpointKey         string
+	clickhouseEndpointTCPPortName string
+	myNodeName                    string
+	myPodName                     string
+	clickhouseIsExternal          bool
+	myClickhouseEndpoint          Endpoint
+	lastNodePodNames              map[string][]string
+	lastServerEndpointMap         map[string]Endpoint
 }
 
-func NewWatcher(myPodName, myPodNamespace, clickhouseEndpointKey string, controllerIPs []string, controllerPort, grpcBufferSize int) (*Watcher, error) {
+func NewWatcher(myNodeName, myPodName, myPodNamespace, clickhouseEndpointKey, clickhouseEndpointTCPPortName string, clickhouseIsExternal bool, controllerIPs []string, controllerPort, grpcBufferSize int) (*Watcher, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		errMsg := fmt.Errorf("get cluster config failed: %v", err)
@@ -76,9 +80,20 @@ func NewWatcher(myPodName, myPodNamespace, clickhouseEndpointKey string, control
 			controllers[i] = controllers[i].To4()
 		}
 	}
-	serverPodNamesWatch := NewServerInstranceInfo(controllers, controllerPort, grpcBufferSize)
+	nodePodNamesWatch := NewServerInstranceInfo(controllers, controllerPort, grpcBufferSize)
 
-	watcher := &Watcher{serverPodNamesWatch, endpointsWatcher, clickhouseEndpointKey, myPodName, Endpoint{}, []string{}}
+	watcher := &Watcher{
+		NodePodNamesWatch:             nodePodNamesWatch,
+		EndpointWatch:                 endpointsWatcher,
+		clickhouseEndpointKey:         clickhouseEndpointKey,
+		clickhouseEndpointTCPPortName: clickhouseEndpointTCPPortName,
+		myNodeName:                    myNodeName,
+		myPodName:                     myPodName,
+		clickhouseIsExternal:          clickhouseIsExternal,
+		lastNodePodNames:              make(map[string][]string),
+		lastServerEndpointMap:         make(map[string]Endpoint),
+	}
+
 	go watcher.Run()
 
 	return watcher, nil
@@ -96,10 +111,10 @@ func (w *Watcher) Run() {
 		}
 
 		if w.myClickhouseEndpoint.Host == "" && w.myClickhouseEndpoint.Port == 0 {
-			w.myClickhouseEndpoint = endpoint
+			w.myClickhouseEndpoint = *endpoint
 		}
 
-		if endpoint != w.myClickhouseEndpoint {
+		if *endpoint != w.myClickhouseEndpoint {
 			log.Warningf("my clickhouse endpoint change from %v to %v", w.myClickhouseEndpoint, endpoint)
 			sleepAndExit()
 		}
@@ -115,31 +130,59 @@ func indexOf(ss []string, s string) int {
 	return -1
 }
 
-// How to get my clickhouse endpoint:
+// How to get my external clickhouse endpoint:
 // 1, Get a list of all 'deepflow-server' pods, and sort by name to find the 'index' of myself pod in it
 // 2. Get the list and total 'len' of all clickhouse endpoints, and sort by IP
 // 3, my corresponding 'clickhouse endpoint' is on position 'index%len'  in the 'clickhouse endpoints list'
-func (w *Watcher) GetMyClickhouseEndpoint() (Endpoint, error) {
-	endpoint := Endpoint{}
-	podNames, err := w.getServerPodNames()
+func (w *Watcher) getMyClickhouseEndpointExternal() (*Endpoint, error) {
+	podNames, err := w.getPodNames()
 	if err != nil {
-		return endpoint, err
+		return nil, err
 	}
 	myIndex := indexOf(podNames, w.myPodName)
 	if myIndex < 0 {
-		return endpoint, fmt.Errorf("can't find my pod name(%s) in pods(%v)", w.myPodName, podNames)
+		return nil, fmt.Errorf("can't find my pod name(%s) in pods(%v)", w.myPodName, podNames)
 	}
-	endpoints, err := w.getEndpoints(w.clickhouseEndpointKey)
+	endpoints, err := w.getEndpoints()
 	if err != nil {
-		return endpoint, err
+		return nil, err
 	}
-	endpoint = endpoints[myIndex%len(endpoints)]
 
-	return endpoint, nil
+	return &endpoints[myIndex%len(endpoints)], nil
+}
+
+func (w *Watcher) getMyClickhouseEndpointInternal() (*Endpoint, error) {
+	nodePodNames, err := w.getNodePodNames()
+	if err != nil {
+		return nil, err
+	}
+	nodeEndpoints, err := w.getNodeEndpoints()
+	if err != nil {
+		return nil, err
+	}
+
+	serverEndpointMap := getServerEndpointMap(nodePodNames, nodeEndpoints)
+	if !serverEndpointMapEqual(w.lastServerEndpointMap, serverEndpointMap) {
+		log.Infof("the correspondence between Server Pod and ClickHouse Endpoint change from %+v to  %+v", w.lastServerEndpointMap, serverEndpointMap)
+		w.lastServerEndpointMap = serverEndpointMap
+	}
+	if endpoint, ok := serverEndpointMap[w.myNodeName+w.myPodName]; ok {
+		return &endpoint, nil
+	}
+
+	return nil, fmt.Errorf("Can't find my clickhouse endpoint, myNodeName: %s myPodName: %s", w.myNodeName, w.myPodName)
+}
+
+func (w *Watcher) GetMyClickhouseEndpoint() (*Endpoint, error) {
+	if w.clickhouseIsExternal {
+		return w.getMyClickhouseEndpointExternal()
+	} else {
+		return w.getMyClickhouseEndpointInternal()
+	}
 }
 
 func (w *Watcher) GetClickhouseEndpointsWithoutMyself() ([]Endpoint, error) {
-	endpoints, err := w.getEndpoints(w.clickhouseEndpointKey)
+	endpoints, err := w.getEndpoints()
 	if err != nil {
 		return nil, err
 	}
@@ -165,37 +208,67 @@ func stringsEqual(a, b []string) bool {
 	return true
 }
 
-func (w *Watcher) getServerPodNames() ([]string, error) {
-	podNames := w.ServerPodNamesWatch.GetServerPodNames()
-	if len(podNames) == 0 {
+func nodePodNamesEqual(a, b map[string][]string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for k, v := range a {
+		if !stringsEqual(v, b[k]) {
+			return false
+		}
+	}
+	return true
+}
+
+func (w *Watcher) getNodePodNames() (map[string][]string, error) {
+	nodePodNames := w.NodePodNamesWatch.GetNodePodNames()
+	if len(nodePodNames) == 0 {
 		return nil, fmt.Errorf("get server pod names empty")
 	}
 
-	if !stringsEqual(podNames, w.lastServerPodNames) {
-		log.Warningf("server pod names change from '%v' to '%v'", w.lastServerPodNames, podNames)
-		w.lastServerPodNames = podNames
+	if !nodePodNamesEqual(nodePodNames, w.lastNodePodNames) {
+		log.Warningf("server node pod names change from '%v' to '%v'", w.lastNodePodNames, nodePodNames)
+		w.lastNodePodNames = nodePodNames
 	}
 
-	return podNames, nil
+	return nodePodNames, nil
 }
 
-func (w *Watcher) getEndpoints(key string) ([]Endpoint, error) {
+func (w *Watcher) getPodNames() ([]string, error) {
+	nodePodNames, err := w.getNodePodNames()
+	if err != nil {
+		return nil, err
+	}
+
+	allPodNames := []string{}
+	for _, podNames := range nodePodNames {
+		allPodNames = append(allPodNames, podNames...)
+	}
+	sort.Slice(allPodNames, func(i, j int) bool {
+		return allPodNames[i] < allPodNames[j]
+	})
+
+	return allPodNames, nil
+}
+
+func (w *Watcher) getNodeEndpoints() (map[string][]Endpoint, error) {
 	for i := 0; i < TIMEOUT; i++ {
 		entries := w.EndpointWatch.Entries()
-		endpoints := []Endpoint{}
+		nodeEndpoints := make(map[string][]Endpoint)
 		for _, v := range entries {
 			e, ok := v.(*corev1.Endpoints)
 			if !ok {
 				continue
 			}
 			ep := e.GetName()
-			if ep != key {
+			if ep != w.clickhouseEndpointKey {
 				continue
 			}
 			for _, v := range e.Subsets {
 				port := uint16(0)
 				for _, p := range v.Ports {
-					if p.Name == "tcp-port" || p.Name == "clickhouse" {
+					if p.Name == w.clickhouseEndpointTCPPortName {
 						port = uint16(p.Port)
 						break
 					}
@@ -204,21 +277,121 @@ func (w *Watcher) getEndpoints(key string) ([]Endpoint, error) {
 					continue
 				}
 				for _, v := range v.Addresses {
-					endpoints = append(endpoints, Endpoint{v.IP, port})
+					nodeName := ""
+					if v.NodeName != nil {
+						nodeName = *v.NodeName
+					}
+					nodeEndpoints[nodeName] = append(nodeEndpoints[nodeName], Endpoint{v.IP, port})
 				}
 			}
 		}
 
-		if len(endpoints) == 0 {
+		if len(nodeEndpoints) == 0 {
 			time.Sleep(time.Second)
 			continue
 		}
 
-		sort.Slice(endpoints, func(i, j int) bool {
-			return endpoints[i].Host < endpoints[j].Host
-		})
-		log.Debugf("get endpoints %v", endpoints)
-		return endpoints, nil
+		log.Debugf("get node endpoints %+v", nodeEndpoints)
+		return nodeEndpoints, nil
 	}
-	return nil, fmt.Errorf("get endpoint(%s) empty, timeout is %d", key, TIMEOUT)
+	return nil, fmt.Errorf("get endpoint(%s) empty, timeout is %d", w.clickhouseEndpointKey, TIMEOUT)
+}
+
+func (w *Watcher) getEndpoints() ([]Endpoint, error) {
+	nodeEndpoints, err := w.getNodeEndpoints()
+	if err != nil {
+		return nil, err
+	}
+	return getAllEndpoints(nodeEndpoints), nil
+}
+
+func getAllEndpoints(nodeEndpointsMap map[string][]Endpoint) []Endpoint {
+	allEndpoints := make([]Endpoint, 0)
+	for _, endpoints := range nodeEndpointsMap {
+		allEndpoints = append(allEndpoints, endpoints...)
+	}
+
+	sort.Slice(allEndpoints, func(i, j int) bool {
+		return allEndpoints[i].Host < allEndpoints[j].Host
+	})
+	return allEndpoints
+}
+
+func getUnusedEndpoints(allEndpoints, matchedEndpoints []Endpoint) []Endpoint {
+	unmatchs := make([]Endpoint, 0)
+	for _, v := range allEndpoints {
+		match := false
+		for _, ep := range matchedEndpoints {
+			if v == ep {
+				match = true
+			}
+		}
+		if !match {
+			unmatchs = append(unmatchs, v)
+		}
+	}
+	sort.Slice(unmatchs, func(i, j int) bool {
+		return unmatchs[i].Host < unmatchs[j].Host
+	})
+	return unmatchs
+}
+
+func serverEndpointMapEqual(a, b map[string]Endpoint) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for k, v := range a {
+		if v != b[k] {
+			return false
+		}
+	}
+	return true
+}
+
+func getServerEndpointMap(nodePodNamesMap map[string][]string, nodeEndpointsMap map[string][]Endpoint) map[string]Endpoint {
+	serverEndpointMap := make(map[string]Endpoint)
+	unassignedServers := make([]string, 0)
+	usedEndpoints := make([]Endpoint, 0)
+	// 1.Prioritize allocating Endpoint on the same Node
+	for nodeName, podNames := range nodePodNamesMap {
+		endpoints := nodeEndpointsMap[nodeName]
+		endpointsCount := len(endpoints)
+		for i, podName := range podNames {
+			if endpointsCount > 0 {
+				endpoint := endpoints[i%endpointsCount]
+				serverEndpointMap[nodeName+podName] = endpoint
+				usedEndpoints = append(usedEndpoints, endpoint)
+			} else {
+				unassignedServers = append(unassignedServers, nodeName+podName)
+			}
+		}
+	}
+
+	if len(unassignedServers) == 0 {
+		return serverEndpointMap
+	}
+
+	// 2.Get unassigned servers and endpoints, sort them separately, and assign them in order.
+	sort.Slice(unassignedServers, func(i, j int) bool {
+		return unassignedServers[i] < unassignedServers[j]
+	})
+
+	allEndpoints := getAllEndpoints(nodeEndpointsMap)
+	allEndpointsCount := len(allEndpoints)
+	if allEndpointsCount == 0 {
+		return serverEndpointMap
+	}
+
+	unusedEndpoints := getUnusedEndpoints(allEndpoints, usedEndpoints)
+	unusedEndpointsCount := len(unusedEndpoints)
+	for i, key := range unassignedServers {
+		if unusedEndpointsCount > 0 {
+			serverEndpointMap[key] = unusedEndpoints[i%unusedEndpointsCount]
+		} else {
+			serverEndpointMap[key] = allEndpoints[i%allEndpointsCount]
+		}
+	}
+
+	return serverEndpointMap
 }
---
+++ server/ingester/config/watcher_test.go
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2022 Yunshan Networks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import (
+	"testing"
+)
+
+func TestGetServerEndpointMapNormal(t *testing.T) {
+	nodePodNamesMap := map[string][]string{
+		"node1": {"pod1"},
+		"node2": {"pod2"},
+		"node3": {"pod3"},
+	}
+	nodeEndpointsMap := map[string][]Endpoint{
+		"node1": {{"1.1.1.1", 9000}},
+		"node2": {{"1.1.1.2", 9000}},
+		"node3": {{"1.1.1.3", 9000}},
+	}
+
+	expect := map[string]Endpoint{
+		"node1pod1": {"1.1.1.1", 9000},
+		"node2pod2": {"1.1.1.2", 9000},
+		"node3pod3": {"1.1.1.3", 9000},
+	}
+	actual := getServerEndpointMap(nodePodNamesMap, nodeEndpointsMap)
+	if !serverEndpointMapEqual(expect, actual) {
+		t.Errorf("Expected %v found %v", expect, actual)
+	}
+}
+
+func TestGetServerEndpointMap2(t *testing.T) {
+	nodePodNamesMap := map[string][]string{
+		"node1": {"pod11", "pod12"},
+		"node2": {"pod21", "pod22"},
+	}
+	nodeEndpointsMap := map[string][]Endpoint{
+		"node1": {{"1.1.1.1", 9000}},
+		"node2": {{"1.1.1.2", 9000}},
+	}
+
+	expect := map[string]Endpoint{
+		"node1pod11": {"1.1.1.1", 9000},
+		"node1pod12": {"1.1.1.1", 9000},
+		"node2pod21": {"1.1.1.2", 9000},
+		"node2pod22": {"1.1.1.2", 9000},
+	}
+	actual := getServerEndpointMap(nodePodNamesMap, nodeEndpointsMap)
+	if !serverEndpointMapEqual(expect, actual) {
+		t.Errorf("Expected %v found %v", expect, actual)
+	}
+}
+
+func TestGetServerEndpointMap3(t *testing.T) {
+	nodePodNamesMap := map[string][]string{
+		"node1": {"pod11", "pod12"},
+		"node2": {"pod21", "pod22"},
+	}
+	nodeEndpointsMap := map[string][]Endpoint{
+		"node1": {{"1.1.1.11", 9000}, {"1.1.1.12", 9000}},
+		"node2": {{"1.1.1.21", 9000}, {"1.1.1.22", 9000}},
+	}
+
+	expect := map[string]Endpoint{
+		"node1pod11": {"1.1.1.11", 9000},
+		"node1pod12": {"1.1.1.12", 9000},
+		"node2pod21": {"1.1.1.21", 9000},
+		"node2pod22": {"1.1.1.22", 9000},
+	}
+	actual := getServerEndpointMap(nodePodNamesMap, nodeEndpointsMap)
+	if !serverEndpointMapEqual(expect, actual) {
+		t.Errorf("Expected %v found %v", expect, actual)
+	}
+}
+
+func TestGetServerEndpointMap4(t *testing.T) {
+	nodePodNamesMap := map[string][]string{
+		"node1": {"pod11"},
+		"node2": {"pod21", "pod22"},
+	}
+	nodeEndpointsMap := map[string][]Endpoint{
+		"node1": {{"1.1.1.11", 9000}, {"1.1.1.12", 9000}},
+		"node2": {{"1.1.1.21", 9000}},
+	}
+
+	expect := map[string]Endpoint{
+		"node1pod11": {"1.1.1.11", 9000},
+		"node2pod21": {"1.1.1.21", 9000},
+		"node2pod22": {"1.1.1.21", 9000},
+	}
+	actual := getServerEndpointMap(nodePodNamesMap, nodeEndpointsMap)
+	if !serverEndpointMapEqual(expect, actual) {
+		t.Errorf("Expected %v found %v", expect, actual)
+	}
+}
