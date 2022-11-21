979235ee11dad58e2d87be9ca894a4a1f5d6b6b7
LU yao <luyao@yunshan.net.cn>
2022-11-21 14:13:36 +0800

    [querier] support tempo

--- server/querier/common/parameters.go
+++ server/querier/common/parameters.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"context"
+	"strings"
 )
 
 type QuerierParams struct {
@@ -11,4 +12,38 @@ type QuerierParams struct {
 	Sql        string
 	DataSource string
 	Context    context.Context
+}
+
+type TempoParams struct {
+	TraceId     string
+	StartTime   string
+	EndTime     string
+	TagName     string
+	MinDuration string
+	MaxDuration string
+	Limit       string
+	Debug       string
+	Filters     []*KeyValue
+	Context     context.Context
+}
+
+func (p *TempoParams) SetFilters(filterStr string) {
+	if filterStr == "" {
+		return
+	}
+	filterSplit := strings.Split(filterStr, " ")
+	for _, filter := range filterSplit {
+		if strings.Contains(filter, "=") {
+			f := strings.Split(filter, "=")
+			p.Filters = append(p.Filters, &KeyValue{
+				Key:   f[0],
+				Value: strings.Trim(f[1], "\""),
+			})
+		}
+	}
+}
+
+type KeyValue struct {
+	Key   string
+	Value string
 }
