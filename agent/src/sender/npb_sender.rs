e89f400bc3c3b3b0fef8249ddb9fed323072304b
yuanchao <yuanchao@yunshan.net>
2022-12-14 15:34:04 +0800

    [Agent] Fix compilation errors in windows

--- agent/src/sender/npb_sender.rs
+++ agent/src/sender/npb_sender.rs
@@ -52,9 +52,9 @@ use public::counter::{Countable, CounterType, CounterValue, OwnedCountable};
 use public::proto::trident::SocketType;
 use public::queue::Receiver;
 #[cfg(unix)]
+use public::utils::net::MAC_ADDR_LEN;
 use public::utils::net::{
     get_route_src_ip_and_mac, get_route_src_ip_interface_name, neighbor_lookup, MacAddr,
-    MAC_ADDR_LEN,
 };
 
 #[cfg(windows)]
@@ -231,8 +231,8 @@ impl IpSender {
             socket,
             underlay_is_ipv6: remote.is_ipv6(),
             remote: match remote {
-                IpAddr::V4(ip) => SockAddr::from(SocketAddrV4::new(ip, 0)),
-                IpAddr::V6(ip) => SockAddr::from(SocketAddrV6::new(ip, 0, 0, 0)),
+                IpAddr::V4(ip) => SockAddr::from(SocketAddrV4::new(ip.clone(), 0)),
+                IpAddr::V6(ip) => SockAddr::from(SocketAddrV6::new(ip.clone(), 0, 0, 0)),
             },
             dst_ip: remote.clone(),
         })
