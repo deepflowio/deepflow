8bcaf7f747a21ff03dd858083f8d31ab6734d9c5
LU yao <luyao@yunshan.net.cn>
2022-12-20 13:58:57 +0800

    [querier] fixbug: tempo get filter error #19931

--- server/querier/common/parameters.go
+++ server/querier/common/parameters.go
@@ -31,12 +31,12 @@ func (p *TempoParams) SetFilters(filterStr string) {
 	if filterStr == "" {
 		return
 	}
-	filterSplit := strings.Split(filterStr, " ")
+	filterSplit := strings.Split(filterStr, "\" ")
 	for _, filter := range filterSplit {
 		if strings.Contains(filter, "=") {
-			f := strings.Split(filter, "=")
+			f := strings.Split(filter, "=\"")
 			p.Filters = append(p.Filters, &KeyValue{
-				Key:   f[0],
+				Key:   strings.Trim(f[0], " "),
 				Value: strings.Trim(f[1], "\""),
 			})
 		}
