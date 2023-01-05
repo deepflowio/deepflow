b6e47a298b23164a3c2110481f8b27bbc09a82ad
LU yao <luyao@yunshan.net.cn>
2023-01-05 14:56:52 +0800

    [querier] support prometheus query api

---
+++ server/querier/prometheus/remote_read.go
@@ -0,0 +1,56 @@
+package prometheus
+
+import (
+	"context"
+	"fmt"
+	//"strings"
+	//"strconv"
+	//"time"
+
+	"github.com/deepflowys/deepflow/server/querier/common"
+	"github.com/deepflowys/deepflow/server/querier/engine/clickhouse"
+	"github.com/google/uuid"
+	//"github.com/k0kubun/pp"
+	logging "github.com/op/go-logging"
+	"github.com/prometheus/prometheus/prompb"
+)
+
+var log = logging.MustGetLogger("promethues")
+
+func PromReaderExecute(req *prompb.ReadRequest, ctx context.Context) (resp *prompb.ReadResponse, err error) {
+	// promrequest trans to sql
+	//pp.Println(req)
+	sql, db, datasource, err := PromReaderTransToSQL(req)
+	//fmt.Println(sql, db)
+	if err != nil {
+		return nil, err
+	}
+	if db == "" {
+		db = "ext_metrics"
+	}
+	query_uuid := uuid.New()
+	args := common.QuerierParams{
+		DB:         db,
+		Sql:        sql,
+		DataSource: datasource,
+		Debug:      "false",
+		QueryUUID:  query_uuid.String(),
+		Context:    ctx,
+	}
+	ckEngine := &clickhouse.CHEngine{DB: args.DB, DataSource: args.DataSource}
+	ckEngine.Init()
+	result, debug, err := ckEngine.ExecuteQuery(&args)
+	if err != nil {
+		// TODO
+		fmt.Printf("%v %v", debug, err)
+		return nil, err
+	}
+	// response trans to prom resp
+	resp, err = RespTransToProm(result)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	//pp.Println(resp)
+	return resp, nil
+}
