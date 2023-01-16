c66749a9bbd554642a10247aea543227bc9b21a5
weiqiang <weiqiang@yunshan.net>
2023-01-16 18:45:41 +0800

    [Server] modify the udp of vtap_group_config to tcp

---
+++ server/controller/db/mysql/migration/rawsql/issu/6.2.1.14.sql
@@ -0,0 +1,4 @@
+UPDATE vtap_group_configuration SET collector_socket_type=NULL WHERE collector_socket_type="UDP";
+UPDATE vtap_group_configuration SET compressor_socket_type=NULL WHERE compressor_socket_type="UDP" or compressor_socket_type="RAW_UDP";
+
+UPDATE db_version SET version = '6.2.1.14';
