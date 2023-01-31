45a5e050c73bbe0ec778cb39139cbe94cb8ae0c1
huanchao <huanchao@yunshan.net>
2023-01-31 15:01:54 +0800

    [Agent] fix guard cgroup check

---
+++ agent/src/utils/cgroups/windows.rs
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 Yunshan Networks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use std::time::Duration;
+use std::{process, thread};
+
+use super::Error;
+use crate::config::handler::EnvironmentAccess;
+
+use log::{info, warn};
+
+pub struct Cgroups {}
+
+impl Cgroups {
+    pub fn new(_pid: u64, _config: EnvironmentAccess) -> Result<Self, Error> {
+        Err(Error::CgroupNotSupported(
+            "Windows agent's cgroup is not supported".to_string(),
+        ))
+    }
+    pub fn start(&self) {}
+    pub fn get_mount_path(&self) -> String {
+        String::default()
+    }
+    pub fn is_v2(&self) -> bool {
+        false
+    }
+    pub fn stop(&self) -> Result<(), Error> {
+        Err(Error::CgroupNotSupported(
+            "Windows agent's cgroup is not supported".to_string(),
+        ))
+    }
+}
+
+pub fn is_kernel_available_for_cgroup() -> bool {
+    false
+}
