e3ab55f07dec57a8337b9d451c44e0453899d3ca
zhuofeng <zhuofeng@yunshan.net.cn>
2023-02-27 17:16:14 +0800

    [server] change TTL async

--- server/ingester/common/common.go
+++ server/ingester/common/common.go
@@ -20,6 +20,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net"
+	"sync"
 
 	"github.com/deepflowio/deepflow/message/trident"
 	"github.com/deepflowio/deepflow/server/libs/datatype"
@@ -38,6 +39,76 @@ const (
 	MODULE_INGESTER             = "ingester."
 	QUEUE_STATS_MODULE_INGESTER = queue.OptionModule(MODULE_INGESTER)
 )
+
+type DBs []*sql.DB
+
+func (s DBs) Exec(query string, args ...any) (sql.Result, error) {
+	var result sql.Result
+	var err error
+	for _, conn := range s {
+		log.Infof("Begin exec SQL: %s", query)
+		result, err = conn.Exec(query, args...)
+		log.Infof("End exec SQL: %s, err: %v", query, err)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, nil
+}
+
+func (s DBs) ExecParallel(query string, args ...any) (sql.Result, error) {
+	var result sql.Result
+	var err error
+	wg := sync.WaitGroup{}
+	for _, conn := range s {
+		wg.Add(1)
+		go func() {
+			log.Infof("Begin exec parallel SQL: %s", query)
+			r, e := conn.Exec(query, args...)
+			log.Infof("End exec parallel SQL: %s, err:%v", query, err)
+			if err == nil {
+				result = r
+				err = e
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	return result, err
+}
+
+func (s DBs) Query(query string, args ...any) (*sql.Rows, error) {
+	var result *sql.Rows
+	var err error
+	for _, conn := range s {
+		result, err = conn.Query(query, args...)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, nil
+}
+
+func (s DBs) Close() error {
+	for _, conn := range s {
+		if err := conn.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func NewCKConnections(addrs []string, username, password string) (DBs, error) {
+	sqlDBs := DBs{}
+	for _, addr := range addrs {
+		connect, err := NewCKConnection(addr, username, password)
+		if err != nil {
+			return nil, err
+		}
+		sqlDBs = append(sqlDBs, connect)
+	}
+	return sqlDBs, nil
+}
 
 func NewCKConnection(addr, username, password string) (*sql.DB, error) {
 	connect, err := sql.Open("clickhouse", fmt.Sprintf("//%s:%s@%s", username, password, addr))
