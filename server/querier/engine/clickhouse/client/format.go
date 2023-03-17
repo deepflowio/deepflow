7b429c136340cd2095f2b4f11150607fb5c4fac1
Song Zhen <songzhen@yunshan.net>
2023-03-17 13:10:05 +0800

    [querier] support nullable int type

--- server/querier/engine/clickhouse/client/format.go
+++ server/querier/engine/clickhouse/client/format.go
@@ -46,21 +46,69 @@ func TransType(value interface{}, columnName, columnDatabaseTypeName string) (in
 		return int(*v), VALUE_TYPE_INT, nil
 	case *int64:
 		return int(*v), VALUE_TYPE_INT, nil
+	case **int8: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
+	case **int16: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
+	case **int32: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
+	case **int64: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
 	case *uint8:
 		return int(*v), VALUE_TYPE_INT, nil
 	case *uint16:
 		return int(*v), VALUE_TYPE_INT, nil
-	case *uint64:
-		return int(*v), VALUE_TYPE_INT, nil
 	case *uint32:
 		return int(*v), VALUE_TYPE_INT, nil
+	case *uint64:
+		return int(*v), VALUE_TYPE_INT, nil
+	case **uint8: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
+	case **uint16: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
+	case **uint32: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
+	case **uint64: // for nullable
+		// NaN, Inf
+		if v == nil || math.IsNaN(float64(**v)) || float64(**v) == math.Inf(1) || float64(**v) == math.Inf(-1) {
+			return nil, VALUE_TYPE_INT, nil
+		}
+		return int(**v), VALUE_TYPE_INT, nil
 	case *time.Time:
 		return v.String(), VALUE_TYPE_STRING, nil
 	case *net.IP:
 		return v.String(), VALUE_TYPE_STRING, nil
 	case **float64: // Nullable(float64)
 		// NaN, Inf
-		if math.IsNaN(**v) || **v == math.Inf(1) || **v == math.Inf(-1) {
+		if v == nil || math.IsNaN(**v) || **v == math.Inf(1) || **v == math.Inf(-1) {
 			return nil, VALUE_TYPE_FLOAT64, nil
 		}
 		return **v, VALUE_TYPE_FLOAT64, nil
