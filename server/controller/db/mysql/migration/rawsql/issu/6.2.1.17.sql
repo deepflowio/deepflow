cf22cb81ad7a7a61fa51e52308de1d03c9d94412
weiqiang <weiqiang@yunshan.net>
2023-03-20 17:40:56 +0800

    [Server] add stored procedures to update table data_source

--- server/controller/db/mysql/migration/rawsql/issu/6.2.1.17.sql
+++ server/controller/db/mysql/migration/rawsql/issu/6.2.1.17.sql
@@ -1,16 +1,34 @@
-START TRANSACTION;
+DROP PROCEDURE IF EXISTS update_data_sources;
+DELIMITER $$
 
-UPDATE data_source SET retention_time = retention_time * 24;
-UPDATE data_source SET name="flow_log.l4_flow_log", tsdb_type="flow_log.l4_flow_log" WHERE name="flow_log.l4";
-UPDATE data_source SET name="flow_log.l7_flow_log", tsdb_type="flow_log.l7_flow_log" WHERE name="flow_log.l7";
+CREATE PROCEDURE update_data_sources()
+BEGIN
+    DECLARE existing_name INT DEFAULT 0;
 
-set @lcuuid = (select uuid());
-INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('flow_log.l4_packet', 'flow_log.l4_packet', 0, 3*24, @lcuuid);
-set @lcuuid = (select uuid());
-INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('flow_log.l7_packet', 'flow_log.l7_packet', 0, 3*24, @lcuuid);
-set @lcuuid = (select uuid());
-INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('deepflow_system', 'deepflow_system', 0, 3*24, @lcuuid);
+    SELECT COUNT(*) INTO existing_name
+    FROM data_source
+    WHERE name = 'flow_log.l4_packet';
 
-UPDATE db_version SET version = '6.2.1.17';
+    IF existing_name = 0 THEN
+        START TRANSACTION;
 
-COMMIT;
+        UPDATE data_source SET retention_time = retention_time * 24;
+        UPDATE data_source SET name="flow_log.l4_flow_log", tsdb_type="flow_log.l4_flow_log" WHERE name="flow_log.l4";
+        UPDATE data_source SET name="flow_log.l7_flow_log", tsdb_type="flow_log.l7_flow_log" WHERE name="flow_log.l7";
+
+        SET @lcuuid = (SELECT UUID());
+        INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('flow_log.l4_packet', 'flow_log.l4_packet', 0, 3*24, @lcuuid);
+        SET @lcuuid = (SELECT UUID());
+        INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('flow_log.l7_packet', 'flow_log.l7_packet', 0, 3*24, @lcuuid);
+        SET @lcuuid = (SELECT UUID());
+        INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('deepflow_system', 'deepflow_system', 0, 3*24, @lcuuid);
+
+        UPDATE db_version SET version = '6.2.1.17';
+
+        COMMIT;
+    END IF;
+END $$
+
+DELIMITER ;
+
+CALL update_data_sources();
