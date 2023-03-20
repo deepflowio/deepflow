be2da262df06ae376f019e5e3ed352f7228be78c
weiqiang <weiqiang@yunshan.net>
2023-03-20 14:59:46 +0800

    [Server] modify data source #20523

---
+++ server/controller/db/mysql/migration/rawsql/issu/6.1.8.14.sql
@@ -0,0 +1,16 @@
+START TRANSACTION;
+
+UPDATE data_source SET retention_time = retention_time * 24;
+UPDATE data_source SET name="flow_log.l4_flow_log", tsdb_type="flow_log.l4_flow_log" WHERE name="flow_log.l4";
+UPDATE data_source SET name="flow_log.l7_flow_log", tsdb_type="flow_log.l7_flow_log" WHERE name="flow_log.l7";
+
+set @lcuuid = (select uuid());
+INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('flow_log.l4_packet', 'flow_log.l4_packet', 0, 3*24, @lcuuid);
+set @lcuuid = (select uuid());
+INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('flow_log.l7_packet', 'flow_log.l7_packet', 0, 3*24, @lcuuid);
+set @lcuuid = (select uuid());
+INSERT INTO data_source (name, tsdb_type, `interval`, retention_time, lcuuid) VALUES ('deepflow_system', 'deepflow_system', 0, 7*24, @lcuuid);
+
+UPDATE db_version SET version = '6.1.8.14';
+
+COMMIT;
