39460498bb3e5bde4be55bf3cf5d3d859650ff79
LIU Chao <42240939+xiaochaoren1@users.noreply.github.com>
2023-03-21 18:54:44 +0800

    Revert "[querier] show tags supprot tap_port_resource"
    This reverts commit 3a2b22a3504e568ae2cf54f220c39a873eb06dae.

--- server/querier/engine/clickhouse/tag/translation.go
+++ server/querier/engine/clickhouse/tag/translation.go
@@ -35,11 +35,6 @@ var DEVICE_MAP = map[string]int{
 	"lb":          VIF_DEVICE_TYPE_LB,
 	"natgw":       VIF_DEVICE_TYPE_NAT_GATEWAY,
 }
-var TAP_PORT_DEVICE_MAP = map[string]int{
-	common.TAP_PORT_HOST:     VIF_DEVICE_TYPE_HOST,
-	common.TAP_PORT_CHOST:    VIF_DEVICE_TYPE_VM,
-	common.TAP_PORT_POD_NODE: VIF_DEVICE_TYPE_POD_NODE,
-}
 
 func GenerateTagResoureMap() map[string]map[string]*Tag {
 	tagResourceMap := make(map[string]map[string]*Tag)
@@ -917,15 +912,5 @@ func GenerateTagResoureMap() map[string]map[string]*Tag {
 			"",
 			"",
 		)}
-	for tapPortResource, deviceTypeValue := range TAP_PORT_DEVICE_MAP {
-		deviceTypeValueStr := strconv.Itoa(deviceTypeValue)
-		tagResourceMap[tapPortResource] = map[string]*Tag{
-			"default": NewTag(
-				"",
-				"",
-				"(toUInt64(vtap_id),toUInt64(tap_port)) IN (SELECT vtap_id,tap_port FROM flow_tag.vtap_port_map WHERE tap_port!=0 AND device_type="+deviceTypeValueStr+" AND device_name %s %s)",
-				"(toUInt64(vtap_id),toUInt64(tap_port)) IN (SELECT vtap_id,tap_port FROM flow_tag.vtap_port_map WHERE tap_port!=0 AND device_type="+deviceTypeValueStr+" AND %s(device_name,%s))",
-			)}
-	}
 	return tagResourceMap
 }
