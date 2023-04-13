fd14a02e5d3be17fbf4a782d03cc59247bec3e76
JIN Jie <jinjie@yunshan.net>
2023-04-13 09:57:13 +0800

    [AGENT] Fix bug in #2872

--- agent/crates/public/src/utils/net/linux.rs
+++ agent/crates/public/src/utils/net/linux.rs
@@ -349,11 +349,13 @@ pub fn links_by_name_regex<S: AsRef<str>>(regex: S) -> Result<Vec<Link>> {
         .filter(|link| {
             if !link.flags.contains(LinkFlags::LOOPBACK) {
                 // filter zero mac
-                if link.mac_addr == MacAddr::ZERO && regex.is_match(&link.name) {
-                    warn!(
-                        "link {} has invalid mac address {}",
-                        link.name, link.mac_addr
-                    );
+                if link.mac_addr == MacAddr::ZERO {
+                    if regex.is_match(&link.name) {
+                        warn!(
+                            "link {} has invalid mac address {}",
+                            link.name, link.mac_addr
+                        );
+                    }
                     return false;
                 }
             }
