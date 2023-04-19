042c6007a776c0f63b5dbe23f8a996b0b7201436
zhuofeng <zhuofeng@yunshan.net.cn>
2023-04-19 22:05:35 +0800

    [server] updata OTLP kafka,mqtt info
    and fix TraceID,SpanID,ParentSpanID fill wrong

--- server/ingester/flow_log/exporter/otlp_test.go
+++ server/ingester/flow_log/exporter/otlp_test.go
@@ -2,48 +2,49 @@ package exporter
 
 import "testing"
 
-func TestGetSQLSpanName(t *testing.T) {
+func TestGetSQLSpanNameAndOperation(t *testing.T) {
 	cases := []struct {
 		sql      string
-		expected string
+		expected [2]string
 	}{
 		{
 			sql:      "select * FROM db.users;",
-			expected: "select db.users",
+			expected: [2]string{"select db.users", "select"},
 		},
 		{
 			sql:      "INSERT into users (id, name) VALUES (1, 'Alice')",
-			expected: "INSERT users",
+			expected: [2]string{"INSERT users", "INSERT"},
 		},
 		{
 			sql:      "UPDATE users SET name='Bob' WHERE id=1",
-			expected: "UPDATE users",
+			expected: [2]string{"UPDATE users", "UPDATE"},
 		},
 		{
 			sql:      "CREATE TABLE users(id INT, name TEXT)",
-			expected: "CREATE users",
+			expected: [2]string{"CREATE users", "CREATE"},
 		},
 		{
 			sql:      "CREATE DATABASE if not exists userdb",
-			expected: "CREATE userdb",
+			expected: [2]string{"CREATE userdb", "CREATE"},
 		},
 		{
 			sql:      "ALTER TABLE users ADD COLUMN email TEXT",
-			expected: "ALTER users",
+			expected: [2]string{"ALTER users", "ALTER"},
 		},
 		{
 			sql:      "DROP TABLE IF EXISTS users",
-			expected: "DROP users",
+			expected: [2]string{"DROP users", "DROP"},
 		},
 		{
 			sql:      "",
-			expected: "unknow",
+			expected: [2]string{"unknow", ""},
 		},
 	}
 	for _, c := range cases {
-		got := getSQLSpanName(c.sql)
+		spanName, operation := getSQLSpanNameAndOperation(c.sql)
+		got := [2]string{spanName, operation}
 		if got != c.expected {
-			t.Errorf("getSQLSpanName(%q) == %q, expected %q", c.sql, got, c.expected)
+			t.Errorf("getSQLSpanNameAndOperation(%q) == %q, expected %q", c.sql, got, c.expected)
 		}
 	}
 }
