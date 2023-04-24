c90fcddd2ac08d5e07e107078a4687d377855020
JIN Jie <jinjie@yunshan.net>
2023-04-24 17:12:38 +0800

    [AGENT] Fix panic in ipv6 environment

--- agent/src/policy/labeler.rs
+++ agent/src/policy/labeler.rs
@@ -15,7 +15,7 @@
  */
 
 use std::collections::HashMap;
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::sync::{Arc, RwLock};
 
 use super::bit::count_trailing_zeros32;
@@ -103,7 +103,11 @@ impl Labeler {
                 }
             }
         }
-        return Ipv4Addr::UNSPECIFIED.into();
+        if is_ipv6 {
+            Ipv6Addr::UNSPECIFIED.into()
+        } else {
+            Ipv4Addr::UNSPECIFIED.into()
+        }
     }
 
     fn get_interface_by_mac(&self, mac: u64) -> Option<PlatformData> {
