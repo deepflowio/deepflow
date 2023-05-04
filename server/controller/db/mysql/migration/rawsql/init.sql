016b55e49b958f45d6f4c6cb1098a76fcb620e1c
Ericsssss <lvqiao@yunshan.net>
2023-05-04 21:27:20 +0800

    [Controller] modify process_start alarm policy #20910
     - run automation test (basic & upgrade) pass

--- server/controller/db/mysql/migration/rawsql/init.sql
+++ server/controller/db/mysql/migration/rawsql/init.sql
@@ -1256,8 +1256,8 @@ set @lcuuid = (select uuid());
 INSERT INTO alarm_policy(user_id, sub_view_type, sub_view_url, sub_view_params, sub_view_metrics, name, level, state,
     app_type, sub_type, contrast_type, target_line_uid, target_line_name, target_field, agg,
     upper_threshold, lower_threshold, lcuuid)
-    values(1, 1, "/v1/stats/querier/UniversalHistory", "{\"DATABASE\":\"deepflow_system\",\"TABLE\":\"deepflow_agent_monitor\",\"include_history\":\"true\",\"interval\":60,\"fill\":0,\"window_size\":5,\"QUERIES\":[{\"QUERY_ID\":\"R1\",\"SELECT\":\"Spread(`metrics.create_time`) AS `process_start`\",\"WHERE\":\"1=1\",\"GROUP_BY\":\"`tag.host`\",\"METRICS\":[\"Spread(`metrics.create_time`) AS `process_start`\"]}]}",
-    "[{\"METRIC_LABEL\":\"process_start\",\"return_field_description\":\"最近 5 分钟进程启动时间变化\",\"unit\":\" 毫秒\"}]", "进程启动",  0, 1, 1, 20, 1, "", "", "process_start", 1, 1, NULL, @lcuuid);
+    values(1, 1, "/v1/stats/querier/UniversalPromHistory", "{\"PROM_SQL\":\"delta(min(deepflow_system__deepflow_agent_monitor__create_time)by(host)[1m:])\",\"interval\":60,\"metric\":\"process_start\",\"time_tag\":\"toi\"}",
+    "[{\"METRIC_LABEL\":\"process_start\",\"return_field_description\":\"最近 1 分钟进程启动时间变化\",\"unit\":\" 毫秒\"}]", "进程启动",  0, 1, 1, 20, 1, "", "", "process_start", 1, 1, NULL, @lcuuid);
 
 set @lcuuid = (select uuid());
 INSERT INTO alarm_policy(sub_view_type, sub_view_url, sub_view_params, sub_view_metrics, name, level, state,
