8a3c064695be8465d81458ccf503e990e93deb84
weiqiang <weiqiang@yunshan.net>
2023-05-11 17:55:00 +0800

    [Server] Fix retention time limit of data source #21033

--- server/controller/config/config.go
+++ server/controller/config/config.go
@@ -47,7 +47,7 @@ type Specification struct {
 	VTapMaxPerGroup            int `default:"10000" yaml:"vtap_max_per_group"`
 	AZMaxPerServer             int `default:"10" yaml:"az_max_per_server"`
 	DataSourceMax              int `default:"15" yaml:"data_source_max"`
-	DataSourceRetentionTimeMax int `default:"1000" yaml:"data_source_retention_time_max"`
+	DataSourceRetentionTimeMax int `default:"24000" yaml:"data_source_retention_time_max"`
 }
 
 type DFWebService struct {
