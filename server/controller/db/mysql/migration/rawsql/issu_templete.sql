e2366cf775885586a3c4e7f834bcd592fc322792
zhengya <zhengya@yunshan.net.cn>
2023-06-21 14:42:44 +0800

    [CONTROLLER/DB] updates sql example

--- server/controller/db/mysql/migration/rawsql/issu_templete.sql
+++ server/controller/db/mysql/migration/rawsql/issu_templete.sql
@@ -1,7 +1,7 @@
 -- This templete is for upgrade using CREATE/DROP/ALTER
 -- modify start, add upgrade sql
 -- example
-INSERT INTO epc (name) VALUE ("example");
+ALTER TABLE go_genesis_ip ADD node_ip CHAR(48) DEFAULT NULL;
 -- update db_version to latest, remeber update DB_VERSION_EXPECTED in migration/version.go
 UPDATE db_version SET version='6.1.1.0';
 -- modify end
