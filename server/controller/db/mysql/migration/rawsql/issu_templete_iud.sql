f892002dcd9c2a74abd038041c68c51a7743d28a
zhengya <zhengya@yunshan.net.cn>
2023-06-26 11:21:55 +0800

    [CONTROLLER/DB] update template comments

--- server/controller/db/mysql/migration/rawsql/issu_templete_iud.sql
+++ server/controller/db/mysql/migration/rawsql/issu_templete_iud.sql
@@ -1,10 +1,12 @@
--- This templete is for upgrade using INSERT/UPDATE/DELETE
+-- This template is for upgrade using INSERT/UPDATE/DELETE
+-- Tractions are needed for these commands to avoid manual rollback if error occurs.
+
 START TRANSACTION;
 
 -- modify start, add upgrade sql
 -- example
 INSERT INTO epc (name) VALUE ("example");
--- update db_version to latest, remeber update DB_VERSION_EXPECTED in migration/version.go
+-- update db_version to latest, remember update DB_VERSION_EXPECTED in migration/version.go
 UPDATE db_version SET version='6.1.1.0';
 -- modify end
 
