06d1ddf4409144871658195331e36ae99c5a7d36
zhuofeng <zhuofeng@yunshan.net.cn>
2023-06-27 18:12:04 +0800

    [ingester] support OTel export sw8 traceid

--- server/ingester/flow_log/exporter/otlp.go
+++ server/ingester/flow_log/exporter/otlp.go
@@ -139,6 +139,7 @@ func L7FlowLogToExportRequest(l7 *log_data.L7FlowLog, universalTagsManager *Univ
 		putIntWithoutZero(spanAttrs, "df.span.syscall_thread_1", int64(l7.SyscallThread1))
 		putIntWithoutZero(spanAttrs, "df.span.syscall_cap_seq_0", int64(l7.SyscallCapSeq0))
 		putIntWithoutZero(spanAttrs, "df.span.syscall_cap_seq_1", int64(l7.SyscallCapSeq1))
+		putStrWithoutEmpty(spanAttrs, "df.span.native.trace_id", l7.TraceId)
 
 		span.SetTraceID(getTraceID(l7.TraceId, l7.ID()))
 		span.SetSpanID(getSpanID(l7.SpanId, l7.ID()))
@@ -278,7 +279,8 @@ func getTraceID(traceID string, id uint64) pcommon.TraceID {
 		copy(id[:], traceId)
 		return pcommon.TraceID(id)
 	}
-	return pcommon.NewTraceIDEmpty()
+
+	return swTraceIDToTraceID(traceID)
 }
 
 func getSpanID(spanID string, id uint64) pcommon.SpanID {
