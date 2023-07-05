12f01b5aa1cbe277cec69daa8659d930a6f36cab
yuanchao <yuanchao@yunshan.net>
2023-07-05 12:03:43 +0800

    [Agent] Watcher components delete poller

--- agent/src/trident.rs
+++ agent/src/trident.rs
@@ -881,9 +881,6 @@ pub enum Components {
 #[cfg(target_os = "linux")]
 pub struct WatcherComponents {
     pub api_watcher: Arc<ApiWatcher>,
-    pub libvirt_xml_extractor: Arc<LibvirtXmlExtractor>, // FIXME: Delete this component
-    pub platform_synchronizer: Arc<PlatformSynchronizer>,
-    pub kubernetes_poller: Arc<GenericPoller>,
     pub domain_name_listener: DomainNameListener,
     pub running: AtomicBool,
     tap_mode: TapMode,
@@ -899,8 +896,6 @@ impl WatcherComponents {
         session: &Arc<Session>,
         synchronizer: &Arc<Synchronizer>,
         exception_handler: ExceptionHandler,
-        libvirt_xml_extractor: Arc<LibvirtXmlExtractor>,
-        platform_synchronizer: Arc<PlatformSynchronizer>,
         agent_mode: RunningMode,
         runtime: Arc<Runtime>,
     ) -> Result<Self> {
@@ -918,24 +913,11 @@ impl WatcherComponents {
             config_handler.static_config.controller_domain_name.clone(),
             config_handler.static_config.controller_ips.clone(),
         );
-        let kubernetes_poller = Arc::new(GenericPoller::new(
-            config_handler.static_config.controller_ips[0].parse()?,
-            config_handler.platform(),
-            config_handler
-                .candidate_config
-                .dispatcher
-                .extra_netns_regex
-                .clone(),
-        ));
-        platform_synchronizer.set_kubernetes_poller(kubernetes_poller.clone());
 
         info!("With ONLY_WATCH_K8S_RESOURCE and IN_CONTAINER environment variables set, the agent will only watch K8s resource");
         Ok(WatcherComponents {
             api_watcher,
-            platform_synchronizer,
-            kubernetes_poller,
             domain_name_listener,
-            libvirt_xml_extractor,
             running: AtomicBool::new(false),
             tap_mode: candidate_config.tap_mode,
             agent_mode,
@@ -951,7 +933,6 @@ impl WatcherComponents {
         if matches!(self.agent_mode, RunningMode::Managed) {
             self.api_watcher.start();
         }
-        self.kubernetes_poller.start();
         self.domain_name_listener.start();
         info!("Started watcher components.");
     }
@@ -961,7 +942,6 @@ impl WatcherComponents {
             return;
         }
         self.api_watcher.stop();
-        self.kubernetes_poller.stop();
         self.domain_name_listener.stop();
         info!("Stopped watcher components.")
     }
@@ -2381,8 +2361,6 @@ impl Components {
                 session,
                 synchronizer,
                 exception_handler,
-                libvirt_xml_extractor,
-                platform_synchronizer,
                 agent_mode,
                 runtime,
             )?;
