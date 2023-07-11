3ef52e0b754fe8bfbccef071cb8be82c79c85663
kangxiang <kangxiang@yunshan.net>
2023-07-11 16:47:48 +0800

    [Controller] vinterface add vtap id

--- server/controller/db/mysql/migration/rawsql/issu/6.3.1.23.sql
+++ server/controller/db/mysql/migration/rawsql/issu/6.3.1.23.sql
@@ -1,3 +1,6 @@
 ALTER TABLE vinterface ADD COLUMN vtap_id INTEGER DEFAULT 0 AFTER netns_id;
+ALTER TABLE vinterface ADD COLUMN vtap_id INTEGER AFTER netns_id;
+
+UPDATE db_version SET version='6.3.1.23';
 
 UPDATE db_version SET version='6.3.1.23';
