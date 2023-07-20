0f322deed7bbd1de6e50c93c2309562e6d4827c7
Jiping Yin <jiping@yunshan.net>
2023-07-20 15:28:15 +0800

    [eBPF] Memory optimization for the profiler
    Using regular expressions, we match process names to establish symbol
    table caching for specific processes, rather than enabling symbol ca-
    ching for all processes. This approach aims to reduce memory usage.
    
    For example:
    
    `^(java|nginx|profiler|telegraf|mysqld|socket_tracer|.*deepflow.*)$`
    
    This regex pattern matches the process names: "java", "nginx", "profiler",
    "telegraf", "mysqld", "socket_tracer", and any process containing the
    substring "deepflow".
    
    You can modify the regular expression according to your needs using the
    `set_profiler_regex()` interface.
    By using this interface, you can customize the regular expression to match
    specific process names that you want to establish symbol table caching for,
    providing flexibility and control over which processes will have symbol caching
    enabled. This allows you to fine-tune the memory usage and profiling behavior
    of the profiler according to your requirements.
    
    The default expression is empty (''), indicating that no profile data will be generated.

--- agent/src/ebpf/user/table.c
+++ agent/src/ebpf/user/table.c
@@ -40,9 +40,6 @@ bool bpf_table_get_value(struct bpf_tracer *tracer,
 	int map_fd = map->fd;
 
 	if ((bpf_lookup_elem(map_fd, &key, val_buf)) != 0) {
-		ebpf_warning("[%s] bpf_map_lookup_elem, err tb_name:%s, key : %"
-			     PRIu64 ", err_message:%s\n", __func__, tb_name, key,
-			     strerror(errno));
 		return false;
 	}
 
