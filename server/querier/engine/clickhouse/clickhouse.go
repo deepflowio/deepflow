d2937bcb88ddf0cd660f570ee3a8c898e0b414a0
王家璇 <jiaxuan@yunshan.net>
2023-07-20 10:23:38 +0800

    [querier] fix metric '*' add error - run automation test (querier_sql) pass

--- server/querier/engine/clickhouse/clickhouse.go
+++ server/querier/engine/clickhouse/clickhouse.go
@@ -1322,7 +1322,7 @@ func LoadDbDescriptions(dbDescriptions map[string]interface{}) error {
 				if err != nil {
 					return err
 				}
-				loadMetrics["*"] = metrics.NewCountAllMertic(table)
+				loadMetrics["*"] = metrics.NewCountAllMertic(table, len(loadMetrics))
 				err = metrics.MergeMetrics(db, table, loadMetrics)
 				if err != nil {
 					return err
--- server/querier/engine/clickhouse/metrics/metrics.go
+++ server/querier/engine/clickhouse/metrics/metrics.go
@@ -91,18 +91,18 @@ func NewReplaceMetrics(dbField string, condition string) *Metrics {
 	}
 }
 
-func NewCountAllMertic(table string) *Metrics {
+func NewCountAllMertic(table string, index int) *Metrics {
 	return NewMetrics(
-		0, "*",
+		index, "*",
 		"*", "", METRICS_TYPE_OTHER,
-		"metrics", []bool{true, true, true}, "", table, "",
+		"Other", []bool{true, true, true}, "", table, "",
 	)
 }
 
 func GetMetrics(field string, db string, table string, ctx context.Context) (*Metrics, bool) {
 	field = strings.Trim(field, "`")
 	if field == "*" {
-		return NewCountAllMertic(table), true
+		return NewCountAllMertic(table, 0), true
 	} else if db == "ext_metrics" || db == "deepflow_system" || table == "l7_flow_log" {
 		fieldSplit := strings.Split(field, ".")
 		if len(fieldSplit) > 1 {
@@ -194,7 +194,7 @@ func GetMetricsByDBTable(db string, table string, where string, ctx context.Cont
 					metrics[k] = v
 				}
 			}
-			loadsLen := len(loads) - 1
+			loadsLen := len(loads)
 			for k, v := range exts {
 				if _, ok := metrics[k]; !ok {
 					v.Index += loadsLen
@@ -202,7 +202,7 @@ func GetMetricsByDBTable(db string, table string, where string, ctx context.Cont
 				}
 			}
 			metrics["metrics"] = NewMetrics(
-				len(metrics)-1, "metrics",
+				len(metrics), "metrics",
 				"metrics", "", METRICS_TYPE_ARRAY,
 				"metrics", []bool{true, true, true}, "", table, "",
 			)
@@ -255,9 +255,6 @@ func GetMetricsDescriptionsByDBTable(db string, table string, where string, ctx 
 	for field, metrics := range allMetrics {
 		if db == "ext_metrics" || db == ckcommon.DB_NAME_PROMETHEUS || db == "deepflow_system" || (table == "l7_flow_log" && strings.Contains(field, "metrics.")) {
 			field = metrics.DisplayName
-		}
-		if field == "*" {
-			metrics.Index = len(allMetrics) - 1
 		}
 		values[metrics.Index] = []interface{}{
 			field, metrics.IsAgg, metrics.DisplayName, metrics.Unit, metrics.Type,
