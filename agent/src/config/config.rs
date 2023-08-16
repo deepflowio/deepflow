3c52fd05a69432b7f86946b20e86daf0bf46dd81
huanchao <huanchao@yunshan.net>
2023-08-16 17:19:58 +0800

    [Agent] Change Prometheus http api addresses

--- agent/src/config/config.rs
+++ agent/src/config/config.rs
@@ -1085,7 +1085,7 @@ pub struct RuntimeConfig {
     pub external_agent_http_proxy_port: u16,
     #[serde(skip)]
     pub tap_mode: TapMode,
-    pub prometheus_http_api_address: String,
+    pub prometheus_http_api_addresses: Vec<String>,
     // TODO: expand and remove
     #[serde(rename = "static_config")]
     pub yaml_config: YamlConfig,
@@ -1187,7 +1187,7 @@ impl RuntimeConfig {
             external_agent_http_proxy_port: 38086,
             tap_mode: TapMode::Local,
             yaml_config: YamlConfig::load("", TapMode::Local).unwrap(), // Default configuration that needs to be corrected to be available
-            prometheus_http_api_address: "".into(),
+            prometheus_http_api_addresses: vec![],
         }
     }
 
@@ -1413,7 +1413,7 @@ impl TryFrom<trident::Config> for RuntimeConfig {
             external_agent_http_proxy_enabled: conf.external_agent_http_proxy_enabled(),
             external_agent_http_proxy_port: conf.external_agent_http_proxy_port() as u16,
             tap_mode: conf.tap_mode(),
-            prometheus_http_api_address: conf.prometheus_http_api_address().to_owned(),
+            prometheus_http_api_addresses: conf.prometheus_http_api_addresses.to_owned(),
             yaml_config: YamlConfig::load(conf.local_config(), conf.tap_mode())?,
         };
         rc.validate()
