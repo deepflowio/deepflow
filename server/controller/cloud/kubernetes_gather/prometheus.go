6f08d8978490214be890e0d4a8dc12214fd73e16
kangxiang <kangxiang@yunshan.net>
2023-08-25 11:26:13 +0800

    [Controller] update prometheus targets

---
+++ server/controller/cloud/kubernetes_gather/prometheus.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023 Yunshan Networks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kubernetes_gather
+
+import (
+	"github.com/deepflowio/deepflow/server/controller/cloud/model"
+	cloudmodel "github.com/deepflowio/deepflow/server/controller/cloud/model"
+	"github.com/deepflowio/deepflow/server/controller/common"
+	"github.com/deepflowio/deepflow/server/controller/genesis"
+	uuid "github.com/satori/go.uuid"
+)
+
+func (k *KubernetesGather) getPrometheusTargets() ([]model.PrometheusTarget, error) {
+	log.Debug("get prometheus target starting")
+	var prometheusTargets []model.PrometheusTarget
+	pTargets, err := genesis.GenesisService.GetPrometheusResponse(k.ClusterID)
+	if err != nil {
+		// TODO: 可能会因为采集器版本的问题阻塞正常对接，暂时只记录问题，后续调整为和k8s info相同的处理方式
+		log.Debug(err.Error())
+	}
+	for _, p := range pTargets {
+		var otherLabelsString string
+		if !p.HonorLabelsConfig {
+			otherLabelsString = p.OtherLabels
+		}
+		prometheusTargets = append(prometheusTargets, cloudmodel.PrometheusTarget{
+			Lcuuid:      common.GetUUID(p.Instance+p.Job+otherLabelsString, uuid.Nil),
+			Job:         p.Job,
+			Instance:    p.Instance,
+			ScrapeURL:   p.ScrapeURL,
+			OtherLabels: otherLabelsString,
+		})
+	}
+	log.Debug("get prometheus target complete")
+	return prometheusTargets, nil
+}
