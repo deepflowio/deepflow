583c9bfac085ed69142e5d4816843aef577f0476
weiqiang <weiqiang@yunshan.net>
2023-08-28 17:35:35 +0800

    [Server/monitor] add type check to monitor

--- server/controller/trisolaris/utils/helper.go
+++ server/controller/trisolaris/utils/helper.go
@@ -25,6 +25,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/deepflowio/deepflow/server/controller/common"
 	"github.com/vishvananda/netlink"
 )
 
@@ -127,4 +128,11 @@ func Lookup(host net.IP) (net.IP, error) {
 		src = route.Src.To4()
 	}
 	return src, nil
+}
+
+func IsVMofBMHtype(htype int) bool {
+	if Find[int]([]int{common.VM_HTYPE_BM_C, common.VM_HTYPE_BM_N, common.VM_HTYPE_BM_S}, htype) == true {
+		return true
+	}
+	return false
 }
