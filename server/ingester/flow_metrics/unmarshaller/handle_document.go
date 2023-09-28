1237433af106518390441d5c9d4b6d6e1fd43082
zhuofeng <zhuofeng@yunshan.net.cn>
2023-09-28 10:03:46 +0800

    [ingester] auto service type support pod group type

--- server/ingester/flow_metrics/unmarshaller/handle_document.go
+++ server/ingester/flow_metrics/unmarshaller/handle_document.go
@@ -132,6 +132,7 @@ func DocumentExpand(doc *app.Document, platformData *grpc.PlatformInfoTable) err
 		t.Code |= MainAddCode
 	}
 
+	podGroupType, podGroupType1 := uint8(0), uint8(0)
 	if info1 != nil {
 		t.RegionID1 = uint16(info1.RegionID)
 		t.HostID1 = uint16(info1.HostID)
@@ -142,6 +143,7 @@ func DocumentExpand(doc *app.Document, platformData *grpc.PlatformInfoTable) err
 		t.PodNSID1 = uint16(info1.PodNSID)
 		t.AZID1 = uint16(info1.AZID)
 		t.PodGroupID1 = info1.PodGroupID
+		podGroupType1 = info1.PodGroupType
 		t.PodID1 = info1.PodID
 		t.PodClusterID1 = uint16(info1.PodClusterID)
 		if common.IsPodServiceIP(t.L3DeviceType1, t.PodID1, t.PodNodeID1) {
@@ -170,7 +172,7 @@ func DocumentExpand(doc *app.Document, platformData *grpc.PlatformInfoTable) err
 		}
 	}
 	t.AutoInstanceID1, t.AutoInstanceType1 = common.GetAutoInstance(t.PodID1, t.GPID1, t.PodNodeID1, t.L3DeviceID1, uint8(t.L3DeviceType1), t.L3EpcID1)
-	t.AutoServiceID1, t.AutoServiceType1 = common.GetAutoService(t.ServiceID1, t.PodGroupID1, t.GPID1, t.PodNodeID1, t.L3DeviceID1, uint8(t.L3DeviceType1), t.L3EpcID1)
+	t.AutoServiceID1, t.AutoServiceType1 = common.GetAutoService(t.ServiceID1, t.PodGroupID1, t.GPID1, t.PodNodeID1, t.L3DeviceID1, uint8(t.L3DeviceType1), podGroupType1, t.L3EpcID1)
 
 	if info != nil {
 		t.RegionID = uint16(info.RegionID)
@@ -182,6 +184,7 @@ func DocumentExpand(doc *app.Document, platformData *grpc.PlatformInfoTable) err
 		t.PodNSID = uint16(info.PodNSID)
 		t.AZID = uint16(info.AZID)
 		t.PodGroupID = info.PodGroupID
+		podGroupType = info.PodGroupType
 		t.PodID = info.PodID
 		t.PodClusterID = uint16(info.PodClusterID)
 		if common.IsPodServiceIP(t.L3DeviceType, t.PodID, t.PodNodeID) {
@@ -224,7 +227,7 @@ func DocumentExpand(doc *app.Document, platformData *grpc.PlatformInfoTable) err
 		}
 	}
 	t.AutoInstanceID, t.AutoInstanceType = common.GetAutoInstance(t.PodID, t.GPID, t.PodNodeID, t.L3DeviceID, uint8(t.L3DeviceType), t.L3EpcID)
-	t.AutoServiceID, t.AutoServiceType = common.GetAutoService(t.ServiceID, t.PodGroupID, t.GPID, t.PodNodeID, t.L3DeviceID, uint8(t.L3DeviceType), t.L3EpcID)
+	t.AutoServiceID, t.AutoServiceType = common.GetAutoService(t.ServiceID, t.PodGroupID, t.GPID, t.PodNodeID, t.L3DeviceID, uint8(t.L3DeviceType), podGroupType, t.L3EpcID)
 
 	if t.SignalSource == SIGNAL_SOURCE_OTEL {
 		// only show OTel data for services as 'server side'
