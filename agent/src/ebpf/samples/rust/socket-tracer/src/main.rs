6908eb08c966a096bb2db3756822cb72946936ab
Jiping Yin <jiping@yunshan.net>
2023-10-09 16:48:09 +0800

    [eBPF] Update sample output

--- agent/src/ebpf/samples/rust/socket-tracer/src/main.rs
+++ agent/src/ebpf/samples/rust/socket-tracer/src/main.rs
@@ -19,6 +19,7 @@ use chrono::FixedOffset;
 use chrono::Utc;
 use socket_tracer::ebpf::*;
 use std::convert::TryInto;
+use std::env::set_var;
 use std::fmt::Write;
 use std::net::IpAddr;
 use std::sync::Mutex;
@@ -331,8 +332,11 @@ fn get_counter(counter_type: u32) -> u32 {
 }
 
 fn main() {
-    //let log_file = CString::new("/var/log/deepflow-ebpf.log".as_bytes()).unwrap();
-    //let log_file_c = log_file.as_c_str();
+    set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let log_file = CString::new("/data/deepflow-ebpf.log".as_bytes()).unwrap();
+    let log_file_c = log_file.as_c_str();
     unsafe {
         enable_ebpf_protocol(SOCK_DATA_HTTP1 as c_int);
         enable_ebpf_protocol(SOCK_DATA_HTTP2 as c_int);
@@ -347,7 +351,7 @@ fn main() {
         enable_ebpf_protocol(SOCK_DATA_KAFKA as c_int);
         enable_ebpf_protocol(SOCK_DATA_MQTT as c_int);
         enable_ebpf_protocol(SOCK_DATA_DNS as c_int);
-	enable_ebpf_protocol(SOCK_DATA_MONGO as c_int);
+        enable_ebpf_protocol(SOCK_DATA_MONGO as c_int);
 
        // set_feature_regex(
        //     FEATURE_UPROBE_OPENSSL,
@@ -379,8 +383,8 @@ fn main() {
 
         // The first parameter passed by a null pointer can be
         // filled with std::ptr::null()
-        if bpf_tracer_init(std::ptr::null(), true) != 0 {
-            println!("bpf_tracer_init() error");
+        if bpf_tracer_init(log_file_c.as_ptr(), true) != 0 {
+            println!("bpf_tracer_init() file:{:?} error", log_file);
             ::std::process::exit(1);
         }
         /*
