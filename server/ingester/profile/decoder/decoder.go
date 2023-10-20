3187602a88b83e6459c2b44378068b1ac82b8e39
taloric <jiawei@yunshan.net>
2023-10-21 00:08:22 +0800

    [ingester] modify container id type

--- server/ingester/profile/decoder/decoder.go
+++ server/ingester/profile/decoder/decoder.go
@@ -164,7 +164,7 @@ func (d *Decoder) handleProfileData(vtapID uint16, decoder *codec.SimpleDecoder)
 			platformData:    d.platformData,
 			IP:              make([]byte, len(profile.Ip)),
 			netNsID:         profile.NetnsId,
-			containerID:     string(profile.ContainerId),
+			containerID:     profile.ContainerId,
 			compressionAlgo: d.compressionAlgo,
 			observer:        &observer{},
 			Counter:         d.counter,
