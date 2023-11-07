be855cff7ed394888314bb1c77909d0110b37a3e
Jiping Yin <jiping@yunshan.net>
2023-11-07 18:11:23 +0800

    [eBPF] Remove PROTO_TLS_HTTP1 and PROTO_TLS_HTTP2

--- agent/src/ebpf/user/socket.h
+++ agent/src/ebpf/user/socket.h
@@ -199,10 +199,6 @@ static inline char *get_proto_name(uint16_t proto_id)
 		return "HTTP1";
 	case PROTO_HTTP2:
 		return "HTTP2";
-	case PROTO_TLS_HTTP1:
-		return "TLS_HTTP1";
-	case PROTO_TLS_HTTP2:
-		return "TLS_HTTP2";
 	case PROTO_MYSQL:
 		return "MySQL";
 	case PROTO_DNS:
@@ -223,6 +219,8 @@ static inline char *get_proto_name(uint16_t proto_id)
 		return "FastCGI";
 	case PROTO_MONGO:
 		return "MongoDB";
+	case PROTO_TLS:
+		return "TLS";
 	default:
 		return "Unknown";
 	}
