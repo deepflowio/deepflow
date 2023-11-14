48e8b0f50a4bf2868a3d2e2368ef54fa5f3995c4
kangxiang <kangxiang@yunshan.net>
2023-11-14 17:24:25 +0800

    [Controller] optimize log alerts

--- server/controller/cloud/cloud.go
+++ server/controller/cloud/cloud.go
@@ -391,13 +391,15 @@ func getContentFromAdditionalResource(domainUUID string) (*mysql.DomainAdditiona
 func (c *Cloud) appendCloudTags(resource model.Resource, additionalResource model.AdditionalResource) model.Resource {
 	chostCloudTags := additionalResource.CHostCloudTags
 	for i, chost := range resource.VMs {
-		if len(chost.CloudTags) != 0 {
-			log.Infof("vm (%s) already tags (%v), do not need to add it", chost.Name, chost.CloudTags)
+		value, ok := chostCloudTags[chost.Lcuuid]
+		if !ok {
 			continue
 		}
-		if value, ok := chostCloudTags[chost.Lcuuid]; ok {
-			resource.VMs[i].CloudTags = value
+		if len(chost.CloudTags) != 0 {
+			log.Infof("vm (%s) already tags (%v), do not need to add (%v)", chost.Name, chost.CloudTags, value)
+			continue
 		}
+		resource.VMs[i].CloudTags = value
 	}
 	podNamespaceCloudTags := additionalResource.PodNamespaceCloudTags
 	for i, podNamespace := range resource.PodNamespaces {
