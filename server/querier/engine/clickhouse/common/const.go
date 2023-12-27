1f1e3d624dbb5cc023c5143b566473098e8526f0
Ericsssss <lvqiao@yunshan.net>
2023-12-27 12:43:20 +0800

    [Querier] modify showtagvalues filter condition #23229
     - modify alarm_policy (flow_map.metrics.drop_by_window)
     - run automation test(basic, querier & upgrade) pass

--- server/querier/engine/clickhouse/common/const.go
+++ server/querier/engine/clickhouse/common/const.go
@@ -36,3 +36,13 @@ var DB_TABLE_MAP = map[string][]string{
 	DB_NAME_PROFILE:         []string{"in_process"},
 	DB_NAME_PROMETHEUS:      []string{"samples"},
 }
+
+var SHOW_TAG_VALUE_MAP = map[string][]string{
+	"ip_resource_map": []string{"ip", "subnet", "region", "az", "host", "chost", "vpc", "router", "dhcpgw", "lb", "lb_listener", "natgw", "redis", "rds", "pod_cluster", "pod_ns", "pod_node", "pod_ingress", "pod_service", "pod_group", "pod"},
+	"pod_ns_map":      []string{"pod_ns", "pod_cluster"},
+	"pod_group_map":   []string{"pod_group", "pod_cluster", "pod_ns"},
+	"pod_service_map": []string{"pod_service", "pod_cluster", "pod_ns"},
+	"pod_map":         []string{"pod", "pod_cluster", "pod_ns", "pod_node", "pod_service", "pod_group"},
+	"chost_map":       []string{"chost", "host", "vpc"},
+	"gprocess_map":    []string{"gprocess", "chost"},
+}
