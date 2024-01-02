84ddf843607fe9938ab3a46e4a56415120011014
JIN Jie <jinjie@yunshan.net>
2024-01-02 17:55:37 +0800

    [AGENT] Fix config ineffective for kubernetes poller
    PlatformConfig cannot detect global enabled config changes

--- agent/src/config/handler.rs
+++ agent/src/config/handler.rs
@@ -263,6 +263,7 @@ pub struct PlatformConfig {
     pub thread_threshold: u32,
     pub tap_mode: TapMode,
     pub os_proc_scan_conf: OsProcScanConfig,
+    pub agent_enabled: bool,
 }
 
 #[derive(Clone, PartialEq, Debug, Eq)]
@@ -984,6 +985,7 @@ impl TryFrom<(Config, RuntimeConfig)> for ModuleConfig {
                 },
                 #[cfg(target_os = "windows")]
                 os_proc_scan_conf: OsProcScanConfig {},
+                agent_enabled: conf.enabled,
             },
             flow: (&conf).into(),
             log_parser: LogParserConfig {
@@ -1782,9 +1784,8 @@ impl ConfigHandler {
                 fn platform_callback(handler: &ConfigHandler, components: &mut AgentComponents) {
                     let conf = &handler.candidate_config.platform;
 
-                    if handler.candidate_config.enabled
-                        && (handler.candidate_config.tap_mode == TapMode::Local
-                            || is_tt_pod(conf.trident_type))
+                    if conf.agent_enabled
+                        && (conf.tap_mode == TapMode::Local || is_tt_pod(conf.trident_type))
                     {
                         if is_tt_pod(conf.trident_type) {
                             components.kubernetes_poller.start();
