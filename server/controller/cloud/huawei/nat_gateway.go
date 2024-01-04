3a840bdfe13207661617d0249a6a385d877fbc71
JIN Jie <jinjie@yunshan.net>
2024-01-04 17:41:00 +0800

    [DEEPFLOW] Change year in license

--- server/controller/cloud/huawei/nat_gateway.go
+++ server/controller/cloud/huawei/nat_gateway.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Yunshan NATGateways
+ * Copyright (c) 2024 Yunshan NATGateways
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
