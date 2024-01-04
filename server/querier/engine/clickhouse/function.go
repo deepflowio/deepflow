d4079cc3053a204c11ba6c2c744e2b3af75502b0
LIU Chao <42240939+xiaochaoren1@users.noreply.github.com>
2024-01-04 12:09:59 +0800

    Revert "[querier] Metric add tag category - run automation test(querier_sql) pass"
    This reverts commit 572c51133495dca62267ebfc94c534e17c9b0c05.

--- server/querier/engine/clickhouse/function.go
+++ server/querier/engine/clickhouse/function.go
@@ -158,15 +158,31 @@ func GetTopKTrans(name string, args []string, alias string, db string, table str
 
 	fieldsLen := len(fields)
 	dbFields := make([]string, fieldsLen)
+	conditions := make([]string, 0, fieldsLen)
 
 	var metricStruct *metrics.Metrics
 	for i, field := range fields {
+		var condition string
+
 		field = strings.Trim(field, "`")
 		metricStruct, ok = metrics.GetAggMetrics(field, db, table, ctx)
 		if !ok || metricStruct.Type == metrics.METRICS_TYPE_ARRAY {
 			return nil, 0, "", nil
 		}
-		dbFields[i] = metricStruct.DBField
+
+		condition = metricStruct.Condition
+		tag, ok := tag.GetTag(field, db, table, "default")
+		if ok {
+			dbFields[i] = tag.TagTranslator
+			if condition == "" {
+				condition = tag.NotNullFilter
+			}
+		} else {
+			dbFields[i] = metricStruct.DBField
+		}
+		if condition != "" {
+			conditions = append(conditions, condition)
+		}
 
 		// 判断算子是否支持单层
 		if levelFlag == view.MODEL_METRICS_LEVEL_FLAG_UNLAY && db != chCommon.DB_NAME_FLOW_LOG {
@@ -180,8 +196,10 @@ func GetTopKTrans(name string, args []string, alias string, db string, table str
 	metricStructCopy := *metricStruct
 	if fieldsLen > 1 {
 		metricStructCopy.DBField = "(" + strings.Join(dbFields, ", ") + ")"
+		metricStructCopy.Condition = "(" + strings.Join(conditions, " AND ") + ")"
 	} else {
 		metricStructCopy.DBField = strings.Join(dbFields, ", ")
+		metricStructCopy.Condition = strings.Join(conditions, " AND ")
 	}
 
 	unit := strings.ReplaceAll(function.UnitOverwrite, "$unit", metricStruct.Unit)
