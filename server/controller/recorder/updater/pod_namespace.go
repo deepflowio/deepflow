c6bee261a9a1a227e56429f6208add33b93be977
kangxiang <kangxiang@yunshan.net>
2024-01-11 19:15:21 +0800

    [Controller] fix cloud tag updated null

--- server/controller/recorder/updater/pod_namespace.go
+++ server/controller/recorder/updater/pod_namespace.go
@@ -86,7 +86,11 @@ func (n *PodNamespace) generateUpdateInfo(diffBase *diffbase.PodNamespace, cloud
 		updateInfo["az"] = cloudItem.AZLcuuid
 	}
 	if cloudcommon.DiffMap(diffBase.CloudTags, cloudItem.CloudTags) {
-		tagsJson, _ := json.Marshal(cloudItem.CloudTags)
+		updateTags := map[string]string{}
+		if cloudItem.CloudTags != nil {
+			updateTags = cloudItem.CloudTags
+		}
+		tagsJson, _ := json.Marshal(updateTags)
 		updateInfo["cloud_tags"] = tagsJson
 	}
 
