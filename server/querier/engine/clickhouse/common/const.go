8fe115a6d963f5284aad42bb67c61db72c4c92d7
Jiaxuan <jiaxuan@yunshan.net>
2024-01-11 10:09:33 +0800

    [querier] modify tag displayname - run automation test (querier) pass

--- server/querier/engine/clickhouse/common/const.go
+++ server/querier/engine/clickhouse/common/const.go
@@ -27,6 +27,10 @@ const DB_NAME_PROMETHEUS = "prometheus"
 const DB_NAME_FLOW_TAG = "flow_tag"
 const IndexTypeIncremetalId = "incremental-id"
 const FormatHex = "hex"
+const TagServerChPrefix = "服务端"
+const TagClientChPrefix = "客户端"
+const TagServerEnPrefix = "Server"
+const TagClientEnPrefix = "Client"
 
 var DB_TABLE_MAP = map[string][]string{
 	DB_NAME_FLOW_LOG:        []string{"l4_flow_log", "l7_flow_log", "l4_packet", "l7_packet"},
