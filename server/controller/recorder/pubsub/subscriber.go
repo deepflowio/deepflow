0f0f661ed6adef60633b4b0b7263c2e26a349303
zhengya <zhengya@yunshan.net.cn>
2024-02-04 14:47:11 +0800

    [CONTROLLER/RECORDER] adds pubsub manager

---
+++ server/controller/recorder/pubsub/subscriber.go
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2023 Yunshan Networks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pubsub
+
+type ResourceChangedSubscriber interface {
+	OnResourceChanged(msg interface{})
+}
+
+type ResourceBatchAddedSubscriber interface {
+	OnResourceBatchAdded(msg interface{})
+}
+
+type ResourceUpdatedSubscriber interface {
+	OnResourceUpdated(msg interface{})
+}
+
+type ResourceBatchDeletedSubscriber interface {
+	OnResourceBatchDeleted(msg interface{})
+}
