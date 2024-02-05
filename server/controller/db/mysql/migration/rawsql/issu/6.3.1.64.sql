7ccdbf8eadde986b014ea9396887d57b6097d36c
weiqiang <weiqiang@yunshan.net>
2024-02-05 17:41:07 +0800

    [Server] fix traffic policy display name

--- server/controller/db/mysql/migration/rawsql/issu/6.3.1.64.sql
+++ server/controller/db/mysql/migration/rawsql/issu/6.3.1.64.sql
@@ -6,13 +6,13 @@ BEGIN
 
     SELECT COUNT(*) INTO existing_display_name
     FROM data_source
-    WHERE display_name = '分发策略';
+    WHERE display_name = '网络策略';
 
     IF existing_display_name = 0 THEN
         START TRANSACTION;
 
         SET @lcuuid = (SELECT UUID());
-        INSERT INTO data_source (display_name, data_table_collection, `interval`, retention_time, lcuuid) VALUES ('分发策略', 'flow_metrics.vtap_acl', 60, 3*24, @lcuuid);
+        INSERT INTO data_source (display_name, data_table_collection, `interval`, retention_time, lcuuid) VALUES ('网络策略', 'flow_metrics.vtap_acl', 60, 3*24, @lcuuid);
 
         UPDATE db_version SET version = '6.3.1.64';
 
