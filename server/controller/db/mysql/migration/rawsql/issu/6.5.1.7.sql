f3311d54da3c7569d8ec96f471e1e6c7fd1ac9b3
weiqiang <weiqiang@yunshan.net>
2024-02-08 11:02:19 +0800

    [Server] rename vtap_flow to network, vtap_app to application, vtap_acl to traffic_policy in data source

--- server/controller/db/mysql/migration/rawsql/issu/6.5.1.7.sql
+++ server/controller/db/mysql/migration/rawsql/issu/6.5.1.7.sql
@@ -4,8 +4,6 @@ UPDATE data_source SET data_table_collection='flow_metrics.network*' where data_
 UPDATE data_source SET data_table_collection='flow_metrics.application*' where data_table_collection='flow_metrics.vtap_app*';
 UPDATE data_source SET data_table_collection='flow_metrics.traffic_policy' where data_table_collection='flow_metrics.vtap_acl';
 
-UPDATE tap_type SET name='云网络' where name='虚拟网络';
-
 -- update db_version to latest
 UPDATE db_version SET version='6.5.1.7';
 
