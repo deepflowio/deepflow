2048ba6c92c5528ceebd9cd82e9b0b31f56bb667
zhuofeng <zhuofeng@yunshan.net.cn>
2024-03-11 15:27:52 +0800

    [libs] fix server panic when update master/slave platformData at same time #5738

--- server/libs/grpc/grpc_platformdata.go
+++ server/libs/grpc/grpc_platformdata.go
@@ -1111,9 +1111,9 @@ func (t *PlatformInfoTable) ReloadMaster() error {
 	t.counter.UpdateServiceTime += serviceTime
 
 	newVersion := response.GetVersionPlatformData()
+	isUnmarshalSuccess := false
 	if newVersion != t.versionPlatformData {
 		platformData := trident.PlatformData{}
-		isUnmarshalSuccess := false
 		if plarformCompressed := response.GetPlatformData(); plarformCompressed != nil {
 			if err := platformData.Unmarshal(plarformCompressed); err != nil {
 				log.Warningf("unmarshal grpc compressed platformData failed as %v", err)
@@ -1125,7 +1125,6 @@ func (t *PlatformInfoTable) ReloadMaster() error {
 		if isUnmarshalSuccess {
 			log.Infof("Update rpc platformdata version %d -> %d  regionID=%d", t.versionPlatformData, newVersion, t.regionID)
 			t.updatePlatformData(&platformData)
-			t.versionPlatformData = newVersion
 			t.otherRegionCount = 0
 		}
 	}
@@ -1133,6 +1132,13 @@ func (t *PlatformInfoTable) ReloadMaster() error {
 	t.counter.UpdatePlatformTime += platformTime
 
 	t.updateOthers(response)
+
+	// the versionPlatformData needs to be updated at the end, otherwise the Slave may have updated the Master's map.
+	//  at this time, updateOthers()->updatePodIps() of Master and QueryPodInfo() of Slave will cause concurrent map write and read panic.
+	if isUnmarshalSuccess {
+		t.versionPlatformData = newVersion
+	}
+
 	return nil
 }
 
