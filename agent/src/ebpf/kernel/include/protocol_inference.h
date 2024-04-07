bfb964431c142c3b0a6a231948ccf29044a0d5c4
liuxiao <liuxiao@yunshan.net>
2024-04-07 12:05:09 +0800

    [eBPF] Fix pulsar inference issue

--- agent/src/ebpf/kernel/include/protocol_inference.h
+++ agent/src/ebpf/kernel/include/protocol_inference.h
@@ -2109,7 +2109,7 @@ static __inline enum message_type infer_pulsar_message(const char *ptr,
 	bpf_probe_read_user(buffer, 4, ptr + 4);
 	short command_size = __bpf_ntohl(*(__u32 *) buffer);
 
-	if (total_size < command_size + 4 || total_size + 4 < count)
+	if (total_size < command_size + 4)
 		return MSG_UNKNOWN;
 
 	if (is_infer_socket_valid(conn_info->socket_info_ptr)) {
@@ -2117,6 +2117,9 @@ static __inline enum message_type infer_pulsar_message(const char *ptr,
 			return MSG_REQUEST;
 		return MSG_UNKNOWN;
 	}
+
+	if (count < total_size + 4)
+		return MSG_UNKNOWN;
 
 	short limit = total_size + 4 < infer_len ? total_size + 4 : infer_len;
 
