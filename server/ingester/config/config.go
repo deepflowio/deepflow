ac1f9ecd47c71ac7edc472436cfa2cf6116feeea
zhuofeng <zhuofeng@yunshan.net.cn>
2024-04-22 10:12:49 +0800

    [ingester] support cleaning CK TTL expired data

--- server/ingester/config/config.go
+++ server/ingester/config/config.go
@@ -76,9 +76,10 @@ type DiskCleanup struct {
 }
 
 type CKDiskMonitor struct {
-	CheckInterval int             `yaml:"check-interval"` // s
-	DiskCleanups  []DiskCleanup   `yaml:"disk-cleanups"`
-	PriorityDrops []DatabaseTable `yaml:"priority-drops"`
+	CheckInterval    int             `yaml:"check-interval"` // s
+	TTLCheckDisabled bool            `yaml:"ttl-check-disabled"`
+	DiskCleanups     []DiskCleanup   `yaml:"disk-cleanups"`
+	PriorityDrops    []DatabaseTable `yaml:"priority-drops"`
 }
 
 type Disk struct {
@@ -445,6 +446,7 @@ func Load(path string) *Config {
 			TCPReaderBuffer: 1 << 20,
 			CKDiskMonitor: CKDiskMonitor{
 				DefaultCheckInterval,
+				false,
 				[]DiskCleanup{
 					{
 						DefaultSystemDiskPrefix,
