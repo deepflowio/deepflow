5f244d7069b367d3379d1314274d041dd07793fe
weiqiang <weiqiang@yunshan.net>
2024-05-10 17:09:47 +0800

    [Server] fix alloc agent when node exceptions

--- server/controller/http/service/analyzer.go
+++ server/controller/http/service/analyzer.go
@@ -413,16 +413,17 @@ func UpdateAnalyzer(
 		return nil, err
 	}
 
+	if err = tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
 	// if state equal to maintaince/exception, trigger realloc analyzer
 	// 如果是将状态修改为运维/异常，则触发对应的采集器重新分配数据节点
 	if state == common.HOST_STATE_MAINTENANCE || state == common.HOST_STATE_EXCEPTION {
 		m.TriggerReallocAnalyzer(dbInfo, analyzer.IP)
 	}
 
-	if err = tx.Commit().Error; err != nil {
-		tx.Rollback()
-		return nil, err
-	}
 	response, _ := GetAnalyzers(orgID, map[string]interface{}{"lcuuid": lcuuid})
 	return &response[0], nil
 }
