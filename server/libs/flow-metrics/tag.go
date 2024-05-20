a5dc03a855285d0abaaa227266998da23d694fd9
zhuofeng <zhuofeng@yunshan.net.cn>
2024-05-20 11:04:42 +0800

    [ingester] direction rename role for exporter #24847

--- server/libs/flow-metrics/tag.go
+++ server/libs/flow-metrics/tag.go
@@ -188,6 +188,11 @@ const (
 	Rest                    = 0
 )
 
+const (
+	ROLE_CLIENT = 0
+	ROLE_SERVER = 1
+)
+
 var TAPSideEnumsString = []string{
 	Rest:                    "rest",
 	Client:                  "c",
@@ -298,7 +303,7 @@ type Field struct {
 	GPID1             uint32     `json:"gprocess_id_1" category:"$tag" sub:"universal_tag" datasource:"nm|am"`
 
 	ACLGID     uint16
-	Direction  DirectionEnum     `json:"role" category:"$tag" sub:"capture_info" datasource:"n|a"`
+	Role       uint8             `json:"role" category:"$tag" sub:"capture_info" enumfile:"role" datasource:"n|a"`
 	Protocol   layers.IPProtocol `json:"protocol" category:"$tag" sub:"network_layer" enumfile:"protocol"`
 	ServerPort uint16            `json:"server_port" category:"$tag" sub:"transport_layer"`
 	VTAPID     uint16            `json:"agent_id" category:"$tag" sub:"capture_info"`
@@ -591,10 +596,10 @@ func (t *Tag) MarshalTo(b []byte) int {
 	}
 
 	if t.Code&Direction != 0 {
-		if t.Direction.IsClientToServer() {
-			offset += copy(b[offset:], ",direction=c2s")
-		} else if t.Direction.IsServerToClient() {
-			offset += copy(b[offset:], ",direction=s2c")
+		if t.Role == ROLE_CLIENT {
+			offset += copy(b[offset:], ",role=c2s")
+		} else if t.Role == ROLE_SERVER {
+			offset += copy(b[offset:], ",role=s2c")
 		}
 	}
 	if t.Code&GPID != 0 {
@@ -1129,12 +1134,7 @@ func (t *Tag) WriteBlock(block *ckdb.Block, time uint32) {
 	}
 
 	if code&Direction != 0 {
-		if t.Direction.IsClientToServer() {
-			// 0: client, 1: server
-			block.Write(uint8(0))
-		} else {
-			block.Write(uint8(1))
-		}
+		block.Write(t.Role)
 	}
 
 	if code&GPID != 0 {
@@ -1364,7 +1364,12 @@ func (t *Tag) ReadFromPB(p *pb.MiniTag) {
 	// The range of EPC ID is [-2,65533], if EPC ID < -2 needs to be transformed into the range.
 	t.L3EpcID = MarshalInt32WithSpecialID(p.Field.L3EpcId)
 	t.L3EpcID1 = MarshalInt32WithSpecialID(p.Field.L3EpcId1)
-	t.Direction = DirectionEnum(p.Field.Direction)
+	direction := DirectionEnum(p.Field.Direction)
+	if direction.IsClientToServer() {
+		t.Role = ROLE_CLIENT
+	} else {
+		t.Role = ROLE_SERVER
+	}
 	t.TAPSide = TAPSideEnum(p.Field.TapSide)
 	t.TAPSideStr = TAPSideEnum(p.Field.TapSide).String()
 	t.Protocol = layers.IPProtocol(p.Field.Protocol)
@@ -1391,7 +1396,7 @@ func (t *Tag) ReadFromPB(p *pb.MiniTag) {
 	t.GPID1 = p.Field.Gpid1
 
 	if p.Field.PodId != 0 {
-		if t.Code&IPPath != 0 && t.Direction.IsServerToClient() {
+		if t.Code&IPPath != 0 && t.Role == ROLE_SERVER {
 			t.PodID1 = p.Field.PodId
 		} else {
 			t.PodID = p.Field.PodId
