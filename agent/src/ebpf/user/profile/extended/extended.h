2dcd26ad0b879fd49580107452e6a71b674132ad
Jiping Yin <jiping@yunshan.net>
2024-05-21 19:51:55 +0800

    [eBPF] Adjust profiler to enhance process event handling

--- agent/src/ebpf/user/profile/extended/extended.h
+++ agent/src/ebpf/user/profile/extended/extended.h
@@ -16,5 +16,33 @@
 
 #ifndef DF_PROFILE_EXT_H
 #define DF_PROFILE_EXT_H
+
+/**
+ * @brief **extended_reader_create()** create an extended reader to
+ * receive perfbuf data.
+ *
+ * You can rewrite the extended_reader_create() function to expand
+ * the functionality of other function stack profiling. Inside it,
+ * you can append new readers to read data from the eBPF perfbuf
+ * and enable new threads to handle the reception.
+ *
+ * @param tracer BPF tracer address
+ * @return 0 on success, non-zero on error
+ */
 int extended_reader_create(struct bpf_tracer *tracer);
+
+/**
+ * @brief **extended_proc_event_handler()** extend the handling of process
+ * execution and exit events.
+ * @param pid Process ID
+ * @param name Process name
+ * @param PROC_EXEC or PROC_EXIT
+ * @return 0 on success, non-zero on error
+ *
+ * For example, you might want to notify an eBPF program of these events
+ * so that eBPF can perform the corresponding handling. Extend the handling
+ * based on your own requirements.
+ */
+int extended_proc_event_handler(int pid, const char *name,
+				enum proc_act_type type);
 #endif /* DF_PROFILE_EXT_H */
