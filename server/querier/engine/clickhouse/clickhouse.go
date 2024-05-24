fab7a8c9fb2965f184bcd146cec9396816c663b7
Ericsssss <lvqiao@yunshan.net>
2024-05-24 11:07:59 +0800

    [Querier] modify query sql orgID translation error #24959
     - run automation test(basic&querier) pass

--- server/querier/engine/clickhouse/clickhouse.go
+++ server/querier/engine/clickhouse/clickhouse.go
@@ -431,6 +431,7 @@ func (e *CHEngine) QuerySlimitSql(sql string, args *common.QuerierParams) (*comm
 		Callbacks:       callbacks,
 		QueryUUID:       query_uuid,
 		ColumnSchemaMap: columnSchemaMap,
+		ORGID:           args.ORGID,
 	}
 	rst, err := chClient.DoQuery(params)
 	if err != nil {
@@ -1004,7 +1005,7 @@ func (e *CHEngine) TransPrometheusTargetIDFilter(expr view.Node) (view.Node, err
 			Password: config.Cfg.Clickhouse.Password,
 			DB:       "flow_tag",
 		}
-		targetLabelRst, err := chClient.DoQuery(&client.QueryParams{Sql: sql})
+		targetLabelRst, err := chClient.DoQuery(&client.QueryParams{Sql: sql, ORGID: e.ORGID})
 		if err != nil {
 			return expr, err
 		}
