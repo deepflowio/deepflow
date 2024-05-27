8442992a9a99be6a5829126ee15f360aa6f88de9
zhuofeng <zhuofeng@yunshan.net.cn>
2024-05-27 18:09:28 +0800

    [ingester] split off-cpu profile by splitting granularity

--- server/ingester/profile/config/config.go
+++ server/ingester/profile/config/config.go
@@ -28,12 +28,13 @@ import (
 var log = logging.MustGetLogger("profile.config")
 
 type Config struct {
-	Base                 *config.Config
-	CKWriterConfig       config.CKWriterConfig `yaml:"profile-ck-writer"`
-	ProfileTTL           int                   `yaml:"profile-ttl-hour"`
-	DecoderQueueCount    int                   `yaml:"profile-decoder-queue-count"`
-	DecoderQueueSize     int                   `yaml:"profile-decoder-queue-size"`
-	CompressionAlgorithm *string               `yaml:"profile-compression-algorithm"`
+	Base                       *config.Config
+	CKWriterConfig             config.CKWriterConfig `yaml:"profile-ck-writer"`
+	ProfileTTL                 int                   `yaml:"profile-ttl-hour"`
+	DecoderQueueCount          int                   `yaml:"profile-decoder-queue-count"`
+	DecoderQueueSize           int                   `yaml:"profile-decoder-queue-size"`
+	CompressionAlgorithm       *string               `yaml:"profile-compression-algorithm"`
+	OffCpuSplittingGranularity int                   `yaml:"profile-off-cpu-splitting-granularity"`
 }
 
 type ProfileConfig struct {
@@ -41,9 +42,10 @@ type ProfileConfig struct {
 }
 
 const (
-	DefaultProfileTTL        = 72 // hour
-	DefaultDecoderQueueCount = 2
-	DefaultDecoderQueueSize  = 1 << 14
+	DefaultProfileTTL                 = 72 // hour
+	DefaultDecoderQueueCount          = 2
+	DefaultDecoderQueueSize           = 1 << 14
+	DefaultOffCpuSplittingGranularity = 1
 )
 
 func (c *Config) Validate() error {
@@ -72,11 +74,12 @@ func (c *Config) Validate() error {
 func Load(base *config.Config, path string) *Config {
 	config := &ProfileConfig{
 		Profile: Config{
-			Base:              base,
-			CKWriterConfig:    config.CKWriterConfig{QueueCount: 1, QueueSize: 100000, BatchSize: 51200, FlushTimeout: 5},
-			ProfileTTL:        DefaultProfileTTL,
-			DecoderQueueCount: DefaultDecoderQueueCount,
-			DecoderQueueSize:  DefaultDecoderQueueSize,
+			Base:                       base,
+			CKWriterConfig:             config.CKWriterConfig{QueueCount: 1, QueueSize: 100000, BatchSize: 51200, FlushTimeout: 5},
+			ProfileTTL:                 DefaultProfileTTL,
+			DecoderQueueCount:          DefaultDecoderQueueCount,
+			DecoderQueueSize:           DefaultDecoderQueueSize,
+			OffCpuSplittingGranularity: DefaultOffCpuSplittingGranularity,
 		},
 	}
 	if _, err := os.Stat(path); os.IsNotExist(err) {
