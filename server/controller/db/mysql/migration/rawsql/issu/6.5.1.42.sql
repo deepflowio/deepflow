fc2e8c98a293e6fd7d79003e8b0527a135251d5a
Ericsssss <lvqiao@yunshan.net>
2024-06-06 15:09:04 +0800

    [Mysql] alarm_policy support deepflow_system split

--- server/controller/db/mysql/migration/rawsql/issu/6.5.1.42.sql
+++ server/controller/db/mysql/migration/rawsql/issu/6.5.1.42.sql
@@ -19,8 +19,11 @@ BEGIN
             INSERT INTO data_source (display_name, data_table_collection, `interval`, retention_time, lcuuid) 
             VALUES ('管理侧监控数据', 'deepflow_admin.*', 0, 7*24, @lcuuid);
         END IF;
+        UPDATE alarm_policy SET query_params = REPLACE(query_params, 'deepflow_system', 'deepflow_admin') WHERE
+        name in ("控制器磁盘空间不足", "控制器系统负载高", "数据节点系统负载高", "数据节点磁盘空间不足", "数据节点数据强制删除", "数据节点数据丢失 (ingester.recviver.metrics.invalid)", "数据节点数据丢失 (ingester.queue.metrics.overwritten)","数据节点数据丢失 (ingester.decoder.metrics.drop_count)", "数据节点数据丢失 (ingester.ckwriter.metrics.write_failed_count)");
     END IF;
-
+    UPDATE alarm_policy SET query_params = REPLACE(query_params, 'deepflow_system', 'deepflow_tenant') WHERE
+    name in ("采集器 CPU 超限", "采集器内存超限", "采集器重启", "采集器所在系统空闲内存低", "采集器 WARN 日志过多", "采集器 ERR 日志过多", "K8s 资源同步滞后", "采集器数据丢失 (dispatcher.metrics.kernel_drops)", "采集器数据丢失 (queue.metrics.overwritten)", "采集器数据丢失 (l7_session_aggr.metrics.throttle-drop)", "采集器数据丢失 (flow_aggr.metrics.drop-in-throttle)", "采集器数据丢失 (ebpf_collector.metrics.kern_lost)", "采集器数据丢失 (ebpf_collector.metrics.user_enqueue_lost)", "采集器数据丢失 (dispatcher.metrics.invalid_packets)", "采集器数据丢失 (dispatcher.metrics.err)", "采集器数据丢失 (flow_map.metrics.drop_by_window)", "采集器数据丢失 (flow_map.metrics.drop_by_capacity)", "采集器数据丢失 (flow_aggr.metrics.drop-before-window)", "采集器数据丢失 (quadruple_generator.metrics.drop-before-window)", "采集器数据丢失 (collector.metrics.drop-before-window)", "采集器数据丢失 (collector.metrics.drop-inactive)", "采集器数据丢失 (collect_sender.metrics.dropped)");
     COMMIT; 
 
 END;
