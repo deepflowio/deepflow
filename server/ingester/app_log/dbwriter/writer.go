6e376ea9d7f5ac708ed84ef2b2b1e10367c912fb
zhuofeng <zhuofeng@yunshan.net.cn>
2024-06-11 15:38:04 +0800

    [ingester] fix app_log type write failed

--- server/ingester/app_log/dbwriter/writer.go
+++ server/ingester/app_log/dbwriter/writer.go
@@ -55,7 +55,7 @@ func NewAppLogWriter(index int, msgType datatype.MessageType, config *config.Con
 	}
 
 	table := LOG_TABLE
-	flowTagWriter, err := flow_tag.NewFlowTagWriter(index, fmt.Sprintf("%s-%s-%d", table+msgType.String(), index), LOG_DB, config.TTL, ckdb.TimeFuncTwelveHour, config.Base, &w.writerConfig)
+	flowTagWriter, err := flow_tag.NewFlowTagWriter(index, fmt.Sprintf("%s-%s-%d", table, msgType.String(), index), LOG_DB, config.TTL, ckdb.TimeFuncTwelveHour, config.Base, &w.writerConfig)
 	if err != nil {
 		return nil, err
 	}
