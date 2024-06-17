2039b6f6c686cfd88a84e9e781044413ba21d9a8
Liu Chao <liuchao@yunshan.net>
2024-06-17 14:26:10 +0800

    [tagrecorder] Fix k8s.env is empty #25435

--- server/controller/tagrecorder/ch_pod_k8s_env.go
+++ server/controller/tagrecorder/ch_pod_k8s_env.go
@@ -62,7 +62,7 @@ func (c *ChPodK8sEnv) onResourceUpdated(sourceID int, fieldsUpdate *message.PodF
 			} else {
 				if oldV != v {
 					updateKey = K8sEnvKey{ID: sourceID, Key: k}
-					updateInfo[k] = v
+					updateInfo["value"] = v
 					db.Where("id = ? and `key` = ?", sourceID, k).First(&chItem)
 					if chItem.ID == 0 {
 						keysToAdd = append(keysToAdd, K8sEnvKey{ID: sourceID, Key: k})
