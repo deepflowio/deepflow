6bc19368b0645ab93dfac927623b3d717fd04c4c
JIN Jie <jinjie@yunshan.net>
2024-07-05 18:16:59 +0800

    fix: Fix remote exec command id from/to u32 conversion

--- agent/crates/public/src/rpc.rs
+++ agent/crates/public/src/rpc.rs
@@ -1,5 +1,5 @@
 pub mod remote_exec {
-    #[derive(Clone, Copy, PartialEq, Eq, Hash)]
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
     pub enum CmdId {
         Community(u32),
         Enterprise(u32),
@@ -29,7 +29,7 @@ pub mod remote_exec {
         fn try_from(id: u32) -> Result<Self, Self::Error> {
             let mask = u32::MAX << CmdId::ID_BITS;
             let inner_id = id & !mask;
-            match id & mask {
+            match id >> CmdId::ID_BITS {
                 0 => Ok(Self::Community(inner_id)),
                 1 => Ok(Self::Enterprise(inner_id)),
                 2 => Ok(Self::Temporary(inner_id)),
@@ -64,5 +64,32 @@ pub mod remote_exec {
         pub output_format: OutputFormat,
         pub desc: &'static str,
         pub command_type: CommandType,
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::CmdId;
+
+        #[test]
+        fn id_conversion() {
+            let ids = vec![
+                CmdId::Community(0),
+                CmdId::Community(435),
+                CmdId::Community(!(u32::MAX << CmdId::ID_BITS)),
+                CmdId::Enterprise(0),
+                CmdId::Enterprise(3451),
+                CmdId::Enterprise(341),
+                CmdId::Enterprise(!(u32::MAX << CmdId::ID_BITS)),
+                CmdId::Temporary(0),
+                CmdId::Temporary(342),
+                CmdId::Temporary(34),
+                CmdId::Temporary(!(u32::MAX << CmdId::ID_BITS)),
+            ];
+
+            for id in ids.iter() {
+                let id_u32: u32 = (*id).into();
+                assert_eq!(*id, CmdId::try_from(id_u32).unwrap());
+            }
+        }
     }
 }
