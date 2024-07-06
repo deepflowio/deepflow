3836465242fcfd094dec29340ff0bc84ad9c626f
Jiping Yin <jiping@yunshan.net>
2024-07-06 11:40:22 +0800

    fix: agent - eBPF Addressing excessive eBPF maps memory usage
    -  Adjust 'max-socket-entries' and 'max-trace-entries'
    The default values for the configuration items 'max-socket-entries' and 'max-trace-entries' have been changed from '524288' to '131072'. We have a monitoring mechanism for 'socket_info_map' and 'trace_map' that automatically cleans up the oldest data once the limit is reached. This poses no risk because new entries are created only when there is data transmission on the socket, rather than during the TCP handshake phase. This ensures that connection information can be restored.
        - The eBPF 'socket_info_map' memory usage in the kernel has been reduced from 72 MB to 18 MB.
        - The eBPF 'trace_map' memory usage in the kernel has been reduced from 56 MB to 14 MB.
    
    -  Adjustment of stack trace map capacity
       Calculation method for stack map capacity:
          `scaling_factor * (ncpus * expected_stack_count_per_cpu)`
        - **scaling_factor:**
        To ensure the integrity and accuracy of data, especially during stack
        tracing in performance analysis or debugging tools, it's crucial to
        include a margin. Stack tracing involves recording the state of program
        call stacks to analyze program behavior and performance. default : 3.0
        - **ncpus:**
        This needs to calculate the total expected number of stack traces
        because sampling is done per CPU.
        - **expected_stack_count_per_cpu:**
        Represents the expected number of stack traces on each CPU.
        Here we set it as the number of samples per second.
    
       The memory consumption range of the kernel is approximately between `1M` bytes and `65M` bytes. To achieve continuous profiling, we utilize two maps, `stack_map_a` and `stack_map_b`, where the calculation pertains to the combined capacity of both maps. Before the change, the memory usage was a fixed 130 MB.

--- agent/src/ebpf/user/profile/perf_profiler.c
+++ agent/src/ebpf/user/profile/perf_profiler.c
@@ -22,6 +22,7 @@
  */
 #ifndef AARCH64_MUSL
 #include <sys/stat.h>
+#include <math.h>
 #include <bcc/perf_reader.h>
 #include "../config.h"
 #include "../utils.h"
@@ -55,6 +56,7 @@
 #define ONCPU_PROFILER_NAME "oncpu"
 #define PROFILER_CTX_ONCPU_IDX THREAD_PROFILER_READER_IDX
 
+extern int sys_cpus_count;
 struct profiler_context *g_ctx_array[PROFILER_CTX_NUM];
 static struct profiler_context oncpu_ctx;
 
@@ -284,11 +286,52 @@ exit:
 	pthread_exit(NULL);
 }
 
+static int stack_trace_map_capacity(struct bpf_tracer *tracer)
+{
+	/*
+	 * Calculation method for stack map capacity:
+	 * 
+	 *  `scaling_factor * (ncpus * expected_stack_count_per_cpu)`
+	 *
+	 * scaling_factor:
+	 *   To ensure the integrity and accuracy of data, especially during stack
+	 *   tracing in performance analysis or debugging tools, it's crucial to
+	 *   include a margin. Stack tracing involves recording the state of program
+	 *   call stacks to analyze program behavior and performance.
+	 * ncpus:
+	 *   This needs to calculate the total expected number of stack traces
+	 *   because sampling is done per CPU.
+	 * expected_stack_count_per_cpu:
+	 *   Represents the expected number of stack traces on each CPU.
+	 *   Here we set it as the number of samples per second.
+	 *
+	 * In kernel, round the given value(capacity) up to nearest power of two.
+	 * ref: https://elixir.bootlin.com/linux/v4.19.313/source/kernel/bpf/stackmap.c#L122
+	 */
+
+	double scaling_factor = STACKMAP_SCALING_FACTOR;
+	int ncpus = sys_cpus_count;
+	int expected_stack_count_per_cpu = tracer->sample_freq;
+	int capacity =
+	    (int)round(scaling_factor * (ncpus * expected_stack_count_per_cpu));
+	capacity = 1 << max_log2(capacity);
+	if (capacity > STACKMAP_CAPACITY_THRESHOLD)
+		capacity = STACKMAP_CAPACITY_THRESHOLD;
+
+	return capacity;
+}
+
 static int create_profiler(struct bpf_tracer *tracer)
 {
 	int ret;
 
 	profiler_tracer = tracer;
+	int cap = stack_trace_map_capacity(tracer);
+	if ((ret = maps_config(tracer, MAP_STACK_A_NAME, cap)))
+		return ret;
+
+	if ((ret = maps_config(tracer, MAP_STACK_B_NAME, cap)))
+		return ret;
 
 	/* load ebpf perf profiler */
 	if (tracer_bpf_load(tracer))
@@ -393,7 +436,7 @@ int stop_continuous_profiler(void)
 			continue;
 		g_ctx_array[i]->profiler_stop = 1;
 	}
-	
+
 	if (profiler_tracer == NULL)
 		return (0);
 
@@ -482,7 +525,7 @@ static void print_cp_tracer_status(struct bpf_tracer *t,
 }
 
 static int cpdbg_sockopt_get(sockoptid_t opt, const void *conf, size_t size,
-			     void **out, size_t *outsize)
+			     void **out, size_t * outsize)
 {
 	return 0;
 }
