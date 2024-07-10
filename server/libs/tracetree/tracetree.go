41ab0bde28b374545eba79096e680d2a5c29570c
zhuofeng <zhuofeng@yunshan.net.cn>
2024-07-10 10:44:55 +0800

    feat: support write span_with_trace_id

--- server/libs/tracetree/tracetree.go
+++ server/libs/tracetree/tracetree.go
@@ -20,34 +20,42 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/deepflowio/deepflow/server/libs/ckdb"
 	"github.com/deepflowio/deepflow/server/libs/codec"
 	"github.com/deepflowio/deepflow/server/libs/pool"
+	"github.com/deepflowio/deepflow/server/libs/utils"
 )
 
 const TRACE_TREE_VERSION = 0x11
 
+func HashSearchIndex(key string) uint64 {
+	return utils.DJBHash(17, key)
+}
+
 type TraceTree struct {
-	Time         uint32
-	TraceidIndex uint64
+	Time        uint32
+	SearchIndex uint64
+	OrgId       uint16
 
-	TraceId string
-
+	TraceId   string
 	SpanInfos []SpanInfo
+
+	encodedSpans []byte
 }
 
 type SpanInfo struct {
-	IsIPv4           bool
 	AutoServiceType0 uint8
-	AutoServiceID0   uint32
-	IP40             uint32
-	IP60             net.IP
-	AppService0      string
-
 	AutoServiceType1 uint8
+	AutoServiceID0   uint32
 	AutoServiceID1   uint32
-	IP41             uint32
-	IP61             net.IP
+	AppService0      string
 	AppService1      string
+
+	IsIPv4 bool
+	IP40   uint32
+	IP60   net.IP
+	IP41   uint32
+	IP61   net.IP
 
 	ResponseDuration uint64
 	ResponseStatus   uint8
@@ -55,6 +63,27 @@ type SpanInfo struct {
 
 func (t *TraceTree) Release() {
 	ReleaseTraceTree(t)
+}
+
+func (t *TraceTree) OrgID() uint16 {
+	return t.OrgId
+}
+
+func (t *TraceTree) WriteBlock(block *ckdb.Block) {
+	t.Encode()
+	block.WriteDateTime(t.Time)
+	block.Write(
+		HashSearchIndex(t.TraceId),
+		utils.String(t.encodedSpans),
+	)
+}
+
+func TraceTreeColumns() []*ckdb.Column {
+	return []*ckdb.Column{
+		ckdb.NewColumn("time", ckdb.DateTime),
+		ckdb.NewColumn("search_index", ckdb.UInt64),
+		ckdb.NewColumn("encoded_span", ckdb.String),
+	}
 }
 
 var poolTraceTree = pool.NewLockFreePool(func() interface{} {
@@ -75,32 +104,64 @@ func ReleaseTraceTree(t *TraceTree) {
 	poolTraceTree.Put(t)
 }
 
-func (t *TraceTree) Encode(encoder *codec.SimpleEncoder) {
+func (t *TraceTree) Encode() {
+	encoder := &codec.SimpleEncoder{}
+	t.encodedSpans = t.encodedSpans[:0]
+	encoder.Init(t.encodedSpans)
 	encoder.WriteU8(TRACE_TREE_VERSION)
 	encoder.WriteString255(t.TraceId)
 	encoder.WriteU16(uint16(len(t.SpanInfos)))
-	for _, spanInfo := range t.SpanInfos {
-		encoder.WriteBool(spanInfo.IsIPv4)
-		encoder.WriteU8(spanInfo.AutoServiceType0)
-		// ....
+	for _, s := range t.SpanInfos {
+		encoder.WriteU8(s.AutoServiceType0)
+		encoder.WriteU8(s.AutoServiceType1)
+		encoder.WriteVarintU32(s.AutoServiceID0)
+		encoder.WriteVarintU32(s.AutoServiceID1)
+		encoder.WriteString255(s.AppService0)
+		encoder.WriteString255(s.AppService1)
+
+		encoder.WriteBool(s.IsIPv4)
+		if s.IsIPv4 {
+			encoder.WriteU32(s.IP40)
+			encoder.WriteU32(s.IP41)
+		} else {
+			encoder.WriteIPv6(s.IP60)
+			encoder.WriteIPv6(s.IP61)
+		}
+		encoder.WriteVarintU64(s.ResponseDuration)
+		encoder.WriteU8(s.ResponseStatus)
 	}
 }
 
 func (t *TraceTree) Decode(decoder *codec.SimpleDecoder) error {
 	version := decoder.ReadU8()
 	if version != TRACE_TREE_VERSION {
-		return fmt.Errorf("current data version is %d expect version is %d", version, TRACE_TREE_VERSION)
+		return fmt.Errorf("trace tree data version is %d expect version is %d", version, TRACE_TREE_VERSION)
 	}
 	t.TraceId = decoder.ReadString255()
 	spanCount := int(decoder.ReadU16())
 	for i := 0; i < spanCount; i++ {
-		t.SpanInfos = append(t.SpanInfos, SpanInfo{})
-		t.SpanInfos[i].IsIPv4 = decoder.ReadBool()
-		t.SpanInfos[i].AutoServiceType0 = decoder.ReadU8()
-		// ....
+		s := SpanInfo{}
+		s.AutoServiceType0 = decoder.ReadU8()
+		s.AutoServiceType1 = decoder.ReadU8()
+		s.AutoServiceID0 = decoder.ReadVarintU32()
+		s.AutoServiceID1 = decoder.ReadVarintU32()
+		s.AppService0 = decoder.ReadString255()
+		s.AppService1 = decoder.ReadString255()
+
+		s.IsIPv4 = decoder.ReadBool()
+		if s.IsIPv4 {
+			s.IP40 = decoder.ReadU32()
+			s.IP41 = decoder.ReadU32()
+		} else {
+			s.IP60 = make([]byte, 16)
+			s.IP61 = make([]byte, 16)
+			decoder.ReadIPv6(s.IP60)
+			decoder.ReadIPv6(s.IP61)
+		}
+		t.SpanInfos = append(t.SpanInfos, s)
 	}
 	if decoder.Failed() {
-		return fmt.Errorf("decode failed, offset is %d, buf length is %d ", decoder.Offset(), len(decoder.Bytes()))
+		return fmt.Errorf("trace tree decode failed, offset is %d, buf length is %d ", decoder.Offset(), len(decoder.Bytes()))
 	}
 	return nil
 }
