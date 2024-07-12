22d7ceabe692fc0f2f9274aaa650201192b23bef
JIN Jie <jinjie@yunshan.net>
2024-07-12 15:29:08 +0800

    feat: Allow overriding remote exec cmdline with async function

--- agent/crates/public/src/rpc.rs
+++ agent/crates/public/src/rpc.rs
@@ -1,5 +1,27 @@
 pub mod remote_exec {
     use std::fmt::{self, Write};
+    use std::process::Output;
+
+    use futures::future::BoxFuture;
+    use thiserror::Error;
+
+    use crate::proto::trident as pb;
+
+    #[derive(Error, Debug)]
+    pub enum Error {
+        #[error("command execution failed with {0}")]
+        CmdExecFailed(#[from] std::io::Error),
+        #[error("param `{0}` not found")]
+        ParamNotFound(String),
+        #[error("kubernetes failed with {0}")]
+        KubeError(#[from] kube::Error),
+        #[error("serialize failed with {0}")]
+        SerializeError(#[from] serde_json::Error),
+        #[error("transparent")]
+        SyscallFailed(String),
+    }
+
+    type Result<T> = std::result::Result<T, Error>;
 
     #[derive(Clone, Copy)]
     pub enum OutputFormat {
@@ -14,23 +36,6 @@ pub mod remote_exec {
         LogPrevious,
     }
 
-    #[derive(Clone, Copy, PartialEq)]
-    pub enum CommandType {
-        System,
-        Kubernetes(KubeCmd),
-        Enterprise(&'static str),
-    }
-
-    impl fmt::Display for CommandType {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match self {
-                Self::System => write!(f, "system"),
-                Self::Kubernetes(_) => write!(f, "kubernetes"),
-                Self::Enterprise(t) => write!(f, "{}", t),
-            }
-        }
-    }
-
     #[derive(Clone, Copy)]
     pub struct Parameter {
         pub name: &'static str,
@@ -43,8 +48,9 @@ pub mod remote_exec {
         pub cmdline: &'static str,
         pub output_format: OutputFormat,
         pub desc: &'static str,
-        pub command_type: CommandType,
+        pub command_type: &'static str,
         pub params: Vec<Parameter>,
+        pub override_cmdline: Option<fn(&Params) -> BoxFuture<'static, Result<Output>>>,
     }
 
     impl Default for Command {
@@ -54,8 +60,9 @@ pub mod remote_exec {
                 cmdline: "",
                 output_format: OutputFormat::Text,
                 desc: "",
-                command_type: CommandType::System,
+                command_type: "",
                 params: Vec::new(),
+                override_cmdline: None,
             }
         }
     }
@@ -88,6 +95,74 @@ pub mod remote_exec {
             }
             id
         }
+
+        pub fn check_params(&self, call_params: &Params) -> Result<()> {
+            for p in self.params.iter() {
+                if call_params.get(p.name).is_none() {
+                    return Err(Error::ParamNotFound(p.name.to_owned()));
+                }
+            }
+            Ok(())
+        }
+    }
+
+    pub struct Params<'a>(pub &'a [pb::Parameter]);
+
+    impl Params<'_> {
+        pub fn is_valid(&self) -> bool {
+            for p in self.0.iter() {
+                if p.key.is_none() {
+                    return false;
+                }
+                let Some(value) = p.value.as_ref() else {
+                    return false;
+                };
+                for c in value.as_bytes() {
+                    match c {
+                        b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'-' | b'_' => (),
+                        _ => return false,
+                    }
+                }
+            }
+            true
+        }
+
+        pub fn get(&self, key: &str) -> Option<&str> {
+            for p in self.0.iter() {
+                match &p.key {
+                    Some(k) if k == key => return p.value.as_ref().map(|s| s.as_str()),
+                    _ => (),
+                }
+            }
+            None
+        }
+    }
+
+    impl fmt::Debug for Params<'_> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{{")?;
+            let mut empty = true;
+            for p in self.0.iter() {
+                let Some(key) = p.key.as_ref() else {
+                    continue;
+                };
+                if empty {
+                    write!(f, " ")?;
+                } else {
+                    write!(f, ", ")?;
+                }
+                if let Some(value) = p.value.as_ref() {
+                    write!(f, "{}: \"{}\"", key, value)?;
+                } else {
+                    write!(f, "{}: null", key)?;
+                }
+                empty = false;
+            }
+            if !empty {
+                write!(f, " ")?;
+            }
+            write!(f, "}}")
+        }
     }
 
     #[cfg(test)]
@@ -100,7 +175,7 @@ pub mod remote_exec {
                 "kubernetes_k5l_-n_$ns_d6e_pod_$pod",
                 &Command {
                     cmdline: "kubectl -n $ns describe pod $pod",
-                    command_type: CommandType::Kubernetes(KubeCmd::DescribePod),
+                    command_type: "kubernetes",
                     ..Default::default()
                 }
                 .gen_id()
@@ -109,7 +184,7 @@ pub mod remote_exec {
                 "kubernetes_k5l_-n_$ns_l2s_--t80_-p_$pod",
                 &Command {
                     cmdline: "kubectl -n $ns logs --tail=10000 -p $pod",
-                    command_type: CommandType::Kubernetes(KubeCmd::LogPrevious),
+                    command_type: "kubernetes",
                     ..Default::default()
                 }
                 .gen_id(),
