ad806875128d0574a59105012a27c7370bac71a9
huanchao <huanchao@yunshan.net>
2024-07-18 18:07:23 +0800

    feat: agent adds the CommandParam fields, modifies the param validation method

--- agent/crates/public/src/rpc.rs
+++ agent/crates/public/src/rpc.rs
@@ -8,12 +8,16 @@ pub mod remote_exec {
 
     use crate::proto::trident as pb;
 
+    pub const DEFAULT_PARAM_REGEX: &'static str = "^[A-Za-z0-9-_]+$";
+
     #[derive(Error, Debug)]
     pub enum Error {
         #[error("command execution failed with {0}")]
         CmdExecFailed(#[from] std::io::Error),
         #[error("param `{0}` not found")]
         ParamNotFound(String),
+        #[error("param `{0}` is invalid")]
+        ParamInvalid(String),
         #[error("kubernetes failed with {0}")]
         KubeError(#[from] kube::Error),
         #[error("serialize failed with {0}")]
@@ -38,9 +42,18 @@ pub mod remote_exec {
     }
 
     #[derive(Clone, Copy)]
+    pub enum ParamType {
+        Text,
+        Boolean,
+    }
+
+    #[derive(Clone, Copy)]
     pub struct Parameter {
         pub name: &'static str,
-        pub regex: &'static str,
+        pub regex: Option<&'static str>,
+        pub required: bool,
+        pub param_type: ParamType,
+        pub description: &'static str,
     }
 
     #[derive(Clone)]
@@ -99,8 +112,22 @@ pub mod remote_exec {
 
         pub fn check_params(&self, call_params: &Params) -> Result<()> {
             for p in self.params.iter() {
-                if call_params.get(p.name).is_none() {
-                    return Err(Error::ParamNotFound(p.name.to_owned()));
+                let value = match call_params.get(p.name) {
+                    Some(v) if v.is_empty() && p.required => {
+                        return Err(Error::ParamInvalid(p.name.to_owned()));
+                    }
+                    Some(v) => v,
+                    None if p.required => {
+                        return Err(Error::ParamNotFound(p.name.to_owned()));
+                    }
+                    _ => continue,
+                };
+                if !value.is_empty()
+                    && !regex::Regex::new(&p.regex.unwrap_or(DEFAULT_PARAM_REGEX))
+                        .unwrap()
+                        .is_match(value)
+                {
+                    return Err(Error::ParamInvalid(p.name.to_owned()));
                 }
             }
             Ok(())
@@ -110,24 +137,6 @@ pub mod remote_exec {
     pub struct Params<'a>(pub &'a [pb::Parameter]);
 
     impl Params<'_> {
-        pub fn is_valid(&self) -> bool {
-            for p in self.0.iter() {
-                if p.key.is_none() {
-                    return false;
-                }
-                let Some(value) = p.value.as_ref() else {
-                    return false;
-                };
-                for c in value.as_bytes() {
-                    match c {
-                        b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'-' | b'_' => (),
-                        _ => return false,
-                    }
-                }
-            }
-            true
-        }
-
         pub fn get(&self, key: &str) -> Option<&str> {
             for p in self.0.iter() {
                 match &p.key {
@@ -190,6 +199,130 @@ pub mod remote_exec {
                 }
                 .gen_id(),
             );
+        }
+
+        #[test]
+        fn test_check_params() {
+            let required_param = Parameter {
+                name: "required",
+                regex: Some("^[0-9]+$"),
+                required: true,
+                param_type: ParamType::Text,
+                description: "",
+            };
+            let required_param2 = Parameter {
+                name: "required2",
+                regex: Some("^[0-9]+$"),
+                required: true,
+                param_type: ParamType::Text,
+                description: "",
+            };
+            let no_required_param = Parameter {
+                name: "no_required",
+                regex: Some("^[0-9]+$"),
+                required: false,
+                param_type: ParamType::Text,
+                description: "",
+            };
+            let no_required_param2 = Parameter {
+                name: "no_required",
+                regex: Some("^[0-9]+$"),
+                required: false,
+                param_type: ParamType::Text,
+                description: "",
+            };
+            let default_regex_param = Parameter {
+                name: "required",
+                regex: None,
+                required: true,
+                param_type: ParamType::Text,
+                description: "",
+            };
+            let required_cmd = Command {
+                cmdline: "test $required",
+                params: vec![required_param, required_param2],
+                ..Default::default()
+            };
+            let no_required_cmd = Command {
+                cmdline: "test $required",
+                params: vec![no_required_param, no_required_param2],
+                ..Default::default()
+            };
+            let default_regex_cmd = Command {
+                cmdline: "test $required",
+                params: vec![default_regex_param],
+                ..Default::default()
+            };
+            let params = vec![
+                pb::Parameter {
+                    key: Some("required".to_string()),
+                    value: Some("123".to_string()),
+                },
+                pb::Parameter {
+                    key: Some("required2".to_string()),
+                    value: Some("456".to_string()),
+                },
+            ];
+            let params = Params(&params);
+            let not_found_params = vec![pb::Parameter {
+                key: Some("not_found".to_string()),
+                value: Some("not_found".to_string()),
+            }];
+            let not_found_params = Params(&not_found_params);
+            let empty_params = vec![pb::Parameter {
+                key: Some("required".to_string()),
+                value: Some("".to_string()),
+            }];
+            let empty_params = Params(&empty_params);
+            let valid_regex_params = vec![
+                pb::Parameter {
+                    key: Some("required".to_string()),
+                    value: Some("123".to_string()),
+                },
+                pb::Parameter {
+                    key: Some("required2".to_string()),
+                    value: Some("456".to_string()),
+                },
+            ];
+            let valid_regex_params = Params(&valid_regex_params);
+            let invalid_regex_params = vec![
+                pb::Parameter {
+                    key: Some("required".to_string()),
+                    value: Some("123".to_string()),
+                },
+                pb::Parameter {
+                    key: Some("required2".to_string()),
+                    value: Some("abc".to_string()),
+                },
+            ];
+            let invalid_regex_params = Params(&invalid_regex_params);
+            let valid_default_regex_params = vec![pb::Parameter {
+                key: Some("required".to_string()),
+                value: Some("123abc-_".to_string()),
+            }];
+            let valid_default_regex_params = Params(&valid_default_regex_params);
+            let invalid_default_regex_params = vec![pb::Parameter {
+                key: Some("required".to_string()),
+                value: Some("123abc-_$".to_string()), // invaild char: $
+            }];
+            let invalid_default_regex_params = Params(&invalid_default_regex_params);
+
+            assert!(required_cmd.check_params(&params).is_ok());
+            assert!(required_cmd.check_params(&not_found_params).is_err());
+            assert!(required_cmd.check_params(&empty_params).is_err());
+            assert!(required_cmd.check_params(&valid_regex_params).is_ok());
+            assert!(required_cmd.check_params(&invalid_regex_params).is_err());
+            assert!(no_required_cmd.check_params(&params).is_ok());
+            assert!(no_required_cmd.check_params(&not_found_params).is_ok());
+            assert!(no_required_cmd.check_params(&empty_params).is_ok());
+            assert!(no_required_cmd.check_params(&valid_regex_params).is_ok());
+            assert!(no_required_cmd.check_params(&invalid_regex_params).is_ok());
+            assert!(default_regex_cmd
+                .check_params(&valid_default_regex_params)
+                .is_ok());
+            assert!(default_regex_cmd
+                .check_params(&invalid_default_regex_params)
+                .is_err());
         }
     }
 }
