8a2a5918d3aef086d02dde05ef3a5fe488b41888
yuanchao <yuanchao@yunshan.net>
2024-08-20 11:04:12 +0800

    fix: incorrect fast path map size

--- agent/src/trident.rs
+++ agent/src/trident.rs
@@ -1613,7 +1613,7 @@ impl AgentComponents {
         let (policy_setter, policy_getter) = Policy::new(
             1.max(yaml_config.src_interfaces.len()),
             yaml_config.first_path_level as usize,
-            yaml_config.fast_path_map_size,
+            yaml_config.get_fast_path_map_size(candidate_config.dispatcher.max_memory),
             yaml_config.forward_capacity,
             yaml_config.fast_path_disabled,
         );
