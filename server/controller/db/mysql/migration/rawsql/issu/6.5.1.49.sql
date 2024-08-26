d918401a8405a6191ac6c5e2ef869d6bdca63de6
weiqiang <weiqiang@yunshan.net>
2024-08-26 14:14:32 +0800

    fix: issu sql version

--- server/controller/db/mysql/migration/rawsql/issu/6.5.1.49.sql
+++ server/controller/db/mysql/migration/rawsql/issu/6.5.1.49.sql
@@ -55,5 +55,5 @@ END;
 CALL UpdateEnableFeatures();
 
 -- update db_version to latest, remeber update DB_VERSION_EXPECT in migrate/init.go
-UPDATE db_version SET version='6.6.1.11';
+UPDATE db_version SET version='6.5.1.49';
 -- modify end
