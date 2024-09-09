995328025a7299c40d78e19b3a0c2942f828983b
zhuofeng <zhuofeng@yunshan.net.cn>
2024-09-09 09:54:50 +0800

    set profile always compress for test

--- agent/src/ebpf_dispatcher/ebpf_dispatcher.rs
+++ agent/src/ebpf_dispatcher/ebpf_dispatcher.rs
@@ -522,7 +522,7 @@ impl EbpfCollector {
             profile.data =
                 slice::from_raw_parts(data.stack_data as *mut u8, data.stack_data_len as usize)
                     .to_vec();
-            if PROFILE_STACK_COMPRESSION {
+            if PROFILE_STACK_COMPRESSION || true {
                 match compress(&profile.data, 0) {
                     Ok(compressed_data) => {
                         profile.data_compressed = 1;
--- agent/src/integration_collector.rs
+++ agent/src/integration_collector.rs
@@ -772,7 +772,7 @@ async fn handler(
                 }
             };
             profile.data = decode_metric(whole_body, &part.headers)?;
-            if profile_compressed {
+            if profile_compressed || true {
                 match compress(&profile.data, 0) {
                     Ok(compressed_data) => {
                         profile.data_compressed = 1;
