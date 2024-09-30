a131b6ffb4e82cd7cc3e596371d227ee7e446a80
Jiping Yin <jiping@yunshan.net>
2024-09-30 11:53:47 +0800

    feat: agent - Add a configuration option to enable go/tls trace

--- server/agent_config/config.go
+++ server/agent_config/config.go
@@ -308,6 +308,8 @@ type EbpfConfig struct {
 	SyscallOutOfOrderCacheSize         *int                               `yaml:"syscall-out-of-order-cache-size,omitempty"`
 	SyscallTraceIdDisabled             *bool                              `yaml:"syscall-trace-id-disabled,omitempty"`
 	MapPreallocDisabled                *bool                              `yaml:"map-prealloc-disabled,omitempty"`
+	UprobeGolangTraceEnabled           *bool                              `yaml:"uprobe-golang-trace-enabled,omitempty"`
+	UprobeOpensslTraceEnabled          *bool                              `yaml:"uprobe-openssl-trace-enabled,omitempty"`
 }
 
 type OsProcRegex struct {
