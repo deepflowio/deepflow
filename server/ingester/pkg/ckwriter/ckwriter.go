16f9394d9a75c015ad7597cb240f075f7ec08e90
zhuofeng <zhuofeng@yunshan.net.cn>
2024-12-11 18:07:23 +0800

    fix: nil pointer may cause panic

--- server/ingester/pkg/ckwriter/ckwriter.go
+++ server/ingester/pkg/ckwriter/ckwriter.go
@@ -380,7 +380,7 @@ func (c *Cache) Add(item CKItem) error {
 }
 
 func (c *Cache) Write() error {
-	if c.batchSize == 0 {
+	if c.batchSize == 0 || IsNil(c.batch) {
 		return nil
 	}
 	conn := c.conns[c.writeCounter%c.connCount]
