994872354e9a01486ae7193d1fc9f19e5372dc1d
zhengya <zhengya@yunshan.net>
2024-12-23 19:28:27 +0800

    feat: recorder cleans connection data according domain

--- server/controller/recorder/cleaner.go
+++ server/controller/recorder/cleaner.go
@@ -143,6 +143,15 @@ func (c *Cleaners) cleanDeletedData() {
 	}
 }
 
+func getIDsByDomainLcuuid[MT constraint.MySQLModel](db *mysql.DB, domainLcuuid string) (ids []int) {
+	var dbItems []*MT
+	db.Where("domain = ?", domainLcuuid).Select("id").Find(&dbItems)
+	for _, item := range dbItems {
+		ids = append(ids, (*item).GetID())
+	}
+	return
+}
+
 func (c *Cleaners) timedCleanDirtyData(sContext context.Context) {
 	c.cleanDirtyData()
 	go func() {
@@ -270,28 +279,25 @@ func (c *Cleaner) cleanDirtyData() {
 	}
 
 	log.Info(c.org.Logf("clean dirty data started"))
-	c.cleanVMDirty()
+
+	var domains []mysql.Domain
+	if err := mysql.Db.Find(&domains).Error; err != nil {
+		log.Errorf("failed to get domains: %v", err)
+		return
+	}
+	for _, domain := range domains {
+		c.cleanPodNodeDirty(domain.Lcuuid)
+		c.cleanVMDirty(domain.Lcuuid)
+	}
+
 	c.cleanNetworkDirty()
 	c.cleanVRouterDirty()
 	c.cleanPodIngressDirty()
 	c.cleanPodServiceDirty()
-	c.cleanPodNodeDirty()
 	c.cleanPodGroupDirty()
 	c.cleanPodDirty()
 	c.cleanVInterfaceDirty()
 	log.Info(c.org.Logf("clean dirty data completed"))
-}
-
-func (c *Cleaner) cleanVMDirty() {
-	vmIDs := getIDs[mysql.VM](c.org.DB)
-	if len(vmIDs) != 0 {
-		var vifs []*mysql.VInterface
-		c.org.DB.Where("devicetype = ? AND deviceid NOT IN ?", ctrlrcommon.VIF_DEVICE_TYPE_VM, vmIDs).Find(&vifs)
-		if len(vifs) != 0 {
-			c.org.DB.Delete(&vifs)
-			log.Error(c.org.Logf(formatLogDeleteABecauseBHasGone(ctrlrcommon.RESOURCE_TYPE_VINTERFACE_EN, ctrlrcommon.RESOURCE_TYPE_VM_EN, vifs)))
-		}
-	}
 }
 
 func (c *Cleaner) cleanNetworkDirty() {
@@ -383,29 +389,48 @@ func (c *Cleaner) cleanPodGroupDirty() {
 	}
 }
 
-func (c *Cleaner) cleanPodNodeDirty() {
-	podNodeIDs := getIDs[mysql.PodNode](c.org.DB)
+func (c *Cleaner) cleanPodNodeDirty(domainLcuuid string) {
+	podNodeIDs := getIDsByDomainLcuuid[mysql.PodNode](c.org.DB, domainLcuuid)
 	if len(podNodeIDs) != 0 {
 		var vifs []*mysql.VInterface
-		c.org.DB.Where("devicetype = ? AND deviceid NOT IN ?", ctrlrcommon.VIF_DEVICE_TYPE_POD_NODE, podNodeIDs).Find(&vifs)
+		c.org.DB.Where("domain = ? AND devicetype = ? AND deviceid NOT IN ?", domainLcuuid, ctrlrcommon.VIF_DEVICE_TYPE_POD_NODE, podNodeIDs).Find(&vifs)
 		if len(vifs) != 0 {
 			c.org.DB.Delete(&vifs)
 			log.Error(c.org.Logf(formatLogDeleteABecauseBHasGone(ctrlrcommon.RESOURCE_TYPE_VINTERFACE_EN, ctrlrcommon.RESOURCE_TYPE_POD_NODE_EN, vifs)))
 		}
 
 		var vmPodNodeConns []*mysql.VMPodNodeConnection
-		c.org.DB.Where("pod_node_id NOT IN ?", podNodeIDs).Find(&vmPodNodeConns)
+		c.org.DB.Where("domain = ? AND pod_node_id NOT IN ?", domainLcuuid, podNodeIDs).Find(&vmPodNodeConns)
 		if len(vmPodNodeConns) != 0 {
 			c.org.DB.Delete(&vmPodNodeConns)
 			log.Error(c.org.Logf(formatLogDeleteABecauseBHasGone(ctrlrcommon.RESOURCE_TYPE_VM_POD_NODE_CONNECTION_EN, ctrlrcommon.RESOURCE_TYPE_POD_NODE_EN, vmPodNodeConns)))
 		}
 
 		var pods []*mysql.Pod
-		c.org.DB.Where("pod_node_id != 0 AND pod_node_id NOT IN ?", podNodeIDs).Find(&pods)
+		c.org.DB.Where("domain = ? AND pod_node_id != 0 AND pod_node_id NOT IN ?", domainLcuuid, podNodeIDs).Find(&pods)
 		if len(pods) != 0 {
 			c.org.DB.Delete(&pods)
 			publishTagrecorder(c.org.DB, pods, ctrlrcommon.RESOURCE_TYPE_POD_EN, c.toolData)
 			log.Error(c.org.Logf(formatLogDeleteABecauseBHasGone(ctrlrcommon.RESOURCE_TYPE_POD_EN, ctrlrcommon.RESOURCE_TYPE_POD_NODE_EN, pods)))
+		}
+	}
+}
+
+func (c *Cleaner) cleanVMDirty(domainLcuuid string) {
+	vmIDs := getIDsByDomainLcuuid[mysql.VM](c.org.DB, domainLcuuid)
+	if len(vmIDs) != 0 {
+		var vifs []*mysql.VInterface
+		mysql.Db.Where("domain = ? AND devicetype = ? AND deviceid NOT IN ?", domainLcuuid, ctrlrcommon.VIF_DEVICE_TYPE_VM, vmIDs).Find(&vifs)
+		if len(vifs) != 0 {
+			mysql.Db.Delete(&vifs)
+			log.Error(c.org.Logf(formatLogDeleteABecauseBHasGone(ctrlrcommon.RESOURCE_TYPE_VINTERFACE_EN, ctrlrcommon.RESOURCE_TYPE_VM_EN, vifs)))
+		}
+
+		var vmPodNodeConns []*mysql.VMPodNodeConnection
+		mysql.Db.Where("domain = ? AND vm_id NOT IN ?", domainLcuuid, vmIDs).Find(&vmPodNodeConns)
+		if len(vmPodNodeConns) != 0 {
+			mysql.Db.Delete(&vmPodNodeConns)
+			log.Error(c.org.Logf(formatLogDeleteABecauseBHasGone(ctrlrcommon.RESOURCE_TYPE_VM_POD_NODE_CONNECTION_EN, ctrlrcommon.RESOURCE_TYPE_VM_EN, vmPodNodeConns)))
 		}
 	}
 }
