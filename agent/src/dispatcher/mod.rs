1ff376b4e130f7f7d3db1a619e009c23c8e15815
yuanchao <yuanchao@yunshan.net>
2025-01-06 19:29:09 +0800

    feat: support for turning off promiscuous mode

--- agent/src/dispatcher/mod.rs
+++ agent/src/dispatcher/mod.rs
@@ -1064,6 +1064,7 @@ impl DispatcherBuilder {
                 .take()
                 .ok_or(Error::ConfigIncomplete("no trim tunnel type".into()))?,
             bond_group_map,
+            promisc_if_indices: vec![],
         };
         collector.register_countable(
             &stats::SingleTagModule("dispatcher", "id", base.id),
