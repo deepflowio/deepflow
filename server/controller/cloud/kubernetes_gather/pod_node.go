686d10a61e636cdf4b1bb1036a26dc83cdf54cbb
kangxiang <kangxiang@yunshan.net>
2025-02-28 14:24:50 +0800

    feat: verify node internal address

--- server/controller/cloud/kubernetes_gather/pod_node.go
+++ server/controller/cloud/kubernetes_gather/pod_node.go
@@ -60,6 +60,10 @@ func (k *KubernetesGather) getPodNodes() (podNodes []model.PodNode, nodeNetwork,
 				continue
 			}
 			nIP := nodeIPitem.Get("address").MustString()
+			if nIP == "" {
+				log.Warningf("invalid node internal address (%s)", nIP)
+				continue
+			}
 			nodeIPs = append(nodeIPs, nIP)
 		}
 		if len(nodeIPs) == 0 {
