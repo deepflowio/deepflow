yunshan-net/sophon

fix: tagrecorder fails to synchronize ch_app_table

server/controller/tagrecorder/db_operator.go
@@ -24,10 +24,10 @@ import (
 )
 
 type operator[MT MySQLChModel, KT ChModelKey] interface {
-	batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *metadb.DB), db *metadb.DB)
-	add(keys []KT, dbItems []MT, db *metadb.DB)
-	update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *metadb.DB)
-	delete(keys []KT, dbItems []MT, db *metadb.DB)
+	batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *metadb.DB) error, db *metadb.DB) error
+	add(keys []KT, dbItems []MT, db *metadb.DB) error
+	update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *metadb.DB) error
+	delete(keys []KT, dbItems []MT, db *metadb.DB) error
 	setConfig(config.ControllerConfig)
 }
 
@@ -46,7 +46,7 @@ func (b *operatorComponent[MT, KT]) setConfig(cfg config.ControllerConfig) {
 	b.cfg = cfg
 }
 
-func (b *operatorComponent[MT, KT]) batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *metadb.DB), db *metadb.DB) {
+func (b *operatorComponent[MT, KT]) batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *metadb.DB) error, db *metadb.DB) error {
 	count := len(items)
 	offset := b.cfg.TagRecorderCfg.MySQLBatchSize
 	var pages int
@@ -61,35 +61,42 @@ func (b *operatorComponent[MT, KT]) batchPage(keys []KT, items []MT, operateFunc
 		if end > count {
 			end = count
 		}
-		operateFunc(keys[start:end], items[start:end], db)
+		err := operateFunc(keys[start:end], items[start:end], db)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func (b *operatorComponent[MT, KT]) add(keys []KT, dbItems []MT, db *metadb.DB) {
+func (b *operatorComponent[MT, KT]) add(keys []KT, dbItems []MT, db *metadb.DB) error {
 	err := db.Clauses(clause.OnConflict{
 		UpdateAll: true,
 	}).Create(&dbItems).Error
 	if err != nil {
 		log.Errorf("add %s (keys: %+v values: %+v) failed: %s", b.resourceTypeName, keys, dbItems, err.Error(), db.LogPrefixORGID) // TODO is key needed?
-		return
+		return err
 	}
 	log.Infof("add %s (keys: %+v values: %+v) success", b.resourceTypeName, keys, dbItems, db.LogPrefixORGID)
+	return nil
 }
 
-func (b *operatorComponent[MT, KT]) update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *metadb.DB) {
+func (b *operatorComponent[MT, KT]) update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *metadb.DB) error {
 	err := db.Model(&oldDBItem).Updates(updateInfo).Error
 	if err != nil {
 		log.Errorf("update %s (key: %+v value: %+v) failed: %s", b.resourceTypeName, key, oldDBItem, err.Error(), db.LogPrefixORGID)
-		return
+		return err
 	}
 	log.Infof("update %s (key: %+v value: %+v, update info: %v) success", b.resourceTypeName, key, oldDBItem, updateInfo, db.LogPrefixORGID)
+	return nil
 }
 
-func (b *operatorComponent[MT, KT]) delete(keys []KT, dbItems []MT, db *metadb.DB) {
+func (b *operatorComponent[MT, KT]) delete(keys []KT, dbItems []MT, db *metadb.DB) error {
 	err := db.Delete(&dbItems).Error
 	if err != nil {
 		log.Errorf("delete %s (keys: %+v values: %+v) failed: %s", b.resourceTypeName, keys, dbItems, err.Error(), db.LogPrefixORGID)
-		return
+		return err
 	}
 	log.Infof("delete %s (keys: %+v values: %+v) success", b.resourceTypeName, keys, dbItems, db.LogPrefixORGID)
+	return nil
 }

deepflowio/deepflow

server/controller/tagrecorder/db_operator.go
@@ -24,10 +24,10 @@ import (
 )
 
 type operator[MT MySQLChModel, KT ChModelKey] interface {
-	batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *mysql.DB), db *mysql.DB)
-	add(keys []KT, dbItems []MT, db *mysql.DB)
-	update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *mysql.DB)
-	delete(keys []KT, dbItems []MT, db *mysql.DB)
+	batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *mysql.DB) error, db *mysql.DB) error
+	add(keys []KT, dbItems []MT, db *mysql.DB) error
+	update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *mysql.DB) error
+	delete(keys []KT, dbItems []MT, db *mysql.DB) error
 	setConfig(config.ControllerConfig)
 }
 
@@ -46,7 +46,7 @@ func (b *operatorComponent[MT, KT]) setConfig(cfg config.ControllerConfig) {
 	b.cfg = cfg
 }
 
-func (b *operatorComponent[MT, KT]) batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *mysql.DB), db *mysql.DB) {
+func (b *operatorComponent[MT, KT]) batchPage(keys []KT, items []MT, operateFunc func([]KT, []MT, *mysql.DB) error, db *mysql.DB) error {
 	count := len(items)
 	offset := b.cfg.TagRecorderCfg.MySQLBatchSize
 	var pages int
@@ -61,35 +61,42 @@ func (b *operatorComponent[MT, KT]) batchPage(keys []KT, items []MT, operateFunc
 		if end > count {
 			end = count
 		}
-		operateFunc(keys[start:end], items[start:end], db)
+		err := operateFunc(keys[start:end], items[start:end], db)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func (b *operatorComponent[MT, KT]) add(keys []KT, dbItems []MT, db *mysql.DB) {
+func (b *operatorComponent[MT, KT]) add(keys []KT, dbItems []MT, db *mysql.DB) error {
 	err := db.Clauses(clause.OnConflict{
 		UpdateAll: true,
 	}).Create(&dbItems).Error
 	if err != nil {
 		log.Errorf("add %s (keys: %+v values: %+v) failed: %s", b.resourceTypeName, keys, dbItems, err.Error(), db.LogPrefixORGID) // TODO is key needed?
-		return
+		return err
 	}
 	log.Infof("add %s (keys: %+v values: %+v) success", b.resourceTypeName, keys, dbItems, db.LogPrefixORGID)
+	return nil
 }
 
-func (b *operatorComponent[MT, KT]) update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *mysql.DB) {
+func (b *operatorComponent[MT, KT]) update(oldDBItem MT, updateInfo map[string]interface{}, key KT, db *mysql.DB) error {
 	err := db.Model(&oldDBItem).Updates(updateInfo).Error
 	if err != nil {
 		log.Errorf("update %s (key: %+v value: %+v) failed: %s", b.resourceTypeName, key, oldDBItem, err.Error(), db.LogPrefixORGID)
-		return
+		return err
 	}
 	log.Infof("update %s (key: %+v value: %+v, update info: %v) success", b.resourceTypeName, key, oldDBItem, updateInfo, db.LogPrefixORGID)
+	return nil
 }
 
-func (b *operatorComponent[MT, KT]) delete(keys []KT, dbItems []MT, db *mysql.DB) {
+func (b *operatorComponent[MT, KT]) delete(keys []KT, dbItems []MT, db *mysql.DB) error {
 	err := db.Delete(&dbItems).Error
 	if err != nil {
 		log.Errorf("delete %s (keys: %+v values: %+v) failed: %s", b.resourceTypeName, keys, dbItems, err.Error(), db.LogPrefixORGID)
-		return
+		return err
 	}
 	log.Infof("delete %s (keys: %+v values: %+v) success", b.resourceTypeName, keys, dbItems, db.LogPrefixORGID)
+	return nil
 }