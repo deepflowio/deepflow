yunshan-net/sophon

feat: tagrecorder support custom service

server/controller/recorder/constraint/constraint.go
@@ -46,7 +46,7 @@ type MySQLModel interface {
 		metadbmodel.PodNode | metadbmodel.VMPodNodeConnection | metadbmodel.PodNamespace | metadbmodel.PodIngress |
 		metadbmodel.PodIngressRule | metadbmodel.PodIngressRuleBackend | metadbmodel.PodService |
 		metadbmodel.PodServicePort | metadbmodel.PodGroup | metadbmodel.PodGroupPort | metadbmodel.PodReplicaSet |
-		metadbmodel.Pod | metadbmodel.Process | metadbmodel.VIP
+		metadbmodel.Pod | metadbmodel.Process | metadbmodel.VIP | metadbmodel.CustomService
 
 	GetID() int
 }

server/controller/recorder/pubsub/message/add.go
@@ -183,3 +183,7 @@ type PodAdd struct {
 type ProcessAdd struct {
 	MySQLItems[metadbmodel.Process]
 }
+
+type CustomServiceAdd struct {
+	MySQLItems[metadbmodel.CustomService]
+}

server/controller/recorder/pubsub/message/delete.go
@@ -249,3 +249,8 @@ type ProcessDelete struct {
 	Lcuuids
 	MySQLItems[metadbmodel.Process]
 }
+
+type CustomServiceDelete struct {
+	Lcuuids
+	MySQLItems[metadbmodel.CustomService]
+}

server/controller/recorder/pubsub/message/update.go
@@ -755,3 +755,12 @@ type ProcessUpdate struct {
 	DiffBase[*diffbase.Process]
 	MySQLData[metadbmodel.Process]
 }
+
+type CustomServiceFieldsUpdate struct {
+	Key
+	Name fieldDetail[string]
+}
+type CustomServiceUpdate struct {
+	Fields[CustomServiceFieldsUpdate]
+	MySQLData[metadbmodel.CustomService]
+}

server/controller/tagrecorder/ch_device.go
@@ -984,3 +984,57 @@ func (c *ChProcessDevice) softDeletedTargetsUpdated(targets []metadbmodel.ChDevi
 		DoUpdates: clause.AssignmentColumns([]string{"name"}),
 	}).Create(&targets)
 }
+
+type ChCustomServiceDevice struct {
+	SubscriberComponent[*message.CustomServiceFieldsUpdate, message.CustomServiceFieldsUpdate, metadbmodel.CustomService, metadbmodel.ChDevice, DeviceKey]
+	resourceTypeToIconID map[IconKey]int
+}
+
+func NewChCustomServiceDevice(resourceTypeToIconID map[IconKey]int) *ChCustomServiceDevice {
+	mng := &ChCustomServiceDevice{
+		newSubscriberComponent[*message.CustomServiceFieldsUpdate, message.CustomServiceFieldsUpdate, metadbmodel.CustomService, metadbmodel.ChDevice, DeviceKey](
+			common.RESOURCE_TYPE_CUSTOM_SERVICE_EN, RESOURCE_TYPE_CH_DEVICE,
+		),
+		resourceTypeToIconID,
+	}
+	mng.subscriberDG = mng
+	return mng
+}
+
+// sourceToTarget implements SubscriberDataGenerator
+func (c *ChCustomServiceDevice) sourceToTarget(md *message.Metadata, source *metadbmodel.CustomService) (keys []DeviceKey, targets []metadbmodel.ChDevice) {
+	iconID := c.resourceTypeToIconID[IconKey{
+		NodeType: RESOURCE_TYPE_CUSTOM_SERVICE,
+	}]
+	sourceName := source.Name
+	keys = append(keys, DeviceKey{DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+		DeviceID: source.ID})
+	targets = append(targets, metadbmodel.ChDevice{
+		DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+		DeviceID:   source.ID,
+		Name:       sourceName,
+		IconID:     iconID,
+		TeamID:     md.TeamID,
+		DomainID:   md.DomainID,
+	})
+	return
+}
+
+// onResourceUpdated implements SubscriberDataGenerator
+func (c *ChCustomServiceDevice) onResourceUpdated(sourceID int, fieldsUpdate *message.CustomServiceFieldsUpdate, db *metadb.DB) {
+	updateInfo := make(map[string]interface{})
+
+	if fieldsUpdate.Name.IsDifferent() {
+		updateInfo["name"] = fieldsUpdate.Name.GetNew()
+	}
+	if len(updateInfo) > 0 {
+		var chItem metadbmodel.ChDevice
+		db.Where("deviceid = ? and devicetype = ?", sourceID, CH_DEVICE_TYPE_CUSTOM_SERVICE).First(&chItem)
+		c.SubscriberComponent.dbOperator.update(chItem, updateInfo, DeviceKey{DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+			DeviceID: sourceID}, db)
+	}
+}
+
+// softDeletedTargetsUpdated implements SubscriberDataGenerator
+func (c *ChCustomServiceDevice) softDeletedTargetsUpdated(targets []metadbmodel.ChDevice, db *metadb.DB) {
+}

server/controller/tagrecorder/check/ch_device.go
@@ -98,6 +98,10 @@ func (d *ChDevice) generateNewData() (map[DeviceKey]metadbmodel.ChDevice, bool) 
 	if !ok {
 		return nil, false
 	}
+	ok = d.generateCustomServiceData(keyToItem)
+	if !ok {
+		return nil, false
+	}
 	d.generateIPData(keyToItem)
 	d.generateInternetData(keyToItem)
 	return keyToItem, true
@@ -722,6 +726,29 @@ func (d *ChDevice) generateProcessData(keyToItem map[DeviceKey]metadbmodel.ChDev
 				DomainID:    tagrecorder.DomainToDomainID[process.Domain],
 				SubDomainID: tagrecorder.SubDomainToSubDomainID[process.SubDomain],
 			}
+		}
+	}
+	return true
+}
+
+func (d *ChDevice) generateCustomServiceData(keyToItem map[DeviceKey]metadbmodel.ChDevice) bool {
+	customServices, err := query.FindInBatches[metadbmodel.CustomService](d.db.Unscoped())
+	if err != nil {
+		log.Errorf(dbQueryResourceFailed(d.resourceTypeName, err), d.db.LogPrefixORGID)
+		return false
+	}
+	for _, customService := range customServices {
+		key := DeviceKey{
+			DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+			DeviceID:   customService.ID,
+		}
+		keyToItem[key] = metadbmodel.ChDevice{
+			DeviceType: CH_DEVICE_TYPE_GPROCESS,
+			DeviceID:   customService.ID,
+			Name:       customService.Name,
+			IconID:     d.resourceTypeToIconID[IconKey{NodeType: RESOURCE_TYPE_CUSTOM_SERVICE}],
+			TeamID:     tagrecorder.DomainToTeamID[customService.Domain],
+			DomainID:   tagrecorder.DomainToDomainID[customService.Domain],
 		}
 	}
 	return true

deepflowio/deepflow

server/controller/recorder/constraint/constraint.go
@@ -46,7 +46,7 @@ type MySQLModel interface {
 		mysqlmodel.PodNode | mysqlmodel.VMPodNodeConnection | mysqlmodel.PodNamespace | mysqlmodel.PodIngress |
 		mysqlmodel.PodIngressRule | mysqlmodel.PodIngressRuleBackend | mysqlmodel.PodService |
 		mysqlmodel.PodServicePort | mysqlmodel.PodGroup | mysqlmodel.PodGroupPort | mysqlmodel.PodReplicaSet |
-		mysqlmodel.Pod | mysqlmodel.Process | mysqlmodel.VIP
+		mysqlmodel.Pod | mysqlmodel.Process | mysqlmodel.VIP | mysqlmodel.CustomService
 
 	GetID() int
 }

server/controller/recorder/pubsub/message/add.go
@@ -183,3 +183,7 @@ type PodAdd struct {
 type ProcessAdd struct {
 	MySQLItems[mysqlmodel.Process]
 }
+
+type CustomServiceAdd struct {
+	MySQLItems[mysqlmodel.CustomService]
+}

server/controller/recorder/pubsub/message/delete.go
@@ -249,3 +249,8 @@ type ProcessDelete struct {
 	Lcuuids
 	MySQLItems[mysqlmodel.Process]
 }
+
+type CustomServiceDelete struct {
+	Lcuuids
+	MySQLItems[mysqlmodel.CustomService]
+}

server/controller/recorder/pubsub/message/update.go
@@ -755,3 +755,12 @@ type ProcessUpdate struct {
 	DiffBase[*diffbase.Process]
 	MySQLData[mysqlmodel.Process]
 }
+
+type CustomServiceFieldsUpdate struct {
+	Key
+	Name fieldDetail[string]
+}
+type CustomServiceUpdate struct {
+	Fields[CustomServiceFieldsUpdate]
+	MySQLData[mysqlmodel.CustomService]
+}

server/controller/tagrecorder/ch_device.go
@@ -984,3 +984,57 @@ func (c *ChProcessDevice) softDeletedTargetsUpdated(targets []mysqlmodel.ChDevic
 		DoUpdates: clause.AssignmentColumns([]string{"name"}),
 	}).Create(&targets)
 }
+
+type ChCustomServiceDevice struct {
+	SubscriberComponent[*message.CustomServiceFieldsUpdate, message.CustomServiceFieldsUpdate, mysqlmodel.CustomService, mysqlmodel.ChDevice, DeviceKey]
+	resourceTypeToIconID map[IconKey]int
+}
+
+func NewChCustomServiceDevice(resourceTypeToIconID map[IconKey]int) *ChCustomServiceDevice {
+	mng := &ChCustomServiceDevice{
+		newSubscriberComponent[*message.CustomServiceFieldsUpdate, message.CustomServiceFieldsUpdate, mysqlmodel.CustomService, mysqlmodel.ChDevice, DeviceKey](
+			common.RESOURCE_TYPE_CUSTOM_SERVICE_EN, RESOURCE_TYPE_CH_DEVICE,
+		),
+		resourceTypeToIconID,
+	}
+	mng.subscriberDG = mng
+	return mng
+}
+
+// sourceToTarget implements SubscriberDataGenerator
+func (c *ChCustomServiceDevice) sourceToTarget(md *message.Metadata, source *mysqlmodel.CustomService) (keys []DeviceKey, targets []mysqlmodel.ChDevice) {
+	iconID := c.resourceTypeToIconID[IconKey{
+		NodeType: RESOURCE_TYPE_CUSTOM_SERVICE,
+	}]
+	sourceName := source.Name
+	keys = append(keys, DeviceKey{DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+		DeviceID: source.ID})
+	targets = append(targets, mysqlmodel.ChDevice{
+		DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+		DeviceID:   source.ID,
+		Name:       sourceName,
+		IconID:     iconID,
+		TeamID:     md.TeamID,
+		DomainID:   md.DomainID,
+	})
+	return
+}
+
+// onResourceUpdated implements SubscriberDataGenerator
+func (c *ChCustomServiceDevice) onResourceUpdated(sourceID int, fieldsUpdate *message.CustomServiceFieldsUpdate, db *mysql.DB) {
+	updateInfo := make(map[string]interface{})
+
+	if fieldsUpdate.Name.IsDifferent() {
+		updateInfo["name"] = fieldsUpdate.Name.GetNew()
+	}
+	if len(updateInfo) > 0 {
+		var chItem mysqlmodel.ChDevice
+		db.Where("deviceid = ? and devicetype = ?", sourceID, CH_DEVICE_TYPE_CUSTOM_SERVICE).First(&chItem)
+		c.SubscriberComponent.dbOperator.update(chItem, updateInfo, DeviceKey{DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+			DeviceID: sourceID}, db)
+	}
+}
+
+// softDeletedTargetsUpdated implements SubscriberDataGenerator
+func (c *ChCustomServiceDevice) softDeletedTargetsUpdated(targets []mysqlmodel.ChDevice, db *mysql.DB) {
+}

server/controller/tagrecorder/check/ch_device.go
@@ -98,6 +98,10 @@ func (d *ChDevice) generateNewData() (map[DeviceKey]mysqlmodel.ChDevice, bool) {
 	if !ok {
 		return nil, false
 	}
+	ok = d.generateCustomServiceData(keyToItem)
+	if !ok {
+		return nil, false
+	}
 	d.generateIPData(keyToItem)
 	d.generateInternetData(keyToItem)
 	return keyToItem, true
@@ -722,6 +726,29 @@ func (d *ChDevice) generateProcessData(keyToItem map[DeviceKey]mysqlmodel.ChDevi
 				DomainID:    tagrecorder.DomainToDomainID[process.Domain],
 				SubDomainID: tagrecorder.SubDomainToSubDomainID[process.SubDomain],
 			}
+		}
+	}
+	return true
+}
+
+func (d *ChDevice) generateCustomServiceData(keyToItem map[DeviceKey]mysqlmodel.ChDevice) bool {
+	customServices, err := query.FindInBatches[mysqlmodel.CustomService](d.db.Unscoped())
+	if err != nil {
+		log.Errorf(dbQueryResourceFailed(d.resourceTypeName, err), d.db.LogPrefixORGID)
+		return false
+	}
+	for _, customService := range customServices {
+		key := DeviceKey{
+			DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+			DeviceID:   customService.ID,
+		}
+		keyToItem[key] = mysqlmodel.ChDevice{
+			DeviceType: CH_DEVICE_TYPE_CUSTOM_SERVICE,
+			DeviceID:   customService.ID,
+			Name:       customService.Name,
+			IconID:     d.resourceTypeToIconID[IconKey{NodeType: RESOURCE_TYPE_CUSTOM_SERVICE}],
+			TeamID:     tagrecorder.DomainToTeamID[customService.Domain],
+			DomainID:   tagrecorder.DomainToDomainID[customService.Domain],
 		}
 	}
 	return true