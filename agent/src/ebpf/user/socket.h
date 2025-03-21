bcff902d0019b0ee9ab802d4e0cae289cf60a940
Jiping Yin <jiping@yunshan.net>
2025-03-21 18:03:18 +0800

    feat: agnet - eBPF Support pure Kprobe/Kretprobe type probing

--- agent/src/ebpf/user/socket.h
+++ agent/src/ebpf/user/socket.h
@@ -49,6 +49,7 @@ enum linux_kernel_type {
 	K_TYPE_VER_3_10,
 	K_TYPE_KFUNC,
 	K_TYPE_RT,
+	K_TYPE_KPROBE,
 };
 
 enum probes_act_type {
@@ -412,4 +413,5 @@ int disable_syscall_trace_id(void);
 void config_probe(struct tracer_probes_conf *tps, int type, const char *fn,
 		  const char *tp_name, bool is_exit);
 void uprobe_match_pid_handle(int feat, int pid, enum match_pids_act act);
+bool is_pure_kprobe_ebpf(void);
 #endif /* DF_USER_SOCKET_H */
