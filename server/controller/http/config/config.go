b5bc42ee68f3c882bba86f842ad76451815f8917
zhengya <zhengya@yunshan.net>
2025-04-02 17:22:16 +0800

    feat: adds http configuration

--- server/controller/http/config/config.go
+++ server/controller/http/config/config.go
@@ -17,6 +17,7 @@
 package config
 
 type Config struct {
-	RedisRefreshInterval int      `default:"3600" yaml:"redis_refresh_interval"`
-	AdditionalDomains    []string `yaml:"additional_domains"`
+	ResourceAPIRedisRefreshInterval int      `default:"3600" yaml:"redis_refresh_interval"`
+	ResourceAPIRedisEnabled         bool     `default:"false" yaml:"resource_api_redis_enabled"`
+	AdditionalDomains               []string `yaml:"additional_domains"`
 }
