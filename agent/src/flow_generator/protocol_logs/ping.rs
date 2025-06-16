e9312a15ce07fc42e63fd524242669f498472f78
JIN Jie <jinjie@yunshan.net>
2025-06-16 17:24:46 +0800

    fix: Incorrect L7Log resp timeout status

--- agent/src/flow_generator/protocol_logs/ping.rs
+++ agent/src/flow_generator/protocol_logs/ping.rs
@@ -162,7 +162,6 @@ impl L7ProtocolParserInterface for PingLog {
                     proto: L7Protocol::Ping,
                     sequence: icmp_data.echo_id_seq as u16,
                     id: (icmp_data.echo_id_seq >> 16) as u16,
-                    status: L7ResponseStatus::Timeout,
                     ..Default::default()
                 };
                 set_captured_byte!(info, param);
