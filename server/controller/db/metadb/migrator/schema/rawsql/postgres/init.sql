75d22afacb89cd3228352625c41bd73d7e274494
zhengya <zhengya@yunshan.net>
2025-09-11 10:46:24 +0800

    feat: add valid field to acl table

--- server/controller/db/metadb/migrator/schema/rawsql/postgres/init.sql
+++ server/controller/db/metadb/migrator/schema/rawsql/postgres/init.sql
@@ -1645,6 +1645,7 @@ CREATE TABLE IF NOT EXISTS acl (
     type                   INTEGER DEFAULT 2,
     tap_type               INTEGER DEFAULT 3,
     state                  INTEGER DEFAULT 1,
+    valid                  INTEGER DEFAULT 1,
     applications           VARCHAR(64) NOT NULL,
     epc_id                 INTEGER,
     src_group_ids          TEXT,
@@ -1666,6 +1667,7 @@ COMMENT ON COLUMN acl.src_group_ids IS 'separated by ,';
 COMMENT ON COLUMN acl.dst_group_ids IS 'separated by ,';
 COMMENT ON COLUMN acl.src_ports IS 'separated by ,';
 COMMENT ON COLUMN acl.dst_ports IS 'separated by ,';
+COMMENT ON COLUMN acl.valid IS '0-invalid; 1-valid';
 
 CREATE TABLE IF NOT EXISTS resource_group (
     id                      SERIAL PRIMARY KEY,
