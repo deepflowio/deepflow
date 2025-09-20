adb7270bd80fbbbdf052757ca7856eff21a6d07e
zhengya <zhengya@yunshan.net>
2025-09-20 15:57:50 +0800

    fix: store wrong process start_time

--- server/controller/recorder/updater/process.go
+++ server/controller/recorder/updater/process.go
@@ -132,6 +132,7 @@ func (p *Process) generateDBItemToAdd(cloudItem *cloudmodel.Process) (*metadbmod
 		PID:         cloudItem.PID,
 		ProcessName: cloudItem.ProcessName,
 		CommandLine: cloudItem.CommandLine,
+		StartTime:   cloudItem.StartTime,
 		UserName:    cloudItem.UserName,
 		ContainerID: cloudItem.ContainerID,
 		OSAPPTags:   cloudItem.OSAPPTags,
