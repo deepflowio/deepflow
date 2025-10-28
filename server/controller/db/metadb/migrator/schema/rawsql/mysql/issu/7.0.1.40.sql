3b2fedc0057d4396fc16f236314e3af63f10cb65
zhengya <zhengya@yunshan.net>
2025-10-28 10:28:18 +0800

    feat: support business service

---
+++ server/controller/db/metadb/migrator/schema/rawsql/mysql/issu/7.0.1.40.sql
@@ -0,0 +1,38 @@
+DROP PROCEDURE IF EXISTS AddColumnIfNotExists;
+
+CREATE PROCEDURE AddColumnIfNotExists(
+    IN tableName VARCHAR(255),
+    IN colName VARCHAR(255),
+    IN colType VARCHAR(255),
+    IN afterCol VARCHAR(255)
+)
+BEGIN
+    DECLARE column_count INT;
+
+    SELECT COUNT(*)
+    INTO column_count
+    FROM information_schema.columns
+    WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = tableName
+    AND column_name = colName;
+
+    IF column_count = 0 THEN
+        SET @sql = CONCAT('ALTER TABLE ', tableName, ' ADD COLUMN ', colName, ' ', colType, ' AFTER ', afterCol);
+        PREPARE stmt FROM @sql;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END;
+
+CALL AddColumnIfNotExists('pod_service', 'uid', "CHAR(64) DEFAULT ''", 'domain');
+CALL AddColumnIfNotExists('pod_group', 'uid', "CHAR(64) DEFAULT ''", 'domain');
+CALL AddColumnIfNotExists('custom_service', 'match_type', "INTEGER DEFAULT 0 COMMENT '0: unkonwn 1: name match 2: uid match'", 'type');
+CALL AddColumnIfNotExists('custom_service', 'pod_namespace_id', "INTEGER DEFAULT 0", 'epc_id');
+CALL AddColumnIfNotExists('custom_service', 'pod_cluster_id', "INTEGER DEFAULT 0", 'epc_id');
+
+DROP PROCEDURE AddColumnIfNotExists;
+
+ALTER TABLE custom_service MODIFY COLUMN type INTEGER DEFAULT 0 COMMENT '0: unknown 1: IP 2: PORT 3: chost 4: pod_service 5: pod_group';
+ALTER TABLE custom_service MODIFY COLUMN match_type INTEGER DEFAULT 0 COMMENT '0: unkonwn 1: name match 2: uid match';
+
+UPDATE db_version SET version='7.0.1.40';
