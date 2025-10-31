9f9f73e75f396e096b805bd9db1f0fa631d31b12
zhengya <zhengya@yunshan.net>
2025-10-31 15:19:01 +0800

    feat: add metadb schema const

---
+++ server/controller/db/metadb/migrator/schema/const.go
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2024 Yunshan Networks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package schema
+
+const (
+	RAW_SQL_ROOT_DIR = "/etc/metadb/schema/rawsql"
+
+	DB_VERSION_TABLE    = "db_version"
+	DB_VERSION_EXPECTED = "6.6.1.55"
+)
