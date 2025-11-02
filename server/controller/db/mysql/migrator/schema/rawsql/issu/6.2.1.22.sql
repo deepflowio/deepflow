ce82b0788292928a0e9d807ec3e820b5db7b0b69
zhengya <zhengya@yunshan.net>
2025-11-02 11:49:01 +0800

    feat: remove mysql directory

--- server/controller/db/mysql/migrator/schema/rawsql/issu/6.2.1.22.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE domain_additional_resource MODIFY COLUMN content LONGTEXT;
-
-UPDATE db_version SET version = '6.2.1.22';
--- server/controller/db/mysql/migrator/schema/rawsql/issu/6.3.1.12.sql
+++
@@ -1,7 +0,0 @@
-
-ALTER TABLE alarm_policy MODIFY COLUMN name CHAR(128) NOT NULL;
-
--- update db_version to latest, remeber update DB_VERSION_EXPECT in migrate/init.go
-UPDATE db_version SET version='6.3.1.12';
--- modify end
-
