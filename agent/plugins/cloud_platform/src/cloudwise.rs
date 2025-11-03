0174256464fb2ef126d82f77c88b2dd517815d2c
zhuofeng <zhuofeng@yunshan.net.cn>
2025-11-03 10:37:29 +0800

    fix: empty trace id shoud not be stored

--- agent/plugins/cloud_platform/src/cloudwise.rs
+++ agent/plugins/cloud_platform/src/cloudwise.rs
@@ -18,10 +18,6 @@
 
 // CloudWise Trace format:
 // CLOUDWISE: JAVA:0:0:0:0:{trace-id}:0:0:0
-pub fn decode_trace_id(s: &str) -> Option<&str> {
-    if s.is_empty() {
-        None
-    } else {
-        Some(s)
-    }
+pub fn decode_trace_id(_: &str) -> Option<&str> {
+    None
 }
