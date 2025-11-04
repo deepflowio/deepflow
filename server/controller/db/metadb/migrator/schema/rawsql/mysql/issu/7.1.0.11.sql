1602ed932b997b28e78e56ab4dca0a5c0224f73f
kangxiang <kangxiang@yunshan.net>
2025-11-04 20:36:02 +0800

    feat: podgroup add network mode

---
+++ server/controller/db/metadb/migrator/schema/rawsql/mysql/issu/7.1.0.11.sql
@@ -0,0 +1,31 @@
+DROP PROCEDURE IF EXISTS AddColumnIfNotExists;
+
+CREATE PROCEDURE AddColumnIfNotExists(
+    IN tableName VARCHAR(255),
+    IN colName VARCHAR(255),
+    IN colType VARCHAR(255),
+    IN afterCol VARCHAR(255)
+)
+BEGIN
+    DECLARE column_count INT;
+
+    SELECT COUNT(*)
+    INTO column_count
+    FROM information_schema.columns
+    WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = tableName
+    AND column_name = colName;
+
+    IF column_count = 0 THEN
+        SET @sql = CONCAT('ALTER TABLE ', tableName, ' ADD COLUMN ', colName, ' ', colType, ' AFTER ', afterCol);
+        PREPARE stmt FROM @sql;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END;
+
+CALL AddColumnIfNotExists('pod_group', 'network_mode', "INTEGER DEFAULT 1 COMMENT '1: Pod network 2: Host network'", 'label');
+
+DROP PROCEDURE AddColumnIfNotExists;
+
+UPDATE db_version SET version='7.1.0.11';
