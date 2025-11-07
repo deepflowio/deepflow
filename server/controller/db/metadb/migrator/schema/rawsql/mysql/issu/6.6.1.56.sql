89fe5a2bab9cf114747e843848078ab7a41f8962
zhengya <zhengya@yunshan.net>
2025-11-07 15:28:59 +0800

    fix: miss adding network_mode cloumn in dm

--- server/controller/db/metadb/migrator/schema/rawsql/mysql/issu/6.6.1.56.sql
+++ server/controller/db/metadb/migrator/schema/rawsql/mysql/issu/6.6.1.56.sql
@@ -1,31 +1,37 @@
-DROP PROCEDURE IF EXISTS AddColumnIfNotExists;
+DROP PROCEDURE IF EXISTS RenameColumnIfExists;
 
-CREATE PROCEDURE AddColumnIfNotExists(
+CREATE PROCEDURE RenameColumnIfExists(
     IN tableName VARCHAR(255),
-    IN colName VARCHAR(255),
-    IN colType VARCHAR(255),
-    IN afterCol VARCHAR(255)
+    IN oldColName VARCHAR(255),
+    IN newColName VARCHAR(255),
+    IN colType VARCHAR(255)
 )
 BEGIN
-    DECLARE column_count INT;
+    DECLARE index_count INT;
 
+    -- check if old column exists
     SELECT COUNT(*)
-    INTO column_count
+    INTO index_count
     FROM information_schema.columns
-    WHERE TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = tableName
-    AND column_name = colName;
+    WHERE table_schema = DATABASE()
+    AND table_name = tableName
+    AND column_name = newColName;
 
-    IF column_count = 0 THEN
-        SET @sql = CONCAT('ALTER TABLE ', tableName, ' ADD COLUMN ', colName, ' ', colType, ' AFTER ', afterCol);
+    -- if new column not exists, rename column
+    IF index_count = 0 THEN
+        SET @sql = CONCAT('ALTER TABLE ', tableName, ' CHANGE ', oldColName, ' ', newColName, ' ', colType);
         PREPARE stmt FROM @sql;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
     END IF;
 END;
 
-CALL AddColumnIfNotExists('pod_group', 'network_mode', "INTEGER DEFAULT 1 COMMENT '1: Pod network 2: Host network'", 'label');
+CALL RenameColumnIfExists('mail_server', 'user', 'user_name', 'TEXT NOT NULL');
+CALL RenameColumnIfExists('plugin', 'user', 'user_name', "INTEGER NOT NULL DEFAULT 1 COMMENT '1: agent 2: server'");
+CALL RenameColumnIfExists('genesis_process', 'user', 'user_name', "VARCHAR(256) DEFAULT ''");
+CALL RenameColumnIfExists('data_source', "`interval`", 'interval_time', "INTEGER NOT NULL COMMENT 'uint: s'");
+CALL RenameColumnIfExists('report_policy', '`interval`', 'interval_time', "enum('1d','1h') NOT NULL DEFAULT '1h'");
 
-DROP PROCEDURE AddColumnIfNotExists;
+DROP PROCEDURE RenameColumnIfExists;
 
 UPDATE db_version SET version='6.6.1.56';
