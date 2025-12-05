7078ffad8ab27c147b05212a3435caec216b08cd
yuanchao <yuanchao@yunshan.net>
2025-12-05 18:31:10 +0800

    feat: add field to dns and mysql

--- agent/src/flow_generator/protocol_logs/sql/mysql.rs
+++ agent/src/flow_generator/protocol_logs/sql/mysql.rs
@@ -1050,14 +1050,35 @@ impl MysqlLog {
             return Err(Error::InvalidLoginInfo("bad filter"));
         }
 
-        match Self::string_null(&payload[LOGIN_USERNAME_OFFSET..]) {
+        let mut offset = LOGIN_USERNAME_OFFSET;
+        let username = match Self::string_null(&payload[LOGIN_USERNAME_OFFSET..]) {
             Some(context) if context.is_ascii() => {
-                info.context = format!("Login username: {}", context);
-                info.generate_endpoint();
+                offset += context.len() + 1;
+                Some(context)
             }
             _ => return Err(Error::InvalidLoginInfo("username not found or not ascii")),
+        };
+
+        // CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA
+        if offset < payload.len() {
+            offset += payload[offset] as usize + 1;
         }
 
+        let mut database = None;
+        if offset < payload.len()
+            && (client_capabilities_flags & CONNECT_WITH_DB) == CONNECT_WITH_DB
+        {
+            database = Self::string_null(&payload[offset..]);
+        }
+
+        if let Some(username) = username {
+            info.context = if let Some(database) = database {
+                format!("Login username: {} db: {}", username, database)
+            } else {
+                format!("Login username: {}", username)
+            };
+            info.generate_endpoint();
+        }
         info.status = L7ResponseStatus::Ok;
 
         Ok(())
