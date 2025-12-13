be16bd5c47dbd12be94ffd2faf59867a6fffa19e
JIN Jie <jinjie@yunshan.net>
2025-12-13 17:22:07 +0800

    docs: Add docs for L7Log derive

--- agent/crates/public/src/rpc.rs
+++ agent/crates/public/src/rpc.rs
@@ -18,8 +18,6 @@ pub mod remote_exec {
         ParamNotFound(String),
         #[error("param `{0}` is invalid")]
         ParamInvalid(String),
-        #[error("kubernetes failed with {0}")]
-        KubeError(#[from] kube::Error),
         #[error("serialize failed with {0}")]
         SerializeError(#[from] serde_json::Error),
         #[error("transparent")]
