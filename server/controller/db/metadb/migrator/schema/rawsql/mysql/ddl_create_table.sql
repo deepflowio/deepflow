ec9b18be9d471afb80efc783d6c8cb96585f0e85
zhengya <zhengya@yunshan.net>
2026-01-08 11:26:58 +0800

    fix: modify biz decode ddl/schema for web product requirement

--- server/controller/db/metadb/migrator/schema/rawsql/mysql/ddl_create_table.sql
+++ server/controller/db/metadb/migrator/schema/rawsql/mysql/ddl_create_table.sql
@@ -2552,16 +2552,6 @@ CREATE TABLE IF NOT EXISTS `resource_version` (
 TRUNCATE TABLE resource_version;
 
 -- Business
-CREATE TABLE IF NOT EXISTS biz_decode_dictionary (
-    id                     INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    team_id                INTEGER DEFAULT 1,
-    name                   VARCHAR(256) NOT NULL,
-    yaml                   MEDIUMTEXT,
-    created_at             DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at             DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-TRUNCATE TABLE biz_decode_dictionary;
-
 CREATE TABLE IF NOT EXISTS biz_decode_policy (
     id                     INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     team_id                INTEGER DEFAULT 1,
@@ -2583,16 +2573,6 @@ CREATE TABLE IF NOT EXISTS biz_decode_policy_field (
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 TRUNCATE TABLE biz_decode_policy_field;
 
-CREATE TABLE IF NOT EXISTS biz_decode_dictionary_policy_field_connection (
-    id                     INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    dictionary_id          INTEGER NOT NULL,
-    policy_field_id        INTEGER NOT NULL,
-    policy_field_node_path VARCHAR(512) NOT NULL,
-    created_at             DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at             DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-TRUNCATE TABLE biz_decode_dictionary_policy_field_connection;
-
 CREATE TABLE IF NOT EXISTS biz_decode_policy_agent_group_connection (
     id                     INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     policy_id              INTEGER NOT NULL,
@@ -2601,3 +2581,23 @@ CREATE TABLE IF NOT EXISTS biz_decode_policy_agent_group_connection (
     updated_at             DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 TRUNCATE TABLE biz_decode_policy_agent_group_connection;
+
+CREATE TABLE IF NOT EXISTS biz_decode_dictionary (
+    id                     INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    team_id                INTEGER DEFAULT 1,
+    name                   VARCHAR(256) NOT NULL,
+    yaml                   MEDIUMTEXT,
+    created_at             DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at             DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+TRUNCATE TABLE biz_decode_dictionary;
+
+CREATE TABLE IF NOT EXISTS biz_decode_custom_protocol (
+    id                     INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    team_id                INTEGER DEFAULT 1,
+    name                   VARCHAR(256) NOT NULL,
+    yaml                   MEDIUMTEXT,
+    created_at             DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at             DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+TRUNCATE TABLE biz_decode_custom_protocol;
