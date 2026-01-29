0eb85f05ffa1e02ac580a601cc0588b8b07be78a
JIN Jie <jinjie@yunshan.net>
2026-01-29 15:21:30 +0800

    chore: More debug info to platform synchronizer

--- agent/src/platform/synchronizer.rs
+++ agent/src/platform/synchronizer.rs
@@ -278,10 +278,21 @@ impl Synchronizer {
                     }
                 };
 
-                debug!(
-                    "syncing version {} -> {} to remote",
-                    args.version, args.peer_version
-                );
+                if args.version == args.peer_version {
+                    debug!("version {} heartbeat to remote", args.version);
+                } else if log::log_enabled!(log::Level::Debug) {
+                    let n_interfaces = msg
+                        .platform_data
+                        .as_ref()
+                        .map(|p| p.interfaces.len())
+                        .unwrap_or(0);
+                    let n_processes = msg
+                        .process_data
+                        .as_ref()
+                        .map(|p| p.process_entries.len())
+                        .unwrap_or(0);
+                    debug!("syncing version {} -> {} to remote with {n_interfaces} interfaces and {n_processes} processes", args.version, args.peer_version);
+                }
                 trace!("genesis_sync request: {msg:?}");
                 match args
                     .runtime
