61b6a57f2cfb51c0bb04c58b5da80849d80696c7
yuanchao <yuanchao@yunshan.net>
2026-05-12 14:52:06 +0800

    refactor: Extract MySQL endpoint from default configuration

--- agent/src/config/config.rs
+++ agent/src/config/config.rs
@@ -1681,7 +1681,7 @@ impl Default for MysqlConfig {
     fn default() -> Self {
         Self {
             decompress_payload: true,
-            endpoint_disabled: true,
+            endpoint_disabled: false,
         }
     }
 }
